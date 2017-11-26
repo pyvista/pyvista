bd0bbb97579f8e3f2d65a117133a38581d8fda62
Alex Kaszynski <akascap@gmail.com>
2017-11-26 23:58:25 +0100

    fixed setup bug

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ with io_open(version_file, mode='r') as fd:
 # pre-compiled vtk available for python3
 install_requires = ['numpy']
 if sys.version[0] == '3':
-    if os.name == 'posix' and sys.version[2] >= 4:
+    if os.name is 'posix' and int(sys.version[2]) >= 4:
         install_requires.append(['vtk'])
-    elif os.name == 'nt' and sys.version[2] >= 6:
+    elif os.name == 'nt' and int(sys.version[2]) >= 6:
         install_requires.append(['vtk'])
 
 setup(
