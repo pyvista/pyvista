1550a60a36352efb78d370060bf26be56a492088
Alex Kaszynski <akascap@gmail.com>
2018-07-06 10:52:49 +0200

    trying to fix travis

--- tests/test_polydata.py
+++ tests/test_polydata.py
@@ -3,8 +3,9 @@ import vtkInterface as vtki
 from vtkInterface import examples
 spherefile = examples.spherefile
 
+
 class TestPolyData(object):
-    # sphere = vtki.PolyData(spherefile)
+    sphere = vtki.PolyData(spherefile)
 
     def test_loadfromfile(self):
         sphere = vtki.PolyData(spherefile)
