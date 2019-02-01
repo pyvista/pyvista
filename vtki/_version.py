d426cd4d4b687ab5762c05b260982d8df7968411
Alex Kaszynski <akascap@gmail.com>
2019-02-01 14:04:52 +0100

    version bump

--- vtki/_version.py
+++ vtki/_version.py
@@ -1,6 +1,6 @@
 """ version info for vtki """
 # major, minor, patch
-version_info = 0, 16, 1
+version_info = 0, 16, 2
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
