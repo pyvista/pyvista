0f05f1ab1b19d3c97e97c913cf3a878f4bc92967
Bane Sullivan <banesullivan@gmail.com>
2019-02-10 21:31:42 -0700

    Add tests for ipy_tools

---
+++ tests/test_ipy_tools.py
@@ -0,0 +1,79 @@
+import sys
+
+import pytest
+import numpy as np
+
+import vtki
+from vtki import QtInteractor
+from vtki.plotting import running_xserver
+from vtki import examples
+
+try:
+    import PyQt5
+    from PyQt5 import Qt
+    from PyQt5.Qt import QMainWindow
+    has_pyqt5 = True
+except:
+    has_pyqt5 = False
+
+
+
+@pytest.mark.skipif(not running_xserver(), reason="Requires X11")
+@pytest.mark.skipif(not has_pyqt5, reason="requires pyqt5")
+def test_ipy_scaled_plotter(qtbot):
+    data = examples.load_uniform()
+    p = vtki.ScaledPlotter(show=False)
+    p.add_mesh(data)
+    p.close()
+
+@pytest.mark.skipif(not running_xserver(), reason="Requires X11")
+@pytest.mark.skipif(not has_pyqt5, reason="requires pyqt5")
+def test_ipy_orthoganl_slicer(qtbot):
+    data = examples.load_uniform()
+    tool = vtki.OrthogonalSlicer(data, show=False)
+    g = tool.tool()
+    g.widget.update()
+    tool.plotter.close()
+
+
+@pytest.mark.skipif(not running_xserver(), reason="Requires X11")
+@pytest.mark.skipif(not has_pyqt5, reason="requires pyqt5")
+def test_ipy_many_slices_along_axis(qtbot):
+    data = examples.load_uniform()
+    tool = vtki.ManySlicesAlongAxis(data, show=False)
+    g = tool.tool()
+    g.widget.update()
+    tool.plotter.close()
+
+
+@pytest.mark.skipif(not running_xserver(), reason="Requires X11")
+@pytest.mark.skipif(not has_pyqt5, reason="requires pyqt5")
+def test_ipy_threshold(qtbot):
+    data = examples.load_uniform()
+    tool = vtki.Threshold(data, show=False)
+    g = tool.tool()
+    g.widget.update()
+    tool.plotter.close()
+
+
+@pytest.mark.skipif(not running_xserver(), reason="Requires X11")
+@pytest.mark.skipif(not has_pyqt5, reason="requires pyqt5")
+def test_ipy_clip(qtbot):
+    data = examples.load_uniform()
+    tool = vtki.Clip(data)
+    g = tool.tool()
+    g.widget.update()
+    tool.plotter.close()
+
+
+@pytest.mark.skipif(not running_xserver(), reason="Requires X11")
+@pytest.mark.skipif(not has_pyqt5, reason="requires pyqt5")
+def test_ipy_clip(qtbot):
+    data = examples.load_uniform()
+    p = vtki.ScaledPlotter()
+    p.add_mesh(data)
+    slicer = vtki.OrthogonalSlicer(data, plotter=p)
+    many = vtki.ManySlicesAlongAxis(data, plotter=p)
+    thresher = vtki.Threshold(data, plotter=p)
+    clipper = vtki.Clip(data, plotter=p)
+    p.close()
