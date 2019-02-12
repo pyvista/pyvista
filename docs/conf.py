c13f6340d080734693d3814e9e961c4ed8a5f1ee
Bane Sullivan <banesullivan@gmail.com>
2019-02-11 18:39:59 -0700

    Add Bane to docs author list

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'vtki'
 copyright = u'2017-2019, Alex Kaszynski'
-author = u'Alex Kaszynski'
+author = u'Alex Kaszynski & Bane Sullivan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
