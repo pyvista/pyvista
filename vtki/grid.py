c9bfe89594b920036be83775632e206501ad7cc1
Bane Sullivan <banesullivan@gmail.com>
2019-03-04 01:16:05 -0700

    Update grid wirter/readers

--- vtki/grid.py
+++ vtki/grid.py
@@ -204,18 +204,22 @@ class RectilinearGrid(vtkRectilinearGrid, Grid):
         # Use legacy writer if vtk is in filename
         if '.vtk' in filename:
             writer = vtk.vtkRectilinearGridWriter()
-            legacy = True
+            if binary:
+                writer.SetFileTypeToBinary()
+            else:
+                writer.SetFileTypeToASCII()
         elif '.vtr' in filename:
             writer = vtk.vtkXMLRectilinearGridWriter()
-            legacy = False
+            if binary:
+                writer.SetDataModeToBinary()
+            else:
+                writer.SetDataModeToAscii()
         else:
             raise Exception('Extension should be either ".vtr" (xml) or' +
                             '".vtk" (legacy)')
         # Write
         writer.SetFileName(filename)
         writer.SetInputData(self)
-        if binary and legacy:
-            writer.SetFileTypeToBinary()
         writer.Write()
 
     @property
@@ -469,18 +473,22 @@ class UniformGrid(vtkImageData, Grid):
         # Use legacy writer if vtk is in filename
         if '.vtk' in filename:
             writer = vtk.vtkDataSetWriter()
-            legacy = True
+            if binary:
+                writer.SetFileTypeToBinary()
+            else:
+                writer.SetFileTypeToASCII()
         elif '.vti' in filename:
             writer = vtk.vtkXMLImageDataWriter()
-            legacy = False
+            if binary:
+                writer.SetDataModeToBinary()
+            else:
+                writer.SetDataModeToAscii()
         else:
             raise Exception('Extension should be either ".vti" (xml) or' +
                             '".vtk" (legacy)')
         # Write
         writer.SetFileName(filename)
         writer.SetInputData(self)
-        if binary and legacy:
-            writer.SetFileTypeToBinary()
         writer.Write()
 
     @property
