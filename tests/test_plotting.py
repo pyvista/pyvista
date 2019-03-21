9e14c330360e5c5eda34efafe9db763d5e2d0baa
Bane Sullivan <banesullivan@gmail.com>
2019-03-21 12:49:50 -0600

    Fix plotting test for 2.7

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -536,7 +536,7 @@ def test_remove_actor():
 @pytest.mark.skipif(NO_PLOTTING, reason="Requires X11")
 def test_image_properties():
     mesh = examples.load_uniform()
-    p = vtki.Plotter(notebook=True)
+    p = vtki.Plotter()
     p.add_mesh(mesh)
     p.show(auto_close=False) # DO NOT close plotter
     # Get RGB image
