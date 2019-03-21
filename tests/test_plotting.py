9c0c03ed0efdb4c6b76a47b8539c630d6fa2b784
Bane Sullivan <banesullivan@gmail.com>
2019-03-21 17:11:18 -0600

    Fix bad plotting test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -536,7 +536,7 @@ def test_remove_actor():
 @pytest.mark.skipif(NO_PLOTTING, reason="Requires X11")
 def test_image_properties():
     mesh = examples.load_uniform()
-    p = vtki.Plotter()
+    p = vtki.Plotter(off_screen=OFF_SCREEN)
     p.add_mesh(mesh)
     p.show(auto_close=False) # DO NOT close plotter
     # Get RGB image
