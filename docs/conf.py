39a8a79252334e12b64038ff4a49164c66538972
Bane Sullivan <banesullivan@gmail.com>
2019-04-02 12:36:03 -0600

    Use sphinx_copybitton extension

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.doctest',
               'sphinx.ext.autosummary',
               'notfound.extension',
+              'sphinx_copybutton',
              ]
 
 # Add any paths that contain templates here, relative to this directory.
