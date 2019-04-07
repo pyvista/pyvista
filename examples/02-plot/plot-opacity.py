3e1415d4c07d44b0f054ba31fbb2fd27befb58a9
Bane Sullivan <banesullivan@gmail.com>
2019-04-06 21:27:04 -0600

    Add knee/DICOM example

--- examples/02-plot/plot-opacity.py
+++ examples/02-plot/plot-opacity.py
@@ -24,3 +24,31 @@ from vtki import examples
 mesh = examples.download_st_helens().warp_by_scalar()
 
 mesh.plot(opacity='linear')
+
+
+################################################################################
+# Opacity mapping is often useful when plotting DICOM images. For example,
+# download the sample knee DICOM image:
+knee = examples.download_knee()
+
+################################################################################
+# And here we inspect the DICOM image with a few different colormaps and opacity
+# mappings:
+p = vtki.Plotter(shape=(2,2), border=False)
+
+p.add_mesh(knee, cmap='bone', show_scalar_bar=0)
+p.view_xy()
+
+p.subplot(0,1)
+p.add_mesh(knee, cmap='bone', opacity='linear', show_scalar_bar=0)
+p.view_xy()
+
+p.subplot(1,0)
+p.add_mesh(knee, cmap='nipy_spectral', show_scalar_bar=0)
+p.view_xy()
+
+p.subplot(1,1)
+p.add_mesh(knee, cmap='nipy_spectral', opacity='linear', show_scalar_bar=0)
+p.view_xy()
+
+p.show()
