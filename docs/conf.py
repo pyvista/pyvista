f9e06191f6a6055b25cebff0491a9c57bc0fb55b
Bane Sullivan <banesullivan@gmail.com>
2019-04-11 23:31:42 -0600

    Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ vtki.OFF_SCREEN = True # Not necessary - simply an insurance policy
 # Preferred plotting style for documentation
 vtki.set_plot_theme('document')
 vtki.rcParams['colorbar_orientation'] = 'vertical'
-vtki.rcParams['window_size'] = np.array([1024, 768]) * 2,
+vtki.rcParams['window_size'] = np.array([1024, 768]) * 2
 # Save figures in specified directory
 vtki.FIGURE_PATH = os.path.join(os.path.abspath('./images/'), 'auto-generated/')
 if not os.path.exists(vtki.FIGURE_PATH):
