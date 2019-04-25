d56876e5bb1c7b76144c121739dcaebd2fd4e63e
Bane Sullivan <banesullivan@gmail.com>
2019-04-25 07:54:06 -0600

    Bump Version: 0.18.3 -> 0.18.4

--- vtki/_version.py
+++ vtki/_version.py
@@ -1,6 +1,6 @@
 """ version info for vtki """
 # major, minor, patch
-version_info = 0, 18, 3
+version_info = 0, 18, 4
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
