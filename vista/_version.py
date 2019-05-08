df3f0334f14ce7a7d62ea028c3ba3fe453b57ca0
Bane Sullivan <banesullivan@gmail.com>
2019-05-08 01:37:12 -0600

    Bump version 0.18.4 → 0.19.0

--- vista/_version.py
+++ vista/_version.py
@@ -1,6 +1,6 @@
 """ version info for vista """
 # major, minor, patch
-version_info = 0, 18, 4
+version_info = 0, 19, 0
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
