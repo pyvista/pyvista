37ded94bc32e352e22029fc0048810911bc39c98
Bane Sullivan <banesullivan@gmail.com>
2019-06-25 00:01:29 -0600

    Break up plotting module

--- pyvista/plotting/__init__.py
+++ pyvista/plotting/__init__.py
@@ -1,4 +1,7 @@
+from .theme import *
+from .tools import *
 from .plotting import *
+from .helpers import *
 from .colors import *
 from .export_vtkjs import export_plotter_vtkjs, get_vtkjs_url
 from .ipy_tools import (Clip, Isocontour, ManySlicesAlongAxis,
