1369dd4d7aa1506ef7bc4b7ba1665962e5be3c1b
banesullivan <banesulli@gmail.com>
2019-08-15 00:18:47 -0600

    Fix color options and fix reference issues in callbacks

--- pyvista/plotting/widgets.py
+++ pyvista/plotting/widgets.py
@@ -11,30 +11,35 @@ class WidgetHelper(object):
     widgets"""
 
     def enable_box_widget(self, bounds=None, factor=1.0, callback=None,
-                          rotation_enabled=True,**kwargs):
-        """Add a box widget to the scene. This function returns a pointer
-        to a :class:`pyvista.PolyData` mesh that will continually update with
-        the box widget.
-
-        You can also pass a callable function that will takes a single
-        argument, the PolyData box, and performs a task with that box.
+                          rotation_enabled=True, color=None, use_planes=False,
+                          **kwargs):
+        """Add a box widget to the scene. This is useless without a callback
+        function. You can pass a callable function that takes a single
+        argument, the PolyData box output from this widget, and performs a
+        task with that box.
         """
         if hasattr(self, 'notebook') and self.notebook:
             raise AssertionError('Box widget not available in notebook plotting')
         if bounds is None:
             bounds = self.bounds
 
-        # This dataset is continually updated by the widget and is return to
-        # the user for use
+        if color is None:
+            color = rcParams['font']['color']
 
         def _the_callback(box_widget, event_id):
             the_box = pyvista.PolyData()
             box_widget.GetPolyData(the_box)
+            planes = vtk.vtkPlanes()
+            box_widget.GetPlanes(planes)
             if hasattr(callback, '__call__'):
-                callback(the_box)
+                if use_planes:
+                    callback(planes)
+                else:
+                    callback(the_box)
             return
 
         self.box_widget = vtk.vtkBoxWidget()
+        self.box_widget.GetOutlineProperty().SetColor(parse_color(color))
         self.box_widget.SetInteractor(self.iren)
         self.box_widget.SetPlaceFactor(factor)
         self.box_widget.SetRotationEnabled(rotation_enabled)
@@ -51,7 +56,8 @@ class WidgetHelper(object):
         return
 
 
-    def add_mesh_clip_box(self, mesh, invert=False, rotation_enabled=True, **kwargs):
+    def add_mesh_clip_box(self, mesh, invert=False, rotation_enabled=True,
+                          widget_color=None, **kwargs):
         """Add a mesh to the scene with a box widget that is used to clip
         the mesh interactively.
 
@@ -77,10 +83,7 @@ class WidgetHelper(object):
 
         actor = self.add_mesh(mesh, name=name, **kwargs)
 
-        def callback(box):
-            planes = vtk.vtkPlanes()
-            # TODO: make sure this object is correctly referenced
-            self.box_widget.GetPlanes(planes)
+        def callback(planes):
             bounds = []
             for i in range(planes.GetNumberOfPlanes()):
                 plane = planes.GetPlane(i)
@@ -91,17 +94,18 @@ class WidgetHelper(object):
             self.add_mesh(self.box_clipped_mesh, name=name, **kwargs)
 
         self.enable_box_widget(bounds=mesh.bounds, factor=1.25,
-                rotation_enabled=rotation_enabled, callback=callback)
+                rotation_enabled=rotation_enabled, callback=callback,
+                use_planes=True, color=widget_color)
 
         return actor
 
 
     def enable_plane_widget(self, origin=None, normal='x', factor=1.25,
-                            callback=None, bounds=None, **kwargs):
-        """Add a plane widget to the scene.
-
-        You can also pass a callable function that will takes a single
-        argument, the plane, and performs a task with that box.
+                            callback=None, bounds=None, color=None, **kwargs):
+        """Add a plane widget to the scene. This is useless without a callback
+        function. You can pass a callable function that takes a single
+        argument, the vtkPlane implicit function output from this widget, and
+        performs a task with that plane.
         """
         if hasattr(self, 'notebook') and self.notebook:
             raise AssertionError('Plane widget not available in notebook plotting')
@@ -113,6 +117,9 @@ class WidgetHelper(object):
         if isinstance(normal, str):
             normal = NORMALS[normal.lower()]
 
+        if color is None:
+            color = rcParams['font']['color']
+
         def _the_callback(plane_widget, event_id):
             the_plane = vtk.vtkPlane()
             plane_widget.GetPlane(the_plane)
@@ -121,6 +128,10 @@ class WidgetHelper(object):
             return
 
         self.plane_widget = vtk.vtkImplicitPlaneWidget()
+        self.plane_widget.GetNormalProperty().SetColor(parse_color(color))
+        self.plane_widget.GetOutlineProperty().SetColor(parse_color(color))
+        self.plane_widget.GetOutlineProperty().SetColor(parse_color(color))
+        self.plane_widget.GetPlaneProperty().SetOpacity(0.5)
         self.plane_widget.SetInteractor(self.iren)
         self.plane_widget.SetPlaceFactor(factor)
         self.plane_widget.PlaceWidget(bounds)
@@ -130,7 +141,6 @@ class WidgetHelper(object):
         self.plane_widget.UpdatePlacement()
         self.plane_widget.On()
         self.plane_widget.AddObserver(vtk.vtkCommand.EndInteractionEvent, _the_callback)
-        self.plane_widget.GetPlane(the_plane)
         _the_callback(self.plane_widget, None) # Trigger immediate update
 
         return
@@ -141,7 +151,8 @@ class WidgetHelper(object):
         return
 
 
-    def add_mesh_clip_plane(self, mesh, invert=False, normal='x', **kwargs):
+    def add_mesh_clip_plane(self, mesh, invert=False, normal='x',
+                            widget_color=None, **kwargs):
         """Add a mesh to the scene with a plane widget that is used to clip
         the mesh interactively.
 
@@ -173,14 +184,14 @@ class WidgetHelper(object):
             self.add_mesh(self.plane_clipped_mesh, name=name, **kwargs)
 
         self.enable_plane_widget(bounds=mesh.bounds, factor=1.25, normal=normal,
-                                 callback=callback)
+                                 callback=callback, color=widget_color)
 
         return actor
 
 
 
-    def add_mesh_slice(self, mesh, normal='x', contour=False, generate_triangles=False,
-                       **kwargs):
+    def add_mesh_slice(self, mesh, normal='x', contour=False,
+                       generate_triangles=False, widget_color=None, **kwargs):
         """Add a mesh to the scene with a plane widget that is used to slice
         the mesh interactively.
 
@@ -220,7 +231,7 @@ class WidgetHelper(object):
             self.add_mesh(self.plane_sliced_mesh, name=name, **kwargs)
 
         self.enable_plane_widget(bounds=mesh.bounds, factor=1.25, normal=normal,
-                                 callback=callback)
+                                 callback=callback, color=widget_color)
 
         _start_interact = lambda obj, event: self.plane_widget.SetDrawPlane(True)
         _stop_interact = lambda obj, event: self.plane_widget.SetDrawPlane(False)
@@ -234,18 +245,20 @@ class WidgetHelper(object):
 
 
     def enable_line_widget(self, bounds=None, factor=1.0, callback=None,
-                           rotation_enabled=True, resolution=100, **kwargs):
-        """Add a line widget to the scene. This function returns a pointer
-        to a :class:`pyvista.PolyData` mesh that will continually update with
-        the kine widget.
-
-        You can also pass a callable function that will take a single
-        argument, the PolyData line, and performs a task with that line.
+                           rotation_enabled=True, resolution=100,
+                           color=None, **kwargs):
+        """Add a line widget to the scene. This is useless without a callback
+        function. You can pass a callable function that takes a single
+        argument, the PolyData line output from this widget, and performs a
+        task with that line.
         """
         if hasattr(self, 'notebook') and self.notebook:
             raise AssertionError('Box widget not available in notebook plotting')
         if bounds is None:
             bounds = self.bounds
+
+        if color is None:
+            color = rcParams['font']['color']
 
         # This dataset is continually updated by the widget and is return to
         # the user for use
@@ -260,6 +273,7 @@ class WidgetHelper(object):
             return
 
         self.line_widget = vtk.vtkLineWidget()
+        self.line_widget.GetLineProperty().SetColor(parse_color(color))
         self.line_widget.SetInteractor(self.iren)
         self.line_widget.SetPlaceFactor(factor)
         self.line_widget.PlaceWidget(bounds)
@@ -281,10 +295,10 @@ class WidgetHelper(object):
     def enable_slider_widget(self, min, max, value=None, title=None,
                              pointa=(.4 ,.9), pointb=(.9, .9), callback=None,
                              color=None):
-        """Add a slider bar widget.
-
-        Callback should be a function that takes a single parameter, the value
-        of the slider.
+        """Add a slider bar widget. This is useless without a callback
+        function. You can pass a callable function that takes a single
+        argument, the value of this slider widget, and performs a
+        task with that value.
         """
 
         if value is None:
