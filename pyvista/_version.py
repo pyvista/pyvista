8e915a2c4b5d3f2dcb096cea3eb1557259dd3bec
banesullivan <banesulli@gmail.com>
2019-09-20 13:43:22 -0600

    Bump Version: 0.22.1 -> 0.22.2

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -1,6 +1,6 @@
 """ version info for pyvista """
 # major, minor, patch
-version_info = 0, 22, 1
+version_info = 0, 22, 2
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
