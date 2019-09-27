935256d9a9b63225d4ea6cab4c1a418d68e7f54b
JevinJ <jevinjones@openmailbox.org>
2019-09-27 00:26:44 -0700

    Make DataObject._field_bool_array_names a set.

--- pyvista/core/common.py
+++ pyvista/core/common.py
@@ -30,7 +30,7 @@ class DataObject(object):
 
     def __init__(self, *args, **kwargs):
         """Initialize the data object."""
-        self._field_bool_array_names = []
+        self._field_bool_array_names = set()
 
 
     def __new__(cls, *args, **kwargs):
@@ -200,7 +200,7 @@ class DataObject(object):
         if isinstance(vtkarr, vtk.vtkBitArray):
             vtkarr = vtk_bit_array_to_char(vtkarr)
             if name not in self._point_bool_array_names:
-                self._field_bool_array_names.append(name)
+                self._field_bool_array_names.add(name)
 
         array = convert_array(vtkarr)
         if array.dtype == np.uint8 and name in self._field_bool_array_names:
@@ -236,7 +236,7 @@ class DataObject(object):
         if scalars.dtype == np.bool:
             scalars = scalars.view(np.uint8)
             if name not in self._field_bool_array_names:
-                self._field_bool_array_names.append(name)
+                self._field_bool_array_names.add(name)
 
         if not scalars.flags.c_contiguous:
             scalars = np.ascontiguousarray(scalars)
