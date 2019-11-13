dafe65d626ee2b6cc6c155a072b1f31a2696a8d2
Bane Sullivan <banesullivan@gmail.com>
2019-11-13 14:03:22 -0700

    Remove direction casting

--- pyvista/utilities/geometric_objects.py
+++ pyvista/utilities/geometric_objects.py
@@ -163,7 +163,6 @@ def CylinderStructured(radius=0.5, height=1.0,
     # Orient properly in user direction
     vx = np.array([0., 0., 1.])
     if not np.allclose(vx, direction):
-        direction = np.array(direction)
         direction = direction / np.linalg.norm(direction)
         vx -= vx.dot(direction) * direction
         vx /= np.linalg.norm(vx)
