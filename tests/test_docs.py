a8ae7e5f9945984df9e3ccadf65505dfbf5de689
banesullivan <banesulli@gmail.com>
2019-11-25 14:44:09 -0700

    Add class test case

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -2,6 +2,28 @@ import pytest
 from pyvista.utilities import copy_function_doc
 
 
+
+class Foo:
+    def goo(self, *args):
+        """This has some documentation.
+
+        Parameters
+        ----------
+        arg0 : int
+            This is an argument.
+
+        """
+        pass
+
+    @copy_function_doc(goo, alias=True)
+    def goo_alias(self, *args):
+        pass
+
+    @copy_function_doc(goo, deprecated=True)
+    def goo_depr(self, *args):
+        pass
+
+
 def test_copy_function_doc():
     """Test copying function documentation."""
 
@@ -57,6 +79,7 @@ def test_copy_function_doc():
         @copy_function_doc(blank)
         def blank2():
             pass
+
     assert bar.__doc__ == foo.__doc__
     assert bar2.__doc__ == foo.__doc__ + '\nSingle line.'
     assert bar3.__doc__ == foo2.__doc__ + 'Single line.'
@@ -64,3 +87,7 @@ def test_copy_function_doc():
     assert dep_fun.__doc__ == foo.__doc__ + '\nDEPRECATED: Please use' + \
         ' ``foo`` instead.\n'
     assert alias_fun.__doc__ == bar4.__doc__ + 'Alias for: ``foo2``.\n'
+    # Test a class
+    assert Foo.goo_alias.__doc__ == Foo.goo.__doc__ + '\nAlias for: ``goo``.\n'
+    assert Foo.goo_depr.__doc__ == Foo.goo.__doc__ + '\nDEPRECATED: Please use' + \
+        ' ``goo`` instead.\n'
