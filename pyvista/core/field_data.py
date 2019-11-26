7ec513f1ec6da8528574bde1fa1e62bc2edeab36
JevinJ <jevinjones@openmailbox.org>
2019-11-25 20:19:17 -0800

    Add field_data.py

---
+++ pyvista/core/field_data.py
@@ -0,0 +1,18 @@
+from vtk.vtkCommonKitPython import vtkFieldData, vtkDataSetAttributes
+
+
+class FieldData(vtkFieldData):
+    def __init__(self, vtk_field_data=None):
+        super().__init__()
+        if vtk_field_data is not None:
+            self.DeepCopy(vtk_field_data)
+
+
+class PointData(vtkDataSetAttributes, FieldData):
+    def __init__(self, vtk_point_data):
+        super().__init__(vtk_point_data)
+
+
+class CellData(vtkDataSetAttributes, FieldData):
+    def __init__(self, vtk_point_data):
+        super().__init__(vtk_point_data)
