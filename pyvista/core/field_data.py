6423e49fc78d7ec1d127e5a4a6e16fe2b963e05a
JevinJ <jevinjones@openmailbox.org>
2019-11-26 17:58:58 -0800

    Make all Data objects wrappers to fix copy issues.

--- pyvista/core/field_data.py
+++ pyvista/core/field_data.py
@@ -1,18 +1,16 @@
-from vtk.vtkCommonKitPython import vtkFieldData, vtkDataSetAttributes
+from vtk.numpy_interface.dataset_adapter import VTKObjectWrapper
 
 
-class FieldData(vtkFieldData):
-    def __init__(self, vtk_field_data=None):
-        super().__init__()
-        if vtk_field_data is not None:
-            self.ShallowCopy(vtk_field_data)
+class FieldData(VTKObjectWrapper):
+    def __init__(self, vtk_field_data):
+        super().__init__(vtkobject=vtk_field_data)
 
 
-class PointData(vtkDataSetAttributes, FieldData):
+class PointData(FieldData):
     def __init__(self, vtk_point_data):
-        super().__init__(vtk_point_data)
+        super().__init__(vtk_field_data=vtk_point_data)
 
 
-class CellData(vtkDataSetAttributes, FieldData):
+class CellData(FieldData):
     def __init__(self, vtk_cell_data):
-        super().__init__(vtk_cell_data)
+        super().__init__(vtk_field_data=vtk_cell_data)
