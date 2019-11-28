0bea58ae7f0d16153b73abda9817b97207bc175a
JevinJ <jevinjones@openmailbox.org>
2019-11-28 15:10:54 -0800

    Add init()

--- pyvista/core/datasetattributes.py
+++ pyvista/core/datasetattributes.py
@@ -1,6 +1,9 @@
 import vtk.numpy_interface.dataset_adapter as dsa
 
 class DataSetAttributes(dsa.DataSetAttributes):
+    def __init__(self, vtkobject, dataset, association):
+        super().__init__(vtkobject, dataset, association)
+
     def __setitem__(self, key, value):
         if self[key] is dsa.NoneArray:
             self.append(narray=value, name=key)
