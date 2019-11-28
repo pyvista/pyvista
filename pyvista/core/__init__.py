57899c360b262650aa5d9566f495c1c3876f2c48
JevinJ <jevinjones@openmailbox.org>
2019-11-28 14:45:18 -0800

    Add DataSetAttributes.py

--- pyvista/core/__init__.py
+++ pyvista/core/__init__.py
@@ -2,6 +2,7 @@
 
 from .common import DataSet, DataObject
 from .composite import MultiBlock
+from .datasetattributes import DataSetAttributes
 from .filters import (CompositeFilters, DataSetFilters, PolyDataFilters,
                       UnstructuredGridFilters, UniformGridFilters)
 from .grid import Grid, RectilinearGrid, UniformGrid
