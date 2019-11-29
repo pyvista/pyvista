05f3622d9137917c988513d0fd380c7594271c6f
JevinJ <jevinjones@openmailbox.org>
2019-11-29 00:11:16 -0800

    Add pyvista_ndarray.from_any() factory method.

--- pyvista/core/datasetattributes.py
+++ pyvista/core/datasetattributes.py
@@ -16,11 +16,16 @@ class DataSetAttributes(VTKObjectWrapper):
         return self.get_array(key)
 
     def __setitem__(self, key, value):
+        if isinstance(value, (list, tuple, str)):
+            value = pyvista_ndarray.from_iter(iterable=)
         if self[key] is None:
             self.append(narray=value, name=key)
         else:
             self.RemoveArray(key)
             self[key] = value
+
+    def __delitem__(self, key):
+        self.RemoveArray(key)
 
     def __contains__(self, item):
         return item in self.keys()
@@ -42,11 +47,10 @@ class DataSetAttributes(VTKObjectWrapper):
         """Appends a new array to the dataset attributes."""
         if narray is None:
             return
-
         if self.Association == ArrayAssociation.POINT:
-            arrLength = self.DataSet.GetNumberOfPoints()
+            array_len = self.DataSet.GetNumberOfPoints()
         elif self.Association == ArrayAssociation.CELL:
-            arrLength = self.DataSet.GetNumberOfCells()
+            array_len = self.DataSet.GetNumberOfCells()
         else:
             array_len = narray.shape[0] if isinstance(narray, numpy.ndarray) else 1
 
@@ -117,3 +121,67 @@ class DataSetAttributes(VTKObjectWrapper):
         return vals
 
 
+
+class pyvista_ndarray(VTKArray):
+    """This is a sub-class of numpy ndarray that stores a
+    reference to a vtk array as well as the owning dataset.
+    The numpy array and vtk array should point to the same
+    memory location."""
+
+    def __new__(cls, ndarray, vtk_array=None, dataset=None):
+        # Input array is an already formed ndarray instance
+        obj = numpy.asarray(ndarray).view(cls)
+        obj._association = ArrayAssociation.FIELD
+        # add the new attributes to the created instance
+        obj.VTKObject = vtk_array
+        if dataset:
+            obj._dataset = vtkWeakReference()
+            # Unless ALL pyvista objects ARE vtk objects, OR, ALL pyvista objects ARE
+            # VTKObjectWrappers, which object gets set matters.
+            if isinstance(dataset, VTKObjectWrapper):
+                obj._dataset.Set(dataset.VTKObject)
+            else:
+                obj._dataset.Set(dataset)
+        return obj
+
+    @classmethod
+    def from_vtk_data_array(cls, vtk_data_array, dataset=None):
+        """Create a ``pyvista_ndarray`` instance from a vtk array.
+
+        Parameters
+        ----------
+        vtk_data_array : vtkDataArray
+            Array to copy.
+
+        dataset : vtkDataSet, optional
+            The vtkDataSet which vtk_data_array belongs to. Required to
+            update or access a dataset when this pyvista_ndarray is updated.
+
+        """
+        narray = numpy_support.vtk_to_numpy(vtk_data_array)
+
+        # Make arrays of 9 components into matrices. Also transpose
+        # as VTK stores matrices in Fortran order
+        shape = narray.shape
+        if len(shape) == 2 and shape[1] == 9:
+            narray = narray.reshape((shape[0], 3, 3)).transpose(0, 2, 1)
+        return cls(narray, vtk_data_array, dataset=dataset)
+
+    @classmethod
+    def from_iter(cls, a, dtype=None, order=None):
+        """Create a ``pyvista_ndarray`` instance from an iterable.
+
+        Parameters
+        ----------
+        a : array_like
+            Input data, in any form that can be converted to an array. 
+            This includes lists, lists of tuples, tuples, tuples of tuples, tuples of lists and ndarrays.
+            
+        dtype : data-type, optional
+            By default, the data-type is inferred from the input data.
+            
+        order : {‘C’, ‘F’}, optional
+            Whether to use row-major (C-style) or column-major (Fortran-style) memory representation. Defaults to ‘C’.
+
+        """
+        return cls(numpy.asarray(a, dtype, order))
