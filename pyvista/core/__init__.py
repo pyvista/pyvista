5be46ea268f70107466a72b7b97230f03f5fab30
JevinJ <jevinjones@openmailbox.org>
2019-11-29 22:02:30 -0800

    Move pyvista_ndarray and create it's own file.

--- pyvista/core/__init__.py
+++ pyvista/core/__init__.py
@@ -5,5 +5,6 @@ from .composite import MultiBlock
 from .filters import (CompositeFilters, DataSetFilters, PolyDataFilters,
                       UnstructuredGridFilters, UniformGridFilters)
 from .grid import Grid, RectilinearGrid, UniformGrid
+from .pyvista_ndarray import pyvista_ndarray
 from .objects import Table, Texture
 from .pointset import PointGrid, PolyData, StructuredGrid, UnstructuredGrid
