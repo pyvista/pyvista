02764fcadb46d55956582b8890b0620d24134084
Keurfon Luu <keurfonluu@outlook.com>
2019-12-05 12:44:56 -0800

    Update pyvista/utilities/fileio.py
    Co-Authored-By: Bane Sullivan <banesullivan@gmail.com>

--- pyvista/utilities/fileio.py
+++ pyvista/utilities/fileio.py
@@ -319,6 +319,7 @@ def save_meshio(filename, mesh, file_format = None, **kwargs):
     """
     from meshio._vtk import vtk_to_meshio_type
 
+    filename = os.path.abspath(os.path.expanduser(filename))
     # Cast to pyvista.UnstructuredGrid
     mesh = mesh.cast_to_unstructured_grid()
 
