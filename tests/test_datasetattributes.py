ddac84075026e598d06c913a3aaa202a2798a489
JevinJ <jevinjones@openmailbox.org>
2020-01-18 19:33:45 -0800

    Add example_grid() fixture, test_init()

--- tests/test_datasetattributes.py
+++ tests/test_datasetattributes.py
@@ -1,2 +1,17 @@
-import pytest
+import numpy as np
+from pytest import fixture
 import pyvista
+from vtk.numpy_interface.dataset_adapter import ArrayAssociation
+
+
+@fixture()
+def example_grid():
+    return pyvista.UnstructuredGrid(pyvista.examples.hexbeamfile).copy()
+
+
+def test_init(example_grid):
+    attributes = pyvista.DataSetAttributes(
+        example_grid.GetPointData(), dataset=example_grid, association=ArrayAssociation.POINT)
+    assert attributes.VTKObject == example_grid.GetPointData()
+    assert attributes.dataset == example_grid
+    assert attributes.association == ArrayAssociation.POINT
