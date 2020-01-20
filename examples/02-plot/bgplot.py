7eed731dd78c84be73a8a68282c98dac001dc966
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-01-20 17:24:55 +0100

    Add simple example

---
+++ examples/02-plot/bgplot.py
@@ -0,0 +1,21 @@
+"""
+BackgroundPlotter example.
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This simple sphinx example uses BackgroundPlotter.
+
+"""
+
+# sphinx_gallery_thumbnail_number = 4
+import pyvista as pv
+
+###############################################################################
+
+p = pv.BackgroundPlotter()
+p.add_mesh(pv.Cone())
+
+###############################################################################
+
+p = pv.BackgroundPlotter()
+p.add_mesh(pv.Cone())
+p.close()
