64245e5ed59d9503e2a9f7af939986ef723571cd
banesullivan <banesulli@gmail.com>
2020-01-30 10:12:42 -0700

    Update extract voi options and docs

--- pyvista/core/filters.py
+++ pyvista/core/filters.py
@@ -3568,7 +3568,7 @@ class UniformGridFilters(DataSetFilters):
         return _get_output(alg)
 
 
-    def extract_subset(dataset, voi):
+    def extract_subset(dataset, voi, rate=(1, 1, 1), boundary=False):
         """Select piece (e.g., volume of interest).
 
         To use this filter set the VOI ivar which are i-j-k min/max indices
@@ -3576,14 +3576,33 @@ class UniformGridFilters(DataSetFilters):
         0-offset.) You can also specify a sampling rate to subsample the
         data.
 
+        Typical applications of this filter are to extract a slice from a
+        volume for image processing, subsampling large volumes to reduce data
+        size, or extracting regions of a volume with interesting data.
+
         Parameters
         ----------
         voi : tuple(int)
-            Length 6 tuple of integers specifying the volume of interest in
-            i-j-k min/max indices.
+            Length 6 iterable of ints: ``(xmin, xmax, ymin, ymax, zmin, zmax)``.
+            These bounds specify the volume of interest in i-j-k min/max
+            indices.
+
+        rate : tuple(int)
+            Length 3 iterable of ints: ``(xrate, yrate, zrate)``.
+            Default: ``(1, 1, 1)``
+
+        boundary : bool
+            Control whether to enforce that the "boundary" of the grid is
+            output in the subsampling process. (This only has effect
+            when the rate in any direction is not equal to 1). When
+            this is on, the subsampling will always include the boundary of
+            the grid even though the sample rate is not an even multiple of
+            the grid dimensions. (By default this is off.)
         """
         alg = vtk.vtkExtractVOI()
         alg.SetVOI(voi)
         alg.SetInputDataObject(dataset)
+        alg.SetSampleRate(rate)
+        alg.SetIncludeBoundary(boundary)
         alg.Update()
         return _get_output(alg)
