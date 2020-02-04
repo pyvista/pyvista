a28036a945864908169c0dba03a03a9e10b8acc5
Flamm, Matthew H <matthewhflamm@gmail.com>
2020-02-04 09:32:36 -0500

    Reverts commits 675ccdd20e158eb897c60014dc29c29f2362d7af, 8c1e0b10a1288a396da25d7869a9fdf5d55ea786, dfa503bfa7a108d7ab03295ce8d18d22295a5d4a, 6029581f019e791dd2623ff81bcff0bdae0d8e1d.

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -1310,8 +1310,8 @@ class BasePlotter(PickingHelper, WidgetHelper):
             prop.SetLineWidth(line_width)
 
         # Add scalar bar if available
-        if title is not None and show_scalar_bar and (not color or _custom_opac):
-            self.add_scalar_bar(title, **scalar_bar_args)
+        if stitle is not None and show_scalar_bar and (not rgb or _custom_opac):
+            self.add_scalar_bar(stitle, **scalar_bar_args)
 
         return actor
 
