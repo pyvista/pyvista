3f57eedde310ed237346211b1968bdba59c5f303
Keurfon Luu <keurfonluu@outlook.com>
2020-02-19 10:51:34 -0800

    do not skip python 3.5

--- tests/test_meshio.py
+++ tests/test_meshio.py
@@ -6,8 +6,6 @@ import pytest
 import pyvista
 from pyvista import examples
 
-TO_TEST = sys.version_info.major == 3 and sys.version_info.minor == 5
-REASON = "See https://github.com/pyvista/pyvista/pull/495 for meshio issues on Python 3.5"
 
 beam = pyvista.UnstructuredGrid(examples.hexbeamfile)
 airplane = examples.load_airplane().cast_to_unstructured_grid()
@@ -15,7 +13,6 @@ uniform = examples.load_uniform().cast_to_unstructured_grid()
 
 
 @pytest.mark.parametrize("mesh_in", [beam, airplane, uniform])
-@pytest.mark.skipif(TO_TEST, reason=REASON)
 def test_meshio(mesh_in, tmpdir):
     # Save and read reference mesh using meshio
     filename = tmpdir.mkdir("tmpdir").join("test_mesh.vtk")
@@ -35,7 +32,6 @@ def test_meshio(mesh_in, tmpdir):
         assert np.allclose(v, mesh.cell_arrays[k.replace(" ", "_")])
 
 
-@pytest.mark.skipif(TO_TEST, reason=REASON)
 def test_file_format():
     from meshio._exceptions import ReadError
     with pytest.raises(ReadError):
