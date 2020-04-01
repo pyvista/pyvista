ce4c4c94b3f5089beb6cd797370d86f6b88caefb
JevinJ <jevinjones@openmailbox.org>
2020-03-31 17:30:57 -0700

    Fix UnstructuredGrid variable name, should be snake_case instead of upper case.

--- tests/test_pyvista_ndarray.py
+++ tests/test_pyvista_ndarray.py
@@ -27,16 +27,16 @@ class TestConstructors:
 
 class TestSetItem:
     def test_setitem_should_change_value(self):
-        GRID = UnstructuredGrid(examples.hexbeamfile)
-        vtk_arr = GRID.GetCellData().GetScalars()
+        grid = UnstructuredGrid(examples.hexbeamfile)
+        vtk_arr = grid.GetCellData().GetScalars()
 
         pv_arr = pyvista_ndarray.from_vtk_data_array(vtk_data_array=vtk_arr, dataset=GRID)
         pv_arr[0] = 99
         assert pv_arr[0] == 99
 
     def test_setitem_should_change_modified_time(self):
-        GRID = UnstructuredGrid(examples.hexbeamfile)
-        vtk_arr = GRID.GetCellData().GetScalars()
+        grid = UnstructuredGrid(examples.hexbeamfile)
+        vtk_arr = grid.GetCellData().GetScalars()
 
         pv_arr = pyvista_ndarray.from_vtk_data_array(vtk_data_array=vtk_arr, dataset=GRID)
         last_modified_time = pv_arr.VTKObject.GetMTime()
