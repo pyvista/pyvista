b753c5fc4a9ac168e2e77edeadd4f71409ee219d
Alex Kaszynski <akascap@gmail.com>
2020-04-07 14:33:12 -0600

    proxy hidden parm on pyvista_ndarray

--- pyvista/core/pyvista_ndarray.py
+++ pyvista/core/pyvista_ndarray.py
@@ -4,6 +4,7 @@ import numpy as np
 from pyvista.utilities.helpers import convert_array, FieldAssociation
 from vtk.numpy_interface.dataset_adapter import VTKObjectWrapper, VTKArray
 from vtk.vtkCommonCore import vtkWeakReference
+from vtk.util.numpy_support import vtk_to_numpy
 
 try:
     from vtk.vtkCommonKitPython import vtkDataArray, vtkAbstractArray
@@ -19,19 +20,20 @@ class pyvista_ndarray(np.ndarray):
     object.
     """
 
-    def __new__(cls, input_array, proxy):
+    def __new__(cls, input_array, proxy=None):
         """Allocate memory for the pyvista ndarray."""
-        obj = np.asarray(input_array).view(cls)
-        cls.proxy = proxy
-        return obj
-
-    def __array_finalize__(self, obj):
-        """Customize array at creation."""
-        if obj is None:
-            return
+        if proxy is None:
+            proxy = input_array
+            input_array = vtk_to_numpy(input_array)
+            obj = np.asarray(input_array).view(cls)
+            cls._proxy = proxy
+            return obj
+        else:
+            obj = np.asarray(input_array).view(cls)
+            cls._proxy = proxy
+            return obj
 
     def __setitem__(self, coords, value):
         """Update the array and update the vtk object."""
         super(pyvista_ndarray, self).__setitem__(coords, value)
-        self.proxy.Modified()
-
+        self._proxy.Modified()
