90c3547957649558e06c397f40f1fa308f4218ec
JevinJ <jevinjones@openmailbox.org>
2020-05-23 00:04:54 -0700

    Rename example_grid_point_attributes->hexbeam_point_attributes, example_grid_field_attributes->hexbeam_field_attributes

--- tests/test_datasetattributes.py
+++ tests/test_datasetattributes.py
@@ -10,29 +10,29 @@ import sys
 
 
 @fixture()
-def example_grid_point_attributes(hexbeam):
+def hexbeam_point_attributes(hexbeam):
     return hexbeam.point_arrays
 
 
 @fixture()
-def example_grid_field_attributes(hexbeam):
+def hexbeam_field_attributes(hexbeam):
     return hexbeam.field_arrays
 
 
 @fixture()
-def insert_arange_narray(example_grid_point_attributes):
-    n_points = example_grid_point_attributes.dataset.GetNumberOfPoints()
+def insert_arange_narray(hexbeam_point_attributes):
+    n_points = hexbeam_point_attributes.dataset.GetNumberOfPoints()
     sample_array = np.arange(n_points)
-    example_grid_point_attributes.append(sample_array, 'sample_array')
-    return example_grid_point_attributes, sample_array
+    hexbeam_point_attributes.append(sample_array, 'sample_array')
+    return hexbeam_point_attributes, sample_array
 
 
 @fixture()
-def insert_bool_array(example_grid_point_attributes):
-    n_points = example_grid_point_attributes.dataset.GetNumberOfPoints()
+def insert_bool_array(hexbeam_point_attributes):
+    n_points = hexbeam_point_attributes.dataset.GetNumberOfPoints()
     sample_array = np.ones(n_points, np.bool)
-    example_grid_point_attributes.append(sample_array, 'sample_array')
-    return example_grid_point_attributes, sample_array
+    hexbeam_point_attributes.append(sample_array, 'sample_array')
+    return hexbeam_point_attributes, sample_array
 
 
 def test_init(hexbeam):
@@ -44,9 +44,9 @@ def test_init(hexbeam):
 
 
 @mark.parametrize('array_key', ['invalid_array_name', -1])
-def test_get_array_should_fail_if_does_not_exist(array_key, example_grid_point_attributes):
+def test_get_array_should_fail_if_does_not_exist(array_key, hexbeam_point_attributes):
     with raises(KeyError):
-        example_grid_point_attributes.get_array(array_key)
+        hexbeam_point_attributes.get_array(array_key)
 
 
 def test_get_array_should_return_bool_array(insert_bool_array):
@@ -61,9 +61,9 @@ def test_get_array_bool_array_should_be_identical(insert_bool_array):
     assert np.array_equal(output_array, sample_array)
 
 
-def test_append_should_not_add_none_array(example_grid_point_attributes):
+def test_append_should_not_add_none_array(hexbeam_point_attributes):
     with raises(TypeError):
-        example_grid_point_attributes.append(None, 'sample_array')
+        hexbeam_point_attributes.append(None, 'sample_array')
 
 
 def test_append_should_contain_array_name(insert_arange_narray):
@@ -90,28 +90,28 @@ def test_contains_should_contain_when_added(insert_arange_narray):
 
 @settings(max_examples=20)
 @given(scalar=integers(min_value=-sys.maxsize - 1, max_value=sys.maxsize))
-def test_append_should_accept_scalar_value(scalar, example_grid_point_attributes):
-    example_grid_point_attributes.append(narray=scalar, name='int_array')
+def test_append_should_accept_scalar_value(scalar, hexbeam_point_attributes):
+    hexbeam_point_attributes.append(narray=scalar, name='int_array')
 
 
 @settings(max_examples=20)
 @given(scalar=integers(min_value=-sys.maxsize - 1, max_value=sys.maxsize))
-def test_append_scalar_value_should_give_array(scalar, example_grid_point_attributes):
-    example_grid_point_attributes.append(narray=scalar, name='int_array')
-    expected = np.full(example_grid_point_attributes.dataset.n_points, scalar)
-    assert np.array_equal(expected, example_grid_point_attributes['int_array'])
+def test_append_scalar_value_should_give_array(scalar, hexbeam_point_attributes):
+    hexbeam_point_attributes.append(narray=scalar, name='int_array')
+    expected = np.full(hexbeam_point_attributes.dataset.n_points, scalar)
+    assert np.array_equal(expected, hexbeam_point_attributes['int_array'])
 
 
 @given(arr=lists(text(alphabet=ascii_letters + digits + whitespace), max_size=16))
-def test_append_string_lists_should_equal(arr, example_grid_field_attributes):
-    example_grid_field_attributes['string_arr'] = arr
-    assert arr == example_grid_field_attributes['string_arr'].tolist()
+def test_append_string_lists_should_equal(arr, hexbeam_field_attributes):
+    hexbeam_field_attributes['string_arr'] = arr
+    assert arr == hexbeam_field_attributes['string_arr'].tolist()
 
 
 @given(arr=arrays(dtype='U', shape=10))
-def test_append_string_array_should_equal(arr, example_grid_field_attributes):
-    example_grid_field_attributes['string_arr'] = arr
-    assert np.array_equiv(arr, example_grid_field_attributes['string_arr'])
+def test_append_string_array_should_equal(arr, hexbeam_field_attributes):
+    hexbeam_field_attributes['string_arr'] = arr
+    assert np.array_equiv(arr, hexbeam_field_attributes['string_arr'])
 
 
 def test_should_remove_array(insert_arange_narray):
@@ -139,29 +139,29 @@ def test_pop_should_return_array(insert_arange_narray):
 
 
 @mark.parametrize('removed_key', [None, 'nonexistant_array_name', '', -1])
-def test_remove_should_fail_on_bad_argument(removed_key, example_grid_point_attributes):
+def test_remove_should_fail_on_bad_argument(removed_key, hexbeam_point_attributes):
     with raises(KeyError):
-        example_grid_point_attributes.remove(removed_key)
+        hexbeam_point_attributes.remove(removed_key)
 
 
 @mark.parametrize('removed_key', [None, 'nonexistant_array_name', '', -1])
-def test_del_should_fail_bad_argument(removed_key, example_grid_point_attributes):
+def test_del_should_fail_bad_argument(removed_key, hexbeam_point_attributes):
     with raises(KeyError):
-        del example_grid_point_attributes[removed_key]
+        del hexbeam_point_attributes[removed_key]
 
 
 @mark.parametrize('removed_key', [None, 'nonexistant_array_name', '', -1])
-def test_pop_should_fail_bad_argument(removed_key, example_grid_point_attributes):
+def test_pop_should_fail_bad_argument(removed_key, hexbeam_point_attributes):
     with raises(KeyError):
-        example_grid_point_attributes.pop(removed_key)
+        hexbeam_point_attributes.pop(removed_key)
 
 
-def test_length_should_increment_on_append(example_grid_point_attributes):
-    initial_len = len(example_grid_point_attributes)
-    n_points = example_grid_point_attributes.dataset.GetNumberOfPoints()
+def test_length_should_increment_on_append(hexbeam_point_attributes):
+    initial_len = len(hexbeam_point_attributes)
+    n_points = hexbeam_point_attributes.dataset.GetNumberOfPoints()
     sample_array = np.arange(n_points)
-    example_grid_point_attributes.append(sample_array, 'sample_array')
-    assert len(example_grid_point_attributes) == initial_len + 1
+    hexbeam_point_attributes.append(sample_array, 'sample_array')
+    assert len(hexbeam_point_attributes) == initial_len + 1
 
 
 def test_length_should_decrement_on_remove(insert_arange_narray):
@@ -210,8 +210,8 @@ def test_value_should_exist(insert_arange_narray):
     raise AssertionError('Array not in values.')
 
 
-def test_active_scalars_setter(example_grid_point_attributes):
-    dsa = example_grid_point_attributes
+def test_active_scalars_setter(hexbeam_point_attributes):
+    dsa = hexbeam_point_attributes
     assert dsa.active_scalars is None
 
     dsa.active_scalars = 'sample_point_scalars'
