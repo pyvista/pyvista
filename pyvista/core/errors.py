faecaf482b28b7fbed4ad198e4fa341e89f5fb31
Alex Kaszynski <akascap@gmail.com>
2020-05-25 08:07:41 +0200

    pydocstyle fixes

--- pyvista/core/errors.py
+++ pyvista/core/errors.py
@@ -1,10 +1,9 @@
-"""Pyvista specific errors"""
+"""Pyvista specific errors."""
 
 
 class NotAllTrianglesError(Exception):
-    """Exception when a mesh does not contain all triangles"""
+    """Exception when a mesh does not contain all triangles."""
 
     def __init__(self, message='Mesh must consist of only triangles'):
         """Empty init."""
-
         Exception.__init__(self, message) 
