d6668b90de37129cf8802e1f7f04bcdf63578f8c
JevinJ <jevinjones@openmailbox.org>
2020-05-27 00:18:10 -0700

    Raise ValueError in parse_font_family()

--- pyvista/plotting/theme.py
+++ pyvista/plotting/theme.py
@@ -172,7 +172,7 @@ def parse_font_family(font_family):
     # check font name
     font_family = font_family.lower()
     if font_family not in ['courier', 'times', 'arial']:
-        raise Exception('Font must be either "courier", "times" '
-                        'or "arial"')
+        raise ValueError('Font must be either "courier", "times" '
+                         'or "arial"')
 
     return FONT_KEYS[font_family]
