a1e913be68d8f565bc887c4e6359c6d400d66885
Jevin Jones <jevinjones@protonmail.com>
2020-05-28 12:24:41 -0700

    Revert exception changes to /examples/ (#767)

--- examples/02-plot/spherical.py
+++ examples/02-plot/spherical.py
@@ -34,8 +34,7 @@ def _cell_bounds(points, bound_position=0.5):
     >>> cell_bounds(a)
     array([-1.25, -0.75, -0.25,  0.25,  0.75,  1.25,  1.75,  2.25])
     """
-    if points.ndim != 1:
-        raise ValueError("Only 1D points are allowed")
+    assert points.ndim == 1, "Only 1D points are allowed"
     diffs = np.diff(points)
     delta = diffs[0] * bound_position
     bounds = np.concatenate([[points[0] - delta], points + delta])
