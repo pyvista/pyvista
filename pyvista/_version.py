4c84b19e807365364aa7a3c8888aaec5baad7ea0
Alex Kaszynski <akascap@gmail.com>
2020-06-02 12:45:08 -0600

    Hotfix/0.24.3 For Examples Path (#774)
    * HOTFIX: Allow examples path failure and override (#773)
    
    * allowing examples path failure and override
    
    * fix spelling error and docsor and docstyle
    
    * version bump to 0.24.3
    
    * fix spelling error

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -1,6 +1,6 @@
 """Version info for pyvista."""
 # major, minor, patch
-version_info = 0, 24, 2
+version_info = 0, 24, 3
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
