cf1876eb78879f7a98ed970099ee0a173e45ceeb
Alex Kaszynski <akascap@gmail.com>
2020-06-04 18:23:15 +0200

    bump version to 0.25.1 due to accidental tag on GH

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -1,6 +1,6 @@
 """Version info for pyvista."""
 # major, minor, patch
-version_info = 0, 25, 0
+version_info = 0, 25, 1
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
