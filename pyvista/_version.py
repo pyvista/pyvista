d227df8146cac5dbc29fd465b4a0cfb07ce440ea
Alex Kaszynski <akascap@gmail.com>
2020-06-04 16:02:43 -0600

    build wheel by default and test wheel install (#778)

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -1,6 +1,6 @@
 """Version info for pyvista."""
 # major, minor, patch
-version_info = 0, 25, 1
+version_info = 0, 25, 2
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
