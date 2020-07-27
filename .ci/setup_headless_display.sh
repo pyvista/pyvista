a0b85ea05b8ae0e28916fd52e734fe7949e067c1
Tetsuo Koyama <tkoyama010@gmail.com>
2020-07-27 12:02:25 +0900

    :new: PYVISTA_VIRTUAL_DISPLAY

--- .ci/setup_headless_display.sh
+++ .ci/setup_headless_display.sh
@@ -4,10 +4,8 @@ set -x
 # sudo apt-get update && sudo apt-get install python-qt4 libgl1-mesa-glx
 sudo apt-get install python-qt4 libgl1-mesa-glx
 sudo apt-get install -y xvfb
-export DISPLAY=:99.0
+export PYVISTA_VIRTUAL_DISPLAY=True
 export PYVISTA_OFF_SCREEN=True
-which Xvfb
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 # Debugging commands:
 # ls -l /etc/init.d/
 # sh -e /etc/init.d/xvfb start
