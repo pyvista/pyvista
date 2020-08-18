64e3435412d42f8d3bfb2dc21e9c1a1602f72db2
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-08-19 01:12:03 +0200

    Control points merging in the glyph filter (#870)
    * Add clean paramter to the glyph filter
    
    * Use tolerance parameter
    
    * Set default tolerance=None

--- pyvista/core/filters.py
+++ pyvista/core/filters.py
@@ -946,7 +946,7 @@ class DataSetFilters:
         return output
 
     def glyph(dataset, orient=True, scale=True, factor=1.0, geom=None,
-              tolerance=0.0, absolute=False, clamping=False, rng=None,
+              tolerance=None, absolute=False, clamping=False, rng=None,
               progress_bar=False):
         """Copy a geometric representation (called a glyph) to every point in the input dataset.
 
@@ -962,15 +962,16 @@ class DataSetFilters:
             Use the active scalars to scale the glyphs
 
         factor : float
-            Scale factor applied to sclaing array
+            Scale factor applied to scaling array
 
         geom : vtk.vtkDataSet
             The geometry to use for the glyph
 
         tolerance : float, optional
             Specify tolerance in terms of fraction of bounding box length.
-            Float value is between 0 and 1. Default is 0.0. If ``absolute``
+            Float value is between 0 and 1. Default is None. If ``absolute``
             is ``True`` then the tolerance can be an absolute distance.
+            If None, points merging as a preprocessing step is disabled.
 
         absolute : bool, optional
             Control if ``tolerance`` is an absolute distance or a fraction.
@@ -987,12 +988,13 @@ class DataSetFilters:
 
         """
         # Clean the points before glyphing
-        small = pyvista.PolyData(dataset.points)
-        small.point_arrays.update(dataset.point_arrays)
-        dataset = small.clean(point_merging=True, merge_tol=tolerance,
-                              lines_to_points=False, polys_to_lines=False,
-                              strips_to_polys=False, inplace=False,
-                              absolute=absolute, progress_bar=progress_bar)
+        if tolerance is not None:
+            small = pyvista.PolyData(dataset.points)
+            small.point_arrays.update(dataset.point_arrays)
+            dataset = small.clean(point_merging=True, merge_tol=tolerance,
+                                  lines_to_points=False, polys_to_lines=False,
+                                  strips_to_polys=False, inplace=False,
+                                  absolute=absolute, progress_bar=progress_bar)
         # Make glyphing geometry
         if geom is None:
             arrow = vtk.vtkArrowSource()
