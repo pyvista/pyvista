0d74721511de6ef8aa202bf0ea3e40aefdd453f3
JevinJ <jevinjones@openmailbox.org>
2020-09-29 18:33:16 -0700

    Fix docstrings.

--- pyvista/core/filters.py
+++ pyvista/core/filters.py
@@ -4060,13 +4060,13 @@ class PolyDataFilters(DataSetFilters):
         >>> sphere = pv.Sphere()
         >>> path = sphere.geodesic(0, 100)
         >>> length = path.compute_arc_length()['arc_length'][-1]
-        >>> print(f'Length is {length.3f}')
+        >>> print(f'Length is {length:.3f}')
         Length is 0.812
 
         This is identical to the geodesic_distance
 
         >>> length = sphere.geodesic_distance(0, 100)
-        >>> print(f'Length is {length.3f}')
+        >>> print(f'Length is {length:.3f}')
         Length is 0.812
 
         You can also plot the arc_length
