f1127546769c54b549600b50a4483fbb11ad7746
Shannon Keough <shannonjkeough@googlemail.com>
2020-10-17 16:37:59 +1100

    less intrusive # doctest: +SKIP

--- pyvista/core/filters.py
+++ pyvista/core/filters.py
@@ -3951,10 +3951,10 @@ class PolyDataFilters(DataSetFilters):
         [1, 0, 0], [0, 1, 0] and [0, 0, 1], and a sphere with radius 0.5 centered at the origin
 
         >>> import pyvista as pv # doctest: +SKIP
-        >>> sphere = pv.Sphere() # doctest: +SKIP
-        >>> points, rays, cells = sphere.multi_ray_trace([[0, 0, 0]]*3, [[1, 0, 0], [0, 1, 0], [0, 0, 1]], first_point=True) # doctest: +SKIP
-        >>> string = ", ".join([f"({point[0]:.3f}, {point[1]:.3f}, {point[2]:.3f})" for point in points]) # doctest: +SKIP
-        >>> print(f'Rays intersected at {string}') # doctest: +SKIP
+        ... sphere = pv.Sphere()
+        ... points, rays, cells = sphere.multi_ray_trace([[0, 0, 0]]*3, [[1, 0, 0], [0, 1, 0], [0, 0, 1]], first_point=True)
+        ... string = ", ".join([f"({point[0]:.3f}, {point[1]:.3f}, {point[2]:.3f})" for point in points])
+        ... print(f'Rays intersected at {string}')
         Rays intersected at (0.499, 0.000, 0.000), (0.000, 0.497, 0.000), (0.000, 0.000, 0.500)
         """
         if not poly_data.is_all_triangles():
