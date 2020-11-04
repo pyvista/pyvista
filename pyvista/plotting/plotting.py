fd1789e3ec387c71140b2598eefd2c9d631d3c50
Alex Kaszynski <akascap@gmail.com>
2020-11-03 23:49:44 -0700

    remove binding to our fly_to_mouse_position (#981)

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -899,7 +899,6 @@ class BasePlotter(PickingHelper, WidgetHelper):
         b_left_down_callback = lambda: self._add_observer('LeftButtonPressEvent', self.left_button_down)
         self.add_key_event('b', b_left_down_callback)
         self.add_key_event('v', lambda: self.isometric_view_interactive())
-        self.add_key_event('f', self.fly_to_mouse_position)
         self.add_key_event('C', lambda: self.enable_cell_picking())
         self.add_key_event('Up', lambda: self.camera.Zoom(1.05))
         self.add_key_event('Down', lambda: self.camera.Zoom(0.95))
