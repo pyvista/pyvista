143b7be98efa40bb1796a7be4d34fb742964bd0f
banesullivan <banesulli@gmail.com>
2020-11-06 12:05:04 -0700

    Add widgets to examples gallery

---
+++ examples/04-widgets/checkbox-widget.py
@@ -0,0 +1,51 @@
+"""
+Checkbox Widget
+~~~~~~~~~~~~~~~
+
+Use a checkbox to turn on/off the visibilty of meshes in a scene.
+
+See :func:`pyvista.WidgetHelper.add_checkbox_button_widget` for more details.
+
+In this example, we will add many meshes to a scene with unique colors and
+create corresponding checkboxes for those meshes of the same color to toggle
+their visibilty in the scene.
+"""
+
+import pyvista as pv
+
+colors = [["ff0000", "28e5da", "0000ff"],
+          ["ffff00", "c8bebe", "f79292"],
+          ["fffff0", "f18c1d", "23dcaa"],
+          ["d785ec", "9d5b13", "e4e0b1"],
+          ["894509", "af45f5", "fff000"]]
+
+class SetVisibilityCallback:
+    """Helper callback to keep a reference to the actor being modified."""
+    def __init__(self, actor):
+        self.actor = actor
+
+    def __call__(self, state):
+        self.actor.SetVisibility(state)
+
+###############################################################################
+
+# Widget size
+size = 50
+
+p = pv.Plotter()
+
+Startpos = 12
+for i, lst in enumerate(colors):
+    for j, color in enumerate(lst):
+        actor = p.add_mesh(pv.Sphere(center=(i, j, 0)), color=color)
+        # Make a seperate callback for each widget
+        callback = SetVisibilityCallback(actor)
+        p.add_checkbox_button_widget(callback, value=True,
+                                     position=(5.0, Startpos), size=size,
+                                     border_size=1,
+                                     color_on=color,
+                                     color_off='grey',
+                                     background_color='grey')
+        Startpos = Startpos + size + (size / 10)
+
+p.show()
