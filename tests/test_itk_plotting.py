1866f3ce59b7ff9dddf0584f3f8f06d150e83a72
Alex Kaszynski <akascap@gmail.com>
2020-11-07 07:28:19 -0700

    Add additional methods to PlotterITK (#980)
    * added additional methods
    
    * revert renderer.py
    
    * remove 'add_actor' from PlotterITK

--- tests/test_itk_plotting.py
+++ tests/test_itk_plotting.py
@@ -20,20 +20,30 @@ except ImportError:
 SPHERE = pyvista.Sphere()
 SPHERE['z'] = SPHERE.points[:, 2]
 
+no_itk = pytest.mark.skipif(NO_PLOTTING or not HAS_ITK, reason="Requires system to support plotting and have itkwidgets.")
 
-@pytest.mark.skipif(NO_PLOTTING or not HAS_ITK, reason="Requires system to support plotting and have itkwidgets.")
+@no_itk
 def test_itk_plotting():
     viewer = pyvista.plot_itk(SPHERE)
     assert isinstance(viewer, itkwidgets.Viewer)
 
 
-@pytest.mark.skipif(NO_PLOTTING or not HAS_ITK, reason="Requires system to support plotting and have itkwidgets.")
+@no_itk
 def test_itk_plotting_points():
+    with pytest.raises(TypeError):
+        viewer = pyvista.plot_itk([1, 2, 3], point_size='foo')
+
     viewer = pyvista.plot_itk(np.random.random((100, 3)))
     assert isinstance(viewer, itkwidgets.Viewer)
 
 
-@pytest.mark.skipif(NO_PLOTTING or not HAS_ITK, reason="Requires system to support plotting and have itkwidgets.")
+def test_itk_plotting_points_polydata():
+    points = pyvista.wrap(np.random.random((100, 3)))
+    viewer = pyvista.plot_itk(points)
+    assert isinstance(viewer, itkwidgets.Viewer)
+
+
+@no_itk
 def test_itk_plotting_class():
     pl = pyvista.PlotterITK()
     pl.add_mesh(SPHERE, scalars='z')
@@ -41,17 +51,30 @@ def test_itk_plotting_class():
     assert isinstance(viewer, itkwidgets.Viewer)
 
 
-@pytest.mark.skipif(NO_PLOTTING or not HAS_ITK, reason="Requires system to support plotting and have itkwidgets.")
+@no_itk
 def test_itk_plotting_class_no_scalars():
     pl = pyvista.PlotterITK()
     pl.add_mesh(SPHERE, color='w')
+    pl.camera_position = [[1, 0, 0], [1, 0, 0], [1, 0, 0]]
+    assert isinstance(pl.camera_position, list)
+    pl.background_color = 'k'
+    assert pl.background_color == (0.0, 0.0, 0.0)
     viewer = pl.show()
     assert isinstance(viewer, itkwidgets.Viewer)
 
 
-@pytest.mark.skipif(NO_PLOTTING or not HAS_ITK, reason="Requires system to support plotting and have itkwidgets.")
+@no_itk
 def test_itk_plotting_class_npndarray_scalars():
     pl = pyvista.PlotterITK()
-    pl.add_mesh(SPHERE, scalars=SPHERE.points[:, 0])
+    pl.add_mesh(SPHERE, scalars=SPHERE.points[:, 0], smooth_shading=True)
+    viewer = pl.show()
+    assert isinstance(viewer, itkwidgets.Viewer)
+
+
+@no_itk
+def test_itk_plotting_class_unstructured(hexbeam):
+    hexbeam.clear_arrays()
+    pl = pyvista.PlotterITK()
+    pl.add_mesh(hexbeam, smooth_shading=True)
     viewer = pl.show()
     assert isinstance(viewer, itkwidgets.Viewer)
