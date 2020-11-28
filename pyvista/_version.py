3d3dbf2972afa192c05d3f6139b9d5fb4e673910
Alex Kaszynski <akascap@gmail.com>
2020-11-28 15:07:48 -0700

    version bump to 0.27.3

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -7,7 +7,7 @@ version_info = 0, 27, 'dev0'
 
 """
 # major, minor, patch
-version_info = 0, 27, 2
+version_info = 0, 27, 3
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
