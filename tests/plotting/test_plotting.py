42b5b09d9a7a5137680c86729ab94d6b4bed1f29
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2020-12-22 02:11:48 +0100

    Bugfix for #1053: `Plotter.update` fails with `interactive_update=True` if `auto_close=False` is not specified (#1056)
    * Bugfix: interactive_update with default auto_close
    
    * Added comment with issue number
    
    * Reordered imports
    
    * Skip test if system doesnt support plotting
    
    * move interactive test to test/plotting/plotting.py
    
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -1370,3 +1370,16 @@ def test_index_vs_loc():
         assert pl.index_to_loc(np.int_(val)) == val
         assert pl.loc_to_index(val) == val
         assert pl.loc_to_index(np.int_(val)) == val
+
+
+@skip_no_plotting
+def test_interactive_update():
+    # Regression test for #1053
+    p = pyvista.Plotter()
+    p.show(interactive_update=True)
+    assert isinstance(p.iren, vtk.vtkRenderWindowInteractor)
+    p.close()
+
+    p = pyvista.Plotter()
+    with pytest.warns(UserWarning):
+        p.show(auto_close=True, interactive_update=True)
