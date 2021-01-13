1661d0050bfdef04c113864a1cdf94639ab86fb0
Phil Chiu <whophilchiu@gmail.com>
2021-01-13 07:14:36 -0800

    Remove print statement in test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -56,7 +56,6 @@ def test_read(tmpdir, use_pathlib):
     types = (pyvista.PolyData, pyvista.PolyData, pyvista.UnstructuredGrid,
              pyvista.PolyData, pyvista.UniformGrid, pyvista.RectilinearGrid)
     for i, filename in enumerate(fnames):
-        print(filename)
         obj = fileio.read(filename)
         assert isinstance(obj, types[i])
     # Now test the standard_reader_routine
