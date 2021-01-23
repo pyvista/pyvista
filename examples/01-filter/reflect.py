f35532aba4ec46f3ce4e842f7dda9326ad06ddb1
Rodrigo Mologni <rodrigomologni@gmail.com>
2021-01-23 20:39:56 -0300

    add reflect example

---
+++ examples/01-filter/reflect.py
@@ -0,0 +1,26 @@
+"""
+.. _ref_reflect_example:
+
+Reflect Meshes
+~~~~~~~~~~~~~~
+
+Reflects a mesh across a plane.
+
+"""
+
+from pyvista import examples
+
+###############################################################################
+# This example demonstrates how to reflect a mesh across a plane.
+#
+# Loads an example mesh:
+airplane = examples.load_airplane()
+
+###############################################################################
+# Reflects the mesh across a plane parallel to Z plane and centered in -100
+# (the geometry input is copied to the output):
+airplane = airplane.reflect('z', copy=True, center=-100)
+
+###############################################################################
+# Plots the reflected mesh:
+airplane.plot(show_edges=True)
