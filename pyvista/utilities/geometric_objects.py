4d69e92d0b4201d146a3138283e5a1e83dae5ca1
Tetsuo Koyama <tkoyama010@gmail.com>
2021-01-23 15:37:38 +0900

    :triangular_flag_on_post: Add option level and quads to function Box

--- pyvista/utilities/geometric_objects.py
+++ pyvista/utilities/geometric_objects.py
@@ -408,17 +408,33 @@ def Cube(center=(0., 0., 0.), x_length=1.0, y_length=1.0, z_length=1.0, bounds=N
     return pyvista.wrap(src.GetOutput())
 
 
-def Box(bounds=(-1.,1.,-1.,1.,-1.,1.)):
+def Box(bounds=(-1.,1.,-1.,1.,-1.,1.), level = 0, quads = True):
     """Create a box with solid faces for the given bounds.
 
     Parameters
     ----------
     bounds : np.ndarray or list
-        Specify the bounding box of the cube. If given, all other arguments are
-        ignored. ``(xMin,xMax, yMin,yMax, zMin,zMax)``
+        Specify the bounding box of the cube.
+        ``(xMin,xMax, yMin,yMax, zMin,zMax)``
+
+    level : int
+        Level of subdivision of the faces.
+
+    quads : bool, optional
+        Flag to tell the source to generate either a quad or two triangle for a set of four points.  Default True
 
     """
-    return Cube(bounds=bounds)
+    if np.array(bounds).size != 6:
+        raise TypeError('Bounds must be given as length 6 tuple: (xMin,xMax, yMin,yMax, zMin,zMax)')
+    src = vtk.vtkTessellatedBoxSource()
+    src.SetLevel(level)
+    if quads is True:
+       src.QuadsOn()
+    else:
+       src.QuadsOff()
+    src.SetBounds(bounds)
+    src.Update()
+    return pyvista.wrap(src.GetOutput())
 
 
 def Cone(center=(0.,0.,0.), direction=(1.,0.,0.), height=1.0, radius=None,
