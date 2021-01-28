bba2e004001e6188f0db20489553c8ba02b95165
Alex Kaszynski <akascap@gmail.com>
2021-01-27 23:36:04 -0700

    add rc notes:

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -5,6 +5,10 @@ For example:
 
 version_info = 0, 27, 'dev0'
 
+When generating pre-release wheels, use '0rcN', for example:
+
+version_info = 0, 28, '0rc1'
+
 """
 # major, minor, patch
 version_info = 0, 28, 0
