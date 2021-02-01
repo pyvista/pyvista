f284f0c6d326adaf304b091d4c7d1fee81217b6b
Alex Kaszynski <akascap@gmail.com>
2021-02-01 07:29:53 -0700

    code style fixes

--- pyvista/utilities/misc.py
+++ pyvista/utilities/misc.py
@@ -1,10 +1,10 @@
-"""Miscelanious pyvista functions"""
+"""Miscellaneous pyvista functions."""
 import vtk
 import numpy as np
 
 
 def _get_vtk_id_type():
-    """Return the numpy datatype responding to vtkIdTypeArray"""
+    """Return the numpy datatype responding to ``vtk.vtkIdTypeArray``."""
     VTK_ID_TYPE_SIZE = vtk.vtkIdTypeArray().GetDataTypeSize()
     if VTK_ID_TYPE_SIZE == 4:
         return np.int32
