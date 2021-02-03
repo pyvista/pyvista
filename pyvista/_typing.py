8f62076efbc4992d5d7ecff89ad2ebaedfe12a10
JevinJ <jevinjones@openmailbox.org>
2021-02-02 17:16:29 -0800

    Update Vector alias to include ndarray.

--- pyvista/_typing.py
+++ pyvista/_typing.py
@@ -4,6 +4,6 @@ from typing import Union, List, Tuple, Sequence
 
 import numpy as np
 
-Vector = Union[List[float], Tuple[float, float, float]]
+Vector = Union[List[float], Tuple[float, float, float], np.ndarray]
 Number = Union[float, int]
 NumericArray = Union[Sequence[Union[float, int]], np.ndarray]
