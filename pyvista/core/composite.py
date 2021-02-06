e57e79159073a5e14f8faa836fc2a115a0c669cd
JevinJ <jevinjones@openmailbox.org>
2021-02-05 23:04:44 -0800

    Fix mypy errors

--- pyvista/core/composite.py
+++ pyvista/core/composite.py
@@ -293,6 +293,8 @@ class MultiBlock(vtkMultiBlockDataSet, CompositeFilters, DataObject):
         3
 
         """
+        i: int = 0
+        name: Optional[str] = None
         if isinstance(index, (np.ndarray, collections.abc.Sequence)) and not isinstance(index, str):
             i, name = index[0], index[1]
         elif isinstance(index, str):
@@ -301,8 +303,8 @@ class MultiBlock(vtkMultiBlockDataSet, CompositeFilters, DataObject):
             except KeyError:
                 i = -1
             name = index
-        else:
-            i, name = index, None  # type: ignore
+        elif isinstance(index, int):
+            i, name = index, None
         if data is not None and not is_pyvista_dataset(data):
             data = wrap(data)
         if i == -1:
