8e1bb4baee9c92ea7949a5016c33fce50da226bd
Bane Sullivan <banesullivan@gmail.com>
2021-02-10 11:14:27 -0700

    Expose start_xvfb (#1151)

--- pyvista/utilities/__init__.py
+++ pyvista/utilities/__init__.py
@@ -10,3 +10,4 @@ from .parametric_objects import *
 from .sphinx_gallery import Scraper, _get_sg_image_scraper
 from .regression import compare_images
 from . import transformations
+from .xvfb import start_xvfb
