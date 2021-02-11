e832b990c236c7ccbcce36336a16b7b312fdad44
JevinJ <jevinjones@openmailbox.org>
2021-02-11 12:39:50 -0800

    Update __init__.py

---
+++ pyvista/core/filters/__init__.py
@@ -0,0 +1,4 @@
+from .compositefilters import CompositeFilters
+from .datasetfilters import DataSetFilters
+from .polydatafilters import PolyDataFilters
+from .unstructuredgridfilters import UnstructuredGridFilters
