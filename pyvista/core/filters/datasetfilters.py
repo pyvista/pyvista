f9cb9c0d04d6bff8bfb74921df2984d6e09b4c41
JevinJ <jevinjones@openmailbox.org>
2021-02-11 14:06:55 -0800

    Optimize imports and remove helper method duplicates.

--- pyvista/core/filters/datasetfilters.py
+++ pyvista/core/filters/datasetfilters.py
@@ -34,29 +34,6 @@ from pyvista.utilities import transformations
 from pyvista.utilities.cells import numpy_to_idarr
 
 
-def _update_alg(alg, progress_bar=False, message=''):
-    """Update an algorithm with or without a progress bar."""
-    if progress_bar:
-        with ProgressMonitor(alg, message=message):
-            alg.Update()
-    else:
-        alg.Update()
-
-
-def _get_output(algorithm, iport=0, iconnection=0, oport=0, active_scalars=None,
-                active_scalars_field='point'):
-    """Get the algorithm's output and copy input's pyvista meta info."""
-    ido = algorithm.GetInputDataObject(iport, iconnection)
-    data = wrap(algorithm.GetOutputDataObject(oport))
-    if not isinstance(data, pyvista.MultiBlock):
-        data.copy_meta_from(ido)
-        if not data.field_arrays and ido.field_arrays:
-            data.field_arrays.update(ido.field_arrays)
-        if active_scalars is not None:
-            data.set_active_scalars(active_scalars, preference=active_scalars_field)
-    return data
-
-
 @abstract_class
 class DataSetFilters:
     """A set of common filters that can be applied to any vtkDataSet."""
--- pyvista/core/filters/unstructuredgridfilters.py
+++ pyvista/core/filters/unstructuredgridfilters.py
@@ -22,44 +22,9 @@ Example
 >>> iso = dataset.contour()
 
 """
-import collections.abc
-import logging
-from functools import wraps
-
-import numpy as np
-import vtk
-from vtk.util.numpy_support import vtk_to_numpy
 
 import pyvista
-from pyvista.utilities import (FieldAssociation, NORMALS, assert_empty_kwargs,
-                               generate_plane, get_array, vtk_id_list_to_array,
-                               wrap, ProgressMonitor, abstract_class)
-from pyvista.utilities.cells import numpy_to_idarr
-from pyvista.core.errors import NotAllTrianglesError
-from pyvista.utilities import transformations
-
-
-def _update_alg(alg, progress_bar=False, message=''):
-    """Update an algorithm with or without a progress bar."""
-    if progress_bar:
-        with ProgressMonitor(alg, message=message):
-            alg.Update()
-    else:
-        alg.Update()
-
-
-def _get_output(algorithm, iport=0, iconnection=0, oport=0, active_scalars=None,
-                active_scalars_field='point'):
-    """Get the algorithm's output and copy input's pyvista meta info."""
-    ido = algorithm.GetInputDataObject(iport, iconnection)
-    data = wrap(algorithm.GetOutputDataObject(oport))
-    if not isinstance(data, pyvista.MultiBlock):
-        data.copy_meta_from(ido)
-        if not data.field_arrays and ido.field_arrays:
-            data.field_arrays.update(ido.field_arrays)
-        if active_scalars is not None:
-            data.set_active_scalars(active_scalars, preference=active_scalars_field)
-    return data
+from pyvista.utilities import (abstract_class)
 
 
 @abstract_class
