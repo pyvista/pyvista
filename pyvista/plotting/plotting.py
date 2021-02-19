b525b95fe60a4165b0ae74b2906798c8cbe52095
Andras Deak <deak.andris@gmail.com>
2021-02-19 21:50:29 +0100

    Fix doctest failure in BasePlotter.where_is
    Fixes #1181

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -4029,14 +4029,14 @@ class BasePlotter(PickingHelper, WidgetHelper):
         >>> import pyvista as pv
         >>> plotter = pv.Plotter(shape=(2, 2))
         >>> plotter.subplot(0, 0)
-        >>> plotter.add_mesh(pv.Box(), name='box')  # doctest:+SKIP
+        >>> _ = plotter.add_mesh(pv.Box(), name='box')
         >>> plotter.subplot(0, 1)
-        >>> plotter.add_mesh(pv.Sphere(), name='sphere')  # doctest:+SKIP
+        >>> _ = plotter.add_mesh(pv.Sphere(), name='sphere')
         >>> plotter.subplot(1, 0)
-        >>> plotter.add_mesh(pv.Box(), name='box')  # doctest:+SKIP
+        >>> _ = plotter.add_mesh(pv.Box(), name='box')
         >>> plotter.subplot(1, 1)
-        >>> plotter.add_mesh(pv.Cone(), name='cone')  # doctest:+SKIP
-        >>> plotter.where_is('box')  # doctest:+SKIP
+        >>> _ = plotter.add_mesh(pv.Cone(), name='cone')
+        >>> plotter.where_is('box')
         [(0, 0), (1, 0)]
 
         """
