99cc80e3a27977f5e17190d06b782fbc9f18baac
Tetsuo Koyama <tkoyama010@gmail.com>
2021-04-05 14:47:00 +0900

    Update pyvista/core/filters.py
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- pyvista/core/filters.py
+++ pyvista/core/filters.py
@@ -2436,8 +2436,8 @@ class DataSetFilters:
             in a cell of the input.  If not given, tolerance is
             automatically generated.
 
-        fname : str
-            The string file name of the save figure.
+        fname : str, optional
+            Save the figure this file name when set.
 
         Examples
         --------
