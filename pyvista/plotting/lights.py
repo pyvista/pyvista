6c378c0ff3482443028e11ce104286e1bf7493bf
Alex Kaszynski <akascap@gmail.com>
2021-04-05 19:48:48 -0600

    Update pyvista/plotting/lights.py
    Co-authored-by: Andras Deak <adeak@users.noreply.github.com>

--- pyvista/plotting/lights.py
+++ pyvista/plotting/lights.py
@@ -99,7 +99,7 @@ class Light(vtkLight):
     >>> light = pv.Light(position=(10, 10, 10))
     >>> light.diffuse_color = 1, 0, 0
 
-    Create a positional actor light at (0, 0, 3) with a cone angle of
+    Create a positional light at (0, 0, 3) with a cone angle of
     30, exponent of 20, and a visible actor.
 
     >>> light = pv.Light(position=(0, 0, 3), show_actor=True,
