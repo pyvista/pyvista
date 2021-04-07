63581c40a666667504bf18dfb58874fc22332816
Tetsuo Koyama <tkoyama010@gmail.com>
2021-04-08 02:11:16 +0900

    Fix Test Package Docstrings error

--- pyvista/plotting/camera.py
+++ pyvista/plotting/camera.py
@@ -298,6 +298,7 @@ class Camera(_vtk.vtkCamera):
         --------
         >>> import pyvista
         >>> plotter = pyvista.Plotter()
+        >>> plotter.camera.direction = (0.0, 0.0, -1.0)
         >>> plotter.camera.direction
         (0.0, 0.0, -1.0)
 
