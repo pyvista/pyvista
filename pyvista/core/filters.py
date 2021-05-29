55032f04ebc13a83c5ad77fd8e37745301b1e0c5
Andras Deak <deak.andris@gmail.com>
2021-05-30 01:56:21 +0200

    Add missing examples import to texture_map_to_sphere docs

--- pyvista/core/filters.py
+++ pyvista/core/filters.py
@@ -1068,6 +1068,7 @@ class DataSetFilters:
         Map a puppy texture to a sphere
 
         >>> import pyvista
+        >>> from pyvista import examples
         >>> sphere = pyvista.Sphere()
         >>> sphere = sphere.texture_map_to_sphere()
         >>> tex = examples.download_puppy_texture()  # doctest:+SKIP
