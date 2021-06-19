690143c076619c839c5e97186325ec306c7b2f1b
Andras Deak <deak.andris@gmail.com>
2021-06-19 22:12:22 +0200

    Change geodesic picking note to note plus versionadded

--- pyvista/plotting/picking.py
+++ pyvista/plotting/picking.py
@@ -444,12 +444,14 @@ class PickingHelper:
             If ``True``, the created geodesic path is a single ordered
             and cleaned line from the first point to the last.
 
-            .. versionchanged:: 0.32.0
+            .. note::
 
                 In older versions there were apparent discontinuities
                 in the resulting path due to the behavior of the
                 underlying VTK filter which corresponds to
                 ``keep_order=False``.
+
+            .. versionadded:: 0.32.0
 
         kwargs : optional
             All remaining keyword arguments are used to control how
