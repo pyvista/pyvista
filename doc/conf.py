c69c355200a5e104c5c5334cfbc4d3a62303e91d
Alex Kaszynski <akascap@gmail.com>
2021-06-22 22:24:41 -0600

    fix copybutton prompt text

--- doc/conf.py
+++ doc/conf.py
@@ -264,7 +264,8 @@ notfound_no_urls_prefix = True
 
 # Copy button customization ---------------------------------------------------
 # exclude traditional Python prompts from the copied code
-copybutton_prompt_text = ">>> "
+copybutton_prompt_text = r'>>> |\.\.\. '
+copybutton_prompt_is_regexp = True
 
 
 # -- Autosummary options
