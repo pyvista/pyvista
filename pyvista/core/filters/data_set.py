4c5545375e2526b94c09d748a0b46ca99405a0eb
Matthew Flamm <matthewhflamm@gmail.com>
2021-07-23 16:11:44 -0400

    fix example

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -301,13 +301,12 @@ class DataSetFilters:
         >>> clipped.plot()
 
         Remove the part of the mesh with "sample_point_scalars" below
-        100.  Since these scalars are already active, there's no need
-        to specify ``scalars=``
+        100.
 
         >>> import pyvista as pv
         >>> from pyvista import examples
         >>> dataset = examples.load_hexbeam()
-        >>> clipped = dataset.clip_scalar(value=100, invert=False)
+        >>> clipped = dataset.clip_scalar(scalars="sample_point_scalars", value=100, invert=False)
         >>> clipped.plot()
 
         """
