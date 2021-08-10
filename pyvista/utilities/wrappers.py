d3e28091f3d250a5d6399f86f420723116c46507
Matthew Flamm <matthewhflamm@gmail.com>
2021-08-10 16:34:47 -0400

    add better docstring with example

--- pyvista/utilities/wrappers.py
+++ pyvista/utilities/wrappers.py
@@ -1,4 +1,25 @@
-"""Wrapper mapping."""
+"""Wrapper mapping.
+
+Setting pyvista._wrappers allows for developers to override the default class used
+to coerce a ``vtkDataSet`` into a pyvista object.  This is useful when creating a
+subclass of a pyvista DataSet class.
+
+Examples
+--------
+A user-defined Foo class is defined that extends the functionality of
+:class:pyvista.PolyData.  This class is set as the default wrapper for
+``vtkPolyData`` objects.
+
+>>> import pyvista
+>>> class Foo(pyvista.PolyData):
+...     pass  # Extend PolyData here
+>>> pyvista._wrappers['vtkPolyData'] = Foo
+>>> uniform_grid = pyvista.UniformGrid()
+>>> surface = uniform_grid.extract_surface()
+>>> assert isinstance(surface, Foo)
+
+"""
+
 import pyvista
 
 _wrappers = {
