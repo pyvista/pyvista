267687f9973e1ed758fb1a77abaaa0e015184ec7
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2021-08-19 18:55:51 -0400

    Add Reader classes (#1536)
    * add Reader class
    
    * add ensight and openfoam to list of readers to implement.
    
    * address doc style
    
    * add all XML data readers
    
    * add ensight and openfoam readers
    
    * add tests for Reader and XML*Readers
    
    * add ensight test
    
    * expand example to include reusing Reader
    
    * codespell fix
    
    * whitespaces
    
    * add list of supported readers
    
    * better class docstrings
    
    * add all readers to pyvista namespace
    
    * folder typo
    
    * reorganize class structure, use get_reader function instead of Reader class
    
    * change fileio get_reader to get_vtk_reader
    
    * whitespace
    
    * more readers
    
    * clarify new structure in example
    
    * DRY: simplify inheritance of BaseReader
    
    * don't instantiate vtkreader in class
    
    * add missing staticmethod wrapper
    
    * use Any type for _class_reader
    
    * add load option to download_*
    
    * add examples to docstrings
    
    * use UpdateInformation
    
    * fix reader example
    
    * fix loading of more complex cases
    
    * use download functions for nonxmlbased reader tests
    
    * add __repr__
    
    * add returns to docstrings
    
    * Add examples to BaseReader and DataArraySelection
    
    * Change name to PointCellDataSelection
    
    * Add to API documentation
    
    * add XMLPRectilinearGridReader
    
    * fix docstring for __repr__
    
    * split up docstring plot line
    
    * clean up documentation
    
    * fixed docstring
    
    * remove duplicate XMLMultiBlockDataReader entry
    
    * Add get_reader, add 'Reader Classes' heading
    
    * use consistent Parallel naming
    
    * used table
    
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

---
+++ examples/00-load/reader.py
@@ -0,0 +1,76 @@
+"""
+.. _reader_example:
+
+Load data using a Reader
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+###############################################################################
+# To have more control over reading data files, use a class based reader.
+# This class allows for more fine-grained control over reading datasets from
+# files.  See :func:`pyvista.get_reader` for a list of file types supported.
+
+import pyvista
+from pyvista import examples
+import numpy as np
+from tempfile import NamedTemporaryFile
+
+###############################################################################
+# An XML PolyData file in ``.vtp`` format is created.  It will be saved in a
+# temporary file for this example.
+
+temp_file = NamedTemporaryFile('w', suffix=".vtp")
+temp_file.name
+
+###############################################################################
+# :class:`pyvista.Sphere` already includes ``Normals`` point data.  Additionally
+# ``height`` point data and ``id`` cell data is added.
+mesh = pyvista.Sphere()
+mesh['height'] = mesh.points[:, 1]
+mesh['id'] = np.arange(mesh.n_cells)
+mesh.save(temp_file.name)
+
+###############################################################################
+# :func:`pyvista.read` function reads all the data in the file. This provides
+# a quick and easy one-liner to read data from files.
+
+new_mesh = pyvista.read(temp_file.name)
+print(f"All arrays: {mesh.array_names}")
+
+###############################################################################
+# Using :func:`pyvista.get_reader` enables more fine-grained control of reading data
+# files. Reading in a ``.vtp``` file uses the :class:`pyvista.XMLPolyDataReader`.
+
+reader = pyvista.get_reader(temp_file.name)
+reader
+# Alternative method: reader = pyvista.XMLPolyDataReader(temp_file.name)
+
+###############################################################################
+# Some reader classes, including this one, offer the ability to inspect the
+# data file before loading all the data. For example, we can access the number
+# and names of point and cell arrays.
+
+print(f"Number of point arrays: {reader.number_point_arrays}")
+print(f"Available point data:   {reader.point_array_names}")
+print(f"Number of cell arrays:  {reader.number_cell_arrays}")
+print(f"Available cell data:    {reader.cell_array_names}")
+
+###############################################################################
+# We can select which data to read by selectively disabling or enabling
+# specific arrays or all arrays.  Here we disable all the cell arrays and
+# the ``Normals`` point array to leave only the ``height`` point array.  The data
+# is finally read into a pyvista object that only has the ``height`` point array.
+
+reader.disable_all_cell_arrays()
+reader.disable_point_array('Normals')
+print(f"Point array status: {reader.all_point_arrays_status}")
+print(f"Cell array status:  {reader.all_cell_arrays_status}")
+reader_mesh = reader.read()
+print(f"Read arrays:        {reader_mesh.array_names}")
+
+###############################################################################
+# We can reuse the reader object to choose different variables if needed.
+
+reader.enable_all_cell_arrays()
+reader_mesh_2 = reader.read()
+print(f"New read arrays: {reader_mesh_2.array_names}")
