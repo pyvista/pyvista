12b6d2b85291319678da7f3930077606ee6fefe9
Alex Kaszynski <akascap@gmail.com>
2021-09-05 07:21:44 -0600

    Update pyvista/plotting/plotting.py

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -2982,7 +2982,8 @@ class BasePlotter(PickingHelper, WidgetHelper):
         --------
         >>> import pyvista
         >>> pl = pyvista.Plotter()
-        >>> actor = pl.add_text('Sample Text', position='upper_right', color='blue', shadow=True)
+        >>> actor = pl.add_text('Sample Text', position='upper_right', color='blue',
+        ...                     shadow=True, font_size=26)
         >>> pl.show()
         
         """
