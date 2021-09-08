fd858a8a7846b1feff71c7f30c5084acaef70ba7
Alex Kaszynski <akascap@gmail.com>
2021-09-08 15:21:55 -0600

    Apply suggestions from code review

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -3260,9 +3260,9 @@ class BasePlotter(PickingHelper, WidgetHelper):
         >>> import numpy as np
         >>> import pyvista
         >>> pl = pyvista.Plotter()       
-        >>> points = np.array([[0, 1, 0], [1, 0, 0], [1, 1, 0], [2,0,0]])
+        >>> points = np.array([[0, 1, 0], [1, 0, 0], [1, 1, 0], [2, 0, 0]])
         >>> pl.add_lines(points, color='yellow', width=3)
-        >>> pl.camera_position='xy'
+        >>> pl.camera_position = 'xy'
         >>> pl.show()
         
         """
@@ -3414,9 +3414,9 @@ class BasePlotter(PickingHelper, WidgetHelper):
         >>> import numpy as np
         >>> import pyvista
         >>> pl = pyvista.Plotter()
-        >>> points = np.array([[0,0,0],
-        ...                    [1,1,0],
-        ...                    [2,0,0]])
+        >>> points = np.array([[0, 0, 0],
+        ...                    [1, 1, 0],
+        ...                    [2, 0, 0]])
         >>> labels = ['Point A', 'Point B', 'Point C']
         >>> actor = pl.add_point_labels(points, labels, italic=True, font_size=20,
         ...                             point_color='red', point_size=20, render_points_as_spheres=True,
