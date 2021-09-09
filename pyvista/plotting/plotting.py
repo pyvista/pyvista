27c2282751834f2f7225595e980529a7473c22cc
Alex Kaszynski <akascap@gmail.com>
2021-09-08 18:42:21 -0600

    fix missing actor return

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -3261,7 +3261,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         >>> import pyvista
         >>> pl = pyvista.Plotter()       
         >>> points = np.array([[0, 1, 0], [1, 0, 0], [1, 1, 0], [2, 0, 0]])
-        >>> pl.add_lines(points, color='yellow', width=3)
+        >>> actor = pl.add_lines(points, color='yellow', width=3)
         >>> pl.camera_position = 'xy'
         >>> pl.show()
         
