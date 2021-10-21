e09c25d019a3803281d0a01a43a19f26f164eb4d
Matthew Flamm <matthewhflamm@gmail.com>
2021-10-21 16:40:38 -0400

    modify point description slightly

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -1885,7 +1885,7 @@ class DataSet(DataSetFilters, DataObject):
         ----------
         point : iterable(float) or np.ndarray
             Coordinates of point to query (length 3) or a ``numpy`` array of ``n``
-            points (size ``n``x3).
+            points with shape ``nx3``.
 
         Returns
         -------
