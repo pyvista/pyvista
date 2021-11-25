231357065abe5afb9d0a03af32bebce411badebc
banesullivan <banesulli@gmail.com>
2021-11-24 22:27:04 -0700

    Fix sphinx_gallery_thumbnail_number

--- examples/01-filter/streamlines_2D.py
+++ examples/01-filter/streamlines_2D.py
@@ -6,7 +6,7 @@
 
 Integrate a vector field to generate streamlines on a 2D surface.
 """
-# sphinx_gallery_thumbnail_number = 2
+# sphinx_gallery_thumbnail_number = 3
 
 ###############################################################################
 # This example generates streamlines of flow around a cylinder in cross flow.
