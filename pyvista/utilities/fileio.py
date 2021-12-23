e4a4e7668e621894737668e6946a7b8d50d86b03
Alex Kaszynski <akascap@gmail.com>
2021-12-23 09:53:24 -0700

    allow mesio 4.X as well

--- pyvista/utilities/fileio.py
+++ pyvista/utilities/fileio.py
@@ -583,8 +583,11 @@ def read_plot3d(filename, q_filenames=(), auto_detect=True, attrs=None):
 
 def from_meshio(mesh):
     """Convert a ``meshio`` mesh instance to a PyVista mesh."""
-    from meshio._vtk_common import meshio_to_vtk_type
-    from meshio.vtk._vtk_42 import vtk_type_to_numnodes
+    try:  # meshio<5.0 compatibility
+        from meshio.vtk._vtk import meshio_to_vtk_type, vtk_type_to_numnodes
+    except ImportError:  # pragma: no cover
+        from meshio._vtk_common import meshio_to_vtk_type
+        from meshio.vtk._vtk_42 import vtk_type_to_numnodes
 
     # Extract cells from meshio.Mesh object
     offset = []
@@ -690,7 +693,10 @@ def save_meshio(filename, mesh, file_format=None, **kwargs):
             "pip install meshio"
         )
 
-    from meshio._vtk_common import vtk_to_meshio_type
+    try:  # for meshio<5.0 compatibility
+        from meshio.vtk._vtk import vtk_to_meshio_type
+    except:  # pragma: no cover
+        from meshio._vtk_common import vtk_to_meshio_type
 
     # Make sure relative paths will work
     filename = os.path.abspath(os.path.expanduser(str(filename)))
