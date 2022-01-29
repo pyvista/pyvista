9c8d3f194006955ba6e5b209be08e662213025e7
Bane Sullivan <banesullivan@gmail.com>
2022-01-28 18:15:17 -0700

    Move Windows CI Tests to GH Actions (#2083)
    * Rename CI Windows var
    
    * Add windows GitHub Actions job
    
    * Simplify global env vars
    
    * Reformat job steps
    
    * Diable broken caching
    
    * Fix typo
    
    * Add better job name
    
    * Add TODO notes
    
    * Remove Azure 🎉
    
    * Attempt cache example data
    
    * Remove pytest-azurepipelines
    
    * Fix syntax
    
    * Fix examples path var
    
    * cleanup
    
    * Attempt cache pip cache dir
    
    * fail-fast: false on linux matrix
    
    * Fix typo

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -52,8 +52,8 @@ IMAGE_CACHE_DIR = os.path.join(THIS_PATH, 'image_cache')
 if not os.path.isdir(IMAGE_CACHE_DIR):
     os.mkdir(IMAGE_CACHE_DIR)
 
-# always set on azure CI
-AZURE_CI_WINDOWS = os.environ.get('AZURE_CI_WINDOWS', 'false').lower() == 'true'
+# always set on Windows CI
+CI_WINDOWS = os.environ.get('CI_WINDOWS', 'false').lower() == 'true'
 
 skip_not_vtk9 = pytest.mark.skipif(not VTK9, reason="Test requires >=VTK v9")
 
@@ -183,7 +183,7 @@ def test_export_gltf(tmpdir, sphere, airplane):
 
 @skip_not_vtk9
 @skip_windows
-@pytest.mark.skipif(AZURE_CI_WINDOWS, reason="Windows CI testing segfaults on pbr")
+@pytest.mark.skipif(CI_WINDOWS, reason="Windows CI testing segfaults on pbr")
 def test_pbr(sphere):
     """Test PBR rendering"""
     texture = examples.load_globe_texture()
