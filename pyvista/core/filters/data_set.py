cbf1e34d8e94b5c6ea7e3a994b346bc762b7ad0c
Tetsuo Koyama <tkoyama010@gmail.com>
2022-02-10 23:48:10 +0900

    Update pyvista/core/filters/data_set.py
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -4368,9 +4368,21 @@ class DataSetFilters:
         First, plot the original cube.
 
         >>> import pyvista
-        >>> mesh = pyvista.Cube()
-        >>> mesh.plot(show_edges=True, line_width=5)
-
+        >>> import numpy as np
+        >>> points = np.array(
+        >>>     [
+        >>>         [0.0, 0.0, 0.0],
+        >>>         [2.0, 0.0, 0.0],
+        >>>         [1.0, 2.0, 0.0],
+        >>>         [1.0, 0.5, 0.0],
+        >>>         [1.5, 1.5, 0.0],
+        >>>         [0.5, 1.5, 0.0],
+        >>>     ]
+       >>> )
+       >>> cells = np.array([6, 0, 1, 2, 3, 4, 5])
+       >>> cell_types = np.array([69])
+       >>> mesh = pyvista.UnstructuredGrid(cells, cell_types, points)
+       >>> mesh.plot(show_edges=True, line_width=5)
         Now, plot the tessellated mesh.
 
         >>> tessellated = mesh.tessellate()
