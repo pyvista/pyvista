b4caf99c1115f68eea04135329dcec3e9954a473
dcbr <15089458+dcbr@users.noreply.github.com>
2022-02-25 22:29:17 +0100

    Add some docs to make_tables

--- doc/make_tables.py
+++ doc/make_tables.py
@@ -1,42 +1,66 @@
+"""This is a helper module to generate tables that can be included in the documentation."""
 import os
 import textwrap
 
 import pyvista as pv
 
+# Paths to directories in which resulting rst files and images are stored.
 CHARTS_TABLE_DIR = "api/plotting/charts"
 CHARTS_IMAGE_DIR = "images/charts"
 COLORS_TABLE_DIR = "api/utilities"
 
 
+def _aligned_dedent(txt):
+    """Custom variant of `textwrap.dedent`.
+
+    Helper method to dedent the provided text up to the special alignment character ``'|'``.
+    """
+    return textwrap.dedent(txt).replace('|', '')
+
+
 class DocTable:
-    """Helper class to create tables for the documentation."""
+    """Helper class to create tables for the documentation.
+
+    The ``generate`` method creates the table rst file (and possibly any other
+    files, such as images, used by the table). This method internally calls
+    the ``fetch_data``, ``get_header`` and ``get_row`` methods, which should be
+    provided by any subclass.
+    Each table is generated from a list of 'row_data' provided by the ``fetch_data``
+    method. The ``get_header`` and ``get_row`` methods generate the required rst
+    for the table's header and table's rows respectively.
+    """
 
     path = None  # Path to the rst file to which the table will be written
 
     @classmethod
     def generate(cls):
+        """Generate this table."""
         assert cls.path is not None, "Subclasses should specify a path."
         if os.path.exists(cls.path):
             os.remove(cls.path)
-        tokens = cls.fetch_tokens()
+        data = cls.fetch_data()
         with open(cls.path, "w", encoding="utf-8") as f:
-            f.write(cls.get_header(tokens))
-            for i, (token, data) in enumerate(tokens.items()):
-                row = cls.get_row(i, token, data)
+            f.write(cls.get_header(data))
+            for i, row_data in enumerate(data):
+                row = cls.get_row(i, row_data)
                 if row is not None:
                     f.write(row)
         pv.close_all()
 
     @classmethod
-    def fetch_tokens(cls):
-        raise NotImplementedError("Subclasses should specify a fetch_tokens method.")
+    def fetch_data(cls):
+        """Get a list of row_data used to generate the table."""
+        raise NotImplementedError("Subclasses should specify a fetch_data method.")
 
     @classmethod
-    def get_header(cls, tokens):
+    def get_header(cls, data):
+        """Get the table's header rst."""
         raise NotImplementedError("Subclasses should specify a table header.")
 
     @classmethod
-    def get_row(cls, i, token, data):
+    def get_row(cls, i, row_data):
+        """Get the rst for the given row. Can return ``None`` if no row should
+        be generated for the provided ``row_data``."""
         raise NotImplementedError("Subclasses should specify a get_row method.")
 
 
@@ -44,40 +68,47 @@ class LineStyleTable(DocTable):
     """Class to generate line style table."""
 
     path = f"{CHARTS_TABLE_DIR}/pen_line_styles.rst"
-    header = """
-.. list-table:: Line styles
-   :widths: 20 40 40
-   :header-rows: 1
-
-   * - Style
-     - Description
-     - Example
-"""
-    row_template = """
-   * - ``"{}"``
-     - {}
-     - .. image:: /{}
-"""
+    header = _aligned_dedent(
+        """
+        |.. list-table:: Line styles
+        |   :widths: 20 40 40
+        |   :header-rows: 1
+        |
+        |   * - Style
+        |     - Description
+        |     - Example
+        """
+    )
+    row_template = _aligned_dedent(
+        """
+        |   * - ``"{}"``
+        |     - {}
+        |     - .. image:: /{}
+        """
+    )
 
     @classmethod
-    def fetch_tokens(cls):
-        return pv.charts.Pen.LINE_STYLES
+    def fetch_data(cls):
+        # Fetch table data from ``LINE_STYLES`` dictionary.
+        return [{"style": ls, **data} for (ls, data) in pv.charts.Pen.LINE_STYLES.items()]
 
     @classmethod
-    def get_header(cls, tokens):
+    def get_header(cls, data):
         return cls.header
 
     @classmethod
-    def get_row(cls, i, token, data):
-        if data["descr"] is None:
-            return None
+    def get_row(cls, i, row_data):
+        if row_data["descr"] is None:
+            return None  # Skip line style if description is set to ``None``.
         else:
+            # Create an image from the given line style and generate the row rst.
             img_path = f"{CHARTS_IMAGE_DIR}/ls_{i}.png"
-            cls.generate_img(token, img_path)
-            return cls.row_template.format(token, data["descr"], img_path)
+            cls.generate_img(row_data["style"], img_path)
+            return cls.row_template.format(row_data["style"], row_data["descr"], img_path)
 
     @staticmethod
     def generate_img(line_style, img_path):
+        """Generate and save an image of the given line_style."""
         p = pv.Plotter(off_screen=True, window_size=[100, 50])
         p.background_color = 'w'
         chart = pv.Chart2D()
@@ -93,40 +124,47 @@ class MarkerStyleTable(DocTable):
     """Class to generate marker style table."""
 
     path = f"{CHARTS_TABLE_DIR}/scatter_marker_styles.rst"
-    header = """
-.. list-table:: Marker styles
-   :widths: 20 40 40
-   :header-rows: 1
-
-   * - Style
-     - Description
-     - Example
-"""
-    row_template = """
-   * - ``"{}"``
-     - {}
-     - .. image:: /{}
-"""
+    header = _aligned_dedent(
+        """
+        |.. list-table:: Marker styles
+        |   :widths: 20 40 40
+        |   :header-rows: 1
+        |
+        |   * - Style
+        |     - Description
+        |     - Example
+        """
+    )
+    row_template = _aligned_dedent(
+        """
+        |   * - ``"{}"``
+        |     - {}
+        |     - .. image:: /{}
+        """
+    )
 
     @classmethod
-    def fetch_tokens(cls):
-        return pv.charts.ScatterPlot2D.MARKER_STYLES
+    def fetch_data(cls):
+        # Fetch table data from ``MARKER_STYLES`` dictionary.
+        return [{"style": ms, **data} for (ms, data) in pv.charts.ScatterPlot2D.MARKER_STYLES.items()]
 
     @classmethod
-    def get_header(cls, tokens):
+    def get_header(cls, data):
         return cls.header
 
     @classmethod
-    def get_row(cls, i, token, data):
-        if data["descr"] is None:
-            return None
+    def get_row(cls, i, row_data):
+        if row_data["descr"] is None:
+            return None  # Skip marker style if description is set to ``None``.
         else:
+            # Create an image from the given marker style and generate the row rst.
             img_path = f"{CHARTS_IMAGE_DIR}/ms_{i}.png"
-            cls.generate_img(token, img_path)
-            return cls.row_template.format(token, data["descr"], img_path)
+            cls.generate_img(row_data["style"], img_path)
+            return cls.row_template.format(row_data["style"], row_data["descr"], img_path)
 
     @staticmethod
     def generate_img(marker_style, img_path):
+        """Generate and save an image of the given marker_style."""
         p = pv.Plotter(off_screen=True, window_size=[100, 100])
         p.background_color = 'w'
         chart = pv.Chart2D()
@@ -143,42 +181,49 @@ class ColorSchemeTable(DocTable):
     """Class to generate color scheme table."""
 
     path = f"{CHARTS_TABLE_DIR}/plot_color_schemes.rst"
-    header = """
-.. list-table:: Color schemes
-   :widths: 15 50 5 30
-   :header-rows: 1
-
-   * - Color scheme
-     - Description
-     - # colors
-     - Example
-"""
-    row_template = """
-   * - ``"{}"``
-     - {}
-     - {}
-     - .. image:: /{}
-"""
+    header = _aligned_dedent(
+        """
+        |.. list-table:: Color schemes
+        |   :widths: 15 50 5 30
+        |   :header-rows: 1
+        |
+        |   * - Color scheme
+        |     - Description
+        |     - # colors
+        |     - Example
+        """
+    )
+    row_template = _aligned_dedent(
+        """
+        |   * - ``"{}"``
+        |     - {}
+        |     - {}
+        |     - .. image:: /{}
+        """
+    )
 
     @classmethod
-    def fetch_tokens(cls):
-        return pv.charts._MultiCompPlot.COLOR_SCHEMES
+    def fetch_data(cls):
+        # Fetch table data from ``COLOR_SCHEMES`` dictionary.
+        return [{"scheme": cs, **data} for (cs, data) in pv.charts._MultiCompPlot.COLOR_SCHEMES.items()]
 
     @classmethod
-    def get_header(cls, tokens):
+    def get_header(cls, data):
         return cls.header
 
     @classmethod
-    def get_row(cls, i, token, data):
-        if data["descr"] is None:
-            return None
+    def get_row(cls, i, row_data):
+        if row_data["descr"] is None:
+            return None  # Skip color scheme if description is set to ``None``.
         else:
+            # Create an image from the given color scheme and generate the row rst.
             img_path = f"{CHARTS_IMAGE_DIR}/cs_{i}.png"
-            n_colors = cls.generate_img(token, img_path)
-            return cls.row_template.format(token, data["descr"], n_colors, img_path)
+            n_colors = cls.generate_img(row_data["scheme"], img_path)
+            return cls.row_template.format(row_data["scheme"], row_data["descr"], n_colors, img_path)
 
     @staticmethod
     def generate_img(color_scheme, img_path):
+        """Generate and save an image of the given color_scheme."""
         p = pv.Plotter(off_screen=True, window_size=[240, 120])
         p.background_color = 'w'
         chart = pv.Chart2D()
@@ -201,49 +246,48 @@ class ColorTable(DocTable):
     """Class to generate colors table."""
 
     path = f"{COLORS_TABLE_DIR}/colors.rst"
-    header = textwrap.dedent(
+    header = _aligned_dedent(
         """
-        .. list-table::
-           :widths: 50 20 30
-           :header-rows: 1
-
-           * - Name
-             - Hex value
-             - Example
+        |.. list-table::
+        |   :widths: 50 20 30
+        |   :header-rows: 1
+        |
+        |   * - Name
+        |     - Hex value
+        |     - Example
         """
     )
-    row_template = textwrap.indent(
-        textwrap.dedent(
-            """
-            * - {}
-              - ``{}``
-              - .. raw:: html
-
-                   <span style='width:100%; height:100%; display:block; background-color: {};'>&nbsp;</span>
-            """
-        ),
-        "   ",  # Extra indent needed to make it part of the table
+    row_template = _aligned_dedent(
+        """
+        |   * - {}
+        |     - ``{}``
+        |     - .. raw:: html
+        |
+        |          <span style='width:100%; height:100%; display:block; background-color: {};'>&nbsp;</span>
+        """
     )
 
     @classmethod
-    def fetch_tokens(cls):
-        tokens = {token: {"hex": hex} for token, hex in pv.hexcolors.items()}
-        for s, token in pv.colors.color_synonyms.items():
-            if "synonyms" not in tokens[token]:
-                tokens[token]["synonyms"] = []
-            tokens[token]["synonyms"].append(s)
-        return tokens
+    def fetch_data(cls):
+        # Fetch table data from ``hexcolors`` dictionary.
+        colors = {name: {"name": name, "hex": hex} for name, hex in pv.hexcolors.items()}
+        # Add synonyms defined in ``color_synonyms`` dictionary.
+        for s, name in pv.colors.color_synonyms.items():
+            if "synonyms" not in colors[name]:
+                colors[name]["synonyms"] = []
+            colors[name]["synonyms"].append(s)
+        return colors.values()
 
     @classmethod
-    def get_header(cls, tokens):
+    def get_header(cls, data):
         return cls.header
 
     @classmethod
-    def get_row(cls, i, token, data):
-        token_template = '``"{}"``'
-        names = [token] + data.get("synonyms", [])
-        name = " or ".join(token_template.format(n) for n in names)
-        return cls.row_template.format(name, data["hex"], data["hex"])
+    def get_row(cls, i, row_data):
+        name_template = '``"{}"``'
+        names = [row_data["name"]] + row_data.get("synonyms", [])
+        name = " or ".join(name_template.format(n) for n in names)
+        return cls.row_template.format(name, row_data["hex"], row_data["hex"])
 
 
 def make_all():
