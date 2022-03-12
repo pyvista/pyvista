521c94603af06540f1e14c36609214f9aca5d182
Tetsuo Koyama <tkoyama010@gmail.com>
2022-03-12 20:56:36 +0900

    Fix example

--- pyvista/utilities/geometric_objects.py
+++ pyvista/utilities/geometric_objects.py
@@ -483,7 +483,7 @@ def MultipleLines(points=[[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]]):
     Create a multiple lines between ``(0, 0, 0)``, ``(1, 1, 1)`` and ``(0, 0, 1)``.
 
     >>> import pyvista
-    >>> mesh = pyvista.Line(points=[[0, 0, 0], [1, 1, 1], [0, 0, 1]])
+    >>> mesh = pyvista.MultipleLines(points=[[0, 0, 0], [1, 1, 1], [0, 0, 1]])
     >>> mesh.plot(color='k', line_width=10)
     """
     points = _coerce_pointslike_arg(points)
