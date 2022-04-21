c94b53e21895d2b9435c8c4376566f4cf519f24e
Matthew Flamm <matthewhflamm@gmail.com>
2022-04-21 09:46:06 -0400

    fix singleSphereAnimation; add example

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -3442,9 +3442,27 @@ def download_single_sphere_animation(load=True):  # pragma: no cover
     pyvista.MultiBlock or str
         DataSet or filename depending on ``load``.
 
+    Examples
+    --------
+    >>> import pyvista
+    >>> from pyvista import examples
+    >>> filename = examples.download_single_sphere_animation(load=False)
+    >>> reader = pyvista.PVDReader(filename)
+    >>> plotter = pyvista.Plotter()
+    >>> plotter.open_gif("single_sphere_pvd.gif")
+    >>> for time_value in reader.time_values:
+    ...     reader.set_active_time_value(time_value)
+    ...     mesh = reader.read()
+    ...     _ = plotter.add_mesh(mesh, smooth_shading=True)
+    ...     _ = plotter.add_text(f"Time: {time_value:.0f}", color="black")
+    ...     plotter.write_frame()
+    ...     plotter.clear()
+    ...     plotter.enable_lightkit()
+    >>> plotter.close()
+
     """
+    _download_file('PVD/paraview/singleSphereAnimation.zip')
     filename, _ = _download_file('PVD/paraview/singleSphereAnimation.pvd')
-    folder, _ = _download_file('PVD/paraview/singleSphereAnimation')
     if not load:
         return filename
     return pyvista.PVDReader(filename).read()
