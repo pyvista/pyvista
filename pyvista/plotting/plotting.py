403f66321bcf8667e5d8df74a28099c3133a06c9
Tetsuo Koyama <tkoyama010@gmail.com>
2022-04-24 15:54:52 +0900

    Add doctest:+SKIP at download_sextant

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -373,7 +373,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         --------
         >>> import pyvista
         >>> from pyvista import examples
-        >>> sextant_file = examples.vrml.download_sextant()
+        >>> sextant_file = examples.vrml.download_sextant()  # doctest:+SKIP
         >>> pl = pyvista.Plotter()  # doctest:+SKIP
         >>> pl.import_vrml(sextant_file)  # doctest:+SKIP
         >>> pl.show()  # doctest:+SKIP
