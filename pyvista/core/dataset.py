58a458349d374ebaa1fb83d0c35f686dd368be97
Phil Chiu <phil.chiu@archer.com>
2022-04-24 09:23:12 -0600

    Fix field -> name in similar methods

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -652,10 +652,10 @@ class DataSet(DataSetFilters, DataObject):
         elif field == FieldAssociation.CELL:
             ret = self.GetCellData().SetActiveScalars(name)
         else:
-            raise ValueError(f'Data field ({field}) not usable')
+            raise ValueError(f'Data field ({name}) not usable')
 
         if ret < 0:
-            raise ValueError(f'Data field ({field}) could not be set as the active scalars')
+            raise ValueError(f'Data field ({name}) could not be set as the active scalars')
 
         self._active_scalars_info = ActiveArrayInfo(field, name)
 
@@ -722,10 +722,10 @@ class DataSet(DataSetFilters, DataObject):
             elif field == FieldAssociation.CELL:
                 ret = self.GetCellData().SetActiveTensors(name)
             else:
-                raise ValueError(f'Data field ({field}) not usable')
+                raise ValueError(f'Data field ({name}) not usable')
 
             if ret < 0:
-                raise ValueError(f'Data field ({field}) could not be set as the active tensors')
+                raise ValueError(f'Data field ({name}) could not be set as the active tensors')
 
         self._active_tensors_info = ActiveArrayInfo(field, name)
 
