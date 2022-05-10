d9518cafcc5a54d3bec3ef314f7a3ac87abaf3ad
Alex Kaszynski <akascap@gmail.com>
2022-05-10 10:03:19 -0600

    add fixture to reset global theme

--- pyvista/conftest.py
+++ pyvista/conftest.py
@@ -10,3 +10,11 @@ def autoclose_plotters():
     """Close all plotters."""
     yield
     pyvista.close_all()
+
+
+@pytest.fixture(autouse=True)
+def reset_gloal_theme():
+    """Reset global_theme."""
+    # this stops any doctest-module tests from overriding the global theme and
+    # creating test side effects
+    pyvista.global_theme.restore_defaults()
