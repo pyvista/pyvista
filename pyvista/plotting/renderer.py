a5192f0fa08d28727e7c3c4f98107ffd7f631c96
Andras Deak <adeak@users.noreply.github.com>
2022-06-01 12:01:45 +0200

    Update pyvista/plotting/renderer.py

--- pyvista/plotting/renderer.py
+++ pyvista/plotting/renderer.py
@@ -2637,7 +2637,7 @@ class Renderer(_vtk.vtkOpenGLRenderer):
         >>> import pyvista as pv
         >>> pl = pv.Plotter(lighting=None)
         >>> cubemap = examples.download_sky_box_cube_map()
-        >>> pl.add_mesh(pv.Sphere(), pbr=True, metallic=0.9, roughness=0.4)
+        >>> _ = pl.add_mesh(pv.Sphere(), pbr=True, metallic=0.9, roughness=0.4)
         >>> pl.set_environment_texture(cubemap)
         >>> pl.camera_position = 'xy'
         >>> pl.show()
