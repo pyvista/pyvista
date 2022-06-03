fe58d39ef17caccc8620047cd9927d3e58e939fc
Félix <felix.olart56@gmail.com>
2022-06-03 08:19:16 +0200

    Update POpenFOAMReader docstring
    Co-authored-by: MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>

--- pyvista/utilities/reader.py
+++ pyvista/utilities/reader.py
@@ -1008,7 +1008,8 @@ class POpenFOAMReader(OpenFOAMReader):
     """Parallel OpenFOAM Reader for .foam files.
 
     Can read parallel-decomposed mesh information and time dependent data.
-    This reader can be used both for parallel reconstructed data and decomposed data.
+    This reader can be used for serial generated data,
+    parallel reconstructed data, and decomposed data.
     """
 
     _class_reader = staticmethod(_vtk.lazy_vtkPOpenFOAMReader)
