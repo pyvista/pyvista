fd54e5fdbb5b07b7758186572d6c23739b480a84
Matthew Flamm <matthewhflamm@gmail.com>
2022-06-12 12:07:20 -0400

    optionally fail when image is missing

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -104,9 +104,10 @@ WINDOWS_SKIP_IMAGE_CACHE = {
 # this must be a session fixture to ensure this runs before any other test
 @pytest.fixture(scope="session", autouse=True)
 def get_cmd_opt(pytestconfig):
-    global glb_reset_image_cache, glb_ignore_image_cache
+    global glb_reset_image_cache, glb_ignore_image_cache, glb_fail_extra_image_cache
     glb_reset_image_cache = pytestconfig.getoption('reset_image_cache')
     glb_ignore_image_cache = pytestconfig.getoption('ignore_image_cache')
+    glb_fail_extra_image_cache = pytestconfig.getoption('fail_extra_image_cache')
 
 
 def verify_cache_image(plotter):
@@ -126,7 +127,7 @@ def verify_cache_image(plotter):
     plotter.show(before_close_callback=verify_cache_image)
 
     """
-    global glb_reset_image_cache, glb_ignore_image_cache
+    global glb_reset_image_cache, glb_ignore_image_cache, glb_fail_extra_image_cache
 
     # Image cache is only valid for VTK9+
     if not VTK9:
@@ -161,13 +162,15 @@ def verify_cache_image(plotter):
     # cached image name
     image_filename = os.path.join(IMAGE_CACHE_DIR, test_name[5:] + '.png')
 
+    if glb_ignore_image_cache:
+        return
+
+    if not os.path.isfile(image_filename) and glb_fail_extra_image_cache:
+        raise RuntimeError(f"{image_filename} does not exist in image cache")
     # simply save the last screenshot if it doesn't exist or the cache
     # is being reset.
     if glb_reset_image_cache or not os.path.isfile(image_filename):
         return plotter.screenshot(image_filename)
-
-    if glb_ignore_image_cache:
-        return
 
     # otherwise, compare with the existing cached image
     error = pyvista.compare_images(image_filename, plotter)
@@ -2328,6 +2331,15 @@ def test_write_gif(sphere, tmpdir):
 
 
 def test_ruler(sphere):
+    sphere = pyvista.Sphere()
+    plotter = pyvista.Plotter()
+    plotter.add_mesh(sphere)
+    plotter.add_ruler([-0.6, -0.6, 0], [0.6, -0.6, 0], font_size_factor=1.2)
+    plotter.view_xy()
+    plotter.show(before_close_callback=verify_cache_image)
+
+
+def test_no_image(sphere):
     sphere = pyvista.Sphere()
     plotter = pyvista.Plotter()
     plotter.add_mesh(sphere)
