2c33c64a6ee0e0bc098b491223666941568609b5
Alex Kaszynski <akascap@gmail.com>
2022-06-24 15:33:34 -0600

    fix windows skip

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -93,17 +93,18 @@ VER_IMAGE_REGRESSION_WARNING = 1000
 # these images vary between Windows when using OSMesa and Linux/MacOS
 # and will not be verified
 WINDOWS_SKIP_IMAGE_CACHE = {
-    'test_user_annotations_scalar_bar_volume',  # occurs even without Windows OSMesa
-    'test_enable_stereo_render',  # occurs even without Windows OSMesa
-    'test_plot_add_scalar_bar',
-    'test_plot_cell_data',
-    'test_scalars_by_name',
-    'test_user_annotations_scalar_bar_volume',
-    'test_plot_string_array',
     'test_cmap_list',
     'test_collision_plot',
     'test_enable_stereo_render',
+    'test_enable_stereo_render',  # occurs even without Windows OSMesa
+    'test_plot_add_scalar_bar',
+    'test_plot_cell_data',
     'test_plot_complex_value',
+    'test_plot_string_array',
+    'test_plot_uniform',
+    'test_scalars_by_name',
+    'test_user_annotations_scalar_bar_volume',
+    'test_user_annotations_scalar_bar_volume',  # occurs even without Windows OSMesa
 }
 
 # these images vary between Linux/Windows and MacOS
