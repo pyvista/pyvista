1b8dcbe46fa7845701b1e5a319cb463df6c7ca07
Andras Deak <deak.andris@gmail.com>
2022-06-27 23:26:16 +0200

    Rotate teapot for flip_z example

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -1340,13 +1340,13 @@ class DataSet(DataSetFilters, DataObject):
         >>> pl = pyvista.Plotter(shape=(1, 2))
         >>> pl.subplot(0, 0)
         >>> pl.show_axes()
-        >>> mesh1 = examples.download_teapot()
+        >>> mesh1 = examples.download_teapot().rotate_x(90, inplace=False)
         >>> _ = pl.add_mesh(mesh1)
         >>> pl.subplot(0, 1)
         >>> pl.show_axes()
         >>> mesh2 = mesh1.flip_z(inplace=False)
         >>> _ = pl.add_mesh(mesh2)
-        >>> pl.show(cpos="xy")
+        >>> pl.show(cpos="xz")
         """
         if point is None:
             point = self.center
