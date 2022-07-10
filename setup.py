88a894084bfb93ddad4a8e38f9856f1b182a3c04
Alex Kaszynski <akascap@gmail.com>
2022-07-10 14:08:41 +0000

    permit rc for python 3.10

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ install_requires = [
     'pillow',
     'appdirs',
     'scooby>=0.5.1',
-    'vtk>=8.1.2,<9.3',
+    'vtk>=8.1.2, <=9.2.0; python_version < "3.10"',
+    'vtk>=8.1.2, <=9.2.0, ==9.2.0rc1; python_version == "3.10"',
 ]
 
 readme_file = os.path.join(filepath, 'README.rst')
