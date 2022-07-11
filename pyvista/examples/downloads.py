dc5bb0f55d45a58135a7e081a57661753829b86b
Alex Kaszynski <akascap@gmail.com>
2022-07-10 23:46:50 -0500

    Update pyvista/examples/downloads.py

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -4250,7 +4250,7 @@ def download_mount_damavand(load=True):  # pragma: no cover
     Download the Damavand dataset and plot it after warping it by its altitude.
 
     >>> from pyvista import examples
-    >>> dataset = download_mount_damavand()
+    >>> dataset = examples.download_mount_damavand()
     >>> dataset = dataset.cell_data_to_point_data()
     >>> dataset = dataset.warp_by_scalar('z', factor=2)
     >>> dataset.plot(cmap='gist_earth', show_scalar_bar=False)
