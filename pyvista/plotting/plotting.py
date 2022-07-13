e7a23cee9e2f8809ddb4afcc4b4c453c51b9d8ce
Alex Rockhill <aprockhill@mailbox.org>
2022-07-13 10:15:25 -0700

    [BUG] Fix clearing point and cell data (#2976)
    * [BUG] Fix clearning point and cell data
    
    * remove mesh data clear
    
    * Add regression test for mesh fiddling in deep_clean()
    
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>
    Co-authored-by: Andras Deak <deak.andris@gmail.com>

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -3369,9 +3369,6 @@ class BasePlotter(PickingHelper, WidgetHelper):
         self.disable_picking()
         if hasattr(self, 'renderers'):
             self.renderers.deep_clean()
-        if getattr(self, 'mesh', None) is not None:
-            self.mesh.point_data = None
-            self.mesh.cell_data = None
         self.mesh = None
         if getattr(self, 'mapper', None) is not None:
             self.mapper.lookup_table = None
