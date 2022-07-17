7ffdca569f82f112a0896ca87a80529dcc35222a
ENIAC-6 <kellysmeehan@gmail.com>
2022-07-17 15:29:16 -0600

    Add background_color example (#3013)
    * Add background_color example
    
    Add example to background_color property.
    
    * clean up example a bit
    
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -1361,7 +1361,21 @@ class BasePlotter(PickingHelper, WidgetHelper):
 
     @property
     def background_color(self):
-        """Return the background color of the active render window."""
+        """Return the background color of the active render window.
+
+        Examples
+        --------
+        Set the background color to ``"pink"`` and plot it.
+
+        >>> import pyvista as pv
+        >>> pl = pv.Plotter()
+        >>> _ = pl.add_mesh(pv.Cube(), show_edges=True)
+        >>> pl.background_color = "pink"
+        >>> pl.background_color
+        Color(name='pink', hex='#ffc0cbff')
+        >>> pl.show()
+
+        """
         return self.renderers.active_renderer.background_color
 
     @background_color.setter
