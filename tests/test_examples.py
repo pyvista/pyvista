20f33126fcc91b679b25e77395cc1b87f83fbd34
Tetsuo Koyama <tkoyama010@gmail.com>
2022-07-18 13:51:59 -0500

    Apply suggestions from code review

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -67,62 +67,62 @@ def test_load_random_hills():
 
 
 def test_load_sun():
-    mesh = examples.load_sun()
+    mesh = examples.planets.load_sun()
     assert mesh.n_cells
     assert mesh.textures["atmosphere"]
 
 
 def test_load_moon():
-    mesh = examples.load_moon()
+    mesh = examples.planets.load_moon()
     assert mesh.n_cells
     assert mesh.textures["surface"]
 
 
 def test_load_mercury():
-    mesh = examples.load_mercury()
+    mesh = examples.planets.load_mercury()
     assert mesh.n_cells
     assert mesh.textures["surface"]
 
 
 def test_load_venus():
-    mesh = examples.load_venus()
+    mesh = examples.planets.load_venus()
     assert mesh.n_cells
     assert mesh.textures["surface"]
     assert mesh.textures["atmosphere"]
 
 
 def test_load_mars():
-    mesh = examples.load_mars()
+    mesh = examples.planets.load_mars()
     assert mesh.n_cells
     assert mesh.textures["surface"]
 
 
 def test_load_jupiter():
-    mesh = examples.load_jupiter()
+    mesh = examples.planets.load_jupiter()
     assert mesh.n_cells
     assert mesh.textures["atmosphere"]
 
 
 def test_load_saturn():
-    mesh = examples.load_saturn()
+    mesh = examples.planets.load_saturn()
     assert mesh.n_cells
     assert mesh.textures["atmosphere"]
 
 
 def test_load_saturn_ring_alpha():
-    mesh = examples.load_saturn_ring_alpha()
+    mesh = examples.planets.load_saturn_ring_alpha()
     assert mesh.n_cells
     assert mesh.textures["atmosphere"]
 
 
 def test_load_uranus():
-    mesh = examples.load_uranus()
+    mesh = examples.planets.load_uranus()
     assert mesh.n_cells
     assert mesh.textures["atmosphere"]
 
 
 def test_load_neptune():
-    mesh = examples.load_neptune()
+    mesh = examples.planets.load_neptune()
     assert mesh.n_cells
     assert mesh.textures["atmosphere"]
 
