77e865f317005624471bc41b48d2103065976ff1
Alex Kaszynski <akascap@gmail.com>
2022-07-26 05:00:29 +0000

    Update pyvista/examples/downloads.py

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -3087,7 +3087,7 @@ def download_cubemap_park():  # pragma: no cover
     >>> pl = pv.Plotter(lighting=None)
     >>> dataset = examples.download_cubemap_park()
     >>> _ = pl.add_actor(dataset.to_skybox())
-    >>> pl.set_environment_texture(dataset)
+    >>> pl.set_environment_texture(dataset, True)
     >>> pl.camera_position = 'xy'
     >>> pl.camera.zoom(0.4)
     >>> _ = pl.add_mesh(pv.Sphere(), pbr=True, roughness=0.1, metallic=0.5)
