e27b2ec66e803465636091e959ffa99ea010575a
Alex Kaszynski <akascap@gmail.com>
2022-07-31 03:18:30 +0000

    bump to 0.36.0

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -15,7 +15,7 @@ Denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 37, 'dev0'
+version_info = 0, 36, 0
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
