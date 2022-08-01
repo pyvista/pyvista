a7a8acb25bb8a060c962ed0148f322280c78049e
Alex Kaszynski <akascap@gmail.com>
2022-08-01 13:46:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,7 @@ install_requires = [
     'pillow',
     'appdirs',
     'scooby>=0.5.1',
-    'vtk>=8.1.2, <=9.2.0; python_version < "3.10"',
-    'vtk~=9.2.0rc1; python_version == "3.10"',
+    'vtk>=8.1.2',
 ]
 
 readme_file = os.path.join(filepath, 'README.rst')
