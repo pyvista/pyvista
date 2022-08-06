0fa806aaa08e8afb9793dae91508acdcba796539
Tetsuo Koyama <tkoyama010@gmail.com>
2022-08-07 07:48:33 +0900

    Apply suggestions from code review
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- pyvista/core/cell.py
+++ pyvista/core/cell.py
@@ -9,7 +9,7 @@ class CellType(IntEnum):
 
     Warnings
     --------
-    The following type needs vtk >=v9.0.0 (See `Implementation of vtkTriQuadraticPyramid cell
+    The following types need ``vtk >=v9.0.0`` (See `Implementation of vtkTriQuadraticPyramid cell
     <https://gitlab.kitware.com/vtk/vtk/-/merge_requests/8295>`_ and `Add Bezier cell types <https://gitlab.kitware.com/vtk/vtk/-/merge_requests/6055>`_ ).
     * TRIQUADRATIC_PYRAMID
     * BEZIER_TRIANGLE
