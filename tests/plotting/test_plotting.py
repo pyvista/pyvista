49326b457784087ffd3a32ba1d90a227a2ffc4d6
Alex Kaszynski <akascap@gmail.com>
2022-08-23 11:34:31 -0600

    fix tests

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -2526,7 +2526,7 @@ def test_ssao_pass():
     if pyvista.vtk_version_info < (9,):
         with pytest.raises(pyvista.core.errors.VTKVersionError):
             pl.enable_ssao()
-            return
+        return
 
     pl.enable_ssao()
     pl.show(before_close_callback=verify_cache_image, auto_close=False)
@@ -2544,7 +2544,7 @@ def test_ssao_pass_from_helper():
     if pyvista.vtk_version_info < (9,):
         with pytest.raises(pyvista.core.errors.VTKVersionError):
             ugrid.plot(ssao=True)
-            return
+        return
 
     ugrid.plot(ssao=True, before_close_callback=verify_cache_image)
 
