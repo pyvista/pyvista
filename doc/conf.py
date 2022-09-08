e3bc965a213593441d04ec7b3e0c611fc1c261a2
MatthewFlamm <matthewhflamm@gmail.com>
2022-09-08 13:14:13 +0000

    use stable version python for links

--- doc/conf.py
+++ doc/conf.py
@@ -222,7 +222,7 @@ add_module_names = False
 # NOTE: if these are changed, then doc/intersphinx/update.sh
 # must be changed accordingly to keep auto-updated mappings working
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/dev', (None, 'intersphinx/python-objects.inv')),
+    'python': ('https://docs.python.org/3', (None, 'intersphinx/python-objects.inv')),
     'scipy': (
         'https://docs.scipy.org/doc/scipy/',
         (None, 'intersphinx/scipy-objects.inv'),
