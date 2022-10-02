d51cb5a850f8c17d89ede18bf932adcc24d67edb
Tetsuo Koyama <tkoyama010@gmail.com>
2022-10-03 08:01:48 +0900

    Apply suggestions from code review
    Co-authored-by: MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -4728,7 +4728,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         if fmt is None:
             fmt = '%.6e'
         scalars = points.point_data[labels]
-        phrase = f'{preamble} ' + fmt
+        phrase = f'{preamble} {fmt}'
         labels = [phrase % val for val in scalars]
         return self.add_point_labels(points, labels, **kwargs)
 
