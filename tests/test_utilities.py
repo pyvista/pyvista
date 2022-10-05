54e1e8d7dfebda3b1696347af133daedcab51d1d
Andras Deak <deak.andris@gmail.com>
2022-10-05 23:06:37 +0200

    Update tests, avoid point cloud inputs

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -308,23 +308,25 @@ def test_get_sg_image_scraper():
     assert callable(scraper)
 
 
-def test_voxelize():
-    mesh = pyvista.PolyData(ex.load_uniform().points)
-    vox = pyvista.voxelize(mesh, 0.5)
+def test_voxelize(uniform):
+    vox = pyvista.voxelize(uniform, 0.5)
     assert vox.n_cells
 
 
-def test_voxelize_non_uniform_density():
-    mesh = pyvista.PolyData(ex.load_uniform().points)
-    vox = pyvista.voxelize(mesh, [0.5, 0.3, 0.2])
+def test_voxelize_non_uniform_density(uniform):
+    vox = pyvista.voxelize(uniform, [0.5, 0.3, 0.2])
     assert vox.n_cells
 
 
-def test_voxelize_throws_when_density_is_not_length_3():
-    with pytest.raises(ValueError) as e:
-        mesh = pyvista.PolyData(ex.load_uniform().points)
-        _ = pyvista.voxelize(mesh, [0.5, 0.3])
-    assert "not enough values to unpack" in str(e.value)
+def test_voxelize_throws_when_density_is_not_length_3(uniform):
+    with pytest.raises(ValueError, match='not enough values to unpack'):
+        pyvista.voxelize(uniform, [0.5, 0.3])
+
+
+def test_voxelize_throws_point_cloud(uniform):
+    with pytest.raises(ValueError, match='must have faces'):
+        mesh = pyvista.PolyData(uniform.points)
+        pyvista.voxelize(mesh)
 
 
 def test_report():
