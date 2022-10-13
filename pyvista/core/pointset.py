d8138aff74a4773597703a8c8de1ee770a33cc43
Alex Kaszynski <akascap@gmail.com>
2022-10-13 07:39:01 -0600

    make pre-commit happy

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -1708,7 +1708,7 @@ class UnstructuredGrid(_vtk.vtkUnstructuredGrid, PointGrid, UnstructuredGridFilt
         Parameters
         ----------
         deep : bool, default=False
-            When ``True``, makes a copy of the points array. 
+            When ``True``, makes a copy of the points array.
             Cells and cell types are always copied.
 
         Returns
