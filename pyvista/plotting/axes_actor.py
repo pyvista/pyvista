e8d99c735fcb2eea58ad8c0d746289b3390db0de
afernand <alex.fernandezluces@ansys.com>
2022-12-13 13:53:08 +0100

    Removes unused functions

--- pyvista/plotting/axes_actor.py
+++ pyvista/plotting/axes_actor.py
@@ -1,9 +1,6 @@
 """Axes actor module."""
 import pyvista as pv
 
-from ._property import Property
-from .mapper import _BaseMapper
-
 
 class AxesActor(pv._vtk.vtkAxesActor):
     """Axes actor wrapper for vtkAxesActor.
@@ -33,68 +30,19 @@ class AxesActor(pv._vtk.vtkAxesActor):
         """Initialize actor."""
         super().__init__()
 
-        def __init__(self, mapper=None, prop=None):
-            """Initialize actor."""
-            super().__init__()
-            if mapper is not None:
-                self.mapper = mapper
-            if prop is None:
-                self.prop = Property()
-
-    @property
-    def mapper(self) -> _BaseMapper:
-        """Return or set actor set of properties.
-
-        Examples
-        --------
-        Create an actor using the :class:`pyvista.Plotter` and then change the
-        visibility of the actor.
-
-        >>> import pyvista as pv
-        >>> pl = pv.Plotter()
-
-        """
-        return self.GetMapper()
-
-    @mapper.setter
-    def mapper(self, obj):
-        return self.SetMapper(obj)
-
-    @property
-    def prop(self):
-        """Return or set AxesActor set of properties.
-
-        Examples
-        --------
-        Create an actor using the :class:`pyvista.Plotter` and then change the
-        visibility of the actor.
-
-        >>> import pyvista as pv
-        >>> pl = pv.Plotter()
-        >>> actor = pl.add_mesh(pv.Sphere())
-
-        """
-        return self.GetProperty()
-
-    @prop.setter
-    def prop(self, obj: Property):
-        self.SetProperty(obj)
-
     @property
     def visibility(self) -> bool:
-        """Return or set actor visibility.
+        """Return or set AxesActor visibility.
 
         Examples
         --------
-        Create an actor using the :class:`pyvista.Plotter` and then change the
-        visibility of the actor.
+        Create an Axes object and then change the
+        visibility of its AxesActor.
 
         >>> import pyvista as pv
-        >>> pl = pv.Plotter()
-        >>> actor = pl.add_mesh(pv.Sphere())
-        >>> actor.visibility = False
-        >>> actor.visibility
-        False
+        >>> axes = pv.Axes()
+        >>> axes.axes_actor.visibility
+        True
 
         """
         return bool(self.GetVisibility())
