f9df2b9157da6ee2968730daa325ab24e88afbfc
beroda <117822458+beroda@users.noreply.github.com>
2023-01-07 23:51:11 +0100

    Convert PyVistaDeprecationWarning to error for tests

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -2753,7 +2753,7 @@ class DataSet(DataSetFilters, DataObject):
         --------
         >>> from pyvista import examples
         >>> mesh = examples.load_hexbeam()
-        >>> mesh.cell_bounds(0)
+        >>> mesh.cell[0].bounds
         (0.0, 0.5, 0.0, 0.5, 0.0, 0.5)
         >>> mesh.point_is_inside_cell(0, [0.2, 0.2, 0.2])
         True
