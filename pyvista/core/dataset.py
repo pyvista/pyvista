d21065de2357c0a4022cce7c2cc982a11e676add
beroda <117822458+beroda@users.noreply.github.com>
2023-01-08 23:37:39 +0100

    Add visual examples

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -2813,6 +2813,40 @@ class DataSet(DataSetFilters, DataObject):
         >>> mesh = examples.download_tetrahedron()
         >>> mesh.cell_neighbors(0,"faces")
         [1, 5, 7]
+
+        Show a visual example
+
+        >>> from functools import partial
+        >>> import pyvista
+        >>> mesh = pyvista.Sphere(theta_resolution=10)
+        >>>
+        >>> pl = pyvista.Plotter(shape=(1,2))
+        >>> pl.link_views()
+        >>> add_point_labels = partial(pl.add_point_labels, text_color="white", font_size=20, shape=None, show_points=False)
+        >>>
+        >>> for i,connection in enumerate(["points","edges"]):
+        ...     pl.subplot(0,i)
+        ...     pl.view_yx()
+        ...     _ = pl.add_title(f"{connection.capitalize()} neighbors",color="red",shadow=True)
+
+        ...     # Add current cell
+        ...     i_cell = 0
+        ...     current_cell = mesh.extract_cells(i_cell)
+        ...     _ = pl.add_mesh(current_cell,show_edges=True, color = "blue" )
+        ...     _ = add_point_labels(current_cell.cell_centers().points, labels=[f"{i_cell}"])
+
+        ...     # Add neighbors
+        ...     ids = mesh.cell_neighbors(i_cell,connection)
+        ...     cells = mesh.extract_cells(ids)
+        ...     _ = pl.add_mesh(cells,color="red", show_edges=True)
+        ...     _ = add_point_labels(cells.cell_centers().points, labels=[f"{i}" for i in ids])
+
+        ...     # Add other cells
+        ...     ids.append(i_cell)
+        ...     others = mesh.extract_cells(ids,invert=True)
+        ...     _ = pl.add_mesh(others,show_edges=True)
+
+        >>> pl.show()
         """
         # Build links as recommended:
         # https://vtk.org/doc/nightly/html/classvtkPolyData.html#adf9caaa01f72972d9a986ba997af0ac7
