09a2156b102abd0023865079e39aa02eaa4297db
Bane Sullivan <bane.sullivan@kitware.com>
2023-01-23 12:21:41 -0700

    Make UI menu re-usable

--- pyvista/trame/ui.py
+++ pyvista/trame/ui.py
@@ -168,7 +168,100 @@ class Viewer:
         buffer.seek(0)
         return memoryview(buffer.read())
 
-    def ui(self, mode=None, default_server_rendering=True, collapse_menu=False, **kwargs):
+    def ui_controls(self, mode=None, default_server_rendering=True, v_show=None):
+        """Create a VRow for the UI controls."""
+        if mode is None:
+            mode = pyvista.global_theme.trame.default_mode
+        if mode not in VALID_UI_MODES:
+            raise ValueError(f'`{mode}` is not a valid mode choice. Use one of: {VALID_UI_MODES}')
+        if mode != 'trame':
+            default_server_rendering = mode == 'server'
+
+        with vuetify.VRow(
+            v_show=v_show,
+            classes='pa-0 ma-0 align-center',
+        ) as row:
+            server = row.server
+            # Listen to state changes
+            server.state.change(self.EDGES)(self.on_edge_visiblity_change)
+            server.state.change(self.GRID)(self.on_grid_visiblity_change)
+            server.state.change(self.OUTLINE)(self.on_outline_visiblity_change)
+            server.state.change(self.AXIS)(self.on_axis_visiblity_change)
+            server.state.change(self.SERVER_RENDERING)(self.on_rendering_mode_change)
+            vuetify.VDivider(vertical=True, classes='mr-1')
+            button(
+                click=self.reset_camera,
+                icon='mdi-arrow-expand-all',
+                tooltip='Reset Camera',
+            )
+            vuetify.VDivider(vertical=True, classes='mx-1')
+            button(
+                click=self.view_isometric,
+                icon='mdi-axis-arrow',
+                tooltip='Perspective view',
+            )
+            button(
+                click=self.view_yz,
+                icon='mdi-axis-x-arrow',
+                tooltip='Reset Camera X',
+            )
+            button(
+                click=self.view_xz,
+                icon='mdi-axis-y-arrow',
+                tooltip='Reset Camera Y',
+            )
+            button(
+                click=self.view_xy,
+                icon='mdi-axis-z-arrow',
+                tooltip='Reset Camera Z',
+            )
+            vuetify.VDivider(vertical=True, classes='mx-1')
+            checkbox(
+                model=(self.EDGES, False),
+                icons=('mdi-grid', 'mdi-grid-off'),
+                tooltip=f"Toggle edge visibility ({{{{ {self.EDGES} ? 'on' : 'off' }}}})",
+            )
+            checkbox(
+                model=(self.OUTLINE, False),
+                icons=('mdi-cube', 'mdi-cube-off'),
+                tooltip=f"Toggle bounding box ({{{{ {self.OUTLINE} ? 'on' : 'off' }}}})",
+            )
+            checkbox(
+                model=(self.GRID, False),
+                icons=('mdi-ruler-square', 'mdi-ruler-square'),
+                tooltip=f"Toggle ruler ({{{{ {self.GRID} ? 'on' : 'off' }}}})",
+            )
+            # Server rendering options
+            if mode == 'trame':
+                vuetify.VDivider(vertical=True, classes='mx-1')
+                checkbox(
+                    model=(self.SERVER_RENDERING, default_server_rendering),
+                    icons=('mdi-dns', 'mdi-open-in-app'),
+                    tooltip=f"Toggle rendering mode ({{{{ {self.SERVER_RENDERING} ? 'remote' : 'local' }}}})",
+                )
+            with vuetify.VRow(
+                v_show=(self.SERVER_RENDERING, default_server_rendering),
+                classes='pa-0 ma-0 align-center',
+            ):
+                checkbox(
+                    model=(self.AXIS, False),
+                    icons=('mdi-axis-arrow-info', 'mdi-axis-arrow-info'),
+                    tooltip=f"Toggle axis ({{{{ {self.AXIS} ? 'on' : 'off' }}}})",
+                )
+
+                def attach_screenshot():
+                    return server.protocol.addAttachment(self.screenshot())
+
+                button(
+                    # Must use single-quote string for JS here
+                    click=f"utils.download('screenshot.png', trigger('{server.trigger_name(attach_screenshot)}'), 'image/png')",
+                    icon='mdi-file-png-box',
+                    tooltip='Save screenshot',
+                )
+
+    def ui(
+        self, mode=None, default_server_rendering=True, collapse_menu=False, add_menu=True, **kwargs
+    ):
         """Generate VContainer for PyVista Plotter.
 
         Parameters
@@ -188,6 +281,9 @@ class Viewer:
         collapse_menu : bool, default: False
             Collapse the UI menu (camera controls, etc.) on start.
 
+        add_menu : bool, default: True
+            Add a UI controls VCard to the VContainer
+
         **kwargs
             Addition keyword arguments are passed to the view being created.
 
@@ -204,96 +300,23 @@ class Viewer:
             classes='pa-0 fill-height',
         ) as container:
             server = container.server
-            # Listen to state changes
-            server.state.change(self.EDGES)(self.on_edge_visiblity_change)
-            server.state.change(self.GRID)(self.on_grid_visiblity_change)
-            server.state.change(self.OUTLINE)(self.on_outline_visiblity_change)
-            server.state.change(self.AXIS)(self.on_axis_visiblity_change)
-            server.state.change(self.SERVER_RENDERING)(self.on_rendering_mode_change)
-            with vuetify.VCard(
-                style='position: absolute; top: 20px; left: 20px; z-index: 1; height: 36px;',
-                classes=(f"{{ 'rounded-circle': !{self.SHOW_UI} }}",),
-            ):
-                with vuetify.VRow(classes='pa-0 ma-0'):
-                    button(
-                        click=f'{self.SHOW_UI}=!{self.SHOW_UI}',
-                        icon='mdi-dots-vertical',
-                        tooltip=f"{{{{ {self.SHOW_UI} ? 'Hide' : 'Show' }}}} menu",
-                    )
-                    with vuetify.VRow(
-                        v_show=(f'{self.SHOW_UI}', not collapse_menu),
-                        classes='pa-0 ma-0 align-center',
-                    ):
-                        vuetify.VDivider(vertical=True, classes='mr-1')
+            if add_menu:
+                server.state[self.SHOW_UI] = not collapse_menu
+                with vuetify.VCard(
+                    style='position: absolute; top: 20px; left: 20px; z-index: 1; height: 36px;',
+                    classes=(f"{{ 'rounded-circle': !{self.SHOW_UI} }}",),
+                ):
+                    with vuetify.VRow(classes='pa-0 ma-0') as row:
                         button(
-                            click=self.reset_camera,
-                            icon='mdi-arrow-expand-all',
-                            tooltip='Reset Camera',
+                            click=f'{self.SHOW_UI}=!{self.SHOW_UI}',
+                            icon='mdi-dots-vertical',
+                            tooltip=f"{{{{ {self.SHOW_UI} ? 'Hide' : 'Show' }}}} menu",
                         )
-                        vuetify.VDivider(vertical=True, classes='mx-1')
-                        button(
-                            click=self.view_isometric,
-                            icon='mdi-axis-arrow',
-                            tooltip='Perspective view',
+                        self.ui_controls(
+                            mode=mode,
+                            default_server_rendering=default_server_rendering,
+                            v_show=(f'{self.SHOW_UI}',),
                         )
-                        button(
-                            click=self.view_yz,
-                            icon='mdi-axis-x-arrow',
-                            tooltip='Reset Camera X',
-                        )
-                        button(
-                            click=self.view_xz,
-                            icon='mdi-axis-y-arrow',
-                            tooltip='Reset Camera Y',
-                        )
-                        button(
-                            click=self.view_xy,
-                            icon='mdi-axis-z-arrow',
-                            tooltip='Reset Camera Z',
-                        )
-                        vuetify.VDivider(vertical=True, classes='mx-1')
-                        checkbox(
-                            model=(self.EDGES, False),
-                            icons=('mdi-grid', 'mdi-grid-off'),
-                            tooltip=f"Toggle edge visibility ({{{{ {self.EDGES} ? 'on' : 'off' }}}})",
-                        )
-                        checkbox(
-                            model=(self.OUTLINE, False),
-                            icons=('mdi-cube', 'mdi-cube-off'),
-                            tooltip=f"Toggle bounding box ({{{{ {self.OUTLINE} ? 'on' : 'off' }}}})",
-                        )
-                        checkbox(
-                            model=(self.GRID, False),
-                            icons=('mdi-ruler-square', 'mdi-ruler-square'),
-                            tooltip=f"Toggle ruler ({{{{ {self.GRID} ? 'on' : 'off' }}}})",
-                        )
-                        # Server rendering options
-                        if mode == 'trame':
-                            vuetify.VDivider(vertical=True, classes='mx-1')
-                            checkbox(
-                                model=(self.SERVER_RENDERING, default_server_rendering),
-                                icons=('mdi-dns', 'mdi-open-in-app'),
-                                tooltip=f"Toggle rendering mode ({{{{ {self.SERVER_RENDERING} ? 'remote' : 'local' }}}})",
-                            )
-                        with vuetify.VRow(
-                            v_show=(self.SERVER_RENDERING, default_server_rendering),
-                            classes='pa-0 ma-0 align-center',
-                        ):
-                            checkbox(
-                                model=(self.AXIS, False),
-                                icons=('mdi-axis-arrow-info', 'mdi-axis-arrow-info'),
-                                tooltip=f"Toggle axis ({{{{ {self.AXIS} ? 'on' : 'off' }}}})",
-                            )
-
-                            def attach_screenshot():
-                                return server.protocol.addAttachment(self.screenshot())
-
-                            button(
-                                # Must use single-quote string for JS here
-                                click=f"utils.download('screenshot.png', trigger('{server.trigger_name(attach_screenshot)}'), 'image/png')",
-                                icon='mdi-file-png-box',
-                                tooltip='Save screenshot',
-                            )
             if mode == 'trame':
                 view = PyVistaRemoteLocalView(
                     self.plotter,
