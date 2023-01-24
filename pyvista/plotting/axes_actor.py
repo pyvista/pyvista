301efa7b1f4dec21a16e536a856129c3a3c442b3
Bane Sullivan <bane.sullivan@kitware.com>
2023-01-24 09:07:09 -0700

    example with add_orientation_widget

--- pyvista/plotting/axes_actor.py
+++ pyvista/plotting/axes_actor.py
@@ -30,6 +30,30 @@ class AxesActor(pv._vtk.vtkAxesActor):
     >>> _ = pl.add_mesh(pv.Sphere())
     >>> pl.show()  # doctest:+SKIP
 
+    Or you can use this as a custom orientation widget with
+    :func:`add_orientation_widget() <pyvista.Renderer.add_orientation_widget>`:
+
+    >>> import pyvista as pv
+
+    >>> axes = pv.Axes()
+    >>> axes_actor = axes.axes_actor
+    >>> axes.axes_actor.shaft_type = 0
+
+    >>> axes_actor.x_axis_shaft_properties.color = (1, 1, 1)
+    >>> axes_actor.y_axis_shaft_properties.color = (1, 1, 1)
+    >>> axes_actor.z_axis_shaft_properties.color = (1, 1, 1)
+
+    >>> axes_actor.x_axis_label = 'U'
+    >>> axes_actor.y_axis_label = 'V'
+    >>> axes_actor.z_axis_label = 'W'
+
+    >>> pl = pv.Plotter()
+    >>> _ = pl.add_mesh(pv.Cone())
+    >>> _ = pl.add_orientation_widget(
+    >>>     axes_actor, viewport=(0, 0, 0.5, 0.5),
+    >>> )
+    >>> pl.show()  # doctest:+SKIP
+
     """
 
     class ShaftType(Enum):
