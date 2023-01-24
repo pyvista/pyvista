549e68619f4c8a6aa4b997d2ab0eedd6ea2a7377
Bane Sullivan <bane.sullivan@kitware.com>
2023-01-24 14:11:43 -0700

    Fix file upload example

--- examples_trame/basic/file_viewer.py
+++ examples_trame/basic/file_viewer.py
@@ -1,6 +1,7 @@
 import tempfile
 
 from trame.app import get_server
+from trame.app.file_upload import ClientFile
 from trame.ui.vuetify import SinglePageLayout
 from trame.widgets import vuetify
 
@@ -16,7 +17,7 @@ pv.OFF_SCREEN = True
 server = get_server()
 state, ctrl = server.state, server.controller
 
-state.trame__title = "Contour"
+state.trame__title = "File Viewer"
 ctrl.on_server_ready.add(ctrl.view_update)
 
 # -----------------------------------------------------------------------------
@@ -26,25 +27,22 @@ ctrl.on_server_ready.add(ctrl.view_update)
 pl = pv.Plotter()
 
 
-@state.change("files")
-def load_client_files(files, **kwargs):
-    if files is None or len(files) == 0:
+@server.state.change("file_exchange")
+def handle(file_exchange, **kwargs):
+    file = ClientFile(file_exchange)
+
+    if file.content:
+        print(file.info)
+        bytes = file.content
+        with tempfile.NamedTemporaryFile(suffix=file.name) as path:
+            with open(path.name, 'wb') as f:
+                f.write(bytes)
+            ds = pv.read(path.name)
+        pl.add_mesh(ds, name=file.name)
+        pl.reset_camera()
+    else:
         pl.clear_actors()
-        return
-
-    if files and len(files):
-        if not files[0].get("content"):
-            return
-
-        for file in files:
-            print(f'Load {file.get("name")}')
-            bytes = file.get("content")
-            with tempfile.NamedTemporaryFile(suffix=file.get("name")) as path:
-                with open(path.name, 'wb') as f:
-                    f.write(bytes)
-                ds = pv.read(path.name)
-            pl.add_mesh(ds, name=file.get("name"))
-            pl.reset_camera()
+        pl.reset_camera()
 
 
 # -----------------------------------------------------------------------------
@@ -58,11 +56,10 @@ with SinglePageLayout(server) as layout:
     with layout.toolbar:
         vuetify.VSpacer()
         vuetify.VFileInput(
-            multiple=True,
             show_size=True,
             small_chips=True,
             truncate_length=25,
-            v_model=("files", None),
+            v_model=("file_exchange", None),
             dense=True,
             hide_details=True,
             style="max-width: 300px;",
@@ -79,10 +76,4 @@ with SinglePageLayout(server) as layout:
             view = plotter_ui(pl)
             ctrl.view_update = view.update
 
-
-# -----------------------------------------------------------------------------
-# CLI
-# -----------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    server.start()
+server.start()
