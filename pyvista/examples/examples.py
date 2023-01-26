fd46d696e3013b0494f7582df7f82fa4be187c72
Alex Kaszynski <akascap@gmail.com>
2023-01-25 17:31:00 -0700

    doc fix

--- pyvista/examples/examples.py
+++ pyvista/examples/examples.py
@@ -457,7 +457,8 @@ def load_hydrogen_orbital(n=1, l=0, m=0, zoom_fac=1.0):
     Examples
     --------
     Plot the 3dxy orbital of a hydrogen atom. This corresponds to the quantum
-    numbers ``n=3``, ``l=2``, and ``m=-
+    numbers ``n=3``, ``l=2``, and ``m=-2``.
+
     >>> from pyvista import examples
     >>> grid = examples.load_hydrogen_orbital(3, 2, -2)
     >>> grid.plot(volume=True, opacity=[1, 0, 1], cmap='magma')
