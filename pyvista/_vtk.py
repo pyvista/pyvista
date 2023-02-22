1f9115d9e1a490530c4db3c542549573765768c8
Bane Sullivan <bane.sullivan@kitware.com>
2023-02-22 15:47:53 -0700

    vtkFiltersParallelDIY2 ModuleNotFoundError -> ImportError (#4054)

--- pyvista/_vtk.py
+++ pyvista/_vtk.py
@@ -335,7 +335,7 @@ if VTK9:
 
     try:
         from vtkmodules.vtkFiltersParallelDIY2 import vtkRedistributeDataSetFilter
-    except ModuleNotFoundError:  # pragma: no cover
+    except ImportError:  # pragma: no cover
         # `vtkmodules.vtkFiltersParallelDIY2` is unavailable in some versions of `vtk` from conda-forge
         pass
     from vtkmodules.vtkFiltersPoints import vtkGaussianKernel, vtkPointInterpolator
