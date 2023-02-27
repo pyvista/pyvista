898684f9220e31e338ddb389916ee092844c586b
Tetsuo Koyama <tkoyama010@gmail.com>
2023-02-27 14:56:10 +0900

    Fix error of docstring

--- pyvista/utilities/reader.py
+++ pyvista/utilities/reader.py
@@ -2344,10 +2344,10 @@ class XdmfReader(BaseReader, PointCellDataSelection):
     --------
     >>> import pyvista
     >>> from pyvista import examples
-    >>> filename = examples.download_dolfinx_mesh(load=False)
+    >>> filename = examples.download_meshio_xdmf(load=False)
     >>> reader = pyvista.get_reader(filename)
     >>> filename.split("/")[-1]  # omit the path
-    'mesh.xdmf'
+    'out.xdmf'
     >>> mesh = reader.read()
     >>> mesh.plot()
 
