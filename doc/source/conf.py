d70bfed32b202241ee8b33c8c60d387906d65326
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-19 10:58:24 +0900

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -525,16 +525,7 @@ htmlhelp_basename = "pyvistadoc"
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # Additional stuff for the LaTeX preamble.
-    #
-    # Latex figure (float) alignment
-    #
-}
+latex_elements = {}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
