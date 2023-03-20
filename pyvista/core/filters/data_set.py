bffe5614e2e3b1389fec20a13e9ea04a59e1571b
beroda <117822458+beroda@users.noreply.github.com>
2023-03-20 22:31:09 +0100

    Feat/add connectivity methods (#3643)
    * Add some connectivity methods with associated tests
    
    * Implement connectivity class. Still WIP
    
    * Adding properties
    
    * Add support for negative indices and add methods in DataSet
    
    * Add more connectivity methods
    
    * Add point neighbors with n_levels option
    
    * Use BuildLinks only if needed
    
    * Extend neighbors capabilities
    
    * Add connectivity tests and class in API
    
    * Add docstring
    
    * Add docstring to class
    
    * Update docstring
    
    * Implement connectivity methods in DataSet
    
    * Cosmetics
    
    * Add docstring
    
    * Add levels methods and docstrings
    
    * Remove DatasetConnectivity class and add docstrings
    
    * Implement invert for extract_cells method
    
    * Add visual examples
    
    * Cosmetics
    
    * Add exception for ExplicitStructuredGrid
    
    * Add docstring for cell_neighbors with levels
    
    * Add docstring for point neighbors with levels
    
    * black formatting
    
    * Remove vtk9 deprecated stuffs
    
    * Remove deprecated example
    
    * pre-commit
    
    * apply blackdoc
    
    * Apply suggestions from code review
    
    * fix examples and links
    
    ---------
    
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -4336,13 +4336,16 @@ class DataSetFilters:
         if show:  # pragma: no cover
             plt.show()
 
-    def extract_cells(self, ind, progress_bar=False):
+    def extract_cells(self, ind, invert=False, progress_bar=False):
         """Return a subset of the grid.
 
         Parameters
         ----------
         ind : numpy.ndarray
             Numpy array of cell indices to be extracted.
+
+        invert : bool, default: False
+            Invert the selection.
 
         progress_bar : bool, default: False
             Display a progress bar to indicate progress.
@@ -4368,6 +4371,10 @@ class DataSetFilters:
         >>> pl.show()
 
         """
+        if invert:
+            _, ind = numpy_to_idarr(ind, return_ind=True)
+            ind = [i for i in range(self.n_cells) if i not in ind]
+
         # Create selection objects
         selectionNode = _vtk.vtkSelectionNode()
         selectionNode.SetFieldType(_vtk.vtkSelectionNode.CELL)
