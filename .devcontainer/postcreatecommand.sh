c3128dfe6e6b364946c49f2f3bc14c54e4a8d149
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-03-21 19:11:45 +0000

    use postcreate and install hooks for precommit

--- .devcontainer/postcreatecommand.sh
+++ .devcontainer/postcreatecommand.sh
@@ -4,6 +4,6 @@ python -m pip install -r requirements.txt
 python -m pip install -r requirements_test.py
 
 python -m pip install pre-commit
-pre-commit install
+pre-commit install --install-hooks
 
 python -m pip install -e .
