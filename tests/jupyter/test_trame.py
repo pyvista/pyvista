d4e81f0b27e76c48b681a22568ad96d5c9856bc8
Alex Kaszynski <akascap@gmail.com>
2023-03-25 18:00:12 -0600

    fix trame grid state

--- tests/jupyter/test_trame.py
+++ tests/jupyter/test_trame.py
@@ -57,6 +57,7 @@ def test_trame():
     pl = pv.Plotter(notebook=True)
     actor = pl.add_mesh(pv.Cone())
     widget = pl.show(return_viewer=True)
+
     assert isinstance(widget, Widget)
     assert 'http://' in widget.src
 
@@ -67,6 +68,8 @@ def test_trame():
         cpos = list(pl.camera_position)
         pl.camera_position = cp[:3]
         assert cpos == pl.camera_position
+
+    # import pdb;pdb.set_trace()
 
     orig_value = actor.prop.show_edges
     server.state[viewer.EDGES] = not orig_value
