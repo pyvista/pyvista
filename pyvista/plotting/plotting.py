8d603a5c11227505a90f3197bdf6158a6aabf15b
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-04-01 21:41:00 +0100

    Plot purple line segments rather than yellow.

--- pyvista/plotting/plotting.py
+++ pyvista/plotting/plotting.py
@@ -5057,7 +5057,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         >>> import pyvista
         >>> pl = pyvista.Plotter()
         >>> points = np.array([[0, 1, 0], [1, 0, 0], [1, 1, 0], [2, 0, 0]])
-        >>> actor = pl.add_lines(points, color='yellow', width=3)
+        >>> actor = pl.add_lines(points, color='purple', width=3)
         >>> pl.camera_position = 'xy'
         >>> pl.show()
         """
