a55830155cf8f8d0720564b7ab1c4471fabcca15
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-16 21:37:57 +0900

    Create Goniometer example

---
+++ examples/02-plot/goniometer.py
@@ -0,0 +1,12 @@
+"""
+.. _goniometer
+
+Goniometer
+~~~~~~~~~~
+
+The 3D-ruler axis style, a flag pole and a goniometer.
+This example is inspired by [vedo's example](https://vedo.embl.es/docs/vedo/addons.html#Goniometer).
+
+"""
+
+import pyvista as pv
