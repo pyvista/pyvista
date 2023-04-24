9b68911b21f4342165105f03d7b32e6fb5c505ce
German <germanmartinezayuso@gmail.com>
2023-04-24 23:08:29 +0700

    Documenting linkcode_resolve usage a bit more.

--- pyvista/utilities/docs.py
+++ pyvista/utilities/docs.py
@@ -23,6 +23,9 @@ def linkcode_resolve(domain, info):
 
     Notes
     -----
+    This function is used by the extension "sphinx.ext.linkcode" to create the "source code"
+    button whose link is edited in this function.
+
     This has been adapted to deal with our "verbose" decorator.
 
     Adapted from mne (mne/utils/docs.py), which was adapted from SciPy (doc/source/conf.py).
