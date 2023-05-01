46b4195a6b78973f84622e38910ecb6405500abb
NATSUHIKO MIZUTANI <mizutani.natsu@gmail.com>
2023-05-01 21:00:36 +0900

    Update pointset.py
    DocString of class StructuredGrid is updated to show that `indexing='ij'` option should be applied to prepare `x, y, z = np.meshgrid(xrng, yrng, zrng, indexing='ij')` .
    
    Details on this document modification can be found at https://github.com/pyvista/pyvista/discussions/4360

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -2001,18 +2001,18 @@ class StructuredGrid(_vtk.vtkStructuredGrid, PointGrid, StructuredGridFilters):
     Create from NumPy arrays.
 
     >>> xrng = np.arange(-10, 10, 2, dtype=np.float32)
-    >>> yrng = np.arange(-10, 10, 2, dtype=np.float32)
-    >>> zrng = np.arange(-10, 10, 2, dtype=np.float32)
-    >>> x, y, z = np.meshgrid(xrng, yrng, zrng)
+    >>> yrng = np.arange(-10, 10, 5, dtype=np.float32)
+    >>> zrng = np.arange(-10, 10, 1, dtype=np.float32)
+    >>> x, y, z = np.meshgrid(xrng, yrng, zrng, indexing='ij')
     >>> grid = pyvista.StructuredGrid(x, y, z)
     >>> grid  # doctest:+SKIP
     StructuredGrid (0x7fb18f2a8580)
-    N Cells:    729
-    N Points:   1000
+    N Cells:    513
+    N Points:   800
     X Bounds:   -1.000e+01, 8.000e+00
-    Y Bounds:   -1.000e+01, 8.000e+00
-    Z Bounds:   -1.000e+01, 8.000e+00
-    Dimensions: 10, 10, 10
+    Y Bounds:   -1.000e+01, 5.000e+00
+    Z Bounds:   -1.000e+01, 9.000e+00
+    Dimensions: 10, 4, 20
     N Arrays:   0
 
     """
