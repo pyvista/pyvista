945adc335d1c6037c369b136eb36dfdd3d7d233d
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-02 12:55:36 +0900

    Use example beam

--- examples/02-plot/dimension-line.py
+++ examples/02-plot/dimension-line.py
@@ -8,28 +8,14 @@ Create a dimension line along 2d structured mesh.
 """
 
 import numpy as np
-
 import pyvista as pv
+from pyvista import examples
 
-ni, nj = 4, 5
-si, sj = 20, 10
+grid = pv.UnstructuredGrid(examples.hexbeamfile)
 
-xcorn = np.arange(0, (ni + 1) * si, si)
-xcorn = np.repeat(xcorn, 2)
-xcorn = xcorn[1:-1]
-xcorn = np.tile(xcorn, 4 * nj)
 
-ycorn = np.arange(0, (nj + 1) * sj, sj)
-ycorn = np.repeat(ycorn, 2)
-ycorn = ycorn[1:-1]
-ycorn = np.tile(ycorn, (2 * ni, 2))
-ycorn = np.transpose(ycorn)
-ycorn = ycorn.flatten()
+plotter = pv.Plotter()
+plotter.add_mesh(grid, show_edges=True, color='tan')
 
-corners = np.stack((xcorn, ycorn))
-corners = corners.transpose()
-
-dims = np.asarray((ni, nj)) + 1
-grid = pv.ExplicitStructuredGrid(dims, corners)
-grid = grid.compute_connectivity()
-grid.plot(show_edges=True)
+plotter.enable_parallel_projection()
+plotter.show()
