34fef7db497ff88f9258d61c66089b76f30cc67e
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-05 23:30:38 +0900

    Fix glyphs

--- examples/98-common/create-dimension-line.py
+++ examples/98-common/create-dimension-line.py
@@ -23,10 +23,10 @@ plotter.add_mesh(grid, show_edges=True, color='tan')
 plotter.enable_parallel_projection()
 
 lines = np.array([[xrng[0], yrng[-1], 0.0], [xrng[-1], yrng[-1], 0.0]])
+lines += np.array([[0.0, 4.0, 0.0], [0.0, 4.0, 0.0]])
 mlines = pv.MultipleLines(lines)
-mlines["Normal"] = np.array([[0.0, 3.0, 0.0], [0.0, 3.0, 0.0]])
-arrows = mlines.glyph(geom=pv.Line(), scale="Normal", factor=3.0, orient="Normal")
-lines += np.array([[0.0, 3.0, 0.0], [0.0, 3.0, 0.0]])
+mlines["Normal"] = np.array([[0.0, 1.0, 0.0], [0.0, 1.0, 0.0]])
+arrows = mlines.glyph(geom=pv.Line(), scale="Normal", factor=2.0, orient="Normal")
 
 pointa = lines[0:-1]
 pointb = lines[1:]
