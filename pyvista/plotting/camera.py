24cc10a5129cfba8d0cb0b0404ba639040c89a1c
Alex Kaszynski <akascap@gmail.com>
2023-05-12 14:48:50 -0600

    minor fixes

--- pyvista/plotting/camera.py
+++ pyvista/plotting/camera.py
@@ -605,7 +605,7 @@ class Camera(_vtk.vtkCamera):
 
         Examples
         --------
-        >>> import pyvista as pv
+        >>> import pyvista
         >>> pl = pyvista.Plotter()
         >>> pl.camera.direction  # doctest:+SKIP
         (-0.5773502691896257, -0.5773502691896257, -0.5773502691896257)
