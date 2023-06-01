b26d367093544348bb3b71534bd3179e0cf78566
Bane Sullivan <bane.sullivan@kitware.com>
2023-06-01 19:51:25 -0400

    Deprecate pyvista.plotting.plotting module

---
+++ pyvista/plotting/plotting/__init__.py
@@ -0,0 +1,30 @@
+"""Deprecated pyvista.plotting.plotting module."""
+import importlib
+import inspect
+import warnings
+
+from pyvista.core.errors import PyVistaDeprecationWarning
+
+
+def __getattr__(name):
+    module = importlib.import_module('pyvista.plotting.plotter')
+    try:
+        value = inspect.getattr_static(module, name)
+        import_path = f'from pyvista.plotting.plotter import {name}'
+    except AttributeError:
+        module = importlib.import_module('pyvista.plotting')
+        try:
+            value = inspect.getattr_static(module, name)
+            import_path = f'from pyvista.plotting import {name}'
+        except AttributeError:
+            raise AttributeError(
+                f'Module `pyvista.plotting.plotting` has been deprecated and we could not automatically find `{name}`.'
+            )
+
+    message = f'The `pyvista.plotting.plotting` module has been deprecated. `{name}` is now imported as: `{import_path}`.'
+
+    warnings.warn(
+        message,
+        PyVistaDeprecationWarning,
+    )
+    return value
