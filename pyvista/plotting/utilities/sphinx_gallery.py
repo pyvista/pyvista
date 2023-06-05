7e006a190a0cf2c817a4379becc8a41d3fc48354
Bane Sullivan <bane.sullivan@kitware.com>
2023-06-05 08:37:20 -0600

    Update pyvista/plotting/utilities/sphinx_gallery.py
    Co-authored-by: Andras Deak <adeak@users.noreply.github.com>

--- pyvista/plotting/utilities/sphinx_gallery.py
+++ pyvista/plotting/utilities/sphinx_gallery.py
@@ -12,7 +12,7 @@ def _get_sg_image_scraper():
     'matplotlib' and 'mayavi'. Details on this implementation can be found in
     `sphinx-gallery/sphinx-gallery/494`_
 
-    This must be at the top level namespace of PyVista.
+    This must be imported into the top level namespace of PyVista.
 
     .. _sphinx-gallery/sphinx-gallery/494: https://github.com/sphinx-gallery/sphinx-gallery/pull/494
     """
