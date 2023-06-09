dd09660abd89035cd8281b89e33076dc5103758d
Bane Sullivan <bane.sullivan@kitware.com>
2023-06-09 01:41:05 -0400

    Validate namespace

--- pyvista/plotting/__init__.py
+++ pyvista/plotting/__init__.py
@@ -6,29 +6,45 @@ from pyvista._plot import plot
 
 from . import _vtk
 from ._property import Property
+from ._typing import Chart, ColorLike
 from .actor import Actor
 from .actor_properties import ActorProperties
 from .axes import Axes
 from .axes_actor import AxesActor
 from .camera import Camera
 from .charts import Chart, Chart2D, ChartBox, ChartMPL, ChartPie
-from .colors import (
-    PARAVIEW_BACKGROUND,
-    Color,
-    ColorLike,
-    color_char_to_word,
-    get_cmap_safe,
-    hexcolors,
-)
+from .colors import PARAVIEW_BACKGROUND, Color, color_char_to_word, get_cmap_safe, hexcolors
 from .composite_mapper import BlockAttributes, CompositeAttributes, CompositePolyDataMapper
 from .cube_axes_actor import CubeAxesActor
+from .errors import InvalidCameraError, RenderWindowUnavailable
 from .export_vtkjs import export_plotter_vtkjs, get_vtkjs_url
 from .helpers import plot_arrows, plot_compare_four
 from .lights import Light
 from .lookup_table import LookupTable
-from .mapper import DataSetMapper, _BaseMapper
-from .plotter import BasePlotter, Plotter, close_all
+from .mapper import (
+    DataSetMapper,
+    FixedPointVolumeRayCastMapper,
+    GPUVolumeRayCastMapper,
+    OpenGLGPUVolumeRayCastMapper,
+    PointGaussianMapper,
+    SmartVolumeMapper,
+    UnstructuredGridVolumeRayCastMapper,
+    _BaseMapper,
+)
+from .picking import PickingHelper
+from .plotter import (
+    _ALL_PLOTTERS,
+    KILL_DISPLAY,
+    SUPPORTED_FORMATS,
+    BasePlotter,
+    Plotter,
+    _warn_xserver,
+    close_all,
+)
+from .render_window_interactor import RenderWindowInteractor
 from .renderer import CameraPosition, Renderer, scale_point
+from .renderers import Renderers
+from .scalar_bars import ScalarBars
 from .texture import Texture, image_to_texture, numpy_to_texture
 from .tools import (
     FONTS,
@@ -36,11 +52,14 @@ from .tools import (
     check_matplotlib_vtk_compatibility,
     create_axes_marker,
     create_axes_orientation_box,
+    normalize,
     opacity_transfer_function,
     parse_font_family,
     system_supports_plotting,
 )
 from .utilities import *
+from .volume import Volume
+from .volume_property import VolumeProperty
 from .widgets import WidgetHelper
 
 
---
+++ tests/namespace/test_plotting_namespace.py
@@ -0,0 +1,25 @@
+import importlib
+import pathlib
+
+import pytest
+
+from pyvista.core.errors import PyVistaDeprecationWarning
+
+namesapce_data = pathlib.Path(__file__).parent / 'namespace-plotting.txt'
+with open(namesapce_data) as f:
+    namespace = f.read().splitlines()
+    # ignore commented data
+    namespace = [n.split(', ')[0] for n in namespace if not n.startswith('#')]
+
+
+@pytest.mark.parametrize('name', namespace)
+def test_plotting_top_namespace(name):
+    module = importlib.import_module('pyvista.plotting')
+    assert hasattr(module, name)
+
+
+def test_common_plotting_import_paths():
+    # These are `pyvista.plotting.plotting` imports found via search on GitHub
+    # across multiple public repositories
+    with pytest.warns(PyVistaDeprecationWarning):
+        from pyvista.plotting.plotting import _ALL_PLOTTERS, BasePlotter, Plotter  # noqa: F401
