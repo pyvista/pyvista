56901459f139abe70b027f05e57ed5ed338b65fb
Alex Kaszynski <akascap@gmail.com>
2023-06-13 09:35:18 -0600

    simply __getattr__

--- pyvista/utilities/__init__.py
+++ pyvista/utilities/__init__.py
@@ -1,4 +1,70 @@
 """Deprecated utilities subpackage."""
-from ._getattr import _getattr_factory
+import importlib
+import inspect
+import warnings
 
-__getattr__ = _getattr_factory(globals())
+# Places to look for the utility
+_MODULES = [
+    'pyvista.core.utilities.cells',
+    'pyvista.core.utilities',
+    'pyvista.core.errors',
+    'pyvista.core',
+    'pyvista.plotting.utilities',
+    'pyvista.plotting.errors',
+    'pyvista.plotting.texture',
+    'pyvista.plotting',
+    'pyvista.report',
+    'pyvista.themes',
+]
+
+
+def _try_import(module, name):
+    """Attempt to import a module."""
+    _module = importlib.import_module(module)
+    try:
+        feature = inspect.getattr_static(_module, name)
+        import_path = f'from {module} import {name}'
+    except AttributeError:
+        return None, None
+    return feature, import_path
+
+
+def __getattr__(name):
+    """Fetch an attribute ``name`` from ``globals()`` and warn if it's from a deprecated module.
+
+    Raises
+    ------
+    AttributeError
+        If the attribute is not found in ``globals()`` and also could not be
+        imported from the modules in ``_MODULES``.
+
+    PyVistaDeprecationWarning
+        If the attribute has been found via importing from the modules in
+        ``_MODULES``, as this implies that the feature has been moved from
+        pyvista.utilities.
+
+    """
+    from pyvista.core.errors import PyVistaDeprecationWarning
+
+    try:
+        return globals()[name]
+    except KeyError:
+        pass
+
+    for module in _MODULES:
+        feature, import_path = _try_import(module, name)
+        if feature is not None:
+            break
+    else:
+        raise AttributeError(
+            f'Module `pyvista.utilities` has been deprecated and we could not automatically find `{name}`. This feature has moved.'
+        ) from None
+
+    message = f'The `pyvista.utilities` module has been deprecated. `{name}` is now imported as: `{import_path}`.'
+
+    warnings.warn(
+        message,
+        PyVistaDeprecationWarning,
+    )
+
+    return feature
