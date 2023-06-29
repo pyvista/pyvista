740d494e10572bac36d32f92d50079479e9c128f
Bane Sullivan <bane.sullivan@kitware.com>
2023-06-28 18:37:58 -0600

    Updaet example

--- examples/02-plot/distance_measurement.py
+++ examples/02-plot/distance_measurement.py
@@ -12,14 +12,14 @@ import pyvista as pv
 cube = pv.Cube()
 cube2 = pv.Cube([10, 10, 0])
 
-p = pv.Plotter(notebook=0)
-p.add_mesh(cube)
-p.add_mesh(cube2)
+pl = pv.Plotter()
+pl.add_mesh(cube)
+pl.add_mesh(cube2)
 
 
 def callback(a, b, distance):
-    p.add_text(f'Distance: {distance:.2f}', name='dist')
+    pl.add_text(f'Distance: {distance:.2f}', name='dist')
 
 
-p.add_distance_widget(callback)
-p.show()
+pl.add_distance_widget(callback)
+pl.show()
