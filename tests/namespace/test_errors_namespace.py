9176377cc30fb1543b19ed6ab280e1d463fa0c92
Bane Sullivan <bane.sullivan@kitware.com>
2023-07-06 11:07:29 -0400

    Cleanup

--- tests/namespace/test_errors_namespace.py
+++ tests/namespace/test_errors_namespace.py
@@ -1,27 +1,27 @@
-import importlib
-import pathlib
-
 import pytest
 
 from pyvista.core.errors import PyVistaDeprecationWarning
 
 
-@pytest.mark.parametrize('name', [
-    'AmbiguousDataError',
-    'DeprecationError',
-    'MissingDataError',
-    'NotAllTrianglesError',
-    'PointSetCellOperationError',
-    'PointSetDimensionReductionError',
-    'PointSetNotSupported',
-    'PyVistaDeprecationWarning',
-    'PyVistaEfficiencyWarning',
-    'PyVistaFutureWarning',
-    'PyVistaPipelineError',
-    'VTKVersionError',
-    'InvalidCameraError',
-    'RenderWindowUnavailable',
-])
+@pytest.mark.parametrize(
+    'name',
+    [
+        'AmbiguousDataError',
+        'DeprecationError',
+        'MissingDataError',
+        'NotAllTrianglesError',
+        'PointSetCellOperationError',
+        'PointSetDimensionReductionError',
+        'PointSetNotSupported',
+        'PyVistaDeprecationWarning',
+        'PyVistaEfficiencyWarning',
+        'PyVistaFutureWarning',
+        'PyVistaPipelineError',
+        'VTKVersionError',
+        'InvalidCameraError',
+        'RenderWindowUnavailable',
+    ],
+)
 def test_utilities_namespace(name):
     with pytest.warns(PyVistaDeprecationWarning):
         import pyvista.errors as errors
