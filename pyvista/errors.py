b5df704d37e98639975f24fb4c021f6bc6dd2b70
Bane Sullivan <bane.sullivan@kitware.com>
2023-07-06 23:14:13 -0400

    pragma: no cover

--- pyvista/errors.py
+++ pyvista/errors.py
@@ -55,7 +55,7 @@ def __getattr__(name):
     else:
         raise AttributeError(
             f'Module `pyvista.errors` has been deprecated and we could not automatically find `{name}`. This feature has moved.'
-        ) from None
+        ) from None  # pragma: no cover
 
     message = f'The `pyvista.errors` module has been deprecated. `{name}` is now imported as: `{import_path}`.'
 
