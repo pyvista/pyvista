7532318bbc03c99da3b689c52c3b90a9aab5d8cf
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-09 05:47:05 -0500

    Fix image

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -2633,7 +2633,7 @@ def test_add_text_latex():
 
 def test_add_text_font_file():
     plotter = pyvista.Plotter()
-    font_file = os.path.join(os.getcwd(), "fonts/Mplus2-Regular.ttf")
+    font_file = os.path.join(os.path.dirname(__file__), "fonts/Mplus2-Regular.ttf")
     plotter.add_text("左上", position='upper_left', font_size=25, color='blue', font_file=font_file)
     plotter.add_text("中央", position=(0.5, 0.5), viewport=True, orientation=-90, font_file=font_file)
     plotter.show()
