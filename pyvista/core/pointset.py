eb7bbad466c60411bfca4a0355422d5ce798b6d1
darikg <darikg@users.noreply.github.com>
2023-07-09 19:28:13 -0400

    Update pyvista/core/pointset.py
    Co-authored-by: Andras Deak <adeak@users.noreply.github.com>

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -888,6 +888,7 @@ class PolyData(_vtk.vtkPolyData, _PointSet, PolyDataFilters):
         Examples
         --------
         Construct a tetrahedron from four triangles
+
         >>> import pyvista as pv
         >>> points = [[1.0, 1, 1], [-1, 1, -1], [1, -1, -1], [-1, -1, 1]]
         >>> faces = [[0, 1, 2], [1, 3, 2], [0, 2, 3], [0, 3, 1]]
