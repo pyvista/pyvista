5a8d52640d844c737b17e9c692ce5f02813149f8
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-07-11 20:29:19 +0000

    add more doctest skip lines

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -3079,7 +3079,7 @@ class DataSetFilters:
         >>> mesh = pv.Sphere(center=(4.5, 4.5, 4.5), radius=4.5)
         >>> grid = examples.load_uniform()
         >>> result = grid.probe(mesh)  # doctest:+SKIP
-        >>> 'Spatial Point Data' in result.point_data
+        >>> 'Spatial Point Data' in result.point_data  # doctest:+SKIP
         True
 
         """
