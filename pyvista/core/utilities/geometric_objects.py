0cd09bdc6aac2c0620895d83a273e47ee079cb33
Andreu Cotanda Oliver <cotoli23@gmail.com>
2023-07-13 23:15:42 +0200

    Fix style

--- pyvista/core/utilities/geometric_objects.py
+++ pyvista/core/utilities/geometric_objects.py
@@ -504,7 +504,9 @@ def MultipleLines(points=[[-0.5, 0.0, 0.0], [0.5, 0.0, 0.0]]):
     Create a multiple lines between ``(0, 0, 0)``, ``(1, 1, 1)`` and ``(0, 0, 1)``.
 
     >>> import pyvista
-    >>> mesh = pyvista.MultipleLines(points=[[0, 0, 0], [1, 1, 1], [0, 0, 1]])
+    >>> mesh = pyvista.MultipleLines(
+    ...     points=[[0, 0, 0], [1, 1, 1], [0, 0, 1]]
+    ... )
     >>> plotter = pyvista.Plotter()
     >>> actor = plotter.add_mesh(mesh)
     >>> plotter.camera.azimuth = 45
