43274a3fe0eba3bdec189b9dd9504e94dabd7737
Bane Sullivan <bane.sullivan@kitware.com>
2023-07-18 15:03:46 -0600

    Apply suggestions from code review
    Co-authored-by: Andras Deak <adeak@users.noreply.github.com>

--- tests/namespace/test_errors_namespace.py
+++ tests/namespace/test_errors_namespace.py
@@ -22,7 +22,7 @@ from pyvista.core.errors import PyVistaDeprecationWarning
 )
 def test_core_errors_namespace(name):
     with pytest.warns(
-        PyVistaDeprecationWarning, match='now imported as: `from pyvista.core.errors import'
+        PyVistaDeprecationWarning, match=rf'now imported as: `from pyvista\.core\.errors import {name}`\.'
     ):
         import pyvista.errors as errors
 
@@ -38,7 +38,7 @@ def test_core_errors_namespace(name):
 )
 def test_plotting_errors_namespace(name):
     with pytest.warns(
-        PyVistaDeprecationWarning, match='now imported as: `from pyvista.plotting.errors import'
+        PyVistaDeprecationWarning, match=rf'now imported as: `from pyvista\.plotting\.errors import {name}`\.'
     ):
         import pyvista.errors as errors
 
