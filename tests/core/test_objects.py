18e6e72b5c08ad02ee500ac6357893cb97e41444
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-22 06:10:19 +0900

    Add Text test

--- tests/core/test_objects.py
+++ tests/core/test_objects.py
@@ -216,3 +216,26 @@ def test_get_data_range():
     nanmin, nanmax = table.get_data_range()
     assert nanmin == np.nanmin(arrays[:, 0])
     assert nanmax == np.nanmax(arrays[:, 0])
+
+
+def test_text():
+    text = pyvista.Text('text')
+    prop = text.prop
+    assert isinstance(prop, pyvista.TextProperty)
+
+def test_text_prop():
+    prop = pyvista.TextProperty()
+    prop.color = "b"
+    prop.opacity = 0.5
+    prop.background_color = "b"
+    prop.background_opacity = 0.5
+    prop.frame = True
+    prop.frame_color = "b"
+    prop.frame_width = 10.0
+    assert prop.color == "b"
+    assert prop.opacity == 0.5
+    assert prop.background_color == "b"
+    assert prop.background_opacity == 0.5
+    assert prop.frame == True
+    assert prop.frame_color == "b"
+    assert prop.frame_width == 10.0
