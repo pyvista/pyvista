638ca0622ee7060c278ade6e3963cb64d045873e
Bane Sullivan <bane.sullivan@kitware.com>
2023-07-21 09:33:24 -0600

    Bump version

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -13,7 +13,7 @@ Denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 40, 3
+version_info = 0, 40, 4
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
