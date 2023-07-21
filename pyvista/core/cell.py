913282b6a6b9ccf1949e54b9f9aff9e12a9b224f
Erik Bedard <bedarder@gmail.com>
2023-07-21 12:11:37 -0700

    Fix docstring

--- pyvista/core/cell.py
+++ pyvista/core/cell.py
@@ -260,7 +260,7 @@ class Cell(_vtk.vtkGenericCell, DataObject):
         >>> import pyvista as pv
         >>> mesh = pv.Sphere()
         >>> mesh.get_cell(0).points
-        array([[0.05405951, 0.        , 0.49706897]
+        array([[0.05405951, 0.        , 0.49706897],
                [0.05287818, 0.0112396 , 0.49706897],
                [0.        , 0.        , 0.5       ]])
         """
