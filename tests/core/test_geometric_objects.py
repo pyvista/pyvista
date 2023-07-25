4a07c520b85d871bbb5e0546cfe11d441a852d44
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-26 06:15:24 +0900

    Update tests/core/test_geometric_objects.py

--- tests/core/test_geometric_objects.py
+++ tests/core/test_geometric_objects.py
@@ -52,7 +52,7 @@ def test_sphere_phi():
     north_hemisphere = pyvista.Sphere(start_phi=0, end_phi=90)
     assert np.all(north_hemisphere.points[:, 2] >= -atol)  # north is above XY plane
     south_hemisphere = pyvista.Sphere(start_phi=90, end_phi=180)
-    assert np.all(south_hemisphere.points[:, 2] <= 0 + atol)  # south is below XY plane
+    assert np.all(south_hemisphere.points[:, 2] <= atol)  # south is below XY plane
 
 
 def test_sphere_theta():
