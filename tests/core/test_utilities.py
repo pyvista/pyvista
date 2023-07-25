25b64a340ef9928b9c912671566392a2e7d269d0
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-25 20:43:15 +0900

    Apply suggestions from code review

--- tests/core/test_utilities.py
+++ tests/core/test_utilities.py
@@ -594,7 +594,7 @@ def test_axis_angle_rotation():
 
 
 @pytest.mark.parametrize("axis,angle,times", [([1, 0, 0], 90, 4), ([1, 0, 0], 180, 2), ([1, 0, 0], 270, 4), ([0, 1, 0], 90, 4), ([0, 1, 0], 180, 2), ([0, 1, 0], 270, 4), ([0, 0, 1], 90, 4), ([0, 0, 1], 180, 2), ([0, 0, 1] , 270, 4)])
-def test_axis_angle_rotation_many_times():
+def test_axis_angle_rotation_many_times(axis,angle,times):
     # yields the exact same input
     expect = np.eye(3)
     actual = expect.copy()
