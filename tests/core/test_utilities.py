Fix `Plane` and `Sphere` normal/direction; fix `axis_rotation_angle` numerical error (#4674)
* Fix `Plane` normal direction

Fixes #4655

* Fix `Sphere` direction

* Remove test dependency on `Sphere`

* Increase precision for 90, 180, and 270 degree rotations

* Fix doctest errors

* Add test cases for exact transformations

* Fix merge test case

* Reduce numerical tolerance of test cases

* Make test case more compact

* Fix docstring

* Fix translate bug and improve direction test coverage

* Fix tests with incorrect Sphere/Plane direction

* Fix failed texture tests

* Apply suggestions from code review

* Apply suggestions from code review

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Apply suggestions from code review

* Update test_geometric_objects.py

* Update test_geometric_objects.py

* Update test_geometric_objects.py

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Update test_utilities.py

* Update test_utilities.py

* Update test_utilities.py

* Apply suggestions from code review

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Update tests/core/test_geometric_objects.py

* Update tests/core/test_geometric_objects.py

---------

Co-authored-by: Erik Bedard <bedarder@gmail.com>
Co-authored-by: Tetsuo Koyama <tkoyama010@gmail.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

tests/core/test_utilities.py
@@ -559,14 +559,8 @@ def test_vtk_error_catcher():
 
 
 def test_axis_angle_rotation():
-    # rotate cube corners around body diagonal
-    points = np.array(
-        [
-            [1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1],
-        ]
-    )
+    # rotate points around body diagonal
+    points = np.eye(3)
     axis = [1, 1, 1]
 
     # no-op case
@@ -597,6 +591,30 @@ def test_axis_angle_rotation():
         transformations.axis_angle_rotation(axis, angle, point=[1, 0, 0, 0])
     with pytest.raises(ValueError):
         transformations.axis_angle_rotation([0, 0, 0], angle)
+
+
+@pytest.mark.parametrize(
+    "axis,angle,times",
+    [
+        ([1, 0, 0], 90, 4),
+        ([1, 0, 0], 180, 2),
+        ([1, 0, 0], 270, 4),
+        ([0, 1, 0], 90, 4),
+        ([0, 1, 0], 180, 2),
+        ([0, 1, 0], 270, 4),
+        ([0, 0, 1], 90, 4),
+        ([0, 0, 1], 180, 2),
+        ([0, 0, 1], 270, 4),
+    ],
+)
+def test_axis_angle_rotation_many_times(axis, angle, times):
+    # yields the exact same input
+    expect = np.eye(3)
+    actual = expect.copy()
+    trans = transformations.axis_angle_rotation(axis, angle)
+    for _ in range(times):
+        actual = transformations.apply_transformation_to_points(trans, actual)
+    assert np.array_equal(actual, expect)
 
 
 def test_reflection():

tests/plotting/test_plotting.py
@@ -98,7 +98,7 @@ def verify_image_cache_wrapper(verify_image_cache):
 @pytest.fixture()
 def multicomp_poly():
     """Create a dataset with vector values on points and cells."""
-    data = pyvista.Plane()
+    data = pyvista.Plane(direction=(0, 0, -1))
 
     vector_values_points = np.empty((data.n_points, 3))
     vector_values_points[:, 0] = np.arange(data.n_points)
@@ -2199,7 +2199,7 @@ def test_plot_shadows():
 
     # add several planes
     for plane_y in [2, 5, 10]:
-        screen = pyvista.Plane(center=(0, plane_y, 0), direction=(0, 1, 0), i_size=5, j_size=5)
+        screen = pyvista.Plane(center=(0, plane_y, 0), direction=(0, -1, 0), i_size=5, j_size=5)
         plotter.add_mesh(screen, color='white')
 
     light = pyvista.Light(
@@ -2233,7 +2233,7 @@ def test_plot_shadows_enable_disable():
 
     # add several planes
     for plane_y in [2, 5, 10]:
-        screen = pyvista.Plane(center=(0, plane_y, 0), direction=(0, 1, 0), i_size=5, j_size=5)
+        screen = pyvista.Plane(center=(0, plane_y, 0), direction=(0, -1, 0), i_size=5, j_size=5)
         plotter.add_mesh(screen, color='white')
 
     light = pyvista.Light(
@@ -2930,7 +2930,7 @@ def test_export_obj(tmpdir, sphere):
 def test_multi_plot_scalars(verify_image_cache):
     verify_image_cache.windows_skip_image_cache = True
     res = 5
-    plane = pyvista.Plane(j_resolution=res, i_resolution=res)
+    plane = pyvista.Plane(j_resolution=res, i_resolution=res, direction=(0, 0, -1))
     plane.clear_data()
     kek = np.arange(res + 1)
     kek = np.tile(kek, (res + 1, 1))