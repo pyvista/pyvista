9f952677609ca77630a3e58f4aeaeeaf627bd50d
Tetsuo Koyama <tkoyama010@gmail.com>
2023-08-02 11:14:22 +0900

    Update composite.py

--- pyvista/core/composite.py
+++ pyvista/core/composite.py
@@ -97,13 +97,7 @@ class MultiBlock(
     _WRITERS = dict.fromkeys(['.vtm', '.vtmb'], _vtk.vtkXMLMultiBlockDataWriter)
 
     def __init__(self, *args, **kwargs) -> None:
-        """Initialize multi block.
-
-        Parameters
-        ----------
-        **kwargs : dict, optional
-            See :func:`pyvista.read` for additional options.
-        """
+        """Initialize multi block."""
         super().__init__()
         deep = kwargs.pop('deep', False)
 
