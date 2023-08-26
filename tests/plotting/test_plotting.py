f28e337ff944e6b93550349cee2215fcb88873c7
Tetsuo Koyama <tkoyama010@gmail.com>
2023-08-27 06:17:04 +0900

    Apply suggestions from code review

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -1864,9 +1864,9 @@ def test_opacity_transfer_functions():
     assert len(mapping) == n
     mapping = pv.opacity_transfer_function('sigmoid_10', n)
     assert len(mapping) == n
-    mapping = pyvista.opacity_transfer_function('foreground', n)
+    mapping = pv.opacity_transfer_function('foreground', n)
     assert len(mapping) == n
-    mapping = pyvista.opacity_transfer_function('foreground', 5)
+    mapping = pv.opacity_transfer_function('foreground', 5)
     assert np.array_equal(mapping, [0, 255, 255, 255, 255])
     with pytest.raises(ValueError):
         mapping = pv.opacity_transfer_function('foo', n)
