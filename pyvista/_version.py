b35134584dcebf78a2b7b577aff0612a8f16a5f1
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-06 09:12:24 +0900

    Update _version.py

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -13,7 +13,7 @@ Denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 42, 0
+version_info = 0, 42, 1
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
