e1f8609df94a68228ac49d2ad482793151ef864f
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-17 19:45:03 +0900

    Update test_geometric_sources.py

--- tests/core/test_geometric_sources.py
+++ tests/core/test_geometric_sources.py
@@ -26,8 +26,8 @@ def test_cylinder_source():
     assert algo.height == 1.0
     assert algo.capping
     assert algo.resolution == 100
-    center = np.random(3)
-    direction = np.direction(3)
+    center = np.random.rand(3)
+    direction = np.random.rand(3)
     algo.center = center
     algo.direction = direction
     assert np.array_equal(algo.center, center)
