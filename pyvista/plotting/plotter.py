d877086edc52dd48cfb2f2cadc9a7ecc858ae965
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2023-09-18 18:35:47 +0200

    feat: add `user_matrix` argument to `add_mesh` and `add_volume`

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -2835,6 +2835,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         metallic=None,
         roughness=None,
         render=True,
+        user_matrix=np.eye(4),
         component=None,
         emissive=None,
         copy_mesh=False,
@@ -3121,6 +3122,12 @@ class BasePlotter(PickingHelper, WidgetHelper):
 
         render : bool, default: True
             Force a render when ``True``.
+
+        user_matrix : np.ndarray | vtk.vtkMatrix4x4, default: np.eye(4)
+            Matrix passed to the Actor class before rendering. This affects the
+            actor/rendering only, not the input volume itself. The user matrix is the
+            last transformation applied to the actor before rendering. Defaults to the
+            identity matrix.
 
         component : int, optional
             Set component of vector valued scalars to plot.  Must be
@@ -3671,6 +3678,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         specular=0.2,  # TODO: different default for volumes
         specular_power=10.0,  # TODO: different default for volumes
         render=True,
+        user_matrix=np.eye(4),
         log_scale=False,
         **kwargs,
     ):
@@ -3888,6 +3896,12 @@ class BasePlotter(PickingHelper, WidgetHelper):
 
         render : bool, default: True
             Force a render when True.
+
+        user_matrix : np.ndarray | vtk.vtkMatrix4x4, default: np.eye(4)
+            Matrix passed to the Volume class before rendering. This affects the
+            actor/rendering only, not the input volume itself. The user matrix is the
+            last transformation applied to the actor before rendering. Defaults to the
+            identity matrix.
 
         log_scale : bool, default: False
             Use log scale when mapping data to colors. Scalars less
@@ -4226,6 +4240,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
 
         self.volume = Volume()
         self.volume.mapper = self.mapper
+        self.volume.user_matrix = user_matrix
 
         self.volume.prop = VolumeProperty(
             lookup_table=self.mapper.lookup_table,
