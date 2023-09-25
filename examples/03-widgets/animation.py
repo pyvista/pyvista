a50b2c8375ba83a212b8cea07a6170d9a24bf8c6
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-25 21:21:45 +0900

    Add animation example

--- examples/03-widgets/animation.py
+++ examples/03-widgets/animation.py
@@ -11,6 +11,7 @@ Inspired by `VTK Animation Examples <https://examples.vtk.org/site/Python/Utilit
 """
 
 import pyvista as pv
+import numpy as np
 
 sphere = pv.Sphere()
 
@@ -26,3 +27,22 @@ pl.add_timer_event(max_steps=200, duration=500, callback=callback)
 
 cpos = [(0.0, 0.0, 10.0), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
 pl.show(cpos=cpos)
+
+###############################################################################
+# Here is the example of plotting sin wave.
+
+algo = pv.MultipleLinesSource(points=[[0.0, 0.0, 0.0], [0.0, np.sin(2.0 * np.pi / 200 * 1), 0.0]])
+
+pl = pv.Plotter()
+actor = pl.add_mesh(algo.output)
+
+def callback(step):
+    algo.points.append([0.0, np.sin(2.0 * np.pi / 200 * (step + 1)), 0.0])
+
+
+pl.add_timer_event(max_steps=200, duration=500, callback=callback)
+
+cpos = [(0.0, 0.0, 10.0), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
+pl.show(cpos=cpos)
+
+
