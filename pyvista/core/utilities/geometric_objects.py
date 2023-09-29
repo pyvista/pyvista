cd3c2ffa430bf4951a74082fd24fc31923ee1e76
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-09-29 18:29:30 +0000

    better examples

--- pyvista/core/utilities/geometric_objects.py
+++ pyvista/core/utilities/geometric_objects.py
@@ -448,29 +448,35 @@ def SolidSphere(
     Create a solid sphere.
 
     >>> import pyvista as pv
+    >>> import numpy as np
     >>> solid_sphere = pv.SolidSphere()
     >>> solid_sphere.plot(show_edges=True)
 
-    A solid sphere is 3D in comparison to the 2d `pyvista.Sphere`
+    A solid sphere is 3D in comparison to the 2d `pyvista.Sphere`.
+    Generate a solid hemi-sphere to see the internal structure.
 
     >>> isinstance(solid_sphere, pv.UnstructuredGrid)
     True
-    >>> clipped_sphere = solid_sphere.clip()
-    >>> clipped_sphere.plot(show_edges=True)
+    >>> partial_solid_sphere = pv.SolidSphere(
+    ...     start_theta=180, end_theta=360
+    ... )
+    >>> partial_solid_sphere.plot(show_edges=True)
 
-    A partial solid sphere can also be generated.
-    A lower resolution is used to see the cell structure.
+    To see the cell structure inside the solid sphere,
+    only 1/4 of the sphere is generate. The are cells exploded
+    and colored by radial position.
 
     >>> partial_solid_sphere = pv.SolidSphere(
-    ...     inner_radius=0.25,
-    ...     outer_radius=0.5,
-    ...     start_theta=0,
-    ...     end_theta=45,
+    ...     start_theta=180,
+    ...     end_theta=360,
     ...     start_phi=0,
     ...     end_phi=90,
-    ...     radius_resolution=3,
+    ...     radius_resolution=5,
     ...     theta_resolution=8,
     ...     phi_resolution=8,
+    ... )
+    >>> partial_solid_sphere["cell_radial_pos"] = np.linalg.norm(
+    ...     partial_solid_sphere.cell_centers().points, axis=-1
     ... )
     >>> partial_solid_sphere.explode(1).plot()
 
