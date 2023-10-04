189e53d2a7275aa53024107ae68e2bdde44b3d63
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-04 13:28:19 +0900

    Apply suggestions from code review

--- tests/core/test_utilities.py
+++ tests/core/test_utilities.py
@@ -750,13 +750,13 @@ def test_cartesian_to_spherical():
     points = np.random.random((1000, 3))
     x, y, z = points.T
     r, theta, phi = pv.cartesian_to_spherical(x, y, z)
-    assert np.allclose(pv.spherical_to_cartesian(r, theta, phi), points)
+    assert np.allclose(pv.spherical_to_cartesian(r, phi, theta), points)
 
 
 def test_spherical_to_cartesian():
     points = np.random.random((1000, 3))
     r, theta, phi = points.T
-    x, y, z = pv.spherical_to_cartesian(r, theta, phi)
+    x, y, z = pv.spherical_to_cartesian(r, phi, theta)
     assert np.allclose(pv.cartesian_to_spherical(x, y, z), points)
 
 
