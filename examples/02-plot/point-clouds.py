a9c27b847741a9e9ba48db7f5c797a203729ade8
Christos Tsolakis <christos.tsolakis@kitware.com>
2023-10-04 16:36:45 -0400

    docs: use static images in examples that interactive does not look right

--- examples/02-plot/point-clouds.py
+++ examples/02-plot/point-clouds.py
@@ -13,6 +13,11 @@ import numpy as np
 import pyvista as pv
 from pyvista import examples
 
+# sphinx_gallery_start_ignore
+# point gaussian does not work in interactive plots
+PYVISTA_GALLERY_FORCE_STATIC_IN_DOCUMENT = True
+# sphinx_gallery_end_ignore
+
 ###############################################################################
 # Compare the Plotting methods
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
