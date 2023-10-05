8cabff8d84aab89fb3d177a64653cc610223b312
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-05 10:35:13 +0900

    Apply suggestions from code review

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -206,7 +206,7 @@ add_module_names = False
 # NOTE: if these are changed, then doc/intersphinx/update.sh
 # must be changed accordingly to keep auto-updated mappings working
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.11', (None, '../intersphinx/python-objects.inv')), # Pin Python 3.11. See #5018.
+    'python': ('https://docs.python.org/3.11', (None, '../intersphinx/python-objects.inv')), # Pin Python 3.11. See https://github.com/pyvista/pyvista/pull/5018 .
     'scipy': (
         'https://docs.scipy.org/doc/scipy/',
         (None, '../intersphinx/scipy-objects.inv'),
