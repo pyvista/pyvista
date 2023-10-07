7834cf5710ce204c8c49cd648fd565ed374a4896
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 21:17:54 +0900

    Fix docstring error

--- pyvista/core/utilities/geometric_sources.py
+++ pyvista/core/utilities/geometric_sources.py
@@ -341,16 +341,14 @@ class CylinderSource(_vtk.vtkCylinderSource):
 
     >>> import pyvista as pv
     >>> pl = pv.Plotter()
-    >>> _ = pl.add_mesh(
-    ...     pyvista.CylinderSource(), show_edges=True, line_width=5
-    ... )
+    >>> _ = pl.add_mesh(pv.CylinderSource(), show_edges=True, line_width=5)
     >>> pl.show()
 
     Visualize the output of :class:`CylinderSource` in a 3D plot.
 
     >>> pl = pv.Plotter()
     >>> _ = pl.add_mesh(
-    ...     pyvista.CylinderSource().output, show_edges=True, line_width=5
+    ...     pv.CylinderSource().output, show_edges=True, line_width=5
     ... )
     >>> pl.show()
 
