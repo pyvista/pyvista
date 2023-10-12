47474e612c55a2784c554c6beff29d8705a29c40
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-13 08:38:44 +0900

    Update test_text.py

--- tests/plotting/test_text.py
+++ tests/plotting/test_text.py
@@ -141,7 +141,7 @@ def test_property_justification_horizontal(prop, justification):
     prop = pv.TextProperty(justification_horizontal=justification[0])
     assert prop.GetJustificationAsString().lower() == justification[1]
     assert prop.justification_horizontal == justification[0]
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='Should be either "left", "center" or "right".'):
         prop.justification_horizontal = "invalid"
 
 
@@ -155,5 +155,5 @@ def test_property_justification_vertical(prop, justification):
     prop = pv.TextProperty(justification_vertical=justification[0])
     assert prop.GetVerticalJustificationAsString().lower() == justification[1]
     assert prop.justification_vertical == justification[0]
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='Should be either "bottom", "center" or "top".'):
         prop.justification_horizontal = "invalid"
