42b820f80b7105e8aebce1ea5b0ac16886e10225
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-10-20 18:17:30 +0000

    use kwargs

--- pyvista/plotting/themes.py
+++ pyvista/plotting/themes.py
@@ -136,8 +136,8 @@ def set_plot_theme(theme):
 # Mostly from https://stackoverflow.com/questions/56579348/how-can-i-force-subclasses-to-have-slots
 class _ForceSlots(type):
     @classmethod
-    def __prepare__(metaclass, name, bases, **kwds):
-        super_prepared = super().__prepare__(metaclass, name, bases, **kwds)
+    def __prepare__(metaclass, name, bases, **kwargs):
+        super_prepared = super().__prepare__(metaclass, name, bases, **kwargs)
         super_prepared['__slots__'] = ()
         return super_prepared
 
