090f102e6bc0173d704c2a3e8988a7ecc98c739d
Erik Bedard <bedarder@gmail.com>
2023-10-24 16:14:34 -0700

    Update docs

--- pyvista/core/input_validation/validate.py
+++ pyvista/core/input_validation/validate.py
@@ -510,14 +510,12 @@ def validate_arrayNx3(arr, /, *, reshape=True, **kwargs):
 
     >>> import pyvista.core.input_validation as valid
     >>> valid.validate_arrayNx3(((1, 2, 3), (4, 5, 6)))
-
     array([[1, 2, 3],
            [4, 5, 6]])
 
     One-dimensional 3-element arrays are automatically reshaped to 2D.
 
     >>> valid.validate_arrayNx3([1, 2, 3])
-
     array([[1, 2, 3]])
 
     Add additional constraints.
@@ -525,7 +523,6 @@ def validate_arrayNx3(arr, /, *, reshape=True, **kwargs):
     >>> valid.validate_arrayNx3(
     ...     ((1, 2, 3), (4, 5, 6)), must_be_in_range=[0, 10]
     ... )
-
     array([[1, 2, 3],
            [4, 5, 6]])
 
