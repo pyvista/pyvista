401a321cec8c6ded022928b4a7ed1b7b07e8cf39
Jan Margeta <jmargeta@gmail.com>
2023-11-06 01:54:52 +0100

    Refer to the contouring example

--- pyvista/core/filters/image_data.py
+++ pyvista/core/filters/image_data.py
@@ -841,7 +841,7 @@ class ImageDataFilters(DataSetFilters):
 
         Examples
         --------
-        See :ref:`surface_nets_example` for a full example using this filter.
+        See :ref:`contouring_example` for a full example using this filter.
         """
         if not hasattr(_vtk, 'vtkSurfaceNets3D'):  # pragma: no cover
             from pyvista.core.errors import VTKVersionError
