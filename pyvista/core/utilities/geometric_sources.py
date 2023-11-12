9d250f691331c7efd73252b7eabf65892dd9ae6e
Tetsuo Koyama <tkoyama010@gmail.com>
2023-11-12 15:12:45 +0900

    Fix import error

--- pyvista/core/utilities/geometric_sources.py
+++ pyvista/core/utilities/geometric_sources.py
@@ -4,11 +4,13 @@ Also includes some pure-python helpers.
 
 """
 from typing import Sequence
+import warnings
 
 import numpy as np
 
 import pyvista
 from pyvista.core import _vtk_core as _vtk
+from pyvista.core.errors import PyVistaDeprecationWarning
 from pyvista.core.utilities.misc import no_new_attr
 
 from .arrays import _coerce_pointslike_arg
