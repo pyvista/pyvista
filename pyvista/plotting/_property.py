270e2bdf1a636ca6d41d834622e99bb7d4d9a116
Tetsuo Koyama <tkoyama010@gmail.com>
2023-11-18 22:44:46 +0900

    Update pyvista/plotting/_property.py

--- pyvista/plotting/_property.py
+++ pyvista/plotting/_property.py
@@ -415,6 +415,28 @@ class Property(_vtk.vtkProperty):
         Edge opacity of the mesh. A single float value that will be applied globally
         edge opacity of the mesh and uniformly applied everywhere. Between 0 and 1.
 
+ 
+        Examples
+        --------
+        Set edge opacity to ``0.5``.
+
+        >>> import pyvista as pv
+        >>> prop = pv.Property()
+        >>> prop.show_edges = True
+        >>> prop.edge_opacity = 0.5
+        >>> prop.edge_opacity
+        0.5
+
+        Visualize default edge opacity of ``1.0``.
+
+        >>> prop.edge_opacity = 1.0
+        >>> prop.plot()
+
+        Visualize opacity of ``0.1``.
+
+        >>> prop.opacity = 0.1
+        >>> prop.plot()
+
         """
         if vtk_version_info < (9, 3):
             return 1.0
