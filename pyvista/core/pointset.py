39620c142c34511168fe198f037f7345e4b5da26
Darik Gamble <darik.gamble@gmail.com>
2023-11-26 11:39:37 -0500

    fix n_faces_strict doc example

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -1128,7 +1128,7 @@ class PolyData(_vtk.vtkPolyData, _PointSet, PolyDataFilters):
         --------
         Create a mesh with one face and one line
         >>> import pyvista as pv
-        >>> mesh = PolyData(
+        >>> mesh = pv.PolyData(
         ...     [(0.0, 0, 0), (1, 0, 0), (0, 1, 0)],
         ...     faces=[3, 0, 1, 2],
         ...     lines=[2, 0, 1],
