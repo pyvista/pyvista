03970ffa9a66d64fe01a602b1fb067ed4a4e45fd
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-11-28 17:16:41 +0000

    adjust examples and add multiple cell types to examples

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -574,27 +574,31 @@ class PolyData(_vtk.vtkPolyData, _PointSet, PolyDataFilters):
     >>> vtkobj = vtk.vtkPolyData()
     >>> mesh = pv.PolyData(vtkobj)
 
-    Initialize from just vertices.
+    Initialize from just points, creating vertices
 
-    >>> vertices = np.array(
-    ...     [[0, 0, 0], [1, 0, 0], [1, 0.5, 0], [0, 0.5, 0]]
-    ... )
-    >>> mesh = pv.PolyData(vertices)
+    >>> points = np.array([[0, 0, 0], [1, 0, 0], [1, 0.5, 0], [0, 0.5, 0]])
+    >>> mesh = pv.PolyData(points)
 
-    Initialize from vertices and faces.
+    Initialize from points and faces, creating polygonal faces.
 
     >>> faces = np.hstack([[3, 0, 1, 2], [3, 0, 3, 2]])
-    >>> mesh = pv.PolyData(vertices, faces)
+    >>> mesh = pv.PolyData(points, faces)
 
-    Initialize from vertices and lines.
+    Initialize from points and lines.
 
     >>> lines = np.hstack([[2, 0, 1], [2, 1, 2]])
-    >>> mesh = pv.PolyData(vertices, lines=lines)
+    >>> mesh = pv.PolyData(points, lines=lines)
 
-    Initialize from vertices and triangle strips.
+    Initialize from points and triangle strips.
 
     >>> strips = np.hstack([[4, 0, 1, 3, 2]])
-    >>> mesh = pv.PolyData(vertices, strips=strips)
+    >>> mesh = pv.PolyData(points, strips=strips)
+
+    It is also possible to create with multiple cell types.
+
+    >>> verts = [1, 0]
+    >>> lines = [2, 1, 2]
+    >>> mesh = pv.PolyData(points, verts=verts, lines=lines)
 
     Initialize from a filename.
 
