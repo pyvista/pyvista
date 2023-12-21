8e3e1866a4a78830949782ed7a4f611669a46265
Tetsuo Koyama <tkoyama010@gmail.com>
2023-12-22 08:56:48 +0900

    Update plotter.py

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -2791,7 +2791,6 @@ class BasePlotter(PickingHelper, WidgetHelper):
                 opacity=vertex_opacity,
                 lighting=lighting,
                 render=False,
-                show_vertices=False,
             )
 
         self.add_actor(
