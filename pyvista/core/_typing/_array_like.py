7267326e39abc281c4cdc4d43671c295746576e8
user27182 <89109579+user27182@users.noreply.github.com>
2023-12-21 11:21:52 -0700

    Update array-like definitions

--- pyvista/core/_typing/_array_like.py
+++ pyvista/core/_typing/_array_like.py
@@ -1,4 +1,4 @@
-"""Generic array type definitions.
+"""Generic array-like type definitions.
 
 Definitions here are loosely based on code in numpy._typing._array_like.
 Some key differences include:
@@ -17,17 +17,14 @@ Some key differences include:
   Here they are excluded (i.e. scalars are not considered to be arrays).
 
 """
-from typing import Any, Sequence, TypeVar, Union
+from typing import Any, Sequence, TypeVar, Union, TypeAlias
 
 import numpy as np
 
-from ._dtype import _DType, _DTypeScalar
-
-# Similar definition to numpy.NDArray, but here we use a specialized TypeVar
-# and helper class to support using generics with builtin types (e.g. int, float)
-NumpyArray = np.ndarray[Any, np.dtype[_DType[_DTypeScalar]]]
-
+# Create alias of npt.NDArray
 _T = TypeVar("_T")
+NumpyArray: TypeAlias = np.ndarray[Any, np.dtype[_T]]
+
 _FiniteNestedSequence = Union[  # Note: scalar types are excluded
     Sequence[_T],
     Sequence[Sequence[_T]],
@@ -36,28 +33,28 @@ _FiniteNestedSequence = Union[  # Note: scalar types are excluded
 ]
 
 _ArrayLike = Union[
-    NumpyArray[_DTypeScalar],
-    _FiniteNestedSequence[_DTypeScalar],
-    _FiniteNestedSequence[NumpyArray[_DTypeScalar]],
+    NumpyArray[_T],
+    _FiniteNestedSequence[_T],
+    _FiniteNestedSequence[NumpyArray[_T]],
 ]
 
 _ArrayLike1D = Union[
-    NumpyArray[_DTypeScalar],
-    Sequence[_DTypeScalar],
-    Sequence[NumpyArray[_DTypeScalar]],
+    NumpyArray[_T],
+    Sequence[_T],
+    Sequence[NumpyArray[_T]],
 ]
 _ArrayLike2D = Union[
-    NumpyArray[_DTypeScalar],
-    Sequence[Sequence[_DTypeScalar]],
-    Sequence[Sequence[NumpyArray[_DTypeScalar]]],
+    NumpyArray[_T],
+    Sequence[Sequence[_T]],
+    Sequence[Sequence[NumpyArray[_T]]],
 ]
 _ArrayLike3D = Union[
-    NumpyArray[_DTypeScalar],
-    Sequence[Sequence[Sequence[_DTypeScalar]]],
-    Sequence[Sequence[Sequence[NumpyArray[_DTypeScalar]]]],
+    NumpyArray[_T],
+    Sequence[Sequence[Sequence[_T]]],
+    Sequence[Sequence[Sequence[NumpyArray[_T]]]],
 ]
 _ArrayLike4D = Union[
-    NumpyArray[_DTypeScalar],
-    Sequence[Sequence[Sequence[Sequence[_DTypeScalar]]]],
-    Sequence[Sequence[Sequence[Sequence[NumpyArray[_DTypeScalar]]]]],
+    NumpyArray[_T],
+    Sequence[Sequence[Sequence[Sequence[_T]]]],
+    Sequence[Sequence[Sequence[Sequence[NumpyArray[_T]]]]],
 ]
