83a33301d3b224c118834bc13fca8cb6b64aee8e
user27182 <89109579+user27182@users.noreply.github.com>
2023-12-28 15:51:03 -0700

    Rename _core.py -> _aliases.py

--- pyvista/core/_typing/__init__.py
+++ pyvista/core/_typing/__init__.py
@@ -1,4 +1,4 @@
 """Type aliases for type hints."""
 # flake8: noqa: F401
+from ._aliases import Array, BoundsLike, Matrix, Number, TransformLike, Vector
 from ._array_like import NumpyArray
-from ._core import Array, BoundsLike, Matrix, Number, TransformLike, Vector
