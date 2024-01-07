bb653b2be94163b01302a6e494a453a4b58a31f1
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-07 19:51:36 +0900

    Update tools.py

--- pyvista/plotting/tools.py
+++ pyvista/plotting/tools.py
@@ -385,8 +385,8 @@ def create_axes_orientation_box(
         axes_actor.SetZMinusFaceText(f"-{zlabel}")
     axes_actor.SetFaceTextVisibility(not labels_off)
     axes_actor.SetTextEdgesVisibility(show_text_edges)
-    #   https://github.com/pyvista/pyvista/pull/5382
-    #   axes_actor.GetTextEdgesProperty().SetColor(edge_color.float_rgb)
+    # https://github.com/pyvista/pyvista/pull/5382
+    # axes_actor.GetTextEdgesProperty().SetColor(edge_color.float_rgb)
     axes_actor.GetTextEdgesProperty().SetLineWidth(line_width)
     axes_actor.GetXPlusFaceProperty().SetColor(x_color.float_rgb)
     axes_actor.GetXMinusFaceProperty().SetColor(x_color.float_rgb)
