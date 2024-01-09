d75c401051088a36b6e17175ed15bd8aa2e5a77d
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-09 22:44:08 +0900

    Update render_window_interactor.py

--- pyvista/plotting/render_window_interactor.py
+++ pyvista/plotting/render_window_interactor.py
@@ -761,71 +761,8 @@ class RenderWindowInteractor:
         >>> plotter.enable_parallel_projection()
         >>> plotter.enable_2d_style()
         >>> plotter.show()  # doctest:+SKIP
+
         """
-        self._style = 'TrackballCamera'
-        self._style_class = None
-        self.update_style()
-
-        def _left_button_press_callback(_obj, event):  # noqa: ARG001 # pragma: no cover
-            """Left button press behavior."""
-            if self.interactor.GetControlKey():
-                self._style_class.StartSpin()
-            elif self.interactor.GetShiftKey():
-                self._style_class.StartDolly()
-            else:
-                self._style_class.StartPan()
-            self._style_class.OnLeftButtonDown()
-
-        def _left_button_release_callback(_obj, event):  # noqa: ARG001  # pragma: no cover
-            """Turn off all left button behavior."""
-            self._style_class.EndSpin()
-            self._style_class.EndPan()
-            self._style_class.EndDolly()
-            self._style_class.OnLeftButtonUp()
-
-        def _middle_button_press_callback(_obj, event):  # noqa: ARG001  # pragma: no cover
-            """Middle button press behavior."""
-            if self.interactor.GetControlKey():
-                self._style_class.StartPan()
-            elif self.interactor.GetShiftKey():
-                self._style_class.StartDolly()
-            else:
-                self._style_class.StartSpin()
-            self._style_class.OnMiddleButtonDown()
-
-        def _middle_button_release_callback(_obj, event):  # noqa: ARG001  # pragma: no cover
-            """Turn off all middle button behavior."""
-            self._style_class.EndSpin()
-            self._style_class.EndPan()
-            self._style_class.EndDolly()
-            self._style_class.OnMiddleButtonUp()
-
-        def _right_button_press_callback(_obj, event):  # noqa: ARG001  # pragma: no cover
-            """Right button press behavior."""
-            if self.interactor.GetControlKey():
-                self._style_class.StartRotate()
-            elif self.interactor.GetShiftKey():
-                # https://github.com/pyvista/pyvista/pull/5336#discussion_r1426060132
-                self._style_class.StartDolly()  # ParaView does a different type of zooming
-            else:
-                self._style_class.StartDolly()
-            self._style_class.OnRightButtonDown()
-
-        def _right_button_release_callback(_obj, event):  # noqa: ARG001  # pragma: no cover
-            """Turn off all right button behavior."""
-            self._style_class.EndRotate()
-            self._style_class.EndDolly()
-            self._style_class.OnRightButtonUp()
-
-        callback = partial(try_callback, _left_button_press_callback)
-        self._style_class.add_observer('LeftButtonPressEvent', callback)
-
-        callback = partial(try_callback, _left_button_release_callback)
-        self._style_class.add_observer('LeftButtonReleaseEvent', callback)
-
-        callback = partial(try_callback, _middle_button_press_callback)
-        self._style_class.add_observer('MiddleButtonPressEvent', callback)
-
         self.enable_custom_trackball_style(
             left="pan",
             shift_left="dolly",
