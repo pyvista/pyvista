3d1c2f38d65815d9e12a019568cb9dafe841c6d3
Tetsuo Koyama <tkoyama010@gmail.com>
2024-01-31 15:43:43 +0900

    Update geometric_sources.py

--- pyvista/core/utilities/geometric_sources.py
+++ pyvista/core/utilities/geometric_sources.py
@@ -1065,7 +1065,7 @@ class PolygonSource(_vtk.vtkRegularPolygonSource):
     Create an 8 sided polygon.
 
     >>> import pyvista as pv
-    >>> source = pv.PolygonSourc(n_sides=8)
+    >>> source = pv.PolygonSource(n_sides=8)
     >>> source.output.plot(show_edges=True, line_width=5)
     """
 
