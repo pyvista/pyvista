b0f4f7b052db2efbc7fe4cd7903ee0c3338835fe
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-07 07:38:15 +0900

    Update widgets.py

--- pyvista/plotting/widgets.py
+++ pyvista/plotting/widgets.py
@@ -2779,9 +2779,9 @@ class WidgetHelper:
         >>> import pyvista as pv
         >>> sphere = pv.Sphere()
         >>> plotter = pv.Plotter(shape=(1, 2))
-        >>> _ = plotter.add_mesh(sphere)
+        >>> _ = plotter.add_mesh(sphere, show_edges=True)
         >>> plotter.subplot(0, 1)
-        >>> _ = plotter.add_mesh(sphere)
+        >>> _ = plotter.add_mesh(sphere, show_edges=True)
         >>> _ = plotter.add_camera3d_widget()
         >>> plotter.show(cpos=plotter.camera_position)
 
