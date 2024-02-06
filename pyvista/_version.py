29a6c87802b1aca52218f2db4d4077680bae272a
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-06 19:07:20 +0900

    🚀 Patch Release 0.43.3

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -13,7 +13,7 @@ Denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 43, 2
+version_info = 0, 43, 3
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
