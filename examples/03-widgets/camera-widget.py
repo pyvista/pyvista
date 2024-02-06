3cbef32abb2adb1160148ef12f63b8f7b457774c
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-06 21:14:34 +0900

    Update examples/03-widgets/camera-widget.py

--- examples/03-widgets/camera-widget.py
+++ examples/03-widgets/camera-widget.py
@@ -17,24 +17,16 @@ import pyvista as pv
 
 sphere = pv.Sphere()
 
-# Renderers and one render window
 plotter = pv.Plotter(window_size=[600, 300], shape=(1, 2))
 plotter.add_mesh(sphere)
 plotter.subplot(0, 1)
 plotter.add_mesh(sphere)
+
 camera3d_renderer = plotter.renderer
-
 render_window = plotter.render_window
-
-# An interactor
 interactor = vtk.vtkRenderWindowInteractor()
 interactor.SetRenderWindow(render_window)
-
 plotter.add_camera3d_widget(interactor)
-
-# Render
 interactor.Initialize()
 render_window.Render()
-
-# Begin mouse interaction
 interactor.Start()
