b9f51ca5c887df49cbcd42c99a26fd267b05eb26
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-09 00:57:40 +0900

    Update examples/01-filter/boundary-cell-quality.py

--- examples/01-filter/boundary-cell-quality.py
+++ examples/01-filter/boundary-cell-quality.py
@@ -4,19 +4,15 @@ reader = vtk.vtkXMLUnstructuredGridReader()
 reader.SetFileName("Data/can.vtu")
 reader.Update()
 
-boundaryMeshQuality = vtk.vtkBoundaryMeshQuality()
-boundaryMeshQuality.SetInputConnection(reader.GetOutputPort())
-boundaryMeshQuality.Update()
-output = boundaryMeshQuality.GetOutput()
+quality = vtk.vtkBoundaryMeshQuality()
+quality.SetInputConnection(reader.GetOutputPort())
+quality.Update()
+output = quality.GetOutput()
 
 distance1 = output.GetCellData().GetArray("DistanceFromCellCenterToFaceCenter")
 distance2 = output.GetCellData().GetArray("DistanceFromCellCenterToFacePlane")
 angle = output.GetCellData().GetArray("AngleFaceNormalAndCellCenterToFaceCenterVector")
 
-distanceEpsilon = 1e-6
 range1 = distance1.GetRange()
-
 range2 = distance2.GetRange()
-
-angleEpsilon = 1e-4
 range3 = angle.GetRange()
