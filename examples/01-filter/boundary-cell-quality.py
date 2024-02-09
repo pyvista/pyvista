0df64e515977595eec924f5d97ae3f1fd675dcf5
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-09 14:45:14 +0900

    Update examples/01-filter/boundary-cell-quality.py

--- examples/01-filter/boundary-cell-quality.py
+++ examples/01-filter/boundary-cell-quality.py
@@ -1,9 +1,21 @@
-import pyvista as pv
+import vtk
 
-mesh = pv.read("Data/can.vtu")
+reader = vtk.vtkXMLUnstructuredGridReader()
+reader.SetFileName("Data/can.vtu")
+reader.Update()
+print(reader.GetOutput())
+# TODO @tkoyama010 check the warning reason of read function.
+# mesh = pv.read("Data/can.vtu")
 
-output = mesh.compute_boundary_mesh_quality()
-
-distance1 = output["DistanceFromCellCenterToFaceCenter"]
-distance2 = output["DistanceFromCellCenterToFacePlane"]
-angle = output["AngleFaceNormalAndCellCenterToFaceCenterVector"]
+# quality = vtk.vtkBoundaryMeshQuality()
+# quality.SetInputConnection(reader.GetOutputPort())
+# quality.Update()
+# output = quality.GetOutput()
+#
+# distance1 = output.GetCellData().GetArray("DistanceFromCellCenterToFaceCenter")
+# distance2 = output.GetCellData().GetArray("DistanceFromCellCenterToFacePlane")
+# angle = output.GetCellData().GetArray("AngleFaceNormalAndCellCenterToFaceCenterVector")
+#
+# range1 = distance1.GetRange()
+# range2 = distance2.GetRange()
+# range3 = angle.GetRange()
