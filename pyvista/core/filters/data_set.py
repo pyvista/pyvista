9dc1dffd7150c84f3ae94c3b9571dbd955753024
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-11 19:09:27 +0900

    Apply suggestions from code review

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -5541,17 +5541,17 @@ class DataSetFilters:
         >>> mesh = examples.download_can_crushed_vtu()
         >>> cqual = mesh.compute_boundary_mesh_quality()
         >>> plotter = pv.Plotter(shape=(2, 2))
-        >>> plotter.add_mesh(mesh, show_edges=True)
+        >>> _ = plotter.add_mesh(mesh, show_edges=True)
         >>> plotter.subplot(1, 0)
-        >>> plotter.add_mesh(
+        >>> _ = plotter.add_mesh(
         ...     cqual, scalars="DistanceFromCellCenterToFaceCenter"
         ... )
         >>> plotter.subplot(0, 1)
-        >>> plotter.add_mesh(
+        >>> _ = plotter.add_mesh(
         ...     cqual, scalars="DistanceFromCellCenterToFacePlane"
         ... )
         >>> plotter.subplot(1, 1)
-        >>> plotter.add_mesh(
+        >>> _ = plotter.add_mesh(
         ...     cqual,
         ...     scalars="AngleFaceNormalAndCellCenterToFaceCenterVector",
         ... )
