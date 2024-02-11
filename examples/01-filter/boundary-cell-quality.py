a71bf1949445492bca4f7ff39310128b56318834
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-11 11:46:06 +0900

    Upate examples/01-filter/boundary-cell-quality.py

--- examples/01-filter/boundary-cell-quality.py
+++ examples/01-filter/boundary-cell-quality.py
@@ -2,8 +2,14 @@ import pyvista as pv
 
 mesh = pv.read("Data/can.vtu")
 
-output = mesh.compute_boundary_mesh_quality()
-
-distance1 = output["DistanceFromCellCenterToFaceCenter"]
-distance2 = output["DistanceFromCellCenterToFacePlane"]
-angle = output["AngleFaceNormalAndCellCenterToFaceCenterVector"]
+# cqual = mesh.compute_boundary_mesh_quality()
+#
+# plotter = pv.Plotter(shape=(2, 2))
+# plotter.add_mesh(mesh)
+# plotter.subplot(1, 0)
+# plotter.add_mesh(cqual.copy(), scalars="DistanceFromCellCenterToFaceCenter")
+# plotter.subplot(0, 1)
+# plotter.add_mesh(cqual.copy(), scalars="DistanceFromCellCenterToFacePlane")
+# plotter.subplot(1, 1)
+# plotter.add_mesh(cqual.copy(), scalars="AngleFaceNormalAndCellCenterToFaceCenterVector")
+# plotter.show()
