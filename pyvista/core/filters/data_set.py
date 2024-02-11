48e5f0ff921aa43217104e31f7a8f10f3ed04c7c
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-11 18:23:35 +0900

    Delete examples/01-filter/boundary-cell-quality.py

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -5533,6 +5533,29 @@ class DataSetFilters:
             Dataset with the computed metrics on the boundary faces of a mesh.
             ``cell_data`` as the ``"CellQuality"`` array.
 
+        Examples
+        --------
+        >>> import pyvista as pv
+        >>> from pyvista import examples
+        >>> mesh = examples.download_can_crushed_vtu()
+        >>> cqual = mesh.compute_boundary_mesh_quality()
+        >>> plotter = pv.Plotter(shape=(2, 2))
+        >>> plotter.add_mesh(mesh, show_edges=True)
+        >>> plotter.subplot(1, 0)
+        >>> plotter.add_mesh(
+        ...     cqual, scalars="DistanceFromCellCenterToFaceCenter"
+        ... )
+        >>> plotter.subplot(0, 1)
+        >>> plotter.add_mesh(
+        ...     cqual, scalars="DistanceFromCellCenterToFacePlane"
+        ... )
+        >>> plotter.subplot(1, 1)
+        >>> plotter.add_mesh(
+        ...     cqual,
+        ...     scalars="AngleFaceNormalAndCellCenterToFaceCenterVector",
+        ... )
+        >>> plotter.show()
+
         """
         alg = _vtk.vtkBoundaryMeshQuality()
         alg.SetInputData(self)
