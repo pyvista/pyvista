67d34a352a9ab151b64abca00c7f328a60bd9f17
user27182 <89109579+user27182@users.noreply.github.com>
2024-02-14 21:56:05 -0700

    Update _type_guards.py

--- pyvista/core/_typing_core/_type_guards.py
+++ pyvista/core/_typing_core/_type_guards.py
@@ -1,31 +1,30 @@
 """Type guards for checking array-like type definitions."""
 
-from itertools import islice
 from typing import Any, Iterable, Sequence, Tuple, Type, TypeVar, cast
 
 import numpy as np
+from typing_extensions import TypeGuard
 
 from ._array_like import _ArrayLikeOrScalar, _NumberType
 
-_BuiltinNumberType = TypeVar('_BuiltinNumberType', float, int, bool, covariant=True)
-
-# TODO: import and return TypeGuard instead of bool for python >= 3.10
-#  (requires typing_extensions for python < 3.10)
+_BuiltinNumberType = TypeVar('_BuiltinNumberType', float, int, bool)
 
 
-def _is_Number(array: _ArrayLikeOrScalar[_NumberType]) -> bool:
+def _is_Number(array: _ArrayLikeOrScalar[_NumberType]) -> TypeGuard[_NumberType]:
     return isinstance(array, (float, int, bool, np.floating, np.integer, np.bool_))
 
 
-def _is_NumberSequence(array: _ArrayLikeOrScalar[_NumberType], N=None) -> bool:
-    return isinstance(array, (tuple, list)) and _has_element_types(array, (float, int), N=N)
+def _is_NumberSequence(
+    array: _ArrayLikeOrScalar[_NumberType],
+) -> TypeGuard[Sequence[_BuiltinNumberType]]:
+    return isinstance(array, (tuple, list)) and _has_element_types(array, (float, int))
 
 
-def _is_NestedNumberSequence(array: _ArrayLikeOrScalar[_NumberType], N=None) -> bool:
+def _is_NestedNumberSequence(array: _ArrayLikeOrScalar[_NumberType]) -> bool:
     if (
         isinstance(array, (tuple, list))
         and len(array) > 0
-        and all(_is_NumberSequence(subarray, N=N) for subarray in array)
+        and all(_is_NumberSequence(subarray) for subarray in array)
     ):
         # We have the correct type, now check all subarray shapes are equal
         array = cast(Sequence[Sequence[_NumberType]], array)
@@ -34,14 +33,6 @@ def _is_NestedNumberSequence(array: _ArrayLikeOrScalar[_NumberType], N=None) -> 
     return False
 
 
-def _has_element_types(array: Iterable[Any], types: Tuple[Type[Any], ...], N=None) -> bool:
-    """Check that iterable elements have the specified type.
-
-    Parameters
-    ----------
-    N : int
-        Only check the first `N` elements. Can be used to reduce the
-        performance cost of this check. Set to `None` to check all elements.
-    """
-    iterator = islice(array, N)
-    return all(isinstance(item, types) for item in iterator)
+def _has_element_types(array: Iterable[Any], types: Tuple[Type[Any], ...]) -> bool:
+    """Check that iterable elements have the specified type."""
+    return all(isinstance(item, types) for item in array)
