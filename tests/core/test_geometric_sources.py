31eb344e856bb6122e56fa52ccfc42d206314a8c
Tetsuo Koyama <tkoyama010@gmail.com>
2024-02-27 05:17:37 +0900

    Update test_geometric_sources.py

--- tests/core/test_geometric_sources.py
+++ tests/core/test_geometric_sources.py
@@ -7,18 +7,18 @@ from pyvista import examples
 from pyvista.core.utilities.geometric_objects import translate
 
 
-@pytest.mark.needs_vtk_version(9, 3, 0)
 def test_capsule_source():
-    algo = pv.CapsuleSource()
-    assert np.array_equal(algo.center, (0.0, 0.0, 0.0))
-    assert np.array_equal(algo.direction, (1.0, 0.0, 0.0))
-    assert algo.radius == 0.5
-    assert algo.cylinder_length == 1.0
-    assert algo.theta_resolution == 30
-    assert algo.phi_resolution == 30
-    direction = np.random.default_rng().random(3)
-    algo.direction = direction
-    assert np.array_equal(algo.direction, direction)
+    if pv.vtk_version_info < (9, 3):
+        algo = pv.CapsuleSource()
+        assert np.array_equal(algo.center, (0.0, 0.0, 0.0))
+        assert np.array_equal(algo.direction, (1.0, 0.0, 0.0))
+        assert algo.radius == 0.5
+        assert algo.cylinder_length == 1.0
+        assert algo.theta_resolution == 30
+        assert algo.phi_resolution == 30
+        direction = np.random.default_rng().random(3)
+        algo.direction = direction
+        assert np.array_equal(algo.direction, direction)
 
 
 def test_cone_source():
