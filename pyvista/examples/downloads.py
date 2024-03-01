37a591517f8553d298e658552955d94a4477e4ef
user27182 <89109579+user27182@users.noreply.github.com>
2024-02-29 17:34:57 -0700

    Fix return value

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -1706,6 +1706,7 @@ def download_frog_tissue(load=True):  # pragma: no cover
         out = _download_and_read('froggy/frogtissue.mhd', load=load)
         if out.get_data_range() != (0, 29):
             warnings.warn("Frog tissue data was not read correctly by VTK.")
+    return out
 
 
 def download_chest(load=True):  # pragma: no cover
