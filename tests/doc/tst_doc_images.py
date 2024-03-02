07cf98a82923aa18c5907a133fa90b8d7b514bf4
user27182 <89109579+user27182@users.noreply.github.com>
2024-03-01 20:57:45 -0700

    Add tests for doc build images

---
+++ tests/doc/tst_doc_images.py
@@ -0,0 +1,143 @@
+"""Test the images generated from building the documentation."""
+
+from collections import namedtuple
+import glob
+import os
+from typing import Dict
+import warnings
+
+from PIL import Image
+import pytest
+
+import pyvista as pv
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+BUILD_DIR = os.path.join(ROOT_DIR, 'doc', '_build')
+BUILD_IMAGE_CACHE = os.path.join(os.path.dirname(__file__), 'build_image_cache')
+
+
+_TestCaseTuple = namedtuple('_TestCaseTuple', ['filename', 'docs_image_path', 'cached_image_path'])
+
+
+def _get_file_paths(dir_: str, ext: str):
+    """Get all paths of files with a specific extension inside a directory tree."""
+    pattern = os.path.join(dir_, '**', '*.' + ext)
+    file_paths = glob.glob(pattern, recursive=True)
+    return file_paths
+
+
+def _flatten_path(path: str):
+    return '_'.join(os.path.split(path))[1:]
+
+
+def _preprocess_build_images(build_images_dir: str, output_dir: str):
+    """Read png images from the build dir, resize them, and save to flat output dir."""
+    input_paths = _get_file_paths(build_images_dir, ext='png')
+    output_paths = []
+    os.makedirs(output_dir, exist_ok=True)
+    for input_path in input_paths:
+        # input image from the docs may come from a nested directory,
+        # so we flatten the file's relative path
+        output_file_name = _flatten_path(os.path.relpath(input_path, build_images_dir))
+        output_path = os.path.join(output_dir, output_file_name)
+        output_paths.append(output_path)
+
+        # Ensure image size is max 400x400 and save to output
+        im = Image.open(input_path)
+        im.thumbnail(size=(400, 400))
+        im.save(output_path)
+
+    return output_paths
+
+
+def _get_test_image_paths():
+    """Preprocess docs images and return their paths."""
+    images_dir = os.path.join(BUILD_DIR, '_images')
+    test_images_dir = os.path.join(BUILD_DIR, '_images_test')
+    test_image_paths = _preprocess_build_images(images_dir, test_images_dir)
+    return test_image_paths
+
+
+def _generate_test_cases():
+    """Generate a list of image test cases.
+    This function:
+        (1) Generates a list of test images from the docs
+        (2) Generates a list of cached images
+        (3) Merges the two lists together and returns separate test cases to
+            comparing all docs images to all cached images
+    """
+    test_cases_dict: Dict = {}
+
+    def add_to_dict(filepath: str, key: str):
+        # Function for stuffing image paths into a dict.
+        # We use a dict to allow for any entry to be made based on image path alone.
+        # This way, we can defer checking for any mismatch between the cached and docs
+        # images to test time.
+        nonlocal test_cases_dict
+        filename = os.path.basename(filepath)
+        try:
+            test_cases_dict[filename]
+        except KeyError:
+            test_cases_dict[filename] = {}
+        test_cases_dict[filename].setdefault(key, filepath)
+
+    # process test images
+    test_image_paths = _get_test_image_paths()
+    [add_to_dict(path, 'docs') for path in test_image_paths]
+
+    # process cached images
+    cached_image_paths = _get_file_paths(BUILD_IMAGE_CACHE, ext='png')
+    [add_to_dict(path, 'cached') for path in cached_image_paths]
+
+    # flatten dict
+    test_cases_list = []
+    for filename, content in sorted(test_cases_dict.items()):
+        doc = content['docs'] if 'docs' in content else None
+        cache = content['cached'] if 'cached' in content else None
+        test_case = _TestCaseTuple(filename=filename, docs_image_path=doc, cached_image_path=cache)
+        test_cases_list.append(test_case)
+
+    return test_cases_list
+
+
+def pytest_generate_tests(metafunc):
+    """Generate parametrized tests."""
+    if 'test_case' in metafunc.fixturenames:
+        # Generate a separate test case for each image being tested
+        test_cases = _generate_test_cases()
+        ids = [case.filename for case in test_cases]
+        metafunc.parametrize('test_case', test_cases, ids=ids)
+
+
+def test_docs(test_case):
+    filename, docs_image_path, cached_image_path = test_case
+    if docs_image_path is None or cached_image_path is None:
+        if docs_image_path is None:
+            exists = 'cache'
+            missing = 'docs'
+        else:
+            exists = 'docs'
+            missing = 'cache'
+        pytest.fail(
+            f"Test setup failed for test case:\n"
+            f"\t{filename}\n"
+            f"The image exists in the {exists}, but is missing from the {missing}."
+        )
+
+    docs_image = pv.read(docs_image_path)
+    cached_image = pv.read(cached_image_path)
+    error = pv.compare_images(docs_image, cached_image)
+
+    allowed_error = 500.0
+    allowed_warning = 200.0
+    if error > allowed_error:
+        pytest.fail(
+            f"{filename} Exceeded image regression error of "
+            f"{allowed_error} with an image error equal to: {error}"
+        )
+    if error > allowed_warning:
+        warnings.warn(
+            f"{filename} Exceeded image regression warning of "
+            f"{allowed_warning} with an image error of "
+            f"{error}"
+        )
