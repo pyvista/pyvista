b4ae2025b3e305491c940a4c1429d4afa6740f14
Tetsuo Koyama <tkoyama010@gmail.com>
2024-03-04 00:47:11 +0900

    Add `trame` integration tests in `examples_trame`

---
+++ examples_trame/tests/test_trame.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import subprocess
+
+import pytest
+
+THIS_PATH = Path(__file__).parent.absolute()
+
+
+@pytest.mark.parametrize(
+    "server_path",
+    [
+        "../advanced/contour.py",
+        "../advanced/custom_ui.py",
+        "../basic/actor_color.py",
+        "../basic/algorithm.py",
+        "../basic/file_viewer.py",
+        "../basic/mesh_scalars.py",
+        "../basic/multi_views.py",
+        "../basic/PyVistaLocalView.py",
+        "../basic/PyVistaRemoteLocalView.py",
+        "../basic/PyVistaRemoteView.py",
+        "../basic/ui_template.py",
+        "../validation/many_actors.py",
+    ],
+)
+def test_serve(server_path):
+    returncode = subprocess.run(
+        ["python", Path(THIS_PATH) / server_path, "--serve", "-t", "1"]
+    ).returncode
+    assert returncode == 0
