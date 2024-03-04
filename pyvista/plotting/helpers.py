dbd38c84136b7eb919f6186b203567e74cdf5121
user27182 <89109579+user27182@users.noreply.github.com>
2024-03-04 14:26:50 -0700

    Update rng for plot_arrows

--- pyvista/plotting/helpers.py
+++ pyvista/plotting/helpers.py
@@ -39,16 +39,17 @@ def plot_arrows(cent, direction, **kwargs):
 
     >>> import numpy as np
     >>> import pyvista as pv
-    >>> cent = np.random.default_rng().random(3)
-    >>> direction = np.random.default_rng().random(3)
+    >>> rng = np.random.default_rng(seed=0)
+    >>> cent = rng.random(3)
+    >>> direction = rng.random(3)
     >>> pv.plot_arrows(cent, direction)
 
     Plot 100 random arrows.
 
     >>> import numpy as np
     >>> import pyvista as pv
-    >>> cent = np.random.default_rng().random((100, 3))
-    >>> direction = np.random.default_rng().random((100, 3))
+    >>> cent = rng.random((100, 3))
+    >>> direction = rng.random((100, 3))
     >>> pv.plot_arrows(cent, direction)
 
     """
