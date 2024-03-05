015a3a84425cc34fb23edf3db8f4197a0cae007b
user27182 <89109579+user27182@users.noreply.github.com>
2024-03-04 17:20:23 -0700

    Update rng for PointSet

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -261,7 +261,7 @@ class PointSet(_vtk.vtkPointSet, _PointSet):
 
     >>> import numpy as np
     >>> import pyvista as pv
-    >>> rng = np.random.default_rng()
+    >>> rng = np.random.default_rng(seed=0)
     >>> points = rng.random((10, 3))
     >>> pset = pv.PointSet(points)
 
