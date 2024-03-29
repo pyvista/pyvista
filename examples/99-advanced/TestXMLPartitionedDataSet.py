6949fc0d29eba12a0f7d3d9416819c3a3a3deafe
Tetsuo Koyama <tkoyama010@gmail.com>
2024-03-29 16:44:55 +0900

    Update TestXMLPartitionedDataSet.py

--- examples/99-advanced/TestXMLPartitionedDataSet.py
+++ examples/99-advanced/TestXMLPartitionedDataSet.py
@@ -6,32 +6,33 @@ from vtkmodules.vtkIOXML import vtkXMLPartitionedDataSetReader
 
 import pyvista as pv
 
-dataset = vtkPartitionedDataSet()
+input_data = vtkPartitionedDataSet()
 
 partition1 = pv.Wavelet(extent=(0, 10, 0, 10, 0, 5))
 partition2 = pv.Wavelet(extent=(0, 10, 0, 10, 5, 10))
 
-dataset.SetPartition(0, partition1)
-dataset.SetPartition(1, partition2)
+input_data.SetPartition(0, partition1)
+input_data.SetPartition(1, partition2)
 
 with tempfile.TemporaryDirectory() as tmpdir:
     file_name = tmpdir + "/testxmlpartds.vtpd"
     w = vtkXMLPartitionedDataSetWriter()
-    w.SetInputData(dataset)
+    w.SetInputData(input_data)
     w.SetFileName(file_name)
     w.Write()
 
     r = vtkXMLPartitionedDataSetReader()
     r.SetFileName(file_name)
     r.Update()
-    dataset_ = r.GetOutputDataObject(0)
+    output_data = r.GetOutputDataObject(0)
 
-number_of_partitions = dataset_.GetNumberOfPartitions()
-assert dataset_.IsA("vtkPartitionedDataSet")
+number_of_partitions = output_data.GetNumberOfPartitions()
+assert output_data.IsA("vtkPartitionedDataSet")
 assert number_of_partitions == 2
 
 for i in range(number_of_partitions):
-    partition = dataset.GetPartition(i)
-    partition_ = dataset_.GetPartition(i)
-    assert partition_.IsA("vtkImageData")
-    assert partition_.GetNumberOfCells() == partition.GetNumberOfCells()
+    assert output_data.GetPartition(i).IsA("vtkImageData")
+    assert (
+        output_data.GetPartition(i).GetNumberOfCells()
+        == input_data.GetPartition(i).GetNumberOfCells()
+    )
