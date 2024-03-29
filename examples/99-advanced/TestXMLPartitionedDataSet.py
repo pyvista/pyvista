e4bb5e683990a58598b3e4017fe89b4ac0c8cbb3
Tetsuo Koyama <tkoyama010@gmail.com>
2024-03-29 15:38:55 +0900

    Update TestXMLPartitionedDataSet.py

--- examples/99-advanced/TestXMLPartitionedDataSet.py
+++ examples/99-advanced/TestXMLPartitionedDataSet.py
@@ -1,57 +1,47 @@
 from pathlib import Path
 
 from vtkmodules import vtkCommonDataModel as dm, vtkImagingCore as ic
-from vtkmodules.test import Testing
 from vtkmodules.util.misc import vtkGetTempDir
 from vtkmodules.vtkIOParallelXML import vtkXMLPartitionedDataSetWriter
 from vtkmodules.vtkIOXML import vtkXMLPartitionedDataSetReader
 
+p = dm.vtkPartitionedDataSet()
 
-class TestXMLPartitionedDataSet(Testing.vtkTest):
+s = ic.vtkRTAnalyticSource()
+s.SetWholeExtent(0, 10, 0, 10, 0, 5)
+s.Update()
 
-    def test(self):
+p1 = dm.vtkImageData()
+p1.ShallowCopy(s.GetOutput())
 
-        p = dm.vtkPartitionedDataSet()
+s.SetWholeExtent(0, 10, 0, 10, 5, 10)
+s.Update()
 
-        s = ic.vtkRTAnalyticSource()
-        s.SetWholeExtent(0, 10, 0, 10, 0, 5)
-        s.Update()
+p2 = dm.vtkImageData()
+p2.ShallowCopy(s.GetOutput())
 
-        p1 = dm.vtkImageData()
-        p1.ShallowCopy(s.GetOutput())
+p.SetPartition(0, p1)
+p.SetPartition(1, p2)
 
-        s.SetWholeExtent(0, 10, 0, 10, 5, 10)
-        s.Update()
+tmpdir = vtkGetTempDir()
+fname = tmpdir + "/testxmlpartds.vtpd"
+w = vtkXMLPartitionedDataSetWriter()
+w.SetInputData(p)
+w.SetFileName(fname)
+w.Write()
 
-        p2 = dm.vtkImageData()
-        p2.ShallowCopy(s.GetOutput())
+r = vtkXMLPartitionedDataSetReader()
+r.SetFileName(fname)
+r.Update()
+o = r.GetOutputDataObject(0)
 
-        p.SetPartition(0, p1)
-        p.SetPartition(1, p2)
+assert o.IsA("vtkPartitionedDataSet")
+np = o.GetNumberOfPartitions()
+assert np == 2
 
-        tmpdir = vtkGetTempDir()
-        fname = tmpdir + "/testxmlpartds.vtpd"
-        w = vtkXMLPartitionedDataSetWriter()
-        w.SetInputData(p)
-        w.SetFileName(fname)
-        w.Write()
-
-        r = vtkXMLPartitionedDataSetReader()
-        r.SetFileName(fname)
-        r.Update()
-        o = r.GetOutputDataObject(0)
-
-        print(o.IsA("vtkPartitionedDataSet"))
-        np = o.GetNumberOfPartitions()
-        assert np == 2
-
-        for i in range(np):
-            d = o.GetPartition(i)
-            d2 = p.GetPartition(i)
-            assert d.IsA("vtkImageData")
-            assert d.GetNumberOfCells() == d2.GetNumberOfCells()
-        Path(fname).unlink()
-
-
-if __name__ == "__main__":
-    Testing.main([(TestXMLPartitionedDataSet, 'test')])
+for i in range(np):
+    d = o.GetPartition(i)
+    d2 = p.GetPartition(i)
+    assert d.IsA("vtkImageData")
+    assert d.GetNumberOfCells() == d2.GetNumberOfCells()
+Path(fname).unlink()
