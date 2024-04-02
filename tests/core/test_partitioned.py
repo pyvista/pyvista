92a22b92e86a8226fbc355ca097f64f595622e12
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-03 07:51:21 +0900

    Update tests/core/test_partitioned.py

--- tests/core/test_partitioned.py
+++ tests/core/test_partitioned.py
@@ -4,8 +4,8 @@ from pyvista import PartitionedDataSet
 
 def test_pop():
     spheres = [pv.Sphere(phi_resolution=i + 3) for i in range(10)]
-    multi = PartitionedDataSet(spheres)
-    assert multi.pop() == spheres[9]
-    assert spheres[9] not in multi
-    assert multi.pop(0) == spheres[0]
-    assert spheres[0] not in multi
+    partitions = PartitionedDataSet(spheres)
+    assert partitions.pop() == spheres[9]
+    assert spheres[9] not in partitions
+    assert partitions.pop(0) == spheres[0]
+    assert spheres[0] not in partitions
