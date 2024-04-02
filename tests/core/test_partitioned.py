fcb3a5be5df658f0deaed0538cb9270dd8ac4556
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-03 08:58:13 +0900

    Update tests/core/test_partitioned.py

--- tests/core/test_partitioned.py
+++ tests/core/test_partitioned.py
@@ -5,6 +5,13 @@ from pyvista import PartitionedDataSet
 from pyvista.core.errors import PartitionedDataSetsNotSupported
 
 
+def test_reverse(sphere):
+    multi = PartitionedDataSet([sphere for i in range(3)])
+    multi.append(pv.Cube())
+    multi.reverse()
+    assert multi[0] == pv.Cube()
+
+
 def test_pop():
     spheres = [pv.Sphere(phi_resolution=i + 3) for i in range(10)]
     partitions = PartitionedDataSet(spheres)
