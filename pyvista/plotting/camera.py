a309b79b823337b3d6761b560ec21adc89e29afa
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-09 08:57:59 +0900

    Update camera.py

--- pyvista/plotting/camera.py
+++ pyvista/plotting/camera.py
@@ -85,7 +85,7 @@ class Camera(_vtk.vtkCamera):
         if trans_count == 1:
             # either but not both are None
             return False
-        if trans_count == 2:  # noqa: SIM102
+        if trans_count == 2:
             if not np.array_equal(this_trans, that_trans):
                 return False
 
