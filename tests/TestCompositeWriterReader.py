95947313f55cb1f19b8df8a63b95e136ff6f88f9
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-11 02:37:48 +0900

    Add composite reader `CompositeDataReader` class

---
+++ tests/TestCompositeWriterReader.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+from vtkmodules import vtkCommonDataModel as dm, vtkImagingCore as ic, vtkIOLegacy as il
+from vtkmodules.test import Testing
+from vtkmodules.util.misc import vtkGetTempDir
+
+
+class TestCompositeWriterReader(Testing.vtkTest):
+
+    def test(self):
+        p = dm.vtkPartitionedDataSet()
+
+        s = ic.vtkRTAnalyticSource()
+        s.SetWholeExtent(0, 10, 0, 10, 0, 5)
+        s.Update()
+
+        p1 = dm.vtkImageData()
+        p1.ShallowCopy(s.GetOutput())
+
+        s.SetWholeExtent(0, 10, 0, 10, 5, 10)
+        s.Update()
+
+        p2 = dm.vtkImageData()
+        p2.ShallowCopy(s.GetOutput())
+
+        p.SetPartition(0, p1)
+        p.SetPartition(1, p2)
+
+        p2 = dm.vtkPartitionedDataSet()
+        p2.ShallowCopy(p)
+
+        c = dm.vtkPartitionedDataSetCollection()
+        c.SetPartitionedDataSet(0, p)
+        c.SetPartitionedDataSet(1, p2)
+
+        tmpdir = vtkGetTempDir()
+        fname = tmpdir + "/testcompowriread.vtk"
+        w = il.vtkCompositeDataWriter()
+        w.SetInputData(c)
+        w.SetFileName(fname)
+        w.Write()
+
+        r = il.vtkCompositeDataReader()
+        r.SetFileName(fname)
+        r.Update()
+        o = r.GetOutputDataObject(0)
+
+        assert o.IsA("vtkPartitionedDataSetCollection")
+        number_of_datasets = o.GetNumberOfPartitionedDataSets()
+        assert number_of_datasets == 2
+
+        for i in range(number_of_datasets):
+            p = o.GetPartitionedDataSet(i)
+            p2 = c.GetPartitionedDataSet(i)
+            assert p.IsA("vtkPartitionedDataSet")
+            assert p.GetNumberOfPartitions() == 2
+            assert p.GetPartition(0).GetNumberOfCells() == p.GetPartition(0).GetNumberOfCells()
+        del r
+        import gc
+
+        gc.collect()
+        Path.unlink(fname)
+
+
+if __name__ == "__main__":
+    Testing.main([(TestCompositeWriterReader, 'test')])
