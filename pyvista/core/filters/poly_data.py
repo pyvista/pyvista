3582a4af980990142c9d7e4bd8c3069a384ac68a
Shannon Keough <shannonjkeough@googlemail.com>
2024-04-22 18:50:18 +1000

    Improve installation instructions for embree in ImportError exception

--- pyvista/core/filters/poly_data.py
+++ pyvista/core/filters/poly_data.py
@@ -2297,13 +2297,15 @@ class PolyDataFilters(DataSetFilters):
             raise NotAllTrianglesError("Input mesh for multi_ray_trace must be all triangles.")
 
         try:
-            import pyembree  # noqa: F401
-            import rtree  # noqa: F401
             import trimesh
+
+            if not trimesh.ray.has_embree:
+                raise ImportError
         except ImportError:
             raise ImportError(
-                "To use multi_ray_trace please install trimesh, rtree and pyembree with:\n"
-                "\tconda install trimesh rtree pyembree",
+                "To use multi_ray_trace please install trimesh, embree (v2.17.7) and pyembree/embreex with:\n"
+                "\tconda install embree=2 trimesh pyembree\nOR\n"
+                "\tpip install trimesh embreex",
             )
 
         origins = np.asarray(origins)
