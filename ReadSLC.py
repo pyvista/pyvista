5b3478e8a767e74a98f51a312292cbf626a9ac8f
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-24 08:37:21 +0900

    Update ReadSLC.py

--- ReadSLC.py
+++ ReadSLC.py
@@ -17,76 +17,70 @@ from vtkmodules.vtkRenderingCore import (
 # noinspection PyUnresolvedReferences
 import vtkmodules.vtkRenderingOpenGL2  # noqa:F401
 
+InputFilename, iso_value = "embryo.slc", 72.0
 
-def main():
-    InputFilename, iso_value = "embryo.slc", 72.0
+colors = vtkNamedColors()
 
-    colors = vtkNamedColors()
+# vtkSLCReader to read.
+reader = vtkSLCReader()
+reader.SetFileName(InputFilename)
+reader.Update()
 
-    # vtkSLCReader to read.
-    reader = vtkSLCReader()
-    reader.SetFileName(InputFilename)
-    reader.Update()
+# Create a mapper.
+mapper = vtkPolyDataMapper()
+mapper.SetInputConnection(reader.GetOutputPort())
 
-    # Create a mapper.
-    mapper = vtkPolyDataMapper()
-    mapper.SetInputConnection(reader.GetOutputPort())
+# Implementing Marching Cubes Algorithm to create the surface using vtkContourFilter object.
+contourFilter = vtkContourFilter()
+contourFilter.SetInputConnection(reader.GetOutputPort())
+# Change the range(2nd and 3rd Parameter) based on your
+# requirement. recommended value for 1st parameter is above 1
+# contourFilter.GenerateValues(5, 80.0, 100.0)
+contourFilter.SetValue(0, iso_value)
 
-    # Implementing Marching Cubes Algorithm to create the surface using vtkContourFilter object.
-    contourFilter = vtkContourFilter()
-    contourFilter.SetInputConnection(reader.GetOutputPort())
-    # Change the range(2nd and 3rd Parameter) based on your
-    # requirement. recommended value for 1st parameter is above 1
-    # contourFilter.GenerateValues(5, 80.0, 100.0)
-    contourFilter.SetValue(0, iso_value)
+outliner = vtkOutlineFilter()
+outliner.SetInputConnection(reader.GetOutputPort())
+outliner.Update()
 
-    outliner = vtkOutlineFilter()
-    outliner.SetInputConnection(reader.GetOutputPort())
-    outliner.Update()
+mapper = vtkPolyDataMapper()
+mapper.SetInputConnection(contourFilter.GetOutputPort())
+mapper.SetScalarVisibility(0)
 
-    mapper = vtkPolyDataMapper()
-    mapper.SetInputConnection(contourFilter.GetOutputPort())
-    mapper.SetScalarVisibility(0)
+actor = vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetDiffuse(0.8)
+actor.GetProperty().SetDiffuseColor(colors.GetColor3d('Ivory'))
+actor.GetProperty().SetSpecular(0.8)
+actor.GetProperty().SetSpecularPower(120.0)
 
-    actor = vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetDiffuse(0.8)
-    actor.GetProperty().SetDiffuseColor(colors.GetColor3d('Ivory'))
-    actor.GetProperty().SetSpecular(0.8)
-    actor.GetProperty().SetSpecularPower(120.0)
+# Create a rendering window and renderer.
+renderer = vtkRenderer()
+renderWindow = vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+renderWindow.SetSize(500, 500)
 
-    # Create a rendering window and renderer.
-    renderer = vtkRenderer()
-    renderWindow = vtkRenderWindow()
-    renderWindow.AddRenderer(renderer)
-    renderWindow.SetSize(500, 500)
+# Create a renderwindowinteractor.
+renderWindowInteractor = vtkRenderWindowInteractor()
+renderWindowInteractor.SetRenderWindow(renderWindow)
 
-    # Create a renderwindowinteractor.
-    renderWindowInteractor = vtkRenderWindowInteractor()
-    renderWindowInteractor.SetRenderWindow(renderWindow)
+# Assign actor to the renderer.
+renderer.AddActor(actor)
+renderer.SetBackground(colors.GetColor3d('SlateGray'))
 
-    # Assign actor to the renderer.
-    renderer.AddActor(actor)
-    renderer.SetBackground(colors.GetColor3d('SlateGray'))
+# Pick a good view
+cam1 = renderer.GetActiveCamera()
+cam1.SetFocalPoint(0.0, 0.0, 0.0)
+cam1.SetPosition(0.0, -1.0, 0.0)
+cam1.SetViewUp(0.0, 0.0, -1.0)
+cam1.Azimuth(-90.0)
+renderer.ResetCamera()
+renderer.ResetCameraClippingRange()
 
-    # Pick a good view
-    cam1 = renderer.GetActiveCamera()
-    cam1.SetFocalPoint(0.0, 0.0, 0.0)
-    cam1.SetPosition(0.0, -1.0, 0.0)
-    cam1.SetViewUp(0.0, 0.0, -1.0)
-    cam1.Azimuth(-90.0)
-    renderer.ResetCamera()
-    renderer.ResetCameraClippingRange()
+renderWindow.SetWindowName('ReadSLC')
+renderWindow.SetSize(640, 512)
+renderWindow.Render()
 
-    renderWindow.SetWindowName('ReadSLC')
-    renderWindow.SetSize(640, 512)
-    renderWindow.Render()
-
-    # Enable user interface interactor.
-    renderWindowInteractor.Initialize()
-    renderWindow.Render()
-    renderWindowInteractor.Start()
-
-
-if __name__ == '__main__':
-    main()
+# Enable user interface interactor.
+renderWindowInteractor.Initialize()
+renderWindow.Render()
+renderWindowInteractor.Start()
