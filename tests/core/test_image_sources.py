7527aeb93ec01213b990c2b379748de67a6f33a5
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-28 20:35:21 +0900

    Add `ImageEllipsoidSource` to create ellipsoid img (#5987)
    * Add `ImageEllipsoidSource` to create ellipsoid img
    
    * Update OverlayTextOnImage.py
    
    * Delete OverlayTextOnImage.py
    
    * Add doc/source/api/utilities/image.rst
    
    * Update tests/core/test_image_sources.py
    
    * Update doc/source/api/utilities/image.rst
    
    * Update test_image_sources.py
    
    * Update image_sources.py

---
+++ tests/core/test_image_sources.py
@@ -0,0 +1,21 @@
+import pyvista as pv
+
+
+def test_image_ellip_soid_source():
+    whole_extent = (-10, 10, -10, 10, -10, 10)
+    center = (0.0, 0.0, 0.0)
+    radius = (5.0, 5.0, 5.0)
+    source = pv.ImageEllipsoidSource(whole_extent=whole_extent, center=center, radius=radius)
+    assert source.whole_extent == whole_extent
+    assert source.center == center
+    assert source.radius == radius
+    whole_extent = (-5, 5, -5, 5, -5, 5)
+    center = (1.0, 1.0, 1.0)
+    radius = (3.0, 3.0, 3.0)
+    source.whole_extent = whole_extent
+    source.center = center
+    source.radius = radius
+    assert source.whole_extent == whole_extent
+    assert source.center == center
+    assert source.radius == radius
+    assert isinstance(source.output, pv.ImageData)
