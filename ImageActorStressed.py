9a27c825a95e0f3af5a3103e3e278b4e9e326d1e
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-29 09:28:19 +0900

    Update ImageActorStressed.py

--- ImageActorStressed.py
+++ ImageActorStressed.py
@@ -1,24 +1,7 @@
-#!/usr/bin/env python
-from vtkmodules.vtkImagingCore import vtkImageShiftScale
-
 import pyvista as pv
 
-gsOne = pv.ImageEllipsoidSource()
-gsOne.SetWholeExtent(0, 999, 0, 999, 0, 0)
-gsOne.SetCenter(500, 500, 0)
-gsOne.SetRadius(300, 400, 0)
-gsOne.SetInValue(0)
-gsOne.SetOutValue(255)
-gsOne.SetOutputScalarTypeToUnsignedChar()
-gsOne.Update()
-ssOne = vtkImageShiftScale()
-ssOne.SetInputConnection(gsOne.GetOutputPort())
-ssOne.SetOutputScalarTypeToUnsignedChar()
-ssOne.SetShift(100)
-ssOne.SetScale(1)
-ssOne.UpdateWholeExtent()
-ssOne.Update()
-source = pv.wrap(gsOne.GetOutput())
-output = pv.wrap(ssOne.GetOutput())
-source.plot(cpos="xy")  # type: ignore[union-attr]
-output.plot(cpos="xy")  # type: ignore[union-attr]
+image = pv.ImageEllipsoidSource()
+image.output.plot(cpos="xy")
+
+shifted_scaled = image.output.shift_scale(shift=100, scale=1)
+shifted_scaled.plot(cpos="xy")
