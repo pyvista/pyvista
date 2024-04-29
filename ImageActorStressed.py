3b8e26e2bd012903325c700b986e52e16fe8264a
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-29 09:36:12 +0900

    Delete ImageActorStressed.py

--- ImageActorStressed.py
+++
@@ -1,7 +0,0 @@
-import pyvista as pv
-
-image = pv.ImageEllipsoidSource()
-image.output.plot(cpos="xy")
-
-shifted_scaled = image.output.shift_scale(shift=100, scale=1)
-shifted_scaled.plot(cpos="xy")
