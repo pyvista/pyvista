9052274c993f1ebf1672b94d0ae4ece2e8e1d400
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-01 12:00:03 +0900

    Update tests/plotting/test_charts.py

--- tests/plotting/test_charts.py
+++ tests/plotting/test_charts.py
@@ -690,7 +690,7 @@ def test_barplot(chart_2d, bar_plot):
     assert plot.orientation == ori
     assert plot.label == l[0]
 
-    with pytest.raises(TypeError, match="Unsupported color type:"):
+    with pytest.raises(ValueError):  # noqa: PT011
         charts.BarPlot(chart_2d, x, y, c[0], ori, l)
     # charts.BarPlot(chart_2d, x, y, c, off, ori, l[0])  # This one is valid
     with pytest.raises(ValueError):  # noqa: PT011
@@ -732,7 +732,7 @@ def test_stackplot(chart_2d, stack_plot):
     assert plot.color == c[0]
     assert plot.label == l[0]
 
-    with pytest.raises(TypeError, match="Unsupported color type:"):
+    with pytest.raises(ValueError):  # noqa: PT011
         charts.StackPlot(chart_2d, x, ys, c[0], l)
     # charts.StackPlot(chart_2d, x, ys, c, l[0])  # This one is valid
     with pytest.raises(ValueError):  # noqa: PT011
