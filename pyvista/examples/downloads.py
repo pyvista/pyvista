873a6fc72d9fb512163cc112fa62d60ac0b4e485
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-02 23:16:40 +0900

    Update pyvista/examples/downloads.py

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -7694,8 +7694,6 @@ def download_m4_total_density(load=True):  # pragma: no cover
     >>> import pyvista as pv
     >>> from pyvista import examples
 
-    >>> pl = pv.Plotter()
-
     >>> filename = examples.download_m4_total_density(load=False)
     >>> reader = pv.get_reader(filename)
     >>> reader.reader.SetHBScale(1.1)
@@ -7705,31 +7703,22 @@ def download_m4_total_density(load=True):  # pragma: no cover
     >>> grid = pv.wrap(reader.reader.GetGridOutput())
     >>> dataset = pv.wrap(reader.reader.GetOutput())
 
-    >>> pl.add_mesh(grid.outline(), color="black")
+    Add the outline and volume to the plotter.
 
-    >>> vol = pl.add_volume(grid)
+    >>> pl = pv.Plotter()
+    >>> outline = pl.add_mesh(grid.outline(), color="black")
+    >>> volume = pl.add_volume(grid)
 
-    >>> sphere = pv.SphereSource(
-    ...     center=(0, 0, 0),
-    ...     radius=1,
-    ...     theta_resolution=16,
-    ...     start_theta=0,
-    ...     end_theta=360,
-    ...     phi_resolution=16,
-    ...     start_phi=0,
-    ...     end_phi=180,
-    ... )
-
-    >>> glyph = dataset.glyph(
-    ...     orient=True,
-    ...     scale=True,
-    ...     factor=0.6,
-    ...     geom=sphere.output,
-    ...     color_mode="scalar",
-    ... )
+    Add atoms and bonds to the plotter.
 
     >>> atoms = pl.add_mesh(
-    ...     glyph,
+    ...     dataset.glyph(
+    ...         orient=True,
+    ...         scale=True,
+    ...         factor=0.6,
+    ...         geom=pv.SphereSource().output,
+    ...         color_mode="scalar",
+    ...     ),
     ...     color="red",
     ...     ambient=0.15,
     ...     diffuse=0.85,
@@ -7737,13 +7726,10 @@ def download_m4_total_density(load=True):  # pragma: no cover
     ...     style="surface",
     ...     interpolation='Gouraud',
     ... )
-
-    >>> tube = dataset.tube(
-    ...     n_sides=16, capping=False, radius=0.2, radius_factor=10
-    ... )
-
     >>> bonds = pl.add_mesh(
-    ...     tube,
+    ...     dataset.tube(
+    ...         n_sides=16, capping=False, radius=0.2, radius_factor=10
+    ...     ),
     ...     color="white",
     ...     ambient=0.15,
     ...     diffuse=0.85,
@@ -7752,7 +7738,6 @@ def download_m4_total_density(load=True):  # pragma: no cover
     ...     interpolation='Gouraud',
     ... )
 
-    >>> pl.set_background('white')
     >>> pl.show(cpos="zx")
 
     .. seealso::
