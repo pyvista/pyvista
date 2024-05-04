df016f289ffa5745cd42655a3cdbe9a5607a9a26
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-04 15:00:30 +0900

    Update NetCDFReader.py

--- NetCDFReader.py
+++ NetCDFReader.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 from vtkmodules.vtkFiltersCore import vtkAssignAttribute, vtkThreshold
 from vtkmodules.vtkFiltersGeometry import vtkDataSetSurfaceFilter
-import vtkmodules.vtkInteractionStyle
-from vtkmodules.vtkRenderingCore import vtkPolyDataMapper
-import vtkmodules.vtkRenderingFreeType
-import vtkmodules.vtkRenderingOpenGL2  # noqa: F401
 
 import pyvista as pv
 from pyvista import examples
@@ -14,7 +10,6 @@ from pyvista import examples
 filename = examples.download_tos_O1_2001_2002(load=False)
 reader = pv.NetCDFCFReader(filename)
 # Set the arrays we want to load.
-reader.reader.UpdateMetaData()
 reader.reader.SetVariableArrayStatus("tos", 1)
 reader.reader.SetSphericalCoordinates(0)
 reader.reader.Update()
@@ -34,11 +29,10 @@ thresh.SetLowerThreshold(10000.0)
 
 surface = vtkDataSetSurfaceFilter()
 surface.SetInputConnection(thresh.GetOutputPort())
-mapper = vtkPolyDataMapper()
+mapper = pv.DataSetMapper()
 mapper.SetInputConnection(surface.GetOutputPort())
 mapper.SetScalarRange(270, 310)
-actor = pv.Actor()
-actor.SetMapper(mapper)
+actor = pv.Actor(mapper=mapper)
 pl = pv.Plotter()
 pl.add_actor(actor)
 pl.show(cpos="xy")
