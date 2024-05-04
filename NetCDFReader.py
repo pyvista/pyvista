e94401c729590d161e2793bdc2911b55755439ec
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-04 12:23:10 +0900

    Update NetCDFReader.py

--- NetCDFReader.py
+++ NetCDFReader.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
-from vtkmodules.util.misc import vtkGetDataRoot
 from vtkmodules.vtkFiltersCore import vtkAssignAttribute, vtkThreshold
 from vtkmodules.vtkFiltersGeometry import vtkDataSetSurfaceFilter
 from vtkmodules.vtkIONetCDF import vtkNetCDFCFReader
 import vtkmodules.vtkInteractionStyle
-from vtkmodules.vtkRenderingCore import vtkActor, vtkPolyDataMapper, vtkRenderer
+from vtkmodules.vtkRenderingCore import vtkPolyDataMapper
 import vtkmodules.vtkRenderingFreeType
 import vtkmodules.vtkRenderingOpenGL2  # noqa: F401
 
 import pyvista as pv
 from pyvista import examples
-
-VTK_DATA_ROOT = vtkGetDataRoot()
 
 # This test checks netCDF reader.  It uses the COARDS convention.
 # Open the file.
@@ -27,16 +24,7 @@ reader.Update()
 # Test unit field arrays
 grid = reader.GetOutput()
 tuarr = grid.GetFieldData().GetAbstractArray("time_units")
-if not tuarr:
-    print("Unable to retrieve time_units field array")
-    exit(1)
 tosuarr = grid.GetFieldData().GetAbstractArray("tos_units")
-if not tosuarr:
-    print("Unable to retrieve tos_units field array")
-    exit(1)
-if tosuarr.GetValue(0) != "K":
-    print("tos_units is not K but " + tosuarr.GetValue(0))
-    exit(1)
 
 aa = vtkAssignAttribute()
 aa.SetInputConnection(reader.GetOutputPort())
@@ -51,10 +39,8 @@ surface.SetInputConnection(thresh.GetOutputPort())
 mapper = vtkPolyDataMapper()
 mapper.SetInputConnection(surface.GetOutputPort())
 mapper.SetScalarRange(270, 310)
-actor = vtkActor()
+actor = pv.Actor()
 actor.SetMapper(mapper)
-ren = vtkRenderer()
-ren.AddActor(actor)
 pl = pv.Plotter()
 pl.add_actor(actor)
 pl.show(cpos="xy")
