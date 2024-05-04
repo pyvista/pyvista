8f933489e33d9773cc5107cf9f2b1ca35aef749f
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-04 15:36:49 +0900

    Update NetCDFReader.py

--- NetCDFReader.py
+++ NetCDFReader.py
@@ -1,36 +1,9 @@
-#!/usr/bin/env python
-from vtkmodules.vtkFiltersCore import vtkAssignAttribute, vtkThreshold
-
 import pyvista as pv
 from pyvista import examples
 
-# This test checks netCDF reader.  It uses the COARDS convention.
-# Open the file.
 filename = examples.download_tos_O1_2001_2002(load=False)
-reader = pv.NetCDFCFReader(filename)
-# Set the arrays we want to load.
-reader.reader.SetVariableArrayStatus("tos", 1)
-reader.reader.SetSphericalCoordinates(0)
-reader.reader.Update()
+reader = pv.get_reader(filename)
 
-# Test unit field arrays
-grid = reader.reader.GetOutput()
-tuarr = grid.GetFieldData().GetAbstractArray("time_units")
-tosuarr = grid.GetFieldData().GetAbstractArray("tos_units")
-
-aa = vtkAssignAttribute()
-aa.SetInputConnection(reader.reader.GetOutputPort())
-aa.Assign("tos", "SCALARS", "POINT_DATA")
-thresh = vtkThreshold()
-thresh.SetInputConnection(aa.GetOutputPort())
-thresh.SetThresholdFunction(vtkThreshold.THRESHOLD_LOWER)
-thresh.SetLowerThreshold(10000.0)
-
-surface = pv.extract_surface_algorithm(thresh.GetOutputPort())
-mapper = pv.DataSetMapper()
-mapper.SetInputConnection(surface.GetOutputPort())
-mapper.SetScalarRange(270, 310)
-actor = pv.Actor(mapper=mapper)
-pl = pv.Plotter()
-pl.add_actor(actor)
-pl.show(cpos="xy")
+grid = reader.read()
+grid.set_active_scalars("tos")
+grid.plot(cpos="xy")
