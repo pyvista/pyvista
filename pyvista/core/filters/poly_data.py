d54c52c820f1671e40678281f92da367d2f84a2c
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-10 06:56:20 +0900

    Apply suggestions from code review

--- pyvista/core/filters/poly_data.py
+++ pyvista/core/filters/poly_data.py
@@ -3791,6 +3791,8 @@ class PolyDataFilters(DataSetFilters):
     def triangulate_contours(self, display_errors=False, progress_bar=False):
         """Triangulate and fill all 2D contours to create polygons.
 
+    .. versionadded:: 0.44.0
+
         This filter will generate triangles to fill all of the 2D contours
         in its input. The input to the filter is a set of lines (not polylines)
         which when joined form loops. The contours may be concave, and may even
