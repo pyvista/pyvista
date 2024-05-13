7dc73208a23937d92a7ec1e8a1912f2cece1b4c4
user27182 <89109579+user27182@users.noreply.github.com>
2024-05-13 00:47:55 -0600

    Fix `extract_values` invert parameter for multi-component values (#6057)
    * Fix extract_values invert for multi-component values
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Tetsuo Koyama <tkoyama010@gmail.com>

--- tests/core/test_dataset_filters.py
+++ tests/core/test_dataset_filters.py
@@ -2517,6 +2517,7 @@ class ComponentModeTestCase(NamedTuple):
     values: Any
     component_mode: Any
     expected: Any
+    expected_invert: Any
 
 
 component_mode_test_cases = [
@@ -2524,42 +2525,63 @@ component_mode_test_cases = [
         values=0,
         component_mode=0,
         expected=[BLACK, GREEN, BLUE],
+        expected_invert=[WHITE, RED],
     ),
     ComponentModeTestCase(
         values=0,
         component_mode=1,
         expected=[BLACK, RED, BLUE],
+        expected_invert=[WHITE, GREEN],
     ),
     ComponentModeTestCase(
         values=0,
         component_mode=2,
         expected=[BLACK, RED, GREEN],
+        expected_invert=[WHITE, BLUE],
     ),
     ComponentModeTestCase(
         values=0,
         component_mode='any',
         expected=[BLACK, RED, GREEN, BLUE],
+        expected_invert=[WHITE],
     ),
     ComponentModeTestCase(
         values=1,
         component_mode='any',
         expected=[WHITE, RED, GREEN, BLUE],
+        expected_invert=[BLACK],
     ),
-    ComponentModeTestCase(values=0, component_mode='all', expected=[BLACK]),
-    ComponentModeTestCase(values=1, component_mode='all', expected=[WHITE]),
-    ComponentModeTestCase(values=BLACK, component_mode='multi', expected=[BLACK]),
+    ComponentModeTestCase(
+        values=0,
+        component_mode='all',
+        expected=[BLACK],
+        expected_invert=[WHITE, RED, GREEN, BLUE],
+    ),
+    ComponentModeTestCase(
+        values=1,
+        component_mode='all',
+        expected=[WHITE],
+        expected_invert=[BLACK, RED, GREEN, BLUE],
+    ),
+    ComponentModeTestCase(
+        values=BLACK,
+        component_mode='multi',
+        expected=[BLACK],
+        expected_invert=[WHITE, RED, GREEN, BLUE],
+    ),
     ComponentModeTestCase(
         values=[WHITE, RED],
         component_mode='multi',
         expected=[WHITE, RED],
+        expected_invert=[BLACK, GREEN, BLUE],
     ),
 ]
 
 
 @pytest.mark.parametrize('values_as_ranges', [True, False])
-@pytest.mark.parametrize('split', [True, False])
+@pytest.mark.parametrize(('split', 'invert'), [(True, False), (False, True), (False, False)])
 @pytest.mark.parametrize(
-    ('values', 'component_mode', 'expected'),
+    ('values', 'component_mode', 'expected', 'expected_invert'),
     component_mode_test_cases,
 )
 @pytest.mark.needs_vtk_version(9, 1, 0)
@@ -2568,6 +2590,8 @@ def test_extract_values_component_mode(
     values,
     component_mode,
     expected,
+    expected_invert,
+    invert,
     split,
     values_as_ranges,
 ):
@@ -2582,12 +2606,13 @@ def test_extract_values_component_mode(
         **values_kwarg,
         component_mode=component_mode,
         split=split,
+        invert=invert,
     )
     single_mesh = extracted.combine() if split else extracted
     actual_points = single_mesh.points
     actual_colors = single_mesh['colors']
-    assert np.array_equal(actual_points, expected)
-    assert np.array_equal(actual_colors, expected)
+    assert np.array_equal(actual_points, expected_invert if invert else expected)
+    assert np.array_equal(actual_colors, expected_invert if invert else expected)
 
 
 @pytest.mark.parametrize(
@@ -2632,7 +2657,7 @@ def test_extract_values_pass_ids(grid4x4, pass_point_ids, pass_cell_ids):
         assert POINT_IDS in extracted.point_data
 
     extracted = grid4x4.extract_values(
-        0,
+        ranges=grid4x4.get_data_range(preference='point'),
         invert=True,
         pass_point_ids=pass_point_ids,
         pass_cell_ids=pass_cell_ids,
@@ -2640,7 +2665,7 @@ def test_extract_values_pass_ids(grid4x4, pass_point_ids, pass_cell_ids):
     if pass_cell_ids:
         assert extracted.cell_data.keys() == []
     if pass_point_ids:
-        assert extracted.cell_data.keys() == []
+        assert extracted.point_data.keys() == []
 
 
 def test_extract_values_empty():
