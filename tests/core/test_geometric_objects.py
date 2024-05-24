ba74c4b693fc4d49cbe1c6217480c78d38d8713e
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-25 02:48:09 +0900

    Apply suggestions from code review

--- tests/core/test_geometric_objects.py
+++ tests/core/test_geometric_objects.py
@@ -410,9 +410,10 @@ def test_capsule():
     assert np.any(capsule.faces)
 
 
+# https://github.com/pyvista/pyvista/pull/6119
 @pytest.mark.paramatrize('center', [(4, 5, 6), (1, 1, 1)])
 @pytest.mark.paramatrize('direction', [(0, 1, -1), (1, 1, 0)])
-def test_capsule_center_corresponds_with_cylinder_center(center, direction):
+def test_capsule_center(center, direction):
     capsule = pv.Capsule(center, direction)
     assert np.allclose(capsule.center, center)
     cylinder = pv.Cylinder(center, direction)
