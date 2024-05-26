d774ed58f52ba2675c8def10fb33616946bf5b25
Tetsuo Koyama <tkoyama010@gmail.com>
2024-05-27 07:59:12 +0900

    Update pyvista/core/molecule.py

--- pyvista/core/molecule.py
+++ pyvista/core/molecule.py
@@ -8,3 +8,25 @@ class Molecule(_vtk.vtkMolecule):
 
     def __init__(self):
         super().__init__()
+
+    @property
+    def n_atoms(self) -> int:
+        """Return the number of atoms.
+
+        Returns
+        -------
+        int
+            Number of atoms in the molecule.
+        """
+        return self.GetNumberOfAtoms()
+
+    @property
+    def n_bonds(self):
+        """Return the number of bonds.
+
+        Returns
+        -------
+        int
+            Number of bonds in the molecule.
+        """
+        return self.GetNumberOfBonds()
--- tests/core/test_molecule.py
+++ tests/core/test_molecule.py
@@ -3,10 +3,10 @@ import pyvista as pv
 
 def test_molecule():
     mol = pv.Molecule()
-    assert mol.GetNumberOfAtoms() == 0
-    assert mol.GetNumberOfBonds() == 0
+    assert mol.n_atoms == 0
+    assert mol.n_bonds == 0
     h1 = mol.AppendAtom(1, 0.0, 0.0, -0.5)
     h2 = mol.AppendAtom(1, 0.0, 0.0, 0.5)
     _ = mol.AppendBond(h1, h2, 1)
-    assert mol.GetNumberOfAtoms() == 2
-    assert mol.GetNumberOfBonds() == 1
+    assert mol.n_atoms == 2
+    assert mol.n_bonds == 1
