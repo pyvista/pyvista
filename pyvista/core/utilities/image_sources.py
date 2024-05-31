66813d1d62fa35be808eff20f25fc83e298df9ee
user27182 <89109579+user27182@users.noreply.github.com>
2024-05-31 13:10:56 -0600

    Add seed to docstring

--- pyvista/core/utilities/image_sources.py
+++ pyvista/core/utilities/image_sources.py
@@ -249,6 +249,7 @@ class ImageNoiseSource(_vtk.vtkImageNoiseSource):
     ...     whole_extent=(0, 200, 0, 200, 0, 0),
     ...     minimum=0,
     ...     maximum=255,
+    ...     seed=0,
     ... )
     >>> source.output.plot(cpos="xy")
     """
