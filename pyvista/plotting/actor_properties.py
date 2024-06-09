2e187f8c8d209a4679629becb7f4d1f50d1fac60
user27182 <89109579+user27182@users.noreply.github.com>
2024-06-09 08:48:08 -0600

    Update deprecation for actor properties

--- pyvista/plotting/actor_properties.py
+++ pyvista/plotting/actor_properties.py
@@ -1,6 +1,6 @@
 """Module containing pyvista implementation of vtkProperty.
 
-.. deprecated:: 0.43.0
+.. deprecated:: 0.44.0
 
     This class is deprecated. Use :class:`pyvista.Property` instead.
 """
@@ -11,15 +11,17 @@ from typing import TYPE_CHECKING
 from typing import Tuple
 import warnings
 
+import pyvista
 from pyvista.core.errors import PyVistaDeprecationWarning
-from pyvista.plotting.opts import InterpolationType
-from pyvista.plotting.opts import RepresentationType
 
-if TYPE_CHECKING:
-    from pyvista.plotting import _vtk
+from .opts import InterpolationType
+from .opts import RepresentationType
+
+if TYPE_CHECKING:  # pragma: no cover
+    from . import _vtk
 
 
-class ActorProperties:
+class ActorProperties:  # pragma:no cover
     """Properties wrapper for ``vtkProperty``.
 
     Contains the surface properties of the object.
@@ -40,8 +42,9 @@ class ActorProperties:
     >>> z_axes_prop.color = (1.0, 1.0, 0.0)
     >>> z_axes_prop.opacity = 0.5
     >>> axes.axes_actor.shaft_type = axes.axes_actor.ShaftType.CYLINDER
+
     >>> pl = pv.Plotter()
-    >>> _ = pl.add_actor(axes.axes_actor)  # doctest:+SKIP
+    >>> _ = pl.add_actor(axes.axes_actor)
     >>> _ = pl.add_mesh(pv.Sphere())
     >>> pl.show()
 
@@ -51,64 +54,67 @@ class ActorProperties:
         super().__init__()
         self.properties = properties
 
+        # Deprecated on v0.44.0, estimated removal on v0.47.0
         warnings.warn(
             "Use of `ActorProperties` is deprecated. Use `pyvista.Property` instead.",
             PyVistaDeprecationWarning,
         )
+        if pyvista._version.version_info >= (0, 47):
+            raise RuntimeError('Remove this deprecated function')
 
     @property
     def color(self):  # numpydoc ignore=RT01
         """Return or set the color of the actor."""
-        return self.properties.GetColor()  # pragma:no cover
+        return self.properties.GetColor()
 
     @color.setter
     def color(self, color: Tuple[float, float, float]):  # numpydoc ignore=GL08
-        self.properties.SetColor(color[0], color[1], color[2])  # pragma:no cover
+        self.properties.SetColor(color[0], color[1], color[2])
 
     @property
     def metallic(self):  # numpydoc ignore=RT01
         """Return or set the metallic coefficient of the surface."""
-        return self.properties.GetMetallic()  # pragma:no cover
+        return self.properties.GetMetallic()
 
     @metallic.setter
     def metallic(self, value: float):  # numpydoc ignore=GL08
-        self.properties.SetMetallic(value)  # pragma:no cover
+        self.properties.SetMetallic(value)
 
     @property
     def roughness(self):  # numpydoc ignore=RT01
         """Return or set the roughness of the surface."""
-        return self.properties.GetRoughness()  # pragma:no cover
+        return self.properties.GetRoughness()
 
     @roughness.setter
     def roughness(self, value: float):  # numpydoc ignore=GL08
-        self.properties.SetRoughness(value)  # pragma:no cover
+        self.properties.SetRoughness(value)
 
     @property
     def anisotropy(self):  # numpydoc ignore=RT01
         """Return or set the anisotropy coefficient."""
-        return self.properties.GetAnisotropy()  # pragma:no cover
+        return self.properties.GetAnisotropy()
 
     @anisotropy.setter
     def anisotropy(self, value: float):  # numpydoc ignore=GL08
-        self.properties.SetAnisotropy(value)  # pragma:no cover
+        self.properties.SetAnisotropy(value)
 
     @property
     def anisotropy_rotation(self):  # numpydoc ignore=RT01
         """Return or set the anisotropy rotation coefficient."""
-        return self.properties.GetAnisotropyRotation()  # pragma:no cover
+        return self.properties.GetAnisotropyRotation()
 
     @anisotropy_rotation.setter
     def anisotropy_rotation(self, value: float):  # numpydoc ignore=GL08
-        self.properties.SetAnisotropyRotation(value)  # pragma:no cover
+        self.properties.SetAnisotropyRotation(value)
 
     @property
     def lighting(self):  # numpydoc ignore=RT01
         """Return or set the lighting activation flag."""
-        return self.properties.GetLighting()  # pragma:no cover
+        return self.properties.GetLighting()
 
     @lighting.setter
     def lighting(self, flag: bool):  # numpydoc ignore=GL08
-        self.properties.SetLighting(flag)  # pragma:no cover
+        self.properties.SetLighting(flag)
 
     @property
     def interpolation_model(self):  # numpydoc ignore=RT01
@@ -116,38 +122,38 @@ class ActorProperties:
 
         Can be any of the options in :class:`pyvista.plotting.opts.InterpolationType` enum.
         """
-        return InterpolationType.from_any(self.properties.GetInterpolation())  # pragma:no cover
+        return InterpolationType.from_any(self.properties.GetInterpolation())
 
     @interpolation_model.setter
     def interpolation_model(self, model: InterpolationType):  # numpydoc ignore=GL08
-        self.properties.SetInterpolation(model.value)  # pragma:no cover
+        self.properties.SetInterpolation(model.value)
 
     @property
     def index_of_refraction(self):  # numpydoc ignore=RT01
         """Return or set the Index Of Refraction of the base layer."""
-        return self.properties.GetBaseIOR()  # pragma:no cover
+        return self.properties.GetBaseIOR()
 
     @index_of_refraction.setter
     def index_of_refraction(self, value: float):  # numpydoc ignore=GL08
-        self.properties.SetBaseIOR(value)  # pragma:no cover
+        self.properties.SetBaseIOR(value)
 
     @property
     def opacity(self):  # numpydoc ignore=RT01
         """Return or set the opacity of the actor."""
-        return self.properties.GetOpacity()  # pragma:no cover
+        return self.properties.GetOpacity()
 
     @opacity.setter
     def opacity(self, value: float):  # numpydoc ignore=GL08
-        self.properties.SetOpacity(value)  # pragma:no cover
+        self.properties.SetOpacity(value)
 
     @property
     def shading(self):  # numpydoc ignore=RT01
         """Return or set the flag to activate the shading."""
-        return self.properties.GetShading()  # pragma:no cover
+        return self.properties.GetShading()
 
     @shading.setter
     def shading(self, is_active: bool):  # numpydoc ignore=GL08
-        self.properties.SetShading(is_active)  # pragma:no cover
+        self.properties.SetShading(is_active)
 
     @property
     def representation(self) -> RepresentationType:  # numpydoc ignore=RT01
@@ -155,23 +161,8 @@ class ActorProperties:
 
         Can be any of the options in :class:`pyvista.plotting.opts.RepresentationType` enum.
         """
-        return RepresentationType.from_any(self.properties.GetRepresentation())  # pragma:no cover
+        return RepresentationType.from_any(self.properties.GetRepresentation())
 
     @representation.setter
     def representation(self, value: RepresentationType):  # numpydoc ignore=GL08
-        self.properties.SetRepresentation(
-            RepresentationType.from_any(value).value,
-        )  # pragma:no cover
-
-    @property
-    def ambient(self):  # numpydoc ignore=RT01
-        """Return or set the ambient lighting coefficient.
-
-        Value should be between 0 and 1.
-
-        """
-        return self.properties.GetAmbient()  # pragma:no cover
-
-    @ambient.setter
-    def ambient(self, ambient: float):  # numpydoc ignore=GL08
-        self.properties.SetAmbient(ambient)  # pragma:no cover
+        self.properties.SetRepresentation(RepresentationType.from_any(value).value)
