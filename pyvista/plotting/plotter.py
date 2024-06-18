2657a0e24f96aad01f9d0eccf5b2d8193a5c9f3c
Tetsuo Koyama <tkoyama010@gmail.com>
2024-06-18 10:38:34 +0900

    Update plotter.py

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -580,7 +580,7 @@ class BasePlotter(PickingHelper, WidgetHelper):
         from vtkmodules.vtkIOImport import vtkOBJImporter
 
         filename = Path(str(filename)).expanduser().resolve()
-        if not Path(filename).is_file():
+        if not filename.is_file():
             raise FileNotFoundError(f'Unable to locate {filename}')
 
         # lazy import here to avoid importing unused modules
