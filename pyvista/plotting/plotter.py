da694b48a1386a08d5face772b123d5303bd5cee
Kevin Marchais <kevinmarchais@gmail.com>
2024-06-23 00:01:07 +0200

    add comment for off_screen attribute check

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -6041,7 +6041,9 @@ class BasePlotter(PickingHelper, WidgetHelper):
                 # Plotter hasn't been rendered or was improperly closed
                 raise RuntimeError('This plotter is closed and unable to save a screenshot.')
 
-            if self._first_time and (hasattr(self, "off_screen") and not self.off_screen):
+            if self._first_time and (
+                hasattr(self, "off_screen") and not self.off_screen
+            ):  # 'off_screen' attribute is specific to Plotter objects.
                 raise RuntimeError(
                     "Nothing to screenshot - call .show first or use the off_screen argument",
                 )
@@ -6233,7 +6235,9 @@ class BasePlotter(PickingHelper, WidgetHelper):
                 else:
                     self.render()
                 sleep_time = step - (time.time() - tstart)
-                if sleep_time > 0 and (hasattr(self, "off_screen") and not self.off_screen):
+                if sleep_time > 0 and (
+                    hasattr(self, "off_screen") and not self.off_screen
+                ):  # 'off_screen' attribute is specific to Plotter objects.
                     time.sleep(sleep_time)
             if write_frames:
                 self.mwriter.close()
