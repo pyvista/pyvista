eb5ca940ffa077111780a3e8d8c7d59f49550571
Alex Kaszynski <akascap@gmail.com>
2024-07-02 14:56:37 -0600

    copy before cleaning

--- pyvista/core/filters/unstructured_grid.py
+++ pyvista/core/filters/unstructured_grid.py
@@ -145,7 +145,7 @@ class UnstructuredGridFilters(DataSetFilters):
             raise VTKVersionError("UnstructuredGrid.clean requires VTK >= 9.2.2") from None
 
         alg = vtkStaticCleanUnstructuredGrid()
-        alg.SetInputDataObject(self)
+        alg.SetInputDataObject(self.copy())
         alg.SetAbsoluteTolerance(True)
         alg.SetTolerance(tolerance)
         alg.SetMergingArray(merging_array_name)
--- tests/core/test_unstructured_grid_filters.py
+++ tests/core/test_unstructured_grid_filters.py
@@ -21,6 +21,7 @@ def test_clean_points():
     points = np.vstack([u_points] * 2)
 
     grid = pv.PolyData(points).cast_to_unstructured_grid()
+    grid_orig = grid.copy()  # ensure grid is not modified in-place
     grid.point_data['data'] = np.hstack((np.zeros(n_unique_points), np.ones(n_unique_points)))
 
     cleaned = grid.clean(produce_merge_map=True)
@@ -28,6 +29,7 @@ def test_clean_points():
     assert cleaned.n_points == n_unique_points
     assert np.allclose(cleaned.point_data['data'], 0.5)
     assert cleaned.point_data['data'].size == n_unique_points
+    assert grid_orig == grid
 
     # verify not averaging works
     cleaned = grid.clean(average_point_data=False)
@@ -43,11 +45,13 @@ def test_clean_points():
     grid = pv.PolyData(points).cast_to_unstructured_grid()
     cleaned = grid.clean(tolerance=1e-5)
     assert cleaned.n_points == points.shape[0]
+    assert grid_orig == grid
 
 
 @skip_lesser_9_2_2
 def test_clean_grid(hexbeam):
     hexbeam_shifted = hexbeam.translate([1, 0, 0])
+    hexbeam_orig = hexbeam.copy()
 
     hexbeam.point_data['data'] = np.zeros(hexbeam.n_points)
     hexbeam_shifted.point_data['data'] = np.ones(hexbeam.n_points)
@@ -55,6 +59,7 @@ def test_clean_grid(hexbeam):
     merged = hexbeam.merge(hexbeam_shifted, merge_points=False)
     cleaned = merged.clean(average_point_data=True, produce_merge_map=False)
     assert 'PointMergeMap' not in cleaned.field_data
+    assert hexbeam_orig == hexbeam
 
     # expect averaging for all merged nodes
     n_merged = merged.n_points - cleaned.n_points
