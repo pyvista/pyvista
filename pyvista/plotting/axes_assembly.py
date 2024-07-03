9df57718c6a62d13cee3012fc205589088ffa409
user27182 <89109579+user27182@users.noreply.github.com>
2024-07-03 15:17:46 -0600

    Add orientation widget example

--- pyvista/plotting/axes_assembly.py
+++ pyvista/plotting/axes_assembly.py
@@ -569,9 +569,25 @@ class SymmetricAxesAssembly(AxesAssembly):
     ...     'up',
     ...     'down',
     ... ]
+    >>> axes_assembly.label_color = 'darkgoldenrod'
 
     >>> pl = pv.Plotter()
     >>> _ = pl.add_actor(axes_assembly)
+    >>> pl.show()
+
+    Add the axes as a custom orientation widget with
+    :func:`~pyvista.Renderer.add_orientation_widget`. We also configure the labels to
+    only show text for the positive axes.
+
+    >>> axes_assembly = pv.SymmetricAxesAssembly(
+    ...     x_label=('X', ""), y_label=('Y', ""), z_label=('Z', "")
+    ... )
+    >>> pl = pv.Plotter()
+    >>> _ = pl.add_mesh(pv.Cone())
+    >>> _ = pl.add_orientation_widget(
+    ...     axes_assembly,
+    ...     viewport=(0, 0, 0.5, 0.5),
+    ... )
     >>> pl.show()
     """
 
