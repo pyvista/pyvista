9955dce87d70da150106234d52e449a9f79fcd4e
user27182 <89109579+user27182@users.noreply.github.com>
2024-07-25 15:15:21 -0600

    Update docstrings

--- pyvista/plotting/axes_assembly.py
+++ pyvista/plotting/axes_assembly.py
@@ -522,9 +522,9 @@ class AxesAssembly(_XYZAssembly):
         Examples
         --------
         >>> import pyvista as pv
-        >>> axes_actor = pv.AxesAssembly()
-        >>> axes_actor.labels = ['X Axis', 'Y Axis', 'Z Axis']
-        >>> axes_actor.labels
+        >>> axes_assembly = pv.AxesAssembly()
+        >>> axes_assembly.labels = ['X Axis', 'Y Axis', 'Z Axis']
+        >>> axes_assembly.labels
         ('X Axis', 'Y Axis', 'Z Axis')
         """
         return self.x_label, self.y_label, self.z_label
@@ -543,9 +543,9 @@ class AxesAssembly(_XYZAssembly):
         Examples
         --------
         >>> import pyvista as pv
-        >>> axes_actor = pv.AxesAssembly()
-        >>> axes_actor.x_label = 'This axis'
-        >>> axes_actor.x_label
+        >>> axes_assembly = pv.AxesAssembly()
+        >>> axes_assembly.x_label = 'This axis'
+        >>> axes_assembly.x_label
         'This axis'
 
         """
@@ -562,9 +562,9 @@ class AxesAssembly(_XYZAssembly):
         Examples
         --------
         >>> import pyvista as pv
-        >>> axes_actor = pv.AxesAssembly()
-        >>> axes_actor.y_label = 'This axis'
-        >>> axes_actor.y_label
+        >>> axes_assembly = pv.AxesAssembly()
+        >>> axes_assembly.y_label = 'This axis'
+        >>> axes_assembly.y_label
         'This axis'
 
         """
@@ -581,9 +581,9 @@ class AxesAssembly(_XYZAssembly):
         Examples
         --------
         >>> import pyvista as pv
-        >>> axes_actor = pv.AxesAssembly()
-        >>> axes_actor.z_label = 'This axis'
-        >>> axes_actor.z_label
+        >>> axes_assembly = pv.AxesAssembly()
+        >>> axes_assembly.z_label = 'This axis'
+        >>> axes_assembly.z_label
         'This axis'
 
         """
@@ -618,14 +618,14 @@ class AxesAssembly(_XYZAssembly):
         Examples
         --------
         >>> import pyvista as pv
-        >>> axes_actor = pv.AxesAssembly()
-        >>> axes_actor.label_position
+        >>> axes_assembly = pv.AxesAssembly()
+        >>> axes_assembly.label_position
         (0.8, 0.8, 0.8)
-        >>> axes_actor.label_position = 0.3
-        >>> axes_actor.label_position
+        >>> axes_assembly.label_position = 0.3
+        >>> axes_assembly.label_position
         (0.3, 0.3, 0.3)
-        >>> axes_actor.label_position = (0.1, 0.4, 0.2)
-        >>> axes_actor.label_position
+        >>> axes_assembly.label_position = (0.1, 0.4, 0.2)
+        >>> axes_assembly.label_position
         (0.1, 0.4, 0.2)
 
         """
@@ -732,52 +732,52 @@ class AxesAssembly(_XYZAssembly):
         single value.
 
         >>> import pyvista as pv
-        >>> axes_actor = pv.AxesAssembly()
-        >>> axes_actor.set_actor_prop('ambient', 0.7)
-        >>> axes_actor.get_actor_prop('ambient')
+        >>> axes_assembly = pv.AxesAssembly()
+        >>> axes_assembly.set_actor_prop('ambient', 0.7)
+        >>> axes_assembly.get_actor_prop('ambient')
         _AxesPropTuple(x_shaft=0.7, y_shaft=0.7, z_shaft=0.7, x_tip=0.7, y_tip=0.7, z_tip=0.7)
 
         Set the property again, but this time set separate values for each part.
 
         >>> values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-        >>> axes_actor.set_actor_prop('ambient', values)
-        >>> axes_actor.get_actor_prop('ambient')
+        >>> axes_assembly.set_actor_prop('ambient', values)
+        >>> axes_assembly.get_actor_prop('ambient')
         _AxesPropTuple(x_shaft=0.1, y_shaft=0.2, z_shaft=0.3, x_tip=0.4, y_tip=0.5, z_tip=0.6)
 
         Set :attr:`~pyvista.Property.opacity` for the x-axis only. The property is set
         for both the axis shaft and tip by default.
 
-        >>> axes_actor.set_actor_prop('opacity', 0.5, axis='x')
-        >>> axes_actor.get_actor_prop('opacity')
+        >>> axes_assembly.set_actor_prop('opacity', 0.5, axis='x')
+        >>> axes_assembly.get_actor_prop('opacity')
         _AxesPropTuple(x_shaft=0.5, y_shaft=1.0, z_shaft=1.0, x_tip=0.5, y_tip=1.0, z_tip=1.0)
 
         Set the property again, but this time set separate values for the shaft and tip.
 
-        >>> axes_actor.set_actor_prop('opacity', [0.3, 0.7], axis='x')
-        >>> axes_actor.get_actor_prop('opacity')
+        >>> axes_assembly.set_actor_prop('opacity', [0.3, 0.7], axis='x')
+        >>> axes_assembly.get_actor_prop('opacity')
         _AxesPropTuple(x_shaft=0.3, y_shaft=1.0, z_shaft=1.0, x_tip=0.7, y_tip=1.0, z_tip=1.0)
 
         Set :attr:`~pyvista.Property.show_edges` for the axes shafts only. The property
         is set for all axes by default.
 
-        >>> axes_actor.set_actor_prop('show_edges', True, part='shaft')
-        >>> axes_actor.get_actor_prop('show_edges')
+        >>> axes_assembly.set_actor_prop('show_edges', True, part='shaft')
+        >>> axes_assembly.get_actor_prop('show_edges')
         _AxesPropTuple(x_shaft=True, y_shaft=True, z_shaft=True, x_tip=False, y_tip=False, z_tip=False)
 
         Set the property again, but this time set separate values for each shaft.
 
-        >>> axes_actor.set_actor_prop(
+        >>> axes_assembly.set_actor_prop(
         ...     'show_edges', [True, False, True], part='shaft'
         ... )
-        >>> axes_actor.get_actor_prop('show_edges')
+        >>> axes_assembly.get_actor_prop('show_edges')
         _AxesPropTuple(x_shaft=True, y_shaft=False, z_shaft=True, x_tip=False, y_tip=False, z_tip=False)
 
         Set :attr:`~pyvista.Property.style` for a single axis and specific part.
 
-        >>> axes_actor.set_actor_prop(
+        >>> axes_assembly.set_actor_prop(
         ...     'style', 'wireframe', axis='x', part='shaft'
         ... )
-        >>> axes_actor.get_actor_prop('style')
+        >>> axes_assembly.get_actor_prop('style')
         _AxesPropTuple(x_shaft='Wireframe', y_shaft='Surface', z_shaft='Surface', x_tip='Surface', y_tip='Surface', z_tip='Surface')
         """
         actors = self._filter_part_actors(axis=axis, part=part)
