b58a8ce8bf242774402181694d439be2d579a401
user27182 <89109579+user27182@users.noreply.github.com>
2024-08-03 00:05:45 -0600

    Update tests

--- tests/typing/test_bounds_tuple.py
+++ tests/typing/test_bounds_tuple.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 
 import pyvista as pv
+from pyvista.core.errors import VTKVersionError
 
 if TYPE_CHECKING:
     from types import ModuleType
@@ -22,11 +23,51 @@ def _get_classes_with_bounds(module: ModuleType) -> list[type]:
 def pytest_generate_tests(metafunc):
     """Generate parametrized tests."""
     if 'class_with_bounds' in metafunc.fixturenames:
-        # Generate a separate test for each class that has bounds
+        # Generate a separate test for any class that has bounds
         core_classes: list[type] = _get_classes_with_bounds(pv.core)
         plotting_classes: list[type] = _get_classes_with_bounds(pv.plotting)
         classes = [*core_classes, *plotting_classes]
         class_names = [class_.__name__ for class_ in classes]
+
+        # List all the classes explicitly
+        expected_names = [
+            'Actor',
+            'AxesAssembly',
+            'AxesAssemblySymmetric',
+            'BasePlotter',
+            'BoxSource',
+            'Cell',
+            'CompositePolyDataMapper',
+            'CubeAxesActor',
+            'CubeSource',
+            'DataSet',
+            'DataSetMapper',
+            'ExplicitStructuredGrid',
+            'FixedPointVolumeRayCastMapper',
+            'GPUVolumeRayCastMapper',
+            'Grid',
+            'ImageData',
+            'Label',
+            'MultiBlock',
+            'OpenGLGPUVolumeRayCastMapper',
+            'OrthogonalPlanesSource',
+            'PlanesAssembly',
+            'Plotter',
+            'PointGaussianMapper',
+            'PointGrid',
+            'PointSet',
+            'PolyData',
+            'Prop3D',
+            'RectilinearGrid',
+            'Renderer',
+            'SmartVolumeMapper',
+            'StructuredGrid',
+            'UnstructuredGrid',
+            'UnstructuredGridVolumeRayCastMapper',
+            'Volume',
+        ]
+        assert sorted(class_names) == sorted(expected_names)
+
         metafunc.parametrize('class_with_bounds', classes, ids=class_names)
 
 
@@ -41,6 +82,8 @@ def test_bounds_tuple(class_with_bounds):
     # Init object but skip if abstract
     try:
         instance = class_with_bounds(**kwargs)
+    except VTKVersionError:
+        pytest.skip('VTK Version not supported.')
     except TypeError as e:
         if 'abstract' in repr(e):
             pytest.skip('Class is abstract.')
