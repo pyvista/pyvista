b3124a52391d11845f7e9a8b091ef2615190a60e
user27182 <89109579+user27182@users.noreply.github.com>
2024-08-07 12:51:18 -0600

    Add flip_normal to PlaneSource

--- pyvista/core/utilities/geometric_sources.py
+++ pyvista/core/utilities/geometric_sources.py
@@ -2380,20 +2380,25 @@ class PlaneSource(_vtk.vtkPlaneSource):
         return wrap(self.GetOutput())
 
     @property
-    def normal(self) -> tuple[float, float, float]:
-        """Get the Location in ``[x, y, z]``.
-
-        Returns
-        -------
-        sequence[float]
-            Location in ``[x, y, z]``.
-        """
+    def normal(self) -> tuple[float, float, float]:  # numpydoc ignore: RT01
+        """Get the plane's normal vector."""
         origin = np.array(self.origin)
         v1 = self.point_a - origin
         v2 = self.point_b - origin
         normal = np.cross(v1, v2)
         norm = np.linalg.norm(normal)
-        return tuple((normal / norm).tolist()) if norm else (0, 0, 1)
+        # Avoid div by zero and return +z normal by default
+        return tuple((normal / norm).tolist()) if norm else (0.0, 0.0, 1.0)
+
+    def flip_normal(self):
+        """Flip the plane's normal.
+
+        This method modifies the plane's :attr:`point_a` and :attr:`point_b` by
+        swapping them.
+        """
+        point_a = self.point_a
+        self.point_a = self.point_b
+        self.point_b = point_a
 
     def push(self, distance: float):  # numpydoc ignore: PR01
         """Translate the plane in the direction of the normal by the distance specified."""
@@ -3707,6 +3712,7 @@ class OrthogonalPlanesSource:
         yz_source, zx_source, xy_source = self._plane_sources
         x_res, y_res, z_res = self.resolution
         x_min, x_max, y_min, y_max, z_min, z_max = self.bounds
+        flip_yz, flip_zx, flip_xy = self.normal_sign == np.array(['-', '-', '-'])
 
         # Compute bounds-related vars
         x_size, y_size, z_size = x_max - x_min, y_max - y_min, z_max - z_min
@@ -3719,6 +3725,8 @@ class OrthogonalPlanesSource:
         xy_source.point_b = 0.0, y_size, 0.0
         xy_source.origin = ORIGIN
         xy_source.center = center
+        if flip_xy:
+            xy_source.flip_normal()
         xy_source.Update()
 
         yz_source.i_resolution = y_res
@@ -3727,6 +3735,8 @@ class OrthogonalPlanesSource:
         yz_source.point_b = 0.0, 0.0, z_size
         yz_source.origin = ORIGIN
         yz_source.center = center
+        if flip_yz:
+            yz_source.flip_normal()
         yz_source.Update()
 
         zx_source.i_resolution = z_res
@@ -3735,17 +3745,17 @@ class OrthogonalPlanesSource:
         zx_source.point_b = x_size, 0.0, 0.0
         zx_source.origin = ORIGIN
         zx_source.center = center
+        if flip_zx:
+            zx_source.flip_normal()
         zx_source.Update()
 
         # Update the output
         output = self._output
         move_vectors = np.diag(self.move)
-        for index, (name, source, plane, sign, move) in enumerate(
-            zip(self.names, self._plane_sources, output, self.normal_sign, move_vectors)
+        for index, (name, source, plane, move) in enumerate(
+            zip(self.names, self._plane_sources, output, move_vectors)
         ):
             plane.copy_from(source.GetOutput())
-            if sign == '-':
-                plane['Normals'] *= -1
             output.set_block_name(index, name)
             plane.points += move
 
