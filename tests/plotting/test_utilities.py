333917628eca18738463dc7fce966a7510ccf9e7
Tetsuo Koyama <tkoyama010@gmail.com>
2024-08-13 01:51:17 +0900

    Deprecate `start_xvfb` to use `vtk-osmesa` instead

---
+++ tests/plotting/test_utilities.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import pytest
+
+import pyvista as pv
+from pyvista.core.errors import PyVistaDeprecationWarning
+
+
+def test_start_xvfb():
+    with pytest.warns(PyVistaDeprecationWarning):
+        pv.start_xvfb()
+        if pv._version.version_info >= (0, 48):
+            raise RuntimeError("Remove this method")
