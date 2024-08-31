36cb7c92460403c3ff0633d38e6a1bb045747cb8
user27182 <89109579+user27182@users.noreply.github.com>
2024-08-30 19:54:55 -0600

    Add QuadraticLinearWedge

--- pyvista/examples/cells.py
+++ pyvista/examples/cells.py
@@ -1300,6 +1300,51 @@ def QuadraticLinearQuadrilateral() -> UnstructuredGrid:
     return _make_isoparametric_unstructured_grid(_vtk.vtkQuadraticLinearQuad())
 
 
+def QuadraticLinearWedge() -> UnstructuredGrid:
+    """Create a :class:`pyvista.UnstructuredGrid` containing a single quadratic linear wedge.
+
+    This cell corresponds to the :attr:`pyvista.CellType.QUADRATIC_LINEAR_WEDGE` cell type.
+
+    Returns
+    -------
+    pyvista.UnstructuredGrid
+        UnstructuredGrid containing a single quadratic linear wedge.
+
+    Examples
+    --------
+    Create and plot a single quadratic linear wedge.
+
+    >>> from pyvista import examples
+    >>> grid = examples.cells.QuadraticLinearWedge()
+    >>> examples.plot_cell(grid)
+
+    List the grid's cells.
+
+    >>> grid.cells
+    array([12,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11])
+
+    List the grid's points.
+
+    >>> grid.points
+    pyvista_ndarray([[0. , 0. , 0. ],
+                     [1. , 0. , 0. ],
+                     [0. , 1. , 0. ],
+                     [0. , 0. , 1. ],
+                     [1. , 0. , 1. ],
+                     [0. , 1. , 1. ],
+                     [0.5, 0. , 0. ],
+                     [0.5, 0.5, 0. ],
+                     [0. , 0.5, 0. ],
+                     [0.5, 0. , 1. ],
+                     [0.5, 0.5, 1. ],
+                     [0. , 0.5, 1. ]])
+
+    >>> grid.celltypes  # same as pyvista.CellType.QUADRATIC_LINEAR_WEDGE
+    array([31], dtype=uint8)
+    """
+    return _make_isoparametric_unstructured_grid(_vtk.vtkQuadraticLinearWedge())
+
+
 def _make_isoparametric_unstructured_grid(vtk_cell: _vtk.vtkCell):
     cell = pyvista.Cell(vtk_cell)
 
