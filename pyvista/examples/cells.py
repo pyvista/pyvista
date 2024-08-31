3b1052ad62b23786ba291aa2e3a67d3d2079015f
user27182 <89109579+user27182@users.noreply.github.com>
2024-08-30 20:02:21 -0600

    Add BiQuadraticQuadraticWedge

--- pyvista/examples/cells.py
+++ pyvista/examples/cells.py
@@ -1345,6 +1345,58 @@ def QuadraticLinearWedge() -> UnstructuredGrid:
     return _make_isoparametric_unstructured_grid(_vtk.vtkQuadraticLinearWedge())
 
 
+def BiQuadraticQuadraticWedge() -> UnstructuredGrid:
+    """Create a :class:`pyvista.UnstructuredGrid` containing a single biquadratic quadratic wedge.
+
+    This cell corresponds to the :attr:`pyvista.CellType.BIQUADRATIC_QUADRATIC_WEDGE` cell type.
+
+    Returns
+    -------
+    pyvista.UnstructuredGrid
+        UnstructuredGrid containing a single biquadratic quadratic wedge.
+
+    Examples
+    --------
+    Create and plot a single biquadratic quadratic wedge.
+
+    >>> from pyvista import examples
+    >>> grid = examples.cells.BiQuadraticQuadraticWedge()
+    >>> examples.plot_cell(grid)
+
+    List the grid's cells.
+
+    >>> grid.cells
+    array([18,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+           16, 17])
+
+    List the grid's points.
+
+    >>> grid.points
+    pyvista_ndarray([[0. , 0. , 0. ],
+                     [1. , 0. , 0. ],
+                     [0. , 1. , 0. ],
+                     [0. , 0. , 1. ],
+                     [1. , 0. , 1. ],
+                     [0. , 1. , 1. ],
+                     [0.5, 0. , 0. ],
+                     [0.5, 0.5, 0. ],
+                     [0. , 0.5, 0. ],
+                     [0.5, 0. , 1. ],
+                     [0.5, 0.5, 1. ],
+                     [0. , 0.5, 1. ],
+                     [0. , 0. , 0.5],
+                     [1. , 0. , 0.5],
+                     [0. , 1. , 0.5],
+                     [0.5, 0. , 0.5],
+                     [0.5, 0.5, 0.5],
+                     [0. , 0.5, 0.5]])
+
+    >>> grid.celltypes  # same as pyvista.CellType.BIQUADRATIC_QUADRATIC_WEDGE
+    array([32], dtype=uint8)
+    """
+    return _make_isoparametric_unstructured_grid(_vtk.vtkBiQuadraticQuadraticWedge())
+
+
 def _make_isoparametric_unstructured_grid(vtk_cell: _vtk.vtkCell):
     cell = pyvista.Cell(vtk_cell)
 
