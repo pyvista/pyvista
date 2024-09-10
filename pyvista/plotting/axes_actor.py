aa1a1719cb197d0c4cf3fa8bfc39df07687860c4
Tetsuo Koyama <tkoyama010@gmail.com>
2024-09-10 09:14:19 +0900

    Update

--- pyvista/plotting/axes_actor.py
+++ pyvista/plotting/axes_actor.py
@@ -617,3 +617,101 @@ class AxesActor(_vtk.vtkAxesActor):
     @z_axis_tip_properties.setter
     def z_axis_tip_properties(self, properties: ActorProperties):  # numpydoc ignore=GL08
         self.z_axis_tip_properties = properties
+
+
+class PolarAxesActor(_vtk.vtkPolarAxesActor):
+    """PolarAxesActor wrapper for vtkPolarAxesActor.
+
+    This actor is used to represent polar axes in a scene. The user can
+    define the geometry to use for the shaft or the tip, and the user
+    can set the text for the radial and azimuthal axes. To see full
+    customization options, refer to `vtkPolarAxesActor Details
+    <https://vtk.org/doc/nightly/html/classvtkPolarAxesActor.html#details>`_.
+
+    Examples
+    --------
+    Customize the polar axes.
+
+    >>> from vtkmodules.vtkRenderingAnnotation import vtkPolarAxesActor
+    >>> import pyvista as pv
+
+    Load the teapot geometry using PyVista's built-in methods
+
+    >>> filename = pv.examples.download_teapot(
+    ...     load=False
+    ... )  # Download only, do not load
+    >>> mesh = pv.read(filename)
+
+    Apply normals to the geometry
+
+    >>> normals = mesh.compute_normals()
+
+    Create a PyVista plotter
+
+    >>> plotter = pv.Plotter()
+
+    Add the geometry actor (teapot) to the plotter
+
+    >>> actor = plotter.add_mesh(
+    ...     normals, color=(0.5, 0.8, 0.3), show_edges=True
+    ... )
+
+    Create an outline filter using VTK and add it to the plotter
+
+    >>> plotter.add_mesh(pv.outline_algorithm(normals), color="white")
+
+    Set up the camera.
+
+    >>> camera = pv.Camera()
+    >>> camera.clipping_range = (1.0, 100.0)
+    >>> camera.focal_point = (0.0, 0.5, 0.0)
+    >>> camera.position = (5.0, 6.0, 14.0)
+
+    Set up the light.
+
+    >>> light = pv.Light()
+    >>> light.focal_point = (0.21406, 1.5, 0.0)
+    >>> light.position = (7.0, 7.0, 4.0)
+
+    Set up the polar axes using VTK and add it to the plotter
+
+    >>> polaxes = vtkPolarAxesActor()
+    >>> polaxes.SetBounds(normals.bounds)
+    >>> polaxes.SetPole(0.5, 1.0, 3.0)
+    >>> polaxes.SetMaximumRadius(3.0)
+    >>> polaxes.SetMinimumAngle(-60.0)
+    >>> polaxes.SetMaximumAngle(210.0)
+    >>> polaxes.SetRequestedNumberOfRadialAxes(10)
+    >>> polaxes.SetCamera(camera)
+    >>> polaxes.SetPolarLabelFormat("%6.1f")
+    >>> polaxes.GetLastRadialAxisProperty().SetColor(0.0, 1.0, 0.0)
+    >>> polaxes.GetSecondaryRadialAxesProperty().SetColor(0.0, 0.0, 1.0)
+    >>> polaxes.GetPolarArcsProperty().SetColor(1.0, 0.0, 0.0)
+    >>> polaxes.GetSecondaryPolarArcsProperty().SetColor(1.0, 0.0, 1.0)
+    >>> polaxes.GetPolarAxisProperty().SetColor(1.0, 0.5, 0.0)
+    >>> polaxes.GetPolarAxisTitleTextProperty().SetColor(0.0, 0.0, 0.0)
+    >>> polaxes.GetPolarAxisLabelTextProperty().SetColor(1.0, 1.0, 0.0)
+    >>> polaxes.GetLastRadialAxisTextProperty().SetColor(0.0, 0.5, 0.0)
+    >>> polaxes.GetSecondaryRadialAxesTextProperty().SetColor(
+    ...     0.0, 1.0, 1.0
+    ... )
+    >>> polaxes.SetScreenSize(9.0)
+
+    Add the polar axes to the plotter
+
+    >>> plotter.add_actor(polaxes)
+
+    Set background color
+
+    >>> plotter.set_background((0.8, 0.8, 0.8))
+
+    Set the camera and light
+
+    >>> plotter.camera = camera
+    >>> plotter.add_light(light)
+
+    Show the result
+
+    >>> plotter.show(window_size=[600, 600])
+
+    """
