a3acfb3dda5064b3e6726713cab7feeb86b95fc5
Tetsuo Koyama <tkoyama010@gmail.com>
2024-10-21 23:14:19 +0900

    Fix typo

--- tests/core/test_validation.py
+++ tests/core/test_validation.py
@@ -976,7 +976,7 @@ def test_cast_to_numpy(as_any, copy, dtype):
 
 
 def test_cast_to_numpy_raises():
-    if vtk_version_info < (9.1) and sys.platform == 'linux':
+    if vtk_version_info < (9, 1) and sys.platform == 'linux':
         err = TypeError
         match = 'Object arrays are not supported.'
     else:
