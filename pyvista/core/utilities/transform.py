41001a6d960f75639e16d4096621876ae84261eb
user27182 <89109579+user27182@users.noreply.github.com>
2024-10-23 11:40:09 -0600

    Fix typo

--- pyvista/core/utilities/transform.py
+++ pyvista/core/utilities/transform.py
@@ -1689,7 +1689,7 @@ class Transform(_vtk.vtkTransform):
 
         >>> T, R, N, S, K = transform.decompose()
 
-        Since the input has no shear or this component is the identity matrix.
+        Since the input has no shear this component is the identity matrix.
         Similarly, there are no reflections so its value is ``1`. All other components
         are recovered perfectly and match the input.
 
