4a0875116e1de04c47e79d680f95765d9dde90a1
user27182 <89109579+user27182@users.noreply.github.com>
2024-10-30 12:13:55 -0600

    Update docstring

--- pyvista/core/_validation/check.py
+++ pyvista/core/_validation/check.py
@@ -672,10 +672,13 @@ def check_ndim(
     Examples
     --------
     Check if an array is one-dimensional
+
     >>> import numpy as np
     >>> from pyvista import _validation
     >>> _validation.check_ndim([1, 2, 3], ndim=1)
+
     Check if an array is two-dimensional or a scalar.
+
     >>> _validation.check_ndim(1, ndim=(0, 2))
 
     """
