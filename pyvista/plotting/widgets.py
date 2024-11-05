5c7f0088b7e3cbb81da3073c822af23bb1f199d0
Mayukh Nath <mayukh_nath@maynat-air.local>
2024-11-04 23:55:28 -0500

    fix: Docstring example for add_radio_button_widget

--- pyvista/plotting/widgets.py
+++ pyvista/plotting/widgets.py
@@ -2699,20 +2699,20 @@ class WidgetHelper:
         ...
         ...     return wrapped_callback
         ...
-        >>> p.add_radio_button_widget(
+        >>> _ = p.add_radio_button_widget(
         ...     set_bg('white'),
         ...     'bgcolor',
         ...     position=(10.0, 200.0),
         ...     title='White',
         ...     value=True,
         ... )
-        >>> p.add_radio_button_widget(
+        >>> _ = p.add_radio_button_widget(
         ...     set_bg('lightblue'),
         ...     'bgcolor',
         ...     position=(10.0, 140.0),
         ...     title='Light Blue',
         ... )
-        >>> p.add_radio_button_widget(
+        >>> _ = p.add_radio_button_widget(
         ...     set_bg('pink'),
         ...     'bgcolor',
         ...     position=(10.0, 80.0),
