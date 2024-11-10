8969a16cb9b467c0f7137e22716a69428bb0b700
user27182 <89109579+user27182@users.noreply.github.com>
2024-11-10 13:29:57 -0700

    Update language to use dependency instead of requirement

--- tests/typing/test_mypy_precommit_dependencies.py
+++ tests/typing/test_mypy_precommit_dependencies.py
@@ -13,12 +13,14 @@ if sys.version_info >= (3, 11):
 else:
     import tomli as tomllib
 
-REPO = 'https://github.com/pyvista/pre-commit-mypy'
+MYPY_PRECOMMIT_REPO = 'https://github.com/pyvista/pre-commit-mypy'
 ROOT_PATH = Path(__file__).parent.parent.parent
-PYPROJECT_TOML = ROOT_PATH / 'pyproject.toml'
+PYPROJECT_TOML_PATH = ROOT_PATH / 'pyproject.toml'
 OPTIONAL_DEPENDENCIES = 'typing'  # From`pip install pyvista[typing]`
 PRE_COMMIT_CONFIG_PATH = ROOT_PATH / '.pre-commit-config.yaml'
-SKIP_PACKAGES = ['mypy']
+SKIP_PACKAGES = [
+    'mypy',  # Mypy is part of typing env but not core dependencies
+]
 
 
 class _TestCaseTuple(NamedTuple):
@@ -28,39 +30,42 @@ class _TestCaseTuple(NamedTuple):
 
 
 def _generate_test_cases():
-    """Generate a list of requirement test cases.
+    """Generate a list of dependency test cases.
     This function:
-        (1) Generates a list of typing requirements defined in a requirements file
-        (2) Generates a list of mypy pre-commit requirements from the pre-commit config
-        (3) Merges the two lists together and returns separate test cases to
-            comparing all typing requirements to all pre-commit requirements
+        (1) Generates a list of typing dependencies defined in `pyproject.toml`
+        (2) Generates a list of mypy pre-commit dependencies from the pre-commit config
+        (3) Merges the two lists together and returns separate test cases for
+            each dependency.
+
     """
     test_cases_dict: dict = {}
 
     def add_to_dict(package_name: str, specifier: set[str], key: str):
-        # Function for stuffing image paths into a dict.
-        # We use a dict to allow for any entry to be made based on image path alone.
-        # This way, we can defer checking for any mismatch between the cached and docs
-        # images to test time.
+        # Function for stuffing dependencies into a dict.
+        # We use a dict to allow for any entry to be made based on package name alone.
+        # This way, we can defer checking for any mismatch between the typing
+        # dependencies and the pre-commit dependencies to test time.
         nonlocal test_cases_dict
         test_name = package_name
         test_cases_dict.setdefault(test_name, {})
         test_cases_dict[test_name].setdefault(key, specifier)
 
-    # process project requirements
-    project_key = PYPROJECT_TOML.name
-    typing_requirements = _get_project_dependencies(PYPROJECT_TOML, extra=OPTIONAL_DEPENDENCIES)
+    # process project dependencies
+    project_key = PYPROJECT_TOML_PATH.name
+    typing_dependencies = _get_project_dependencies(
+        PYPROJECT_TOML_PATH, extra=OPTIONAL_DEPENDENCIES
+    )
     [
         add_to_dict(package, specifier, key=project_key)
-        for package, specifier in typing_requirements.items()
+        for package, specifier in typing_dependencies.items()
     ]
 
-    # process pre-commit requirements
+    # process pre-commit dependencies
     precommit_key = PRE_COMMIT_CONFIG_PATH.name
-    precommit_requirements = _get_mypy_precommit_requirements(PRE_COMMIT_CONFIG_PATH)
+    precommit_dependencies = _get_mypy_precommit_dependencies(PRE_COMMIT_CONFIG_PATH)
     [
         add_to_dict(package, specifier, key=precommit_key)
-        for package, specifier in precommit_requirements.items()
+        for package, specifier in precommit_dependencies.items()
     ]
 
     # flatten dict
@@ -80,10 +85,10 @@ def _generate_test_cases():
 
 def pytest_generate_tests(metafunc):
     """Generate parametrized tests."""
-    if 'requirement_test_case' in metafunc.fixturenames:
+    if 'dependency_test_case' in metafunc.fixturenames:
         test_cases = _generate_test_cases()
         ids = [case.package_name for case in test_cases]
-        metafunc.parametrize('requirement_test_case', test_cases, ids=ids)
+        metafunc.parametrize('dependency_test_case', test_cases, ids=ids)
 
 
 def _get_project_dependencies(file_path, extra: str | None = None) -> dict[str, set[str]]:
@@ -115,49 +120,49 @@ def _get_project_dependencies(file_path, extra: str | None = None) -> dict[str, 
             dependencies.update(_get_project_dependencies(file_path, extra=new_extra))
             continue
 
-        _add_requirement_to_dependencies_dict(dependencies, dep)
+        _add_dependency_to_dependencies_dict(dependencies, dep)
     return dependencies
 
 
-def _add_requirement_to_dependencies_dict(dependencies: dict[str, set[str]], req: str):
-    req = Requirement(req)
-    dependencies.setdefault(req.name, set())
-    specifier = str(req.specifier)
+def _add_dependency_to_dependencies_dict(dependencies: dict[str, set[str]], dep: str):
+    dep = Requirement(dep)
+    dependencies.setdefault(dep.name, set())
+    specifier = str(dep.specifier)
     if specifier != '':
         if ',' in specifier:
-            dependencies[req.name].update(specifier.split(','))
+            dependencies[dep.name].update(specifier.split(','))
         else:
-            dependencies[req.name].add(specifier)
+            dependencies[dep.name].add(specifier)
 
 
-def _get_mypy_precommit_requirements(file_path):
+def _get_mypy_precommit_dependencies(file_path):
     # Get pre-commit config
     with open(file_path) as file:  # noqa: PTH123
         config = yaml.safe_load(file)
 
     # Extract mypy hook dependencies
     for repo in config['repos']:
-        if repo['repo'] == REPO:
-            # Update repo config
+        if repo['repo'] == MYPY_PRECOMMIT_REPO:
             hook = repo['hooks'][0]
             assert hook['id'] == 'mypy'
-            requirements_list = hook['additional_dependencies']
-            requirements_dict = {}
-            for item in requirements_list:
-                _add_requirement_to_dependencies_dict(requirements_dict, item)
-            return requirements_dict
+            dependencies_list = hook['additional_dependencies']
+            dependencies_dict = {}
+            for item in dependencies_list:
+                _add_dependency_to_dependencies_dict(dependencies_dict, item)
+            return dependencies_dict
 
     raise RuntimeError(
-        f'Pre-commit repo:\n\t{REPO}\n' f'does not exist in config:\n\t{PRE_COMMIT_CONFIG_PATH!s}\n'
+        f'Pre-commit repo:\n\t{MYPY_PRECOMMIT_REPO}\n'
+        f'does not exist in config:\n\t{PRE_COMMIT_CONFIG_PATH!s}\n'
     )
 
 
-def test_requirement(requirement_test_case):
-    package_name, project_specifier, precommit_specifier = requirement_test_case
+def test_dependency(dependency_test_case):
+    package_name, project_specifier, precommit_specifier = dependency_test_case
     if package_name in SKIP_PACKAGES:
         pytest.skip('Known skips.')
 
-    msg = _test_both_requirements_exist(*requirement_test_case)
+    msg = _test_both_dependencies_exist(*dependency_test_case)
     if msg:
         pytest.fail(msg)
     if project_specifier != precommit_specifier:
@@ -165,28 +170,28 @@ def test_requirement(requirement_test_case):
         precommit_specifier_str = ','.join(str(s) for s in precommit_specifier)
         pytest.fail(
             f"The `mypy` pre-commit dependency '{package_name+precommit_specifier_str}' in '{PRE_COMMIT_CONFIG_PATH.name}'\n'"
-            f"must match the `pyvista[typing]` dependency '{package_name+project_specifier_str}' in '{PYPROJECT_TOML.name}'."
+            f"must match the `pyvista[typing]` dependency '{package_name+project_specifier_str}' in '{PYPROJECT_TOML_PATH.name}'."
         )
 
 
-def _test_both_requirements_exist(package_name, project_requirement, precommit_requirement):
-    if project_requirement is None or precommit_requirement is None:
-        if project_requirement is None:
-            assert precommit_requirement is not None
-            missing = f"`typing` dependencies in '{PYPROJECT_TOML.name}'"
+def _test_both_dependencies_exist(package_name, project_dependency, precommit_dependency):
+    if project_dependency is None or precommit_dependency is None:
+        if project_dependency is None:
+            assert precommit_dependency is not None
+            missing = f"`typing` dependencies in '{PYPROJECT_TOML_PATH.name}'"
             exists = f"'{PRE_COMMIT_CONFIG_PATH.name}'"
             action_precommit = 'removed from'
             action_project = 'added to'
         else:
-            assert project_requirement is not None
+            assert project_dependency is not None
             missing = f"'{PRE_COMMIT_CONFIG_PATH.name}'"
-            exists = f"`typing` dependencies in '{PYPROJECT_TOML.name}'"
+            exists = f"`typing` dependencies in '{PYPROJECT_TOML_PATH.name}'"
             action_precommit = 'added to'
             action_project = 'removed from'
         return (
             f"Test setup failed for package '{package_name}'.\n"
             f'The dependency exists in {exists} but is missing from {missing}.\n'
             f'The package should be {action_precommit} the `pre-commit` `mypy` config '
-            f'or {action_project} the project requirements.'
+            f'or {action_project} the project dependencies.'
         )
     return None
