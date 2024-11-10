31b61e5aebe20145835ea45d1038f01ee4460c42
Tetsuo Koyama <tkoyama010@gmail.com>
2024-11-10 15:56:24 +0900

    Update

--- examples/01-filter/ruled_surface_filter.py
+++ examples/01-filter/ruled_surface_filter.py
@@ -3,47 +3,17 @@
 # noinspection PyUnresolvedReferences
 from __future__ import annotations
 
-from vtkmodules.vtkCommonCore import vtkPoints
-from vtkmodules.vtkCommonDataModel import vtkCellArray
-from vtkmodules.vtkCommonDataModel import vtkLine
-from vtkmodules.vtkCommonDataModel import vtkPolyData
 from vtkmodules.vtkFiltersModeling import vtkRuledSurfaceFilter
 
 import pyvista as pv
 
 pv.set_jupyter_backend('static')
 
-# Create a rendering window and renderer
 plotter = pv.Plotter()
+polydata = pv.PolyData(
+    [[0, 0, 1], [1, 0, 0], [0, 1, 0], [1, 1, 1]], lines=[[2, 0, 1], [2, 2, 3]], force_float=False
+)
 
-# Create the points for the lines.
-points = vtkPoints()
-points.InsertPoint(0, 0, 0, 1)
-points.InsertPoint(1, 1, 0, 0)
-points.InsertPoint(2, 0, 1, 0)
-points.InsertPoint(3, 1, 1, 1)
-
-# Create line1
-line1 = vtkLine()
-line1.GetPointIds().SetId(0, 0)
-line1.GetPointIds().SetId(1, 1)
-
-# Create line2
-line2 = vtkLine()
-line2.GetPointIds().SetId(0, 2)
-line2.GetPointIds().SetId(1, 3)
-
-# Create a cellArray containing the lines
-lines = vtkCellArray()
-lines.InsertNextCell(line1)
-lines.InsertNextCell(line2)
-
-# Create the vtkPolyData to contain the points and cellArray with the lines
-polydata = vtkPolyData()
-polydata.SetPoints(points)
-polydata.SetLines(lines)
-
-# Create the ruledSurfaceFilter from the polydata containing the lines
 ruledSurfaceFilter = vtkRuledSurfaceFilter()
 ruledSurfaceFilter.SetInputData(polydata)
 ruledSurfaceFilter.SetResolution(21, 21)
