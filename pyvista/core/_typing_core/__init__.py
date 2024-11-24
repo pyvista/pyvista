743e3f6746f6d044299b48aa1375a60caff76202
user27182 <89109579+user27182@users.noreply.github.com>
2024-11-24 02:20:24 -0700

    Update init

--- pyvista/core/_typing_core/__init__.py
+++ pyvista/core/_typing_core/__init__.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from ._aliases import ArrayLike  # noqa: F401
 from ._aliases import BoundsTuple  # noqa: F401
 from ._aliases import CellArrayLike  # noqa: F401
@@ -15,7 +13,3 @@ from ._aliases import TransformLike  # noqa: F401
 from ._aliases import VectorLike  # noqa: F401
 from ._array_like import NumberType  # noqa: F401
 from ._array_like import NumpyArray  # noqa: F401
-
-if TYPE_CHECKING:  # pragma: no cover
-    # Avoid circular imports
-    from ._dataset_types import DataSetType  # noqa: F401
