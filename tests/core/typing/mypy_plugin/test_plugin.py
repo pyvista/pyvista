3a294549b135e159bafcc159b93ae8c78c12e3a9
user27182 <89109579+user27182@users.noreply.github.com>
2024-12-18 18:02:57 -0700

    Only run mypy on code block and plugin files

--- tests/core/typing/mypy_plugin/test_plugin.py
+++ tests/core/typing/mypy_plugin/test_plugin.py
@@ -4,7 +4,6 @@ import os
 import sys
 from pathlib import Path
 import subprocess
-
 import pytest
 
 from pyvista.typing.mypy_plugin import promote_type
@@ -12,6 +11,7 @@ from pyvista.typing.mypy_plugin import promote_type
 TEST_DIR = str(Path(__file__).parent)
 ROOT_DIR = str(Path(TEST_DIR).parent.parent.parent.parent)
 assert 'tests' in os.listdir(ROOT_DIR)
+MYPY_PLUGIN_MODULE = str(Path(ROOT_DIR) / 'pyvista' / 'typing' / 'mypy_plugin.py' )
 MYPY_CONFIG_FILE_NO_PLUGIN = str(Path(TEST_DIR) / 'mypy_no_plugin.ini')
 MYPY_CONFIG_FILE_USE_PLUGIN = str(Path(TEST_DIR) / 'mypy_use_plugin.ini')
 
@@ -45,7 +45,7 @@ cases = [
         use_plugin=False,
         promote_type=float,
         arg_type=float,
-        expected_output='<string>:9: error: Argument 1 to "foo" has incompatible type "DuckType"; expected "float"  [arg-type]',
+        expected_output='py:9: error: Argument 1 to "foo" has incompatible type "DuckType"; expected "float"  [arg-type]',
     ),
     dict(  # Same as above, but use the plugin.
         use_plugin=True, promote_type=float, arg_type=float, expected_output=''
@@ -57,17 +57,14 @@ cases = [
         use_plugin=True,
         promote_type=float,
         arg_type=int,
-        expected_output='<string>:9: error: Argument 1 to "foo" has incompatible type "DuckType"; expected "int"  [arg-type]',
+        expected_output='py:9: error: Argument 1 to "foo" has incompatible type "DuckType"; expected "int"  [arg-type]',
     ),
 ]
 
-# Skip linux tests - mypy doggedly insists on analyzing the entire pyvista repo and fails
-# There is still indirect coverage for 'linux' through the `mypy` CI
-@pytest.mark.skipif(sys.platform == 'linux', reason="Cannot configure test paths correctly.")
 @pytest.mark.parametrize(
     ('use_plugin', 'promote_type', 'arg_type', 'expected_output'), [case.values() for case in cases]
 )
-def test_promote_type_static(use_plugin, promote_type, arg_type, expected_output):
+def test_promote_type_static(use_plugin, promote_type, arg_type, expected_output, tmp_path):
     code = f"""
 from pyvista.typing.mypy_plugin import promote_type
 
@@ -78,7 +75,7 @@ y: DuckType
 def foo(x: {arg_type.__name__}) -> None: ...
 foo(y)
 """
-    mypy_output = _run_mypy_code(code, use_plugin=use_plugin)
+    mypy_output = _run_mypy_code(code, use_plugin=use_plugin, tmp_path=tmp_path)
     stdout = str(mypy_output.stdout.decode('utf-8'))
     code = mypy_output.returncode
     if expected_output == '':
@@ -88,16 +85,24 @@ foo(y)
         assert expected_output in stdout
 
 
-def _run_mypy_code(code, use_plugin):
-    # Call mypy from the project root dir with or without the plugin enabled
+def _run_mypy_code(code, use_plugin, tmp_path):
+    """Call mypy from the project root dir with or without the plugin enabled."""
+    # Save code to tmp file
+    tmp_file = tmp_path / 'code.py'
+    with open(tmp_file, 'w', encoding='utf-8') as file:
+        file.write(code)
+
     cwd = Path.cwd()
     try:
         os.chdir(ROOT_DIR)
 
         config = MYPY_CONFIG_FILE_USE_PLUGIN if use_plugin else MYPY_CONFIG_FILE_NO_PLUGIN
-        args = ['mypy', '--show-traceback', '-c', code]
+        args = ['mypy', '--show-traceback']
 
         args.extend(['--config-file', config])
+
+        # Only run mypy on the code block and plugin module files
+        args.extend([ tmp_file, MYPY_PLUGIN_MODULE])
         return subprocess.run(args, capture_output=True)
     finally:
         os.chdir(cwd)
