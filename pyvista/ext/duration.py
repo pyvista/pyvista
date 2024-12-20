eb3749973cdcbd6720924521681177d85ae48b3b
user27182 <89109579+user27182@users.noreply.github.com>
2024-12-20 16:57:43 -0700

    Add copyright

--- pyvista/ext/duration.py
+++ pyvista/ext/duration.py
@@ -1,4 +1,40 @@
-"""Measure document reading durations."""
+"""Measure document reading durations.
+
+This is a modified version of `sphinx.ext.duration`.
+It has been modified to show more entries (default is 5, which is too few).
+
+License for Sphinx
+==================
+
+Unless otherwise indicated, all code in the Sphinx project is licenced under the
+two clause BSD licence below.
+
+Copyright (c) 2007-2024 by the Sphinx team (see AUTHORS file).
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 from __future__ import annotations
 
@@ -21,6 +57,7 @@ if TYPE_CHECKING:
         reading_durations: dict[str, float]
 
 
+N_DURATIONS = 100  # Set how many entries to show duration for
 logger = logging.getLogger(__name__)
 
 
@@ -78,15 +115,14 @@ def on_build_finished(app: Sphinx, error: Exception) -> None:
     if not domain.reading_durations:
         return
     durations = sorted(domain.reading_durations.items(), key=itemgetter(1), reverse=True)
-    n_durations = 100
 
     logger.info('')
     logger.info(
         __(
-            f'====================== slowest {n_durations} reading durations (seconds) ======================='
+            f'====================== slowest {N_DURATIONS} reading durations (seconds) ======================='
         )
     )
-    for docname, d in islice(durations, n_durations):
+    for docname, d in islice(durations, N_DURATIONS):
         logger.info(f'{d:.3f} {docname}')
 
 
