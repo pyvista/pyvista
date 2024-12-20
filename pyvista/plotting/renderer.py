72d12939b114f7103ac18a049ee37688b260b92b
user27182 <89109579+user27182@users.noreply.github.com>
2024-12-19 19:59:22 -0700

    Revert changes

--- pyvista/plotting/renderer.py
+++ pyvista/plotting/renderer.py
@@ -3447,6 +3447,17 @@ class Renderer(_vtk.vtkOpenGLRenderer):
         even when disabling the default lightkit, the scene lighting will still
         be mapped onto the actor.
 
+        >>> from pyvista import examples
+        >>> import pyvista as pv
+        >>> pl = pv.Plotter(lighting=None)
+        >>> cubemap = examples.download_sky_box_cube_map()
+        >>> _ = pl.add_mesh(
+        ...     pv.Sphere(), pbr=True, metallic=0.9, roughness=0.4
+        ... )
+        >>> pl.set_environment_texture(cubemap)
+        >>> pl.camera_position = 'xy'
+        >>> pl.show()
+
         """
         # cube_map textures cannot use spherical harmonics
         if texture.cube_map:
