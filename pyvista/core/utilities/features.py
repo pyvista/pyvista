fdb217d87d15e3f847c5d432b08a2cab68c22ad4
user27182 <89109579+user27182@users.noreply.github.com>
2024-12-30 18:27:54 -0700

    Remove examples

--- pyvista/core/utilities/features.py
+++ pyvista/core/utilities/features.py
@@ -423,59 +423,6 @@ def voxelize(
     pyvista.PolyDataFilters.voxelize_binary_mask
         Similar function that returns a :class:`pyvista.ImageData` with point data.
 
-    Examples
-    --------
-    Create an equal density voxelized mesh.
-
-    >>> import pyvista as pv
-    >>> from pyvista import examples
-    >>> mesh = examples.download_bunny_coarse().clean()
-    >>> vox = pv.voxelize(mesh, density=0.01)  # doctest: +SKIP
-    >>> vox.plot(show_edges=True)  # doctest: +SKIP
-
-    Create a voxelized mesh using unequal density dimensions.
-
-    >>> vox = pv.voxelize(mesh, density=[0.01, 0.005, 0.002])  # doctest: +SKIP
-    >>> vox.plot(show_edges=True)  # doctest: +SKIP
-
-    Create an equal density voxel volume without enclosing input mesh.
-
-    >>> vox = pv.voxelize(mesh, density=0.01)  # doctest: +SKIP
-    >>> vox = vox.select_enclosed_points(mesh, tolerance=0.0)  # doctest: +SKIP
-    >>> vox.plot(scalars='SelectedPoints', show_edges=True)  # doctest: +SKIP
-
-    Create an equal density voxel volume enclosing input mesh.
-
-    >>> vox = pv.voxelize(mesh, density=0.01, enclosed=True)  # doctest: +SKIP
-    >>> vox = vox.select_enclosed_points(mesh, tolerance=0.0)  # doctest: +SKIP
-    >>> vox.plot(scalars='SelectedPoints', show_edges=True)  # doctest: +SKIP
-
-    Create a voxelized mesh that does not fit the input mesh's bounds. Notice the
-    cropped rectangular box.
-
-    >>> mesh = pv.Cube(x_length=0.25)
-    >>> vox = pv.voxelize(mesh=mesh, density=0.2)  # doctest: +SKIP
-    >>> pl = pv.Plotter()
-    >>> _ = pl.add_mesh(
-    ...     mesh=vox, show_edges=True, color='yellow'
-    ... )  # doctest: +SKIP
-    >>> _ = pl.add_mesh(mesh=mesh, show_edges=True, line_width=5, opacity=0.4)
-    >>> pl.show()
-
-    Create a voxelized mesh that fits the input mesh's bounds. The rectangular mesh is
-    now complete. Notice that the voxel size was updated to fit the bounds in the first
-    direction.
-
-    >>> vox = pv.voxelize(
-    ...     mesh=mesh, density=0.2, fit_bounds=True
-    ... )  # doctest: +SKIP
-    >>> pl = pv.Plotter()
-    >>> _ = pl.add_mesh(
-    ...     mesh=vox, show_edges=True, color='yellow'
-    ... )  # doctest: +SKIP
-    >>> _ = pl.add_mesh(mesh=mesh, show_edges=True, line_width=5, opacity=0.4)
-    >>> pl.show()
-
     """
     warnings.warn('deprecated', PyVistaDeprecationWarning)
     if not pyvista.is_pyvista_dataset(mesh):
