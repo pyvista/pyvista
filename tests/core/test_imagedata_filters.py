4993cf9269b8254db96679141ef747cc537dd84e
user27182 <89109579+user27182@users.noreply.github.com>
2025-01-04 11:02:05 -0700

    Update tests

--- tests/core/test_imagedata_filters.py
+++ tests/core/test_imagedata_filters.py
@@ -349,43 +349,12 @@ def test_contour_labels_pad_background(labeled_image):
 @pytest.mark.parametrize('boundary_type', ['all', 'internal', 'external'])
 @pytest.mark.parametrize('simplify_output', [True, False, None])
 @pytest.mark.needs_vtk_version(9, 3, 0)
-def test_simplify_output(labeled_image, boundary_type, simplify_output):
+def test_contour_labels_simplify_output(labeled_image, boundary_type, simplify_output):
     poly = labeled_image.contour_labels(boundary_type, simplify_output=simplify_output)
     expected_ndim = (
         1 if simplify_output or (simplify_output is None and boundary_type == 'external') else 2
     )
     assert poly[BOUNDARY_LABELS].ndim == expected_ndim
-
-
-@pytest.mark.needs_vtk_version(9, 3, 0)
-def test_contour_labels_simplify_output(labeled_image):
-    # Test `None` (implicit behavior)
-    poly = labeled_image.contour_labels('external')
-    labels = poly[BOUNDARY_LABELS]
-    assert labels.ndim == 1
-    assert np.array_equal(np.unique(labels), [2, 5])
-
-    poly = labeled_image.contour_labels('internal')
-    labels = poly[BOUNDARY_LABELS]
-    assert labels.ndim == 2
-    assert np.array_equal(np.unique(labels, axis=0), [[2, 5]])
-
-    poly = labeled_image.contour_labels('all')
-    labels = poly[BOUNDARY_LABELS]
-    assert labels.ndim == 2
-    assert np.array_equal(np.unique(labels, axis=0), [[2, 0], [2, 5], [5, 0]])
-
-    # Test `True`
-    poly = labeled_image.contour_labels('external', simplify_output=True)
-    assert poly[BOUNDARY_LABELS].ndim == 1
-    poly = labeled_image.contour_labels('internal', simplify_output=True)
-    assert poly[BOUNDARY_LABELS].ndim == 1
-
-    # Test `False`
-    poly = labeled_image.contour_labels('external', simplify_output=False)
-    assert poly[BOUNDARY_LABELS].ndim == 2
-    poly = labeled_image.contour_labels('internal', simplify_output=False)
-    assert poly[BOUNDARY_LABELS].ndim == 2
 
 
 @pytest.mark.needs_vtk_version(9, 3, 0)
