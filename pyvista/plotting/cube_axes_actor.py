99c854e71895413667d9d287bb87d91016926cd1
beroda <117822458+beroda@users.noreply.github.com>
2025-01-09 00:31:52 +0100

    Fix typing

--- pyvista/plotting/cube_axes_actor.py
+++ pyvista/plotting/cube_axes_actor.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from collections.abc import MutableSequence
 from typing import TYPE_CHECKING
 from typing import cast
 import warnings
@@ -294,26 +295,26 @@ class CubeAxesActor(_vtk.vtkCubeAxesActor):
         """Return or set the distance between title and labels."""
         if pyvista.vtk_version_info >= (9, 3):
             offx, offy = (_vtk.reference(0.0), _vtk.reference(0.0))
-            self.GetTitleOffset(offx, offy)
-            return offx, offy
+            self.GetTitleOffset(offx, offy)  # type: ignore[call-overload]
+            return offx, offy  # type: ignore[return-value]
 
         return self.GetTitleOffset()
 
     @title_offset.setter
-    def title_offset(self, offset: float | tuple[float, float]):
+    def title_offset(self, offset: float | MutableSequence[float]):
         vtk_geq_9_3 = pyvista.vtk_version_info >= (9, 3)
 
         if vtk_geq_9_3:
             if isinstance(offset, float):
-                msg = f'Setting title_offset with a float is deprecated from vtk >= 9.3. Accepts now a tuple of (x,y) offsets. Setting the x offset to {(x:=0.0)}'
+                msg = f'Setting title_offset with a float is deprecated from vtk >= 9.3. Accepts now a sequence of (x,y) offsets. Setting the x offset to {(x:=0.0)}'
                 warnings.warn(msg, UserWarning)
-                self.SetTitleOffset((x, offset))
+                self.SetTitleOffset([x, offset])
             else:
                 self.SetTitleOffset(offset)
             return
 
-        if isinstance(offset, tuple):
-            msg = f'Setting title_offset with a tuple is only supported from vtk >= 9.3. Considering only the second value (ie. y-offset) of {(y:=offset[1])}'
+        if isinstance(offset, MutableSequence):
+            msg = f'Setting title_offset with a sequence is only supported from vtk >= 9.3. Considering only the second value (ie. y-offset) of {(y:=offset[1])}'
             warnings.warn(msg, UserWarning)
             self.SetTitleOffset(y)
             return
