7cdcbd6b5b39315d727915abc6f5a572c8ec1a03
user27182 <89109579+user27182@users.noreply.github.com>
2025-01-10 18:44:44 -0700

    Revert changes

--- pyvista/core/_vtk_core.py
+++ pyvista/core/_vtk_core.py
@@ -103,6 +103,7 @@ from vtkmodules.vtkCommonCore import VTK_ARIAL as VTK_ARIAL
 from vtkmodules.vtkCommonCore import VTK_COURIER as VTK_COURIER
 from vtkmodules.vtkCommonCore import VTK_FONT_FILE as VTK_FONT_FILE
 from vtkmodules.vtkCommonCore import VTK_TIMES as VTK_TIMES
+from vtkmodules.vtkCommonCore import VTK_UNSIGNED_CHAR as VTK_UNSIGNED_CHAR
 from vtkmodules.vtkCommonCore import buffer_shared as buffer_shared  # type: ignore[attr-defined]
 from vtkmodules.vtkCommonCore import mutable as mutable
 from vtkmodules.vtkCommonCore import reference as reference
@@ -281,12 +282,6 @@ with contextlib.suppress(ImportError):  # Introduced prior to VTK 9.2
     from vtkmodules.vtkCommonDataModel import VTK_TRIQUADRATIC_PYRAMID as VTK_TRIQUADRATIC_PYRAMID
     from vtkmodules.vtkCommonDataModel import vtkTriQuadraticPyramid as vtkTriQuadraticPyramid
 
-from vtkmodules.util.vtkConstants import VTK_DOUBLE as VTK_DOUBLE
-from vtkmodules.util.vtkConstants import VTK_DOUBLE_MAX as VTK_DOUBLE_MAX
-from vtkmodules.util.vtkConstants import VTK_DOUBLE_MIN as VTK_DOUBLE_MIN
-from vtkmodules.util.vtkConstants import VTK_SHORT as VTK_SHORT
-from vtkmodules.util.vtkConstants import VTK_UNSIGNED_CHAR as VTK_UNSIGNED_CHAR
-from vtkmodules.util.vtkConstants import VTK_UNSIGNED_SHORT as VTK_UNSIGNED_SHORT
 from vtkmodules.vtkCommonExecutionModel import vtkAlgorithm as vtkAlgorithm
 from vtkmodules.vtkCommonExecutionModel import vtkAlgorithmOutput as vtkAlgorithmOutput
 from vtkmodules.vtkCommonExecutionModel import vtkCompositeDataPipeline as vtkCompositeDataPipeline
--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -4897,8 +4897,3 @@ def test_bitwise_and_or_of_polydata(operator):
     pl.add_mesh(result, color='lightblue')
     pl.camera_position = 'xz'
     pl.show()
-
-
-# def test_resample_imagedata(uniform):
-#
-#     uniform.resample
