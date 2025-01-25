6c46688e7cccbb300ca5d18a1804513e112285fa
Bane Sullivan <banesullivan@gmail.com>
2025-01-24 18:37:52 -0800

    Handle issue with UnstructuredGrid.faces in VTK 9.4+

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -2057,6 +2057,20 @@ class UnstructuredGrid(PointGrid, UnstructuredGridFilters, _vtk.vtkUnstructuredG
         return _vtk.vtkCellArray() if cells is None else cells  # type: ignore[redundant-expr]
 
     @property
+    def faces(self):
+        """Invalid property.
+
+        Override the faces property to return nothing since VTK 9.4+ attempts
+        to map this to GetFaces which in previous versions was a method that
+        returns None for UnstructuredGrids. However, in VTK 9.4+ GetFaces for
+        UnstructuredGrids is a deprecated with preference for GetPolyhedronFaces.
+        For now, we just return None until we can figure out a better solution.
+
+        Issue reported in https://gitlab.kitware.com/vtk/vtk/-/issues/19591
+        """
+        return None
+
+    @property
     def cells_dict(self) -> dict[int, NumpyArray[float]]:  # numpydoc ignore=RT01
         """Return a dictionary that contains all cells mapped from cell types.
 
