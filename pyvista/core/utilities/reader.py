37fdc71a5bf1246c5a5aa7f193da290899064398
user27182 <89109579+user27182@users.noreply.github.com>
2025-01-25 13:01:21 -0700

    Fix docstring

--- pyvista/core/utilities/reader.py
+++ pyvista/core/utilities/reader.py
@@ -2491,10 +2491,15 @@ class HDRReader(BaseReader):
     >>> reader = pv.get_reader(filename)
     >>> mesh = reader.read()
     >>> mesh
-    Texture (...)
-      Components:   3
-      Cube Map:     False
-      Dimensions:   4096, 2048
+    ImageData (...)
+      N Cells:      8382465
+      N Points:     8388608
+      X Bounds:     0.000e+00, 4.095e+03
+      Y Bounds:     0.000e+00, 2.047e+03
+      Z Bounds:     0.000e+00, 0.000e+00
+      Dimensions:   4096, 2048, 1
+      Spacing:      1.000e+00, 1.000e+00, 1.000e+00
+      N Arrays:     1
 
     """
 
