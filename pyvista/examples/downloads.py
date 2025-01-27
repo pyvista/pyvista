02016a256a6f5fc57f6b1957a095e1575c56f2a2
user27182 <89109579+user27182@users.noreply.github.com>
2025-01-27 15:48:28 -0700

    Downsample damavand volcano example

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -3951,13 +3951,24 @@ def download_damavand_volcano(load=True):  # pragma: no cover
 
     Examples
     --------
+    Load the dataset.
+
     >>> from pyvista import examples
+    >>> dataset = examples.download_damavand_volcano()
+
+    Use :meth:`~pyvista.ImageDataFilters.resample` to downsample it before plotting.
+
+    >>> dataset = dataset.resample(0.5)
+    >>> dataset.dimensions
+    (140, 116, 85)
+
+    Plot it.
+
     >>> cpos = [
     ...     [4.66316700e04, 4.32796241e06, -3.82467050e05],
     ...     [5.52532740e05, 3.98017300e06, -2.47450000e04],
     ...     [4.10000000e-01, -2.90000000e-01, -8.60000000e-01],
     ... ]
-    >>> dataset = examples.download_damavand_volcano()
     >>> dataset.plot(cpos=cpos, cmap='reds', show_scalar_bar=False, volume=True)
 
     .. seealso::
