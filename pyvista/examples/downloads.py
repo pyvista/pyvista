89ae315066afc0fa9d3368f4bad444b4ac7931fd
user27182 <89109579+user27182@users.noreply.github.com>
2025-01-31 17:48:57 -0700

    Revert unused changes

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -4394,23 +4394,17 @@ def download_cubemap_space_4k(load=True):  # pragma: no cover
 
     Examples
     --------
-    Load and plot the texture.
+    Display the cubemap as both an environment texture and an actor.
 
     >>> import pyvista as pv
     >>> from pyvista import examples
     >>> cubemap = examples.download_cubemap_space_4k()
-    >>> cubemap.plot()
-
-    Display the cubemap as both an environment texture and an actor.
-
-    >>> pl = pv.Plotter(lighting=None)  # doctest:+SKIP
-    >>> _ = pl.add_actor(cubemap.to_skybox())  # doctest:+SKIP
-    >>> pl.set_environment_texture(cubemap, True)  # doctest:+SKIP
-    >>> pl.camera.zoom(0.4)  # doctest:+SKIP
-    >>> _ = pl.add_mesh(
-    ...     pv.Sphere(), pbr=True, roughness=0.24, metallic=1.0
-    ... )  # doctest:+SKIP
-    >>> pl.show()  # doctest:+SKIP
+    >>> pl = pv.Plotter(lighting=None)
+    >>> _ = pl.add_actor(cubemap.to_skybox())
+    >>> pl.set_environment_texture(cubemap, True)
+    >>> pl.camera.zoom(0.4)
+    >>> _ = pl.add_mesh(pv.Sphere(), pbr=True, roughness=0.24, metallic=1.0)
+    >>> pl.show()
 
     .. seealso::
 
@@ -4462,25 +4456,19 @@ def download_cubemap_space_16k(load=True):  # pragma: no cover
 
     Examples
     --------
-    Load and plot the texture. Note that here we're displaying the 4k version as the
-    16k version is a bit too expensive to display in the documentation.
+    Display the cubemap as both an environment texture and an actor. Note that
+    here we're displaying the 4k as the 16k is a bit too expensive to display
+    in the documentation.
 
     >>> import pyvista as pv
     >>> from pyvista import examples
     >>> cubemap = examples.download_cubemap_space_4k()
-    >>> cubemap.plot()
-
-    Display the cubemap as both an environment texture and an actor.
-
-    >>> cubemap = examples.download_cubemap_space_4k()  # doctest:+SKIP
-    >>> pl = pv.Plotter(lighting=None)  # doctest:+SKIP
-    >>> _ = pl.add_actor(cubemap.to_skybox())  # doctest:+SKIP
-    >>> pl.set_environment_texture(cubemap, True)  # doctest:+SKIP
-    >>> pl.camera.zoom(0.4)  # doctest:+SKIP
-    >>> _ = pl.add_mesh(
-    ...     pv.Sphere(), pbr=True, roughness=0.24, metallic=1.0
-    ... )  # doctest:+SKIP
-    >>> pl.show()  # doctest:+SKIP
+    >>> pl = pv.Plotter(lighting=None)
+    >>> _ = pl.add_actor(cubemap.to_skybox())
+    >>> pl.set_environment_texture(cubemap, True)
+    >>> pl.camera.zoom(0.4)
+    >>> _ = pl.add_mesh(pv.Sphere(), pbr=True, roughness=0.24, metallic=1.0)
+    >>> pl.show()
 
     .. seealso::
 
