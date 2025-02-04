6d3bf16ff5d9329c90c6d88ac71f6599325781c3
user27182 <89109579+user27182@users.noreply.github.com>
2025-02-03 20:33:43 -0700

    Fix rst

--- pyvista/core/composite.py
+++ pyvista/core/composite.py
@@ -328,7 +328,7 @@ class MultiBlock(
 
                 .. warning::
 
-                    The ``MultiBlock``'s :meth:`keys` do not enforce unique values. If any name
+                    ``MultiBlock`` :meth:`keys` do not enforce unique values. If any name
                     is duplicated in any nested blocks, then the names of these blocks
                     in the flattened ``MultiBlock`` will also be duplicated and may be
                     ambiguous.
