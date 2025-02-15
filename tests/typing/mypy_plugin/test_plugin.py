c22309ce82276766910af7fb958b79edc0281f9d
user27182 <89109579+user27182@users.noreply.github.com>
2025-02-15 13:20:36 -0700

    Try to fix mypy path issue

--- tests/typing/mypy_plugin/test_plugin.py
+++ tests/typing/mypy_plugin/test_plugin.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 from pathlib import Path
 import subprocess
+import sys
 
 import pytest
 
@@ -97,7 +98,7 @@ def _run_mypy_code(code, use_plugin, tmp_path):
     try:
         # Use '--follow-imports=skip' to only analyze the files passed to mypy
         # otherwise it will analyze the entire pyvista library
-        args = ['mypy', '--show-traceback', '--follow-imports=skip']
+        args = [sys.executable, '-m', 'mypy', '--show-traceback', '--follow-imports=skip']
 
         # Set config file
         config = MYPY_CONFIG_FILE_USE_PLUGIN if use_plugin else MYPY_CONFIG_FILE_NO_PLUGIN
