8c74c0f7e05c7dfb74f16f7232903f65cee4a450
user27182 <89109579+user27182@users.noreply.github.com>
2025-02-15 19:19:56 -0700

    Revert "Use suprocess env"
    This reverts commit f31fe6f5961e9d8b5a8cf99adbaed5e6c11166e8.

--- tests/typing/mypy_plugin/test_plugin.py
+++ tests/typing/mypy_plugin/test_plugin.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 from pathlib import Path
 import subprocess
-import sys
 
 import pytest
 
@@ -98,7 +97,7 @@ def _run_mypy_code(code, use_plugin, tmp_path):
     try:
         # Use '--follow-imports=skip' to only analyze the files passed to mypy
         # otherwise it will analyze the entire pyvista library
-        args = [sys.executable, '-m', 'mypy', '--show-traceback', '--follow-imports=skip']
+        args = ['mypy', '--show-traceback', '--follow-imports=skip']
 
         # Set config file
         config = MYPY_CONFIG_FILE_USE_PLUGIN if use_plugin else MYPY_CONFIG_FILE_NO_PLUGIN
@@ -106,9 +105,6 @@ def _run_mypy_code(code, use_plugin, tmp_path):
 
         # Only run mypy on the code block and plugin module files
         args.extend([tmp_file, MYPY_PLUGIN_MODULE])
-
-        env = os.environ.copy()
-        env['PYTHONPATH'] = ROOT_DIR  # Add project root so that "import pyvista" works
-        return subprocess.run(args, capture_output=True, env=env)
+        return subprocess.run(args, capture_output=True)
     finally:
         os.chdir(cwd)
