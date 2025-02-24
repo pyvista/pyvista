5a53258863f972cadab4606e0452542dd432ea20
beroda <117822458+beroda@users.noreply.github.com>
2025-02-24 22:29:31 +0100

    Add xvfb raises tests

--- tests/plotting/test_utilities.py
+++ tests/plotting/test_utilities.py
@@ -2,11 +2,17 @@ from __future__ import annotations
 
 import os
 import platform
+import re
+from typing import TYPE_CHECKING
 
 import pytest
 
 import pyvista as pv
 from pyvista.core.errors import PyVistaDeprecationWarning
+from pyvista.plotting.utilities import xvfb
+
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
 
 skip_windows = pytest.mark.skipif(os.name == 'nt', reason='Test fails on Windows')
 skip_mac = pytest.mark.skipif(
@@ -22,3 +28,23 @@ def test_start_xvfb():
         pv.start_xvfb()
     if pv._version.version_info[:2] > (0, 48):
         raise RuntimeError('Remove this method')
+
+
+def test_start_xvfb_raises(monkeypatch: pytest.MonkeyPatch, mocker: MockerFixture):
+    monkeypatch.setattr(os, 'name', 'foo')
+    with (
+        pytest.raises(OSError, match='`start_xvfb` is only supported on Linux'),
+        pytest.warns(PyVistaDeprecationWarning),
+    ):
+        pv.start_xvfb()
+
+    monkeypatch.setattr(os, 'name', 'posix')
+
+    m = mocker.patch.object(os, 'system')
+    m.return_value = True
+
+    with (
+        pytest.raises(OSError, match=re.escape(xvfb.XVFB_INSTALL_NOTES)),
+        pytest.warns(PyVistaDeprecationWarning),
+    ):
+        pv.start_xvfb()
