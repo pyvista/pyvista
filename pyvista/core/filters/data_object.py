142a2ab1e7daf5c91d836b6121e43c87017704f1
user27182 <89109579+user27182@users.noreply.github.com>
2025-03-01 23:42:11 -0700

    Compute visible bounds by default (#7206)
    * Compute visible bounds by default
    
    * Fix type errors
    
    * Update docs
    
    * Remove actor ignores
    
    * Add tests
    
    * Remove actor name
    
    * Update docs
    
    * Use ComputeVisiblePropBounds
    
    * Fix dtype
    
    * Add files via upload
    
    * Fix example with CubeAxesActor
    
    ---------
    
    Co-authored-by: Tetsuo Koyama <tkoyama010@gmail.com>
    Co-authored-by: Bane Sullivan <banesullivan@gmail.com>

--- pyvista/core/filters/data_object.py
+++ pyvista/core/filters/data_object.py
@@ -752,17 +752,22 @@ class DataObjectFilters:
         --------
         >>> import pyvista as pv
         >>> from pyvista import examples
-        >>> pl = pv.Plotter(shape=(1, 2))
-        >>> pl.subplot(0, 0)
-        >>> pl.show_axes()
-        >>> _ = pl.show_grid()
         >>> mesh1 = examples.download_teapot()
+        >>> mesh2 = mesh1.scale([10.0, 10.0, 10.0], inplace=False)
+
+        Plot meshes side-by-side
+
+        >>> pl = pv.Plotter(shape=(1, 2))
+        >>> # Create plot with unscaled mesh
+        >>> pl.subplot(0, 0)
         >>> _ = pl.add_mesh(mesh1)
-        >>> pl.subplot(0, 1)
         >>> pl.show_axes()
         >>> _ = pl.show_grid()
-        >>> mesh2 = mesh1.scale([10.0, 10.0, 10.0], inplace=False)
+        >>> # Create plot with scaled mesh
+        >>> pl.subplot(0, 1)
         >>> _ = pl.add_mesh(mesh2)
+        >>> pl.show_axes()
+        >>> _ = pl.show_grid()
         >>> pl.show(cpos='xy')
 
         """
