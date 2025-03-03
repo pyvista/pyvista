05e733de32b8fc157aa2edd2483741d5d1b0aae0
Bane Sullivan <banesullivan@gmail.com>
2025-03-02 20:04:57 -0800

    Set test_plot_cubemap_alone as high_variance_test

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -3982,8 +3982,9 @@ def test_plot_texture_flip_y(texture):
 @pytest.mark.needs_vtk_version(9, 2, 0)
 @pytest.mark.skipif(CI_WINDOWS, reason='Windows CI testing segfaults on pbr')
 @pytest.mark.skipif(pv.vtk_version_info >= (9, 3), reason='This is broken on VTK 9.3')
-def test_plot_cubemap_alone(cubemap):
+def test_plot_cubemap_alone(cubemap, verify_image_cache):
     """Test plotting directly from the Texture class."""
+    verify_image_cache.high_variance_test = True
     cubemap.plot()
 
 
