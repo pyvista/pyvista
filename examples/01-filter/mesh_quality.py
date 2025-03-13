10334867248fd03da09b66e2b787b82a216b98f5
user27182 <89109579+user27182@users.noreply.github.com>
2025-03-13 16:37:55 -0600

    Update example using subplot

--- examples/01-filter/mesh_quality.py
+++ examples/01-filter/mesh_quality.py
@@ -6,29 +6,58 @@ Computing Mesh Quality
 
 Leverage powerful VTK algorithms for computing mesh quality.
 
-Here we will use the :func:`pyvista.DataSetFilters.compute_cell_quality` filter
+Here we will use the :func:`~pyvista.DataSetFilters.compute_cell_quality` filter
 to compute the cell qualities. For a full list of the various quality metrics
 available, please refer to the documentation for that filter.
 """
 
 from __future__ import annotations
 
+import pyvista as pv
 from pyvista import examples
 
 mesh = examples.download_cow().triangulate().decimate(0.7)
 
-cpos = [
-    (10.10963531890468, 4.61130688407898, -4.503884867626516),
-    (1.2896420468715433, -0.055387528972708225, 1.1228250502811408),
-    (-0.2970769821136617, 0.9100381451936025, 0.2890948650371137),
-]
+# %%
+# Compute the cell quality. By default, the ``'scaled_jacobian'`` measure is computed.
+
+mesh = mesh.compute_cell_quality()
+mesh
 
 # %%
-# Compute the cell quality. Note that there are many different quality measures
-qual = mesh.compute_cell_quality(quality_measure='scaled_jacobian')
-qual
+# Plot the mesh. We define a custom method to add a mesh and set default plot values.
+
+
+def add_mesh(plotter, mesh_, scalars=None, cmap='bwr', show_edges=True, zoom=1.3):
+    plotter.add_mesh(mesh_, scalars=scalars, cmap=cmap, show_edges=show_edges)
+    plotter.view_xy()
+    plotter.camera.zoom(zoom)
+
+
+pl = pv.Plotter()
+add_mesh(pl, mesh, scalars='scaled_jacobian')
+pl.show()
 
 # %%
-qual.plot(cpos=cpos, scalars='CellQuality')
+# Note that there are many different quality measures, many of which may only apply to
+# 2D cells or 3D cells (see :class:`~pyvista.CellType` for more information about cell
+# types). For :attr:`~pyvista.CellType.TRIANGLE` and :attr:`~pyvista.CellType.QUAD`
+# cells, the following measures produce meaningful values.
+
+measures = ['area', 'max_angle', 'min_angle', 'shape']
+mesh = mesh.compute_cell_quality(measures)
+mesh
+
+
 # %%
-# .. tags:: filter
+pl = pv.Plotter(shape=(2, 2))
+pl.link_views()
+pl.subplot(0, 0)
+add_mesh(pl, mesh, scalars=measures[0])
+pl.subplot(0, 1)
+add_mesh(pl, mesh, scalars=measures[1])
+pl.subplot(1, 0)
+add_mesh(pl, mesh, scalars=measures[2])
+pl.subplot(1, 1)
+add_mesh(pl, mesh, scalars=measures[3])
+pl.show()
