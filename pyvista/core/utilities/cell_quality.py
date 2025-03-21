144132010c52c687b22e2c8e76f500b05d656f41
user27182 <89109579+user27182@users.noreply.github.com>
2025-03-21 07:52:57 -0600

    Add examples and use floats

--- pyvista/core/utilities/cell_quality.py
+++ pyvista/core/utilities/cell_quality.py
@@ -67,81 +67,105 @@ R22 = sqrt(2) / 2
 R33 = sqrt(3) / 3
 
 _CELL_QUALITY_INFO = [
-    CellQualityInfo(CellType.TRIANGLE, 'area', (0, INF), (0, INF), (0, INF), sqrt(3) / 4),
-    CellQualityInfo(CellType.TRIANGLE, 'aspect_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TRIANGLE, 'aspect_frobenius', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TRIANGLE, 'condition', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TRIANGLE, 'distortion', (0.5, 1), (0, 1), (-INF, INF), 1),
-    # CellQualityInfo(CellType.TRIANGLE, 'edge_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TRIANGLE, 'max_angle', (60, 90), (60, 180), (0, 180), 60),
-    CellQualityInfo(CellType.TRIANGLE, 'min_angle', (30, 60), (0, 60), (0, 360), 60),
+    CellQualityInfo(CellType.TRIANGLE, 'area', (0.0, INF), (0.0, INF), (0.0, INF), sqrt(3.0) / 4.0),
+    CellQualityInfo(CellType.TRIANGLE, 'aspect_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TRIANGLE, 'aspect_frobenius', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TRIANGLE, 'condition', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TRIANGLE, 'distortion', (0.5, 1.0), (0.0, 1.0), (-INF, INF), 1.0),
+    # CellQualityInfo(CellType.TRIANGLE, 'edge_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
     CellQualityInfo(
-        CellType.TRIANGLE, 'scaled_jacobian', (0.5, 2 * R33), (-2 * R33, 2 * R33), (-INF, INF), 1
+        CellType.TRIANGLE, 'max_angle', (60.0, 90.0), (60.0, 180.0), (0.0, 180.0), 60.0
     ),
-    CellQualityInfo(CellType.TRIANGLE, 'radius_ratio', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TRIANGLE, 'shape', (0.25, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.TRIANGLE, 'shape_and_size', (0.25, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.QUAD, 'area', (0, INF), (0, INF), (-INF, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'aspect_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'condition', (1, 4), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'distortion', (0.5, 1), (0, 1), (-INF, INF), 1),
-    # CellQualityInfo(CellType.QUAD, 'edge_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'jacobian', (0, INF), (0, INF), (-INF, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'max_aspect_frobenius', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'max_angle', (90, 135), (90, 360), (0, 360), 90),
-    CellQualityInfo(CellType.QUAD, 'max_edge_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'med_aspect_frobenius', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'min_angle', (45, 90), (0, 90), (0, 360), 90),
-    CellQualityInfo(CellType.QUAD, 'oddy', (0, 0.5), (0, INF), (0, INF), 0),
-    CellQualityInfo(CellType.QUAD, 'radius_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'relative_size_squared', (0.3, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.QUAD, 'scaled_jacobian', (0.3, 1), (-1, 1), (-1, 1), 1),
-    CellQualityInfo(CellType.QUAD, 'shape', (0.3, 1), (0, 1), (0, 1), 1),
-    CellQualityInfo(CellType.QUAD, 'shape_and_size', (0.2, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.QUAD, 'shear', (0.3, 1), (0, 1), (0, 1), 1),
-    CellQualityInfo(CellType.QUAD, 'shear_and_size', (0.2, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.QUAD, 'skew', (0.5, 1), (0, 1), (0, 1), 1),
-    CellQualityInfo(CellType.QUAD, 'stretch', (0.25, 1), (0, 1), (0, INF), 1),
-    CellQualityInfo(CellType.QUAD, 'taper', (0, 0.7), (0, INF), (0, INF), 0),
-    CellQualityInfo(CellType.QUAD, 'warpage', (0, 0.7), (0, 2), (0, INF), 0),
-    # CellQualityInfo(CellType.TETRA, 'aspect_beta', (1, 3), (1, INF), (1, INF), 1),
-    # CellQualityInfo(CellType.TETRA, 'aspect_delta', (0.1, INF), (0, INF), (0, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'aspect_frobenius', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'aspect_gamma', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'aspect_ratio', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'collapse_ratio', (0.1, INF), (0, INF), (0, INF), sqrt(6) / 3),
-    CellQualityInfo(CellType.TETRA, 'condition', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'distortion', (0.5, 1), (0, 1), (-INF, INF), 0),
-    # CellQualityInfo(CellType.TETRA, 'edge_ratio', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'jacobian', (0, INF), (0, INF), (-INF, INF), sqrt(2) / 2),
-    CellQualityInfo(CellType.TETRA, 'min_angle', (40, ANGLE), (0, ANGLE), (0, 360), ANGLE),
-    CellQualityInfo(CellType.TETRA, 'radius_ratio', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'relative_size_squared', (0.3, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.TETRA, 'scaled_jacobian', (0.5, R22), (-R22, R22), (-INF, INF), 1),
-    CellQualityInfo(CellType.TETRA, 'shape', (0.3, 1), (0, 1), (0, 1), 1),
-    CellQualityInfo(CellType.TETRA, 'shape_and_size', (0.2, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.TETRA, 'volume', (0, INF), (-INF, INF), (-INF, INF), sqrt(2) / 12),
-    CellQualityInfo(CellType.HEXAHEDRON, 'diagonal', (0.65, 1), (0, 1), (1, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'dimension', None, (0, INF), (0, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'distortion', (0.5, 1), (0, 1), (-INF, INF), 1),
-    # CellQualityInfo(CellType.HEXAHEDRON, 'edge_ratio', None, (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'jacobian', (0, INF), (0, INF), (-INF, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'max_edge_ratio', (1, 1.3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'max_aspect_frobenius', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'med_aspect_frobenius', (1, 3), (1, INF), (1, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'oddy', (0, 0.5), (0, INF), (0, INF), 0),
-    CellQualityInfo(CellType.HEXAHEDRON, 'relative_size_squared', (0.5, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.HEXAHEDRON, 'scaled_jacobian', (0.5, 1), (-1, 1), (-1, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'shape', (0.3, 1), (0, 1), (0, 1), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'shape_and_size', (0.2, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.HEXAHEDRON, 'shear', (0.3, 1), (0, 1), (0, 1), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'shear_and_size', (0.2, 1), (0, 1), (0, 1), None),
-    CellQualityInfo(CellType.HEXAHEDRON, 'skew', (0, 0.5), (0, 1), (0, INF), 0),
-    CellQualityInfo(CellType.HEXAHEDRON, 'stretch', (0.25, 1), (0, 1), (0, INF), 1),
-    CellQualityInfo(CellType.HEXAHEDRON, 'taper', (0, 0.5), (0, INF), (0, INF), 0),
-    CellQualityInfo(CellType.HEXAHEDRON, 'volume', (0, INF), (0, INF), (-INF, INF), 2),
-    CellQualityInfo(CellType.PYRAMID, 'volume', (0, INF), (-INF, INF), (-INF, INF), sqrt(2) / 6),
-    CellQualityInfo(CellType.WEDGE, 'volume', (0, INF), (-INF, INF), (-INF, INF), sqrt(3) / 4),
+    CellQualityInfo(CellType.TRIANGLE, 'min_angle', (30.0, 60.0), (0.0, 60.0), (0.0, 360.0), 60.0),
+    CellQualityInfo(
+        CellType.TRIANGLE, 'scaled_jacobian', (0.5, 2 * R33), (-2 * R33, 2 * R33), (-INF, INF), 1.0
+    ),
+    CellQualityInfo(CellType.TRIANGLE, 'radius_ratio', (1.0, 3.0), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TRIANGLE, 'shape', (0.25, 1.0), (0.0, 1.0), (0.0, 1.0), None),
+    CellQualityInfo(CellType.TRIANGLE, 'shape_and_size', (0.25, 1.0), (0.0, 1.0), (0.0, 1.0), None),
+    CellQualityInfo(CellType.QUAD, 'area', (0.0, INF), (0.0, INF), (-INF, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'aspect_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'condition', (1.0, 4), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'distortion', (0.5, 1.0), (0.0, 1.0), (-INF, INF), 1.0),
+    # CellQualityInfo(CellType.QUAD, 'edge_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'jacobian', (0.0, INF), (0.0, INF), (-INF, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'max_aspect_frobenius', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'max_angle', (90.0, 135.0), (90.0, 360.0), (0.0, 360.0), 90.0),
+    CellQualityInfo(CellType.QUAD, 'max_edge_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'med_aspect_frobenius', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'min_angle', (45.0, 90.0), (0.0, 90.0), (0.0, 360.0), 90.0),
+    CellQualityInfo(CellType.QUAD, 'oddy', (0.0, 0.5), (0.0, INF), (0.0, INF), 0.0),
+    CellQualityInfo(CellType.QUAD, 'radius_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(
+        CellType.QUAD, 'relative_size_squared', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), None
+    ),
+    CellQualityInfo(CellType.QUAD, 'scaled_jacobian', (0.3, 1.0), (-1, 1.0), (-1, 1.0), 1.0),
+    CellQualityInfo(CellType.QUAD, 'shape', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), 1.0),
+    CellQualityInfo(CellType.QUAD, 'shape_and_size', (0.2, 1.0), (0.0, 1.0), (0.0, 1.0), None),
+    CellQualityInfo(CellType.QUAD, 'shear', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), 1.0),
+    CellQualityInfo(CellType.QUAD, 'shear_and_size', (0.2, 1.0), (0.0, 1.0), (0.0, 1.0), None),
+    CellQualityInfo(CellType.QUAD, 'skew', (0.5, 1.0), (0.0, 1.0), (0.0, 1.0), 1.0),
+    CellQualityInfo(CellType.QUAD, 'stretch', (0.25, 1.0), (0.0, 1.0), (0.0, INF), 1.0),
+    CellQualityInfo(CellType.QUAD, 'taper', (0.0, 0.7), (0.0, INF), (0.0, INF), 0.0),
+    CellQualityInfo(CellType.QUAD, 'warpage', (0.0, 0.7), (0.0, 2.0), (0.0, INF), 0.0),
+    # CellQualityInfo(CellType.TETRA, 'aspect_beta', (1.0, 3), (1.0, INF), (1.0, INF), 1.0),
+    # CellQualityInfo(CellType.TETRA, 'aspect_delta', (0.1, INF), (0.0, INF), (0.0, INF), 1.0),
+    CellQualityInfo(CellType.TETRA, 'aspect_frobenius', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TETRA, 'aspect_gamma', (1.0, 3.0), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TETRA, 'aspect_ratio', (1.0, 3.0), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(
+        CellType.TETRA, 'collapse_ratio', (0.1, INF), (0.0, INF), (0.0, INF), sqrt(6.0) / 3.0
+    ),
+    CellQualityInfo(CellType.TETRA, 'condition', (1.0, 3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TETRA, 'distortion', (0.5, 1.0), (0.0, 1.0), (-INF, INF), 0.0),
+    # CellQualityInfo(CellType.TETRA, 'edge_ratio', (1.0, 3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.TETRA, 'jacobian', (0.0, INF), (0.0, INF), (-INF, INF), R22),
+    CellQualityInfo(CellType.TETRA, 'min_angle', (40, ANGLE), (0.0, ANGLE), (0.0, 360), ANGLE),
+    CellQualityInfo(CellType.TETRA, 'radius_ratio', (1.0, 3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(
+        CellType.TETRA, 'relative_size_squared', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), None
+    ),
+    CellQualityInfo(CellType.TETRA, 'scaled_jacobian', (0.5, R22), (-R22, R22), (-INF, INF), 1.0),
+    CellQualityInfo(CellType.TETRA, 'shape', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), 1.0),
+    CellQualityInfo(CellType.TETRA, 'shape_and_size', (0.2, 1.0), (0.0, 1.0), (0.0, 1.0), None),
+    CellQualityInfo(
+        CellType.TETRA, 'volume', (0.0, INF), (-INF, INF), (-INF, INF), sqrt(2.0) / 12.0
+    ),
+    CellQualityInfo(CellType.HEXAHEDRON, 'diagonal', (0.65, 1.0), (0.0, 1.0), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'dimension', None, (0.0, INF), (0.0, INF), 1.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'distortion', (0.5, 1.0), (0.0, 1.0), (-INF, INF), 1.0),
+    # CellQualityInfo(CellType.HEXAHEDRON, 'edge_ratio', None, (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'jacobian', (0.0, INF), (0.0, INF), (-INF, INF), 1.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'max_edge_ratio', (1.0, 1.3), (1.0, INF), (1.0, INF), 1.0),
+    CellQualityInfo(
+        CellType.HEXAHEDRON, 'max_aspect_frobenius', (1.0, 3), (1.0, INF), (1.0, INF), 1.0
+    ),
+    CellQualityInfo(
+        CellType.HEXAHEDRON, 'med_aspect_frobenius', (1.0, 3), (1.0, INF), (1.0, INF), 1.0
+    ),
+    CellQualityInfo(CellType.HEXAHEDRON, 'oddy', (0.0, 0.5), (0.0, INF), (0.0, INF), 0.0),
+    CellQualityInfo(
+        CellType.HEXAHEDRON, 'relative_size_squared', (0.5, 1.0), (0.0, 1.0), (0.0, 1.0), None
+    ),
+    CellQualityInfo(CellType.HEXAHEDRON, 'scaled_jacobian', (0.5, 1.0), (-1, 1.0), (-1, INF), 1.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'shape', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), 1.0),
+    CellQualityInfo(
+        CellType.HEXAHEDRON, 'shape_and_size', (0.2, 1.0), (0.0, 1.0), (0.0, 1.0), None
+    ),
+    CellQualityInfo(CellType.HEXAHEDRON, 'shear', (0.3, 1.0), (0.0, 1.0), (0.0, 1.0), 1.0),
+    CellQualityInfo(
+        CellType.HEXAHEDRON, 'shear_and_size', (0.2, 1.0), (0.0, 1.0), (0.0, 1.0), None
+    ),
+    CellQualityInfo(CellType.HEXAHEDRON, 'skew', (0.0, 0.5), (0.0, 1.0), (0.0, INF), 0.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'stretch', (0.25, 1.0), (0.0, 1.0), (0.0, INF), 1.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'taper', (0.0, 0.5), (0.0, INF), (0.0, INF), 0.0),
+    CellQualityInfo(CellType.HEXAHEDRON, 'volume', (0.0, INF), (0.0, INF), (-INF, INF), 2.0),
+    CellQualityInfo(
+        CellType.PYRAMID, 'volume', (0.0, INF), (-INF, INF), (-INF, INF), sqrt(2.0) / 6.0
+    ),
+    CellQualityInfo(
+        CellType.WEDGE, 'volume', (0.0, INF), (-INF, INF), (-INF, INF), sqrt(3.0) / 4.0
+    ),
 ]
 
 # Create lookup dict
@@ -204,6 +228,42 @@ def cell_quality_info(cell_type: CellType | str, measure: _CellQualityLiteral) -
     ------
     ValueError
         If info is not available for the specified cell type or measure.
+
+    Examples
+    --------
+    Get cell quality info for :attr:`~pyvista.CellType.TRIANGLE` cells and the
+    ``'scaled_jacobian'`` quality measure.
+
+    >>> import pyvista as pv
+    >>> info_tri = pv.cell_quality_info(pv.CellType.TRIANGLE, 'scaled_jacobian')
+    >>> info_tri
+    CellQualityInfo(cell_type=<CellType.TRIANGLE: 5>, quality_measure='scaled_jacobian', acceptable_range=(0.5, 1.1547005383792515), normal_range=(-1.1547005383792515, 1.1547005383792515), full_range=(-inf, inf), unit_cell_value=1.0)
+
+    Show the acceptable range for this measure.
+
+    >>> info_tri.acceptable_range
+    (0.5, 1.1547005383792515)
+
+    Show the value of this measure for equilateral triangles with edge length of one.
+
+    >>> info_tri.unit_cell_value
+    1.0
+
+    Get info for the same measure but for :attr:`~pyvista.CellType.QUAD` cells.
+
+    >>> info_quad = pv.cell_quality_info(pv.CellType.QUAD, 'scaled_jacobian')
+    >>> info_quad
+    CellQualityInfo(cell_type=<CellType.QUAD: 9>, quality_measure='scaled_jacobian', acceptable_range=(0.3, 1.0), normal_range=(-1, 1.0), full_range=(-1, 1.0), unit_cell_value=1.0)
+
+    Show the acceptable range. Note that it differs for quads compared to triangles.
+
+    >>> info_quad.acceptable_range
+    (0.3, 1.0)
+
+    Show the value of this measure for a square cell with edge length of one.
+
+    >>> info_quad.unit_cell_value
+    1.0
 
     """
 
