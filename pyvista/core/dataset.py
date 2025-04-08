27c9da6458c5fec31d60485388f2e7ed66da0f58
user27182 <89109579+user27182@users.noreply.github.com>
2025-04-08 16:09:59 -0600

    Fix using cell vectors with `DataSet.arrows` (#7377)
    * Fix cell data with arrows
    
    * Add test
    
    * Fix typing
    
    * Fix docstring
    
    * Compute point vectors
    
    * Add files via upload
    
    * Revert use of Box
    
    * Add files via upload
    
    * Pre-commit
    
    ---------
    
    Co-authored-by: Tetsuo Koyama <tkoyama010@gmail.com>

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -587,9 +587,12 @@ class DataSet(DataSetFilters, DataObject):
     ) -> pyvista.PolyData | None:
         """Return a glyph representation of the active vector data as arrows.
 
-        Arrows will be located at the points of the mesh and
+        Arrows will be located at the points or cells of the mesh and
         their size will be dependent on the norm of the vector.
-        Their direction will be the "direction" of the vector
+        Their direction will be the "direction" of the vector.
+
+        If there are both active point and cell vectors, preference is
+        given to the point vectors.
 
         Returns
         -------
@@ -598,28 +601,41 @@ class DataSet(DataSetFilters, DataObject):
 
         Examples
         --------
-        Create a mesh, compute the normals and set them active, and
-        plot the active vectors.
+        Create a mesh, compute the normals and set them active.
 
         >>> import pyvista as pv
         >>> mesh = pv.Cube()
         >>> mesh_w_normals = mesh.compute_normals()
         >>> mesh_w_normals.active_vectors_name = 'Normals'
+
+        Plot the active vectors as arrows. Show the original mesh as wireframe for
+        context.
+
         >>> arrows = mesh_w_normals.arrows
-        >>> arrows.plot(show_scalar_bar=False)
+        >>> pl = pv.Plotter()
+        >>> _ = pl.add_mesh(mesh, style='wireframe')
+        >>> _ = pl.add_mesh(arrows, color='red')
+        >>> pl.show()
 
         """
-        vectors, vectors_name = self.active_vectors, self.active_vectors_name
-        if vectors is None or vectors_name is None:
+        vectors = self.active_vectors
+        if vectors is None:
             return None
 
         if vectors.ndim != 2:
             msg = 'Active vectors are not vectors.'
             raise ValueError(msg)
 
+        field, vectors_name = self.active_vectors_info
+        # Cast type since we know name is not None since vectors is not None at this point
+        vectors_name = cast('str', vectors_name)
+
         scale_name = f'{vectors_name} Magnitude'
         scale = np.linalg.norm(vectors, axis=1)
-        self.point_data.set_array(scale, scale_name)
+        if field == FieldAssociation.POINT:
+            self.point_data.set_array(scale, scale_name)
+        else:
+            self.cell_data.set_array(scale, scale_name)
         return self.glyph(orient=vectors_name, scale=scale_name)
 
     @property
