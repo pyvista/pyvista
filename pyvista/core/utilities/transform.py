0fdb918b15059ce44ab2bfdeb2dedd26dda2c47f
user27182 <89109579+user27182@users.noreply.github.com>
2025-04-13 06:53:42 -0600

    Make `bool` parameters of `Transform.apply_to` methods keyword-only (#7407)
    * Make Transform.apply_to methods keyword-only
    
    * Allow mode as positional arg
    
    ---------
    
    Co-authored-by: Tetsuo Koyama <tkoyama010@gmail.com>

--- pyvista/core/utilities/transform.py
+++ pyvista/core/utilities/transform.py
@@ -1743,6 +1743,7 @@ class Transform(_vtk.vtkPyVistaOverride, _vtk.vtkTransform):
         self,
         points: VectorLike[float] | MatrixLike[float],
         /,
+        *,
         inverse: bool = False,
         copy: bool = True,
     ) -> NumpyArray[float]:
@@ -1789,6 +1790,7 @@ class Transform(_vtk.vtkPyVistaOverride, _vtk.vtkTransform):
         self,
         vectors: VectorLike[float] | MatrixLike[float],
         /,
+        *,
         inverse: bool = False,
         copy: bool = True,
     ) -> NumpyArray[float]:
@@ -1836,6 +1838,7 @@ class Transform(_vtk.vtkPyVistaOverride, _vtk.vtkTransform):
         dataset: _DataSetOrMultiBlockType,
         /,
         mode: Literal['active_vectors', 'all_vectors'] = 'active_vectors',
+        *,
         copy: bool = True,
         inverse: bool = False,
     ) -> _DataSetOrMultiBlockType:
@@ -1892,6 +1895,7 @@ class Transform(_vtk.vtkPyVistaOverride, _vtk.vtkTransform):
         actor: Prop3D,
         /,
         mode: Literal['pre-multiply', 'post-multiply', 'replace'] = 'post-multiply',
+        *,
         copy: bool = True,
         inverse: bool = False,
     ) -> Prop3D:
