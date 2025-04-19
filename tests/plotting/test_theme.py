af9d54daef9f65b4dfbe29c6f7523a57bf0e2799
beroda <117822458+beroda@users.noreply.github.com>
2025-04-19 21:46:24 +0200

    Fix env

--- tests/plotting/test_theme.py
+++ tests/plotting/test_theme.py
@@ -498,7 +498,8 @@ def test_plotter_set_theme():
 
 
 @pytest.mark.filterwarnings(
-    'ignore:The jupyter_extension_available flag is read only and is automatically detected:UserWarning'
+    'ignore:The jupyter_extension_available flag is read only and is automatically detected:UserWarning',
+    'ignore:SyntaxWarning',  # due to unescaped backslash in trame-vuetify
 )
 def test_load_theme(tmpdir, default_theme):
     filename = str(tmpdir.mkdir('tmpdir').join('tmp.json'))
