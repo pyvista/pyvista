7b3fd0d4a0a6b5a2054ac2447984364632a2268a
Alex Kaszynski <akascap@gmail.com>
2025-05-05 08:33:57 -0600

    improve structured grid example

--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -2506,22 +2506,28 @@ class StructuredGrid(PointGrid, StructuredGridFilters, _vtk.vtkStructuredGrid):
     >>> vtkgrid = vtk.vtkStructuredGrid()
     >>> grid = pv.StructuredGrid(vtkgrid)
 
-    Create from NumPy arrays.
+    Create from NumPy arrays using :func:`numpy.meshgrid`. Note the usage of
+    ``ij`` indexing.
 
-    >>> xrng = np.arange(-10, 10, 2, dtype=np.float32)
-    >>> yrng = np.arange(-10, 10, 5, dtype=np.float32)
-    >>> zrng = np.arange(-10, 10, 1, dtype=np.float32)
+    >>> xrng = np.linspace(-5, 5, 10)
+    >>> yrng = np.linspace(-8, 8, 4)
+    >>> zrng = np.linspace(-7, 4, 20)
     >>> x, y, z = np.meshgrid(xrng, yrng, zrng, indexing='ij')
     >>> grid = pv.StructuredGrid(x, y, z)
     >>> grid
     StructuredGrid (...)
       N Cells:      513
       N Points:     800
-      X Bounds:     -1.000e+01, 8.000e+00
-      Y Bounds:     -1.000e+01, 5.000e+00
-      Z Bounds:     -1.000e+01, 9.000e+00
+      X Bounds:     -5.000e+00, 5.000e+00
+      Y Bounds:     -8.000e+00, 8.000e+00
+      Z Bounds:     -7.000e+00, 4.000e+00
       Dimensions:   10, 4, 20
       N Arrays:     0
+
+    Also note how the grid dimensions match the shape of the input arrays.
+
+    >>> (xrng.size, yrng.size, zrng.size)
+    (10, 4, 20)
 
     """
 
