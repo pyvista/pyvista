fbc0b57a8a7a6c4e360195f08ab988947c307a1f
user27182 <89109579+user27182@users.noreply.github.com>
2025-05-08 15:18:14 -0600

    Conditionally build the sphinx gallery

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,8 +48,13 @@ if not Path(pyvista.FIGURE_PATH).exists():
     Path(pyvista.FIGURE_PATH).mkdir()
 
 # necessary when building the sphinx gallery
-pyvista.BUILDING_GALLERY = True
-os.environ['PYVISTA_BUILDING_GALLERY'] = 'true'
+BUILDING_GALLERY_ENV_NAME = 'PYVISTA_BUILDING_GALLERY'
+env_building_gallery = os.environ.get(BUILDING_GALLERY_ENV_NAME, None)
+if env_building_gallery is None:
+    os.environ[BUILDING_GALLERY_ENV_NAME] = 'true'
+    pyvista.BUILDING_GALLERY = True
+elif env_building_gallery.lower() == 'false':
+    pyvista.BUILDING_GALLERY = False
 
 # SG warnings
 import warnings
@@ -92,7 +97,6 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx_copybutton',
     'sphinx_design',
-    'sphinx_gallery.gen_gallery',
     'sphinxcontrib.asciinema',
     'sphinx_tags',
     'sphinx_toolbox.more_autodoc.overloads',
@@ -101,6 +105,8 @@ extensions = [
     'sphinxext.opengraph',
     'sphinx_sitemap',
 ]
+if pyvista.BUILDING_GALLERY:
+    extensions.append('sphinx_gallery.gen_gallery')
 
 # Configuration of pyvista.ext.coverage
 coverage_additional_modules = [
