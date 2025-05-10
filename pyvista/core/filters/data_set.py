459dcddcdcc18cc9a4420e48679f236e6cbba789
user27182 <89109579+user27182@users.noreply.github.com>
2025-05-10 10:41:41 -0600

    Fix streamlines ref

--- pyvista/core/filters/data_set.py
+++ pyvista/core/filters/data_set.py
@@ -3233,7 +3233,7 @@ class DataSetFilters(DataObjectFilters):
         >>> plotter.view_xy()
         >>> plotter.show()
 
-        See :ref:`streamlines_2D_example` for more examples using this filter.
+        See :ref:`2d_streamlines_example` for more examples using this filter.
 
         """
         if integrator_type not in [2, 4]:
