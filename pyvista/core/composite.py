82b227e2b94ecc01e9f6fc441c66693a2b10329e
user27182 <89109579+user27182@users.noreply.github.com>
2025-05-28 11:25:38 -0600

    Update docstrings

--- pyvista/core/composite.py
+++ pyvista/core/composite.py
@@ -1238,7 +1238,12 @@ class MultiBlock(
         ... ]
         >>> blocks = pv.MultiBlock(data)
         >>> blocks.bounds
-        BoundsTuple(x_min=-0.5, x_max=2.5, y_min=-0.5, y_max=2.5, z_min=-0.5, z_max=0.5)
+        BoundsTuple(x_min = -0.5
+                    x_max =  2.5
+                    y_min = -0.5
+                    y_max =  2.5
+                    z_min = -0.5
+                    z_max =  0.5)
 
         """
         # apply reduction of min and max over each block
--- pyvista/core/pointset.py
+++ pyvista/core/pointset.py
@@ -3418,10 +3418,20 @@ class ExplicitStructuredGrid(PointGrid, _vtk.vtkExplicitStructuredGrid):
         >>> grid = examples.load_explicit_structured()
         >>> grid = grid.hide_cells(range(80, 120))
         >>> grid.bounds
-        BoundsTuple(x_min=0.0, x_max=80.0, y_min=0.0, y_max=50.0, z_min=0.0, z_max=6.0)
+        BoundsTuple(x_min =  0.0
+                    x_max = 80.0
+                    y_min =  0.0
+                    y_max = 50.0
+                    z_min =  0.0
+                    z_max =  6.0)
 
         >>> grid.visible_bounds
-        BoundsTuple(x_min=0.0, x_max=80.0, y_min=0.0, y_max=50.0, z_min=0.0, z_max=4.0)
+        BoundsTuple(x_min =  0.0
+                    x_max = 80.0
+                    y_min =  0.0
+                    y_max = 50.0
+                    z_min =  0.0
+                    z_max =  4.0)
 
         """
         name = _vtk.vtkDataSetAttributes.GhostArrayName()
