d8764e20ce1a5b8e190e9ecd27f08ce42169523e
user27182 <89109579+user27182@users.noreply.github.com>
2025-06-11 11:54:59 -0600

    Keep path relative

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -86,7 +86,7 @@ if 'PYVISTA_USERDATA_PATH' in os.environ:  # pragma: no cover
         warnings.warn('Ignoring invalid {PYVISTA_USERDATA_PATH')
         USER_DATA_PATH = default_user_data_path
     else:
-        USER_DATA_PATH = Path(os.environ['PYVISTA_USERDATA_PATH']).resolve()
+        USER_DATA_PATH = str(Path(os.environ['PYVISTA_USERDATA_PATH']))
 else:
     # use default pooch path
     USER_DATA_PATH = default_user_data_path
