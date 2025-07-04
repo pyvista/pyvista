5a90009427c8cef4c21fd6fd88ac9d806a8fd169
user27182 <89109579+user27182@users.noreply.github.com>
2025-07-03 21:31:47 -0600

    Update test

--- tests/core/test_imagedata_filters.py
+++ tests/core/test_imagedata_filters.py
@@ -417,8 +417,9 @@ def test_contour_labels_raises_vtkversionerror():
 @pytest.mark.needs_vtk_version(9, 3, 0)
 def test_contour_labels_empty_input(frog_tissues):
     voi = frog_tissues.extract_subset((10, 100, 20, 200, 20, 80))
-    assert voi.active_scalars.mean() == 0
-    surface = voi.contour_labels()
+    background_value = 0
+    assert np.all(voi.active_scalars == background_value)
+    surface = voi.contour_labels(background_value=background_value)
     assert surface.is_empty
 
 
