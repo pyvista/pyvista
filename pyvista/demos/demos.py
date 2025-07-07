1f559a4dd0414fbcd0cc010ea53bcaf354561dda
user27182 <89109579+user27182@users.noreply.github.com>
2025-07-07 00:00:49 -0600

    Bump NumPy to 2.3.1

--- pyvista/demos/demos.py
+++ pyvista/demos/demos.py
@@ -257,7 +257,7 @@ def plot_wave(fps=30, frequency=1, wavetime=3, notebook=None):  # noqa: PLR0917
     # Make data
     X = np.arange(-10, 10, 0.25)
     Y = np.arange(-10, 10, 0.25)
-    X, Y = np.meshgrid(X, Y)  # type: ignore[assignment]
+    X, Y = np.meshgrid(X, Y)
     R = np.sqrt(X**2 + Y**2)
     Z = np.sin(R)
 
