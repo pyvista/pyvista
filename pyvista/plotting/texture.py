8fd6aa1e4cab1b2caa6b4214ccebb8ed43076c4d
user27182 <89109579+user27182@users.noreply.github.com>
2025-07-16 14:45:07 -0600

    Fix texture.interpolate docstring

--- pyvista/plotting/texture.py
+++ pyvista/plotting/texture.py
@@ -173,12 +173,12 @@ class Texture(_NoNewAttrMixinAuto, DataObject, _vtk.vtkTexture):
 
         >>> from pyvista import examples
         >>> texture = examples.download_masonry_texture()
-        >>> texture.interpolation = False
+        >>> texture.interpolate = False
         >>> texture.plot(cpos='xy', zoom=3)
 
         Plot the same texture with interpolation.
 
-        >>> texture.interpolation = True
+        >>> texture.interpolate = True
         >>> texture.plot(cpos='xy', zoom=3)
 
         """
