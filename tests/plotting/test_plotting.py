c74b73e3843c5ac23affdfc7cfc705795c0127a6
Tetsuo Koyama <tkoyama010@gmail.com>
2025-07-20 10:25:45 +0900

    Update tests/plotting/test_plotting.py
    Co-authored-by: user27182 <89109579+user27182@users.noreply.github.com>

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -824,6 +824,7 @@ def test_plot_show_bounds(sphere):
 
 
 def test_plot_label_fmt(sphere, verify_image_cache):
+    # TODO: Change this to (9, 6, 0) when VTK 9.6 is released
     if pyvista.vtk_version_info < (9, 5, 99):
         # Axis labels changed substantially in VTK 9.6
         verify_image_cache.skip = True
