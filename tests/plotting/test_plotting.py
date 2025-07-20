7e76747f17be1eff99f78642f348da083be410da
Tetsuo Koyama <tkoyama010@gmail.com>
2025-07-20 10:24:22 +0900

    Update tests/plotting/test_plotting.py
    Co-authored-by: user27182 <89109579+user27182@users.noreply.github.com>

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -654,6 +654,7 @@ def test_plotter_shape_invalid():
 
 
 def test_plot_bounds_axes_with_no_data(verify_image_cache):
+    # TODO: Change this to (9, 6, 0) when VTK 9.6 is released
     if pyvista.vtk_version_info < (9, 5, 99):
         # Axis labels changed substantially in VTK 9.6
         verify_image_cache.skip = True
