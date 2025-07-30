799819b28646e0bc85d06ff5e9478fba7f58ac27
user27182 <89109579+user27182@users.noreply.github.com>
2025-07-29 21:01:14 -0600

    Update dikhololo_nigh docstring

--- pyvista/examples/downloads.py
+++ pyvista/examples/downloads.py
@@ -7323,19 +7323,17 @@ def download_dikhololo_night(load=True):  # noqa: FBT002
 
     Examples
     --------
-    >>> import pyvista as pv
     >>> from pyvista import examples
-    >>> gltf_file = examples.gltf.download_damaged_helmet()
     >>> texture = examples.download_dikhololo_night()
-    >>> pl = pv.Plotter()
-    >>> pl.import_gltf(gltf_file)
-    >>> pl.set_environment_texture(texture)
-    >>> pl.show()
+    >>> texture.plot()
 
     .. seealso::
 
         :ref:`Dikhololo Night Dataset <dikhololo_night_dataset>`
             See this dataset in the Dataset Gallery for more info.
+
+        :ref:`load_gltf_example`
+            See additional examples using this dataset.
 
     """
     return _download_dataset(_dataset_dikhololo_night, load=load)
