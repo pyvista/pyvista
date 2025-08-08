665efdebb216f5c0d0f929c6bdacb1341e57e614
Tetsuo Koyama <tkoyama010@gmail.com>
2025-08-09 05:18:04 +0900

    Update tests/core/test_imagedata_filters.py

--- tests/core/test_imagedata_filters.py
+++ tests/core/test_imagedata_filters.py
@@ -493,7 +493,6 @@ def test_points_to_cells(uniform_many_scalars, active_scalars, copy):
     for array_in, array_out in zip(
         point_voxel_image.point_data.keys(),
         cell_voxel_image.cell_data.keys(),
-        strict=False,
     ):
         shares_memory = np.shares_memory(point_voxel_image[array_in], cell_voxel_image[array_out])
         assert not shares_memory if copy else shares_memory
