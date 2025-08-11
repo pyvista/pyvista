f2001f9e7ef99fee4404024bd3fe8121dbb5b263
Tetsuo Koyama <tkoyama010@gmail.com>
2025-08-11 19:02:28 +0900

    Bump version to 0.46.1
    Backport fixes from main branch:
    - #7791: Add minimum typing-extensions version
    - #7787: Allow using matplotlib cmap objects with get_cmap_safe and global theme
    - #7788: Fix camera position typing to allow nested sequence
    - #7802: fix Text is not set
    
    Fixes #7792

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -16,7 +16,7 @@ Denotes the first release candidate.
 # major, minor, patch
 from __future__ import annotations
 
-version_info = 0, 46, 0
+version_info = 0, 46, 1
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
