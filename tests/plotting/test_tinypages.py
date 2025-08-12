8f5fb2f2ea96b94dfd5eca63ca0f84bedebddd45
Alex Kaszynski <akascap@gmail.com>
2025-08-12 12:36:56 -0600

    gc issues are... perplexing

--- tests/plotting/test_tinypages.py
+++ tests/plotting/test_tinypages.py
@@ -10,7 +10,6 @@ import sys
 
 import pytest
 
-from pyvista.ext.plot_directive import hash_plot_code
 from pyvista.plotting import system_supports_plotting
 from tests.conftest import flaky_test
 
@@ -137,6 +136,7 @@ def test_tinypages(tmp_path, ename, evalue):
     assert b'This is a matplotlib plot.' in html_contents
 
 
+@pytest.mark.skip_windows('path issues on Azure Windows CI')
 def test_parallel(tmp_path: Path) -> None:
     """Ensure that labeling image serial fails."""
     html_dir = tmp_path / 'html'
@@ -178,42 +178,3 @@ def test_parallel(tmp_path: Path) -> None:
     assert not err
 
     assert len(list(html_dir.glob('**/*.png'))) == 27
-
-
-def test_hash_plot_code_consistency():
-    code = 'import matplotlib.pyplot as plt\nplt.plot([1, 2, 3])'
-    options = {}
-
-    hash1 = hash_plot_code(code, options)
-    hash2 = hash_plot_code(code, options)
-    assert hash1 == hash2
-    assert len(hash1) == 16
-
-    different_code = 'plt.plot([4, 5, 6])'
-    hash3 = hash_plot_code(different_code, options)
-    assert hash1 != hash3
-
-
-def test_hash_plot_code_normalization():
-    code_with_noise = (
-        'import matplotlib.pyplot as plt  # plotting lib\n\nplt.plot([1, 2, 3])  # make plot\n\n'
-    )
-    code_clean = 'import matplotlib.pyplot as plt\nplt.plot([1, 2, 3])'
-    doctest_code = '>>> import matplotlib.pyplot as plt\n>>> plt.plot([1, 2, 3])'
-    options = {}
-
-    hash1 = hash_plot_code(code_with_noise, options)
-    hash2 = hash_plot_code(code_clean, options)
-    hash3 = hash_plot_code(doctest_code, options)
-    assert hash1 == hash2 == hash3
-
-
-def test_hash_plot_code_context_option():
-    code = 'plt.plot([1, 2, 3])'
-
-    hash_no_context = hash_plot_code(code, {})
-    hash_with_context = hash_plot_code(code, {'context': True})
-    hash_other_option = hash_plot_code(code, {'other': True})
-
-    assert hash_no_context != hash_with_context
-    assert hash_no_context == hash_other_option
