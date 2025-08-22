c6958d6e2c95780d217c28ce8e29b0c8eaf7dc1e
user27182 <89109579+user27182@users.noreply.github.com>
2025-08-22 17:47:08 -0600

    Remove docstring

--- pyvista/core/utilities/observers.py
+++ pyvista/core/utilities/observers.py
@@ -69,10 +69,6 @@ class VtkErrorCatcher:
     --------
     Catch VTK errors using the context manager.
 
-    >>> import pyvista as pv
-    >>> with pv.VtkErrorCatcher() as error_catcher:
-    ...     sphere = pv.Sphere()
-
     """
 
     @_deprecate_positional_args
