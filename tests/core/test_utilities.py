3a815e61eebffbd4d6024ee6a46e55fae240308f
user27182 <89109579+user27182@users.noreply.github.com>
2025-08-21 23:07:23 -0600

    Update test regex

--- tests/core/test_utilities.py
+++ tests/core/test_utilities.py
@@ -1332,8 +1332,8 @@ def test_no_new_attr_mixin(no_new_attributes_mixin_subclass):
     eggs = 'eggs'
 
     match = (
-        "Attribute 'ham' does not exist and cannot be added to class 'A'\n"
-        'Use `pv.set_new_attribute` to set new attributes.'
+        "Attribute 'ham' does not exist and cannot be added to class 'A'\nUse "
+        "`pyvista.set_new_attribute` or `pyvista.allow_new_attributes` to set new attributes."
     )
     with pytest.raises(pv.PyVistaAttributeError, match=match):
         setattr(a, ham, eggs)
