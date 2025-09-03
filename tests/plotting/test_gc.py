254301cc6a9cd28b673781ef965537c46912437a
Alex Kaszynski <akascap@gmail.com>
2025-09-03 09:45:06 -0600

    revert

--- tests/plotting/test_gc.py
+++ tests/plotting/test_gc.py
@@ -15,43 +15,19 @@ def test_leak_vtk() -> None:
     sphere.self_ref = sphere
 
 
-@pytest.mark.parametrize(
-    'decorator, expected_exit',
-    [
-        ('@pytest.mark.expect_check_gc_fail', pytest.ExitCode.OK),
-        ('', pytest.ExitCode.TESTS_FAILED),
-    ],
-)
-def test_expect_check_gc_fail(
-    pytester: pytest.Pytester,
-    decorator: str,
-    expected_exit: ExitStatus,
-):
-    tests = f"""
-    import pytest
-
-    {decorator}
-    def test_leak_pv(sphere) -> None:
-        points = sphere.points
-        points.VTKObject._ref = points.VTKObject
-    """
-    p = pytester.makepyfile(tests)
-    results = pytester.runpytest(p)
-
-    # Check pytest exit code
-    assert results.ret == expected_exit
-
-    if decorator:
-        # With the marker, the test should pass
-        results.assert_outcomes(passed=1)
-    else:
-        # Without the marker, the leak should trigger failure
-        results.assert_outcomes(failed=1)
+@pytest.mark.expect_check_gc_fail
+def test_leak_pv(sphere) -> None:
+    """A VTK leak within a pyvista object with a simple self-reference."""
+    points = sphere.points
+    points.VTKObject._ref = points.VTKObject
 
 
 @pytest.mark.expect_check_gc_fail
 def test_leak_pv_plotter() -> None:
     """Trigger a leak in pyvista.Plotter by disabling cleanup ."""
+
+    def noop(*args: Any, **kwargs: Any):  # noqa: ARG001
+        return None
 
     pl = pv.Plotter()
     pl.add_mesh(pv.Sphere())
