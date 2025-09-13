c556a05d10074ecae199166ec3c3b411fb83b078
user27182 <89109579+user27182@users.noreply.github.com>
2025-09-13 08:38:58 -0600

    Fix render window class name in Report and reduce report execution time (#7949)
    * Fix render window class name in Report
    
    * Cache mathtext support
    
    * Fix test
    
    * Catch exceptions
    
    * Add coverage
    
    * Fix typing
    
    * Set as None if gpu is false
    
    * Update test_utilities.py
    
    * Update test_utilities.py
    
    * Update mathtext caching
    
    * Use original mathtext command
    
    * Use vtk code for gpu info
    
    * Simplify imports for MathText check
    
    * Add print
    
    * Fix render window

--- tests/core/test_utilities.py
+++ tests/core/test_utilities.py
@@ -561,10 +561,12 @@ def test_report():
     report = pv.Report(gpu=True)
     assert report is not None
     assert 'GPU Details : None' not in report.__repr__()
+    assert re.search(r'Render Window : (vtk\w+RenderWindow|error)', report.__repr__())
+    assert 'vtkRenderWindow' not in report.__repr__()  # must not be abstract
     report = pv.Report(gpu=False)
     assert report is not None
     assert 'GPU Details : None' in report.__repr__()
-    assert re.search(r'Render Window : vtk\w+RenderWindow', report.__repr__())
+    assert 'Render Window : None' in report.__repr__()
     assert 'User Data Path' not in report.__repr__()
 
 
