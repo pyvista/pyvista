8c6c4cf2cb542bf613c9886d9952a95ef38b8f93
user27182 <89109579+user27182@users.noreply.github.com>
2025-09-18 23:37:30 -0600

    Update border mode

--- pyvista/core/filters/image_data.py
+++ pyvista/core/filters/image_data.py
@@ -4261,7 +4261,7 @@ class ImageDataFilters(DataSetFilters):
         elif border_mode == 'wrap':
             interpolator.SetBorderModeToRepeat()
         else:  # pragma: no cover
-            msg = f"Unexpected interpolation mode '{interpolation}'."  # type: ignore[unreachable]
+            msg = f"Unexpected border mode '{border_mode}'."
             raise RuntimeError(msg)
 
         if anti_aliasing and np.any(magnification_factors < 1.0):
