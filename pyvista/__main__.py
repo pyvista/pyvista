dcf72f85c77b63e6a8e4a87218c46df523d3ef10
user27182 <89109579+user27182@users.noreply.github.com>
2025-09-28 16:52:42 -0600

    Fix syntax

--- pyvista/__main__.py
+++ pyvista/__main__.py
@@ -43,7 +43,7 @@ def _parse_args_and_kwargs(args: list[str]) -> tuple[list[Any], dict[str, Any]]:
     py_kwargs = {}
     positional = True
     for arg in args:
-        if not '=' in arg:
+        if '=' not in arg:
             # Expect positonal py arg
             if not positional:
                 msg = f'Positional argument {arg} must not follow a keyword argument.'
