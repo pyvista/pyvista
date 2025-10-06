3277e58acdace14b2e49cef54f1a031e0b5f7662
beroda <117822458+beroda@users.noreply.github.com>
2025-10-06 10:02:36 +0200

    Add subset of plot command to CLI

--- pyvista/__main__.py
+++ pyvista/__main__.py
@@ -3,14 +3,19 @@
 from __future__ import annotations
 
 from functools import wraps
+from typing import Annotated
+from typing import Literal
 from typing import get_type_hints
 import warnings
 
 from cyclopts import App
+from cyclopts import Parameter
 
 import pyvista
 from pyvista import Report
 from pyvista.core.errors import PyVistaDeprecationWarning
+
+# from pyvista.plotting._typing import ColorLike, Color
 
 # Assign annotations to be able to use the Report class using
 # cyclopts when __future__ annotations are enabled. See https://github.com/BrianPugh/cyclopts/issues/570
@@ -31,6 +36,61 @@ app = App(
 @wraps(Report)
 def report(*args, **kwargs):  # noqa: ANN201, D103
     return Report(*args, **kwargs)
+
+
+@app.command
+def _plot(
+    files: Annotated[list[str], Parameter(consume_multiple=True)],
+    *,
+    off_screen: bool | None = None,
+    full_screen: bool | None = None,
+    screenshot: str | None = None,
+    interactive: bool = True,
+    window_size: list[int] | None = None,
+    show_bounds: bool = False,
+    show_axes: bool | None = None,
+    notebook: bool | None = None,
+    background: str | None = None,
+    text: str = '',
+    eye_dome_lighting: bool = False,
+    volume: bool = False,
+    parallel_projection: bool = False,
+    return_cpos: bool = False,
+    anti_aliasing: Literal['ssaa', 'msaa', 'fxaa'] | bool | None = None,
+    zoom: float | str | None = None,
+    border: bool = False,
+    border_color: str = 'k',
+    border_width: float = 2.0,
+    ssao: bool = False,
+    **kwargs,
+) -> None:
+    pyvista.plot(
+        var_item=files,
+        off_screen=off_screen,
+        full_screen=full_screen,
+        screenshot=screenshot,
+        interactive=interactive,
+        window_size=window_size,
+        show_bounds=show_bounds,
+        show_axes=show_axes,
+        notebook=notebook,
+        background=background,
+        text=text,
+        eye_dome_lighting=eye_dome_lighting,
+        volume=volume,
+        parallel_projection=parallel_projection,
+        return_cpos=return_cpos,
+        anti_aliasing=anti_aliasing,
+        zoom=zoom,
+        border=border,
+        border_color=border_color,
+        border_width=border_width,
+        ssao=ssao,
+        **kwargs,
+    )
+
+
+_plot.__doc__ = pyvista.plot.__doc__
 
 
 def main(argv: list[str] | str | None = None) -> None:
