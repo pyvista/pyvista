3c5332000bd96e415aaf08b8ae86212e789d7028
Alex Kaszynski <akascap@gmail.com>
2025-10-10 08:10:20 -0600

    Enable parallel testing (#7885)
    * tox with -n4
    
    * parallel with 2 cores
    
    * correct syntax
    
    * pass -n4 only for linux
    
    * remove all downloads from test collection
    
    * decrease windows timeout to 30 minutes
    
    * always use parallel
    
    * copy source directory for tinypages
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * fix gif write race condition; use env:PARALLEL in tox
    
    * conditional parallel
    
    * always parallel for core testing
    
    * Update tox.ini
    
    Co-authored-by: user27182 <89109579+user27182@users.noreply.github.com>
    
    * Bump pytest-pyvista
    
    * Remove white space
    
    * Update tox.ini
    
    Co-authored-by: edabor <117822458+edabor@users.noreply.github.com>
    
    * move parallel config for VTK< 9.4 to tox
    
    * Update toxfile.py
    
    Co-authored-by: edabor <117822458+edabor@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: user27182 <89109579+user27182@users.noreply.github.com>
    Co-authored-by: edabor <117822458+edabor@users.noreply.github.com>

--- toxfile.py
+++ toxfile.py
@@ -1,8 +1,10 @@
 from __future__ import annotations  # noqa: D100
 
 import os
+import re
 from typing import TYPE_CHECKING
 
+from packaging.version import Version
 from tox.plugin import impl
 
 if TYPE_CHECKING:
@@ -12,8 +14,28 @@ if TYPE_CHECKING:
 
 @impl
 def tox_add_env_config(env_conf: EnvConfigSet, state: State) -> None:  # noqa: ARG001, D103
-    if env_conf.env_name not in ['docs-build', 'doctest-modules']:
+    if os.environ.get('CI', 'false').lower() != 'true':
         return
 
-    if os.environ.get('CI', 'false').lower() == 'true':
+    if env_conf.env_name in ['docs-build', 'doctest-modules']:
         env_conf['set_env'].update({'VTK_DEFAULT_OPENGL_WINDOW': 'vtkEGLRenderWindow'})
+        return
+
+    # For plotting tests on Linux with vtk < 9.4, some segfaults have been spotted
+    # See https://github.com/pyvista/pyvista/pull/7885#issuecomment-3263323390
+    # Therefore, the "PARALLEL" env variable is updated
+    if ('plotting' in env_conf.name) and os.environ['RUNNER_OS'] == 'Linux':
+        # Test the vtk version from TOX_FACTOR env variable
+        reg = re.compile('.*-vtk_(.*)$')
+        if not (factor := os.environ.get('TOX_FACTOR')):
+            return
+
+        if not (m := re.match(reg, factor)):
+            msg = 'The `TOX_FACTOR` env variable is malformed. Could not get the vtk version.'
+            raise ValueError(msg)
+
+        vtk_version = m.group(1)
+        val = '-n4' if vtk_version == 'latest' or Version(vtk_version) >= Version('9.4.0') else ''
+
+        updated = {'PARALLEL': val}
+        env_conf['set_env'].update(updated)
