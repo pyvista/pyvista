8ed7097a5888484722df8e9a45fa6baaf652d894
user27182 <89109579+user27182@users.noreply.github.com>
2025-10-12 10:36:17 -0600

    Revert everything

--- tests/core/test_imagedata_filters.py
+++ tests/core/test_imagedata_filters.py
@@ -2117,25 +2117,3 @@ def test_morphological_filters_custom_kernel_size():
 
     closed = volume.close(kernel_size=(3, 2, 1))
     assert isinstance(closed, pv.ImageData)
-
-
-@pytest.mark.parametrize(
-    ('axis', 'dimensions_out'), [(0, (2, 1, 1)), (1, (1, 2, 1)), (2, (1, 1, 2))]
-)
-@pytest.mark.parametrize(
-    'dtypes', [(int, float), (float, float), (int, int), (np.int64, np.uint8)]
-)
-def test_stack(axis, dimensions_out, dtypes):
-    array_a = np.array([0], dtype=dtypes[0])
-    array_b = np.array([1], dtype=dtypes[1])
-
-    image_a = pv.ImageData(dimensions=(1, 1, 1))
-    image_a['A'] = array_a
-    image_b = pv.ImageData(dimensions=(1, 1, 1))
-    image_b['B'] = array_b
-
-    stacked_image = image_a.stack(image_b, axis=axis)
-    assert stacked_image.array_names == ['A']
-    image_array = stacked_image['A']
-    assert stacked_image.dimensions == dimensions_out
-    assert np.array_equal(image_array, [0, 1])
