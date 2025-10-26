a8fe65825dc56d494ff2049d97897f00f099840e
user27182 <89109579+user27182@users.noreply.github.com>
2025-10-26 14:31:59 -0600

    Update _version.py

--- pyvista/_version.py
+++ pyvista/_version.py
@@ -16,7 +16,7 @@ Denotes the first release candidate.
 # major, minor, patch
 from __future__ import annotations
 
-version_info = 0, 46, 3
+version_info = 0, 46, 4
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
