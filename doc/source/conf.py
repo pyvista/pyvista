2fcbe321e36c5acb6e6b58dd0cce770d0188f6ec
user27182 <89109579+user27182@users.noreply.github.com>
2025-11-01 23:48:20 -0600

    Ignore FiveArrays

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -216,6 +216,7 @@ nitpick_ignore_regex = [
     (r'py:.*', '.*FieldLiteral'),
     (r'py:.*', '.*RowLiteral'),
     (r'py:.*', '.*_SerializedDictArray'),
+    (r'py:.*', '.*_FiveArrays'),
     #
     # PyVista AxesAssembly-related types
     (r'py:.*', '.*GeometryTypes'),
