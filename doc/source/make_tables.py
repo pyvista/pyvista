146d92c5c43b41507fdbc2fb5d24bead50af72d2
user27182 <89109579+user27182@users.noreply.github.com>
2025-11-13 08:02:55 -0700

    Update make_tables.py

--- doc/source/make_tables.py
+++ doc/source/make_tables.py
@@ -233,7 +233,7 @@ def _meshio_info_dict():
         name = io_class.__name__.removeprefix('vtk').removesuffix('Reader').removesuffix('Writer')
         if not name.startswith('VTKP'):
             name = name.removeprefix('VTK')
-        for class_name in ['DataSet', *get_args(_mesh_types)]:
+        for class_name in ['DataSet', 'Image', 'Data', 'Partitioned', *get_args(_mesh_types)]:
             name = name.replace(class_name, '')
         if name == '':
             # Use a single extension as the format name
