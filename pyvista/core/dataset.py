a8defe17f428b8ae1435648740da8b58b1fabcf9
user27182 <89109579+user27182@users.noreply.github.com>
2025-12-08 13:23:46 -0700

    Format

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -122,7 +122,8 @@ class _MeshValidator:
     def __init__(
         self,
         mesh: DataSet,
-        validation_fields: _MeshValidationOptions | Sequence[_MeshValidationOptions] = _DEFAULT_MESH_VALIDATION_ARGS,
+        validation_fields: _MeshValidationOptions
+        | Sequence[_MeshValidationOptions] = _DEFAULT_MESH_VALIDATION_ARGS,
     ) -> None:
         allowed_array_fields = get_args(_ArrayValidationOptions)
         allowed_cell_fields = get_args(_CellValidationOptions)
@@ -3197,7 +3198,8 @@ class DataSet(DataSetFilters, DataObject):
 
     def validate(
         self,
-        validation_fields: _MeshValidationOptions | Sequence[_MeshValidationOptions] = _DEFAULT_MESH_VALIDATION_ARGS,
+        validation_fields: _MeshValidationOptions
+        | Sequence[_MeshValidationOptions] = _DEFAULT_MESH_VALIDATION_ARGS,
         action: Literal['warn', 'report'] = 'warn',
     ) -> ValidationReport | None:
         """Validate point and cell data arrays match n_points and n_cells, respectively.
