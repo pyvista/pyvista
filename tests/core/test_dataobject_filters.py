c6982ae4e751d9efb7d9ed8c5c2453e9c7475cd6
user27182 <89109579+user27182@users.noreply.github.com>
2026-01-08 14:56:43 -0700

    Skip explicit grid on Linux

--- tests/core/test_dataobject_filters.py
+++ tests/core/test_dataobject_filters.py
@@ -1922,6 +1922,12 @@ def test_validate_mesh_pointset(ant):
     assert actual == expected
 
 
+def test_validate_mesh_explicit_structured_grid():
+    grid = examples.load_explicit_structured()
+    report = grid.validate_mesh()
+    print(report)
+
+
 def test_cell_validator_pointset_raises():
     match = 'Cell operations are not supported'
     with pytest.raises(pv.PointSetCellOperationError, match=match):
@@ -2170,6 +2176,9 @@ def test_init_invalid_mesh(invalid_random_polydata, tmp_path, as_grid, validate)
 )
 @pytest.mark.parametrize('validate', [True, 'data'])
 def test_init_mesh_validate(mesh, validate):
+    if isinstance(mesh, pv.ExplicitStructuredGrid) and platform.system() == 'Linux':
+        pytest.skip('Crashes parallel workers when coverage is enabled')
+
     mesh_type = type(mesh)
     if mesh_type is pv.MultiBlock:
         _add_invalid_arrays(mesh[0])
