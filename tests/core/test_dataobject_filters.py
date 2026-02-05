ca60f09a22769d18e4fb4e84c1f1fc60fe709528
user27182 <89109579+user27182@users.noreply.github.com>
2026-02-05 12:49:12 -0700

    Consolidate extract_surface tests in test_dataobject_filters.py

--- tests/core/test_dataobject_filters.py
+++ tests/core/test_dataobject_filters.py
@@ -25,6 +25,7 @@ from pyvista import VTKVersionError
 from pyvista import examples
 from pyvista.core import _vtk_core as _vtk
 from pyvista.core.filters.data_object import _CELL_VALIDATOR_BIT_FIELD
+from pyvista.core.filters.data_object import _SENTINEL
 from pyvista.core.filters.data_object import _get_cell_quality_measures
 from pyvista.core.utilities.cell_quality import _CellQualityLiteral
 from tests.core.test_dataset_filters import HYPOTHESIS_MAX_EXAMPLES
@@ -2182,3 +2183,119 @@ def test_validate_mesh_explicit_structured_grid():
     grid = examples.load_explicit_structured()
     valid_grid = pv.ExplicitStructuredGrid(grid, validate=True)
     assert valid_grid == grid
+
+
+def test_extract_surface_multiblock_no_args(multiblock_all_with_nested_and_none):
+    # Get output directly from vtkCompositeDataGeometryFilter
+    poly_from_vtk_filter = multiblock_all_with_nested_and_none._composite_geometry_filter()
+
+    # Test branch without any config options, similar to vtkCompositeDataGeometryFilter
+    kwargs = dict(
+        algorithm='dataset_surface',
+        pass_cellid=False,
+        pass_pointid=False,
+        progress_bar=False,
+    )
+    poly_no_config = multiblock_all_with_nested_and_none.extract_surface(**kwargs)
+    assert poly_no_config == poly_from_vtk_filter
+
+
+@pytest.mark.parametrize('algorithm', ['geometry', 'dataset_surface', None, _SENTINEL])
+@pytest.mark.parametrize('bool_kwargs', [True, False])
+def test_extract_surface_datasets(multiblock_all, algorithm, bool_kwargs):
+    kwargs = dict(
+        algorithm=algorithm,
+        progress_bar=bool_kwargs,
+        pass_cellid=bool_kwargs,
+        pass_pointid=bool_kwargs,
+    )
+    for dataobj in (*multiblock_all, multiblock_all):
+        if algorithm is _SENTINEL:
+            with pytest.warns(pv.PyVistaFutureWarning):
+                surf = dataobj.extract_surface(**kwargs)
+        else:
+            surf = dataobj.extract_surface(**kwargs)
+
+        assert surf is not None
+        assert isinstance(surf, pv.PolyData)
+        assert ('vtkOriginalPointIds' in surf.point_data) == bool_kwargs
+        assert ('vtkOriginalCellIds' in surf.cell_data) == bool_kwargs
+
+
+@pytest.mark.parametrize('as_multiblock', [True, False])
+def test_extract_surface_nonlinear(as_multiblock):
+    # create a single quadratic hexahedral cell
+    lin_pts = np.array(
+        [
+            [-1, -1, -1],  # node 0
+            [1, -1, -1],  # node 1
+            [1, 1, -1],  # node 2
+            [-1, 1, -1],  # node 3
+            [-1, -1, 1],  # node 4
+            [1, -1, 1],  # node 5
+            [1, 1, 1],  # node 6
+            [-1, 1, 1],  # node 7
+        ],
+        np.double,
+    )
+
+    quad_pts = np.array(
+        [
+            (lin_pts[1] + lin_pts[0]) / 2,  # between point 0 and 1
+            (lin_pts[1] + lin_pts[2]) / 2,  # between point 1 and 2
+            (lin_pts[2] + lin_pts[3]) / 2,  # and so on...
+            (lin_pts[3] + lin_pts[0]) / 2,
+            (lin_pts[4] + lin_pts[5]) / 2,
+            (lin_pts[5] + lin_pts[6]) / 2,
+            (lin_pts[6] + lin_pts[7]) / 2,
+            (lin_pts[7] + lin_pts[4]) / 2,
+            (lin_pts[0] + lin_pts[4]) / 2,
+            (lin_pts[1] + lin_pts[5]) / 2,
+            (lin_pts[2] + lin_pts[6]) / 2,
+            (lin_pts[3] + lin_pts[7]) / 2,
+        ],
+    )
+
+    # introduce a minor variation to the location of the mid-side points
+    quad_pts += np.random.default_rng().random(quad_pts.shape) * 0.25
+    pts = np.vstack((lin_pts, quad_pts))
+
+    cells = np.hstack((20, np.arange(20))).astype(np.int64, copy=False)
+    celltypes = np.array([pv.CellType.QUADRATIC_HEXAHEDRON])
+    grid = pv.UnstructuredGrid(cells, celltypes, pts)
+    grid = grid.cast_to_multiblock() if as_multiblock else grid
+
+    # expect each face to be divided 6 times since it has a midside node
+    surf = grid.extract_surface(algorithm=None, progress_bar=True)
+    assert surf.n_faces_strict == 36
+    surf = grid.extract_surface(algorithm='dataset_surface', progress_bar=True)
+    assert surf.n_faces_strict == 36
+
+    # expect each face to be divided several more times than the linear extraction
+    surf_subdivided = grid.extract_surface(
+        algorithm=None, nonlinear_subdivision=5, progress_bar=True
+    )
+    assert surf_subdivided.n_faces_strict > surf.n_faces_strict
+    match = (
+        'geometry algorithm cannot process non-linear cells and therefore '
+        'cannot be used to control non-linear subdivision.'
+    )
+    with pytest.raises(ValueError, match=match):
+        grid.extract_surface(algorithm='geometry', nonlinear_subdivision=5)
+
+    if as_multiblock:
+        expected_error = RuntimeError
+        match = 'could not be applied to the block at index 0'
+    else:
+        expected_error = ValueError
+        match = (
+            'Mesh contains non-linear cells which cannot be processed by the geometry algorithm.'
+        )
+    with pytest.raises(expected_error, match=match):
+        grid.extract_surface(algorithm='geometry')
+
+    # No subdivision, expect one face per cell
+    surf_no_subdivide = grid.extract_surface(
+        algorithm=None, nonlinear_subdivision=0, progress_bar=True
+    )
+    assert surf_no_subdivide.n_faces_strict == 6
