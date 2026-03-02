Fix precision with cast_to_unstructured_grid

pyvista/core/filters/__init__.py
@@ -97,6 +97,7 @@ def _get_output(
     """Get the algorithm's output and copy input's pyvista meta info."""
     ido = cast('pv.DataObject', wrap(algorithm.GetInputDataObject(iport, iconnection)))
     data = cast('pv.DataObject', wrap(algorithm.GetOutputDataObject(oport)))
+    _check_output_points_precision(ido, data, points_dtype=points_dtype, algorithm=algorithm)
     if not isinstance(data, pv.MultiBlock):
         data.copy_meta_from(ido, deep=True)
         if not data.field_data and ido.field_data:
@@ -106,11 +107,10 @@ def _get_output(
     # return a PointSet if input is a pointset
     if isinstance(ido, pv.PointSet):
         return data.cast_to_pointset()
-    _set_output_points_precision(ido, data, points_dtype=points_dtype, algorithm=algorithm)
     return data
 
 
-def _set_output_points_precision(mesh_in, mesh_out, *, points_dtype, algorithm):
+def _check_output_points_precision(mesh_in, mesh_out, *, points_dtype, algorithm):
     precision = points_dtype if points_dtype is not None else pv.POINTS_PRECISION
     if precision is not None:
         points_in = getattr(mesh_in, 'points', None)
@@ -120,6 +120,11 @@ def _set_output_points_precision(mesh_in, mesh_out, *, points_dtype, algorithm):
                 precision == 'default' and points_in.dtype == np.double
             )
             if requires_double and points_out.dtype != np.double:
+                # Handle edge case with no points
+                if points_in.size == 0 and points_out.size == 0:
+                    mesh_out.points_to_double()
+                    return
+
                 msg = (
                     f'{algorithm.__class__.__name__} did not generate '
                     f'points with double precision.\n'

tests/conftest.py
@@ -143,7 +143,8 @@ def cube():
 
 @pytest.fixture
 def airplane():
-    return examples.load_airplane()
+    mesh = examples.load_airplane()
+    return mesh.points_to_double() if np.double == pv.POINTS_PRECISION else mesh
 
 
 @pytest.fixture
@@ -215,13 +216,18 @@ def tri_cylinder():
 
 
 @pytest.fixture
-def datasets():
+def structured():
+    return examples.load_structured()
+
+
+@pytest.fixture
+def datasets(uniform, rectilinear, hexbeam, airplane, structured):
     return [
-        examples.load_uniform(),  # ImageData
-        examples.load_rectilinear(),  # RectilinearGrid
-        examples.load_hexbeam(),  # UnstructuredGrid
-        examples.load_airplane().points_to_double(),  # PolyData
-        examples.load_structured(),  # StructuredGrid
+        uniform,  # ImageData
+        rectilinear,  # RectilinearGrid
+        hexbeam,  # UnstructuredGrid
+        airplane,  # PolyData
+        structured,  # StructuredGrid
     ]
 
 

tests/core/test_utilities.py
@@ -509,6 +509,7 @@ def test_voxelize_throws_point_cloud(hexbeam):
             pv.voxelize(mesh)
 
 
+@pytest.mark.usefixtures('force_points_precision_single')
 def test_voxelize_volume_default_density(uniform):
     with pytest.warns(pv.PyVistaDeprecationWarning):
         expected = pv.voxelize_volume(uniform, density=uniform.length / 100).n_cells
@@ -536,6 +537,7 @@ def test_voxelize_volume_no_face_mesh(rectilinear):
             pv.voxelize_volume(rectilinear, density={0.5, 0.3})
 
 
+@pytest.mark.usefixtures('force_points_precision_single')
 @pytest.mark.parametrize('function', [pv.voxelize_volume, pv.voxelize])
 def test_voxelize_enclosed_bounds(function, ant):
     with pytest.warns(pv.PyVistaDeprecationWarning):
@@ -550,6 +552,7 @@ def test_voxelize_enclosed_bounds(function, ant):
     assert vox.bounds.z_max >= ant.bounds.z_max
 
 
+@pytest.mark.usefixtures('force_points_precision_single')
 @pytest.mark.parametrize('function', [pv.voxelize_volume, pv.voxelize])
 def test_voxelize_fit_bounds(function, uniform):
     with pytest.warns(pv.PyVistaDeprecationWarning):
@@ -1824,7 +1827,7 @@ class CasesTransformApply:
 
     @pytest.mark.filterwarnings('ignore:Points is not a float type.*:UserWarning')
     def case_polydata_float32(self):
-        return pv.PolyData(np.atleast_2d(VECTOR)), True, pv.PolyData, np.float32
+        return pv.PolyData(np.atleast_2d(VECTOR)), True, pv.PolyData, float
 
     @pytest.mark.filterwarnings('ignore:Points is not a float type.*:UserWarning')
     def case_polydata_int(self):
@@ -1832,7 +1835,7 @@ class CasesTransformApply:
             pv.PolyData(np.atleast_2d(VECTOR).astype(int)),
             True,
             pv.PolyData,
-            np.float32,
+            float,
         )
 
     def case_polydata_float(self):