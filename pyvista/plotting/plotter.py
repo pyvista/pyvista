5fad45041f251569267fb65a8f6864f61923cc90
jasonc <jasoncrist@cristelectronics.com>
2026-03-08 18:24:35 -0700

    Use Windows-conditional ctypes import in plotter

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -11,12 +11,13 @@ import contextlib
 from contextlib import contextmanager
 from contextlib import suppress
 from copy import deepcopy
-import ctypes
 from functools import wraps
 import io
 from itertools import cycle
 import logging
 import os
+if os.name == 'nt':
+    import ctypes
 from pathlib import Path
 import platform
 import sys
