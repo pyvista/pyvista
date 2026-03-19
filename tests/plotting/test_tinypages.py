ef0a69750815b9cfeb9b2dd55ebd8f0eac2b871e
user27182 <89109579+user27182@users.noreply.github.com>
2026-03-19 13:06:45 -0600

    Restore Windows tiny pages skip

--- tests/plotting/test_tinypages.py
+++ tests/plotting/test_tinypages.py
@@ -133,6 +133,7 @@ def test_tinypages(tmp_path: Path, ename: str, evalue: str):
     assert b'This is a matplotlib plot.' in html_contents
 
 
+@pytest.mark.skip_windows('path issues, e.g. image file not readable')
 @pytest.mark.skip_check_gc
 def test_parallel(tmp_path: Path) -> None:
     """Ensure that labeling image serial fails."""
