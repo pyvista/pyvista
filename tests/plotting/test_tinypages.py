2ef5074119f94859715b4230db27e60ffdbe4bb3
user27182 <89109579+user27182@users.noreply.github.com>
2026-03-19 11:00:12 -0600

    Do not skip tiny pages tests

--- tests/plotting/test_tinypages.py
+++ tests/plotting/test_tinypages.py
@@ -133,8 +133,6 @@ def test_tinypages(tmp_path: Path, ename: str, evalue: str):
     assert b'This is a matplotlib plot.' in html_contents
 
 
-@pytest.mark.skip_windows('path issues on Azure Windows CI')
-@pytest.mark.skip_mac('Apple Silicon issues with "buildPipelineState failed"')
 @pytest.mark.skip_check_gc
 def test_parallel(tmp_path: Path) -> None:
     """Ensure that labeling image serial fails."""
