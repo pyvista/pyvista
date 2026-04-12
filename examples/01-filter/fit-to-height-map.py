14ba3eae8acc81213d8ea65d573151eb49a04205
Tetsuo Koyama <tkoyama010@gmail.com>
2026-04-12 16:11:46 +0900

    Fix example: add anchor ref and rename plotter to pl

--- examples/01-filter/fit-to-height-map.py
+++ examples/01-filter/fit-to-height-map.py
@@ -1,4 +1,6 @@
 """
+.. _fit_to_height_map_example:
+
 Fit Polygons to a Height Map
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -9,6 +11,7 @@ Fit polygonal data to a height map image using
 from __future__ import annotations
 
 import numpy as np
+
 import pyvista as pv
 
 # Create a height map (ImageData with elevation values)
@@ -42,23 +45,23 @@ result2 = polygon2.fit_to_height_map(
 terrain = height_map.warp_by_scalar(scalars='elevation')
 
 # Plot
-plotter = pv.Plotter(shape=(1, 2))
+pl = pv.Plotter(shape=(1, 2))
 
-plotter.add_mesh(terrain, scalars='elevation', opacity=0.7, cmap='terrain')
-plotter.add_mesh(result1, color='red', opacity=0.9)
-plotter.add_mesh(result2, color='blue', opacity=0.9)
-plotter.add_text('Point Projection', position='upper_left', font_size=12)
-plotter.view_isometric()
+pl.add_mesh(terrain, scalars='elevation', opacity=0.7, cmap='terrain')
+pl.add_mesh(result1, color='red', opacity=0.9)
+pl.add_mesh(result2, color='blue', opacity=0.9)
+pl.add_text('Point Projection', position='upper_left', font_size=12)
+pl.view_isometric()
 
-plotter.subplot(1, 1)
+pl.subplot(1, 1)
 result_cell = polygon1.fit_to_height_map(
     height_map,
     fitting_strategy='cell_average_height',
     use_height_map_offset=True,
 )
-plotter.add_mesh(terrain, scalars='elevation', opacity=0.7, cmap='terrain')
-plotter.add_mesh(result_cell, color='red', opacity=0.9)
-plotter.add_text('Cell Average Height', position='upper_left', font_size=12)
-plotter.view_isometric()
+pl.add_mesh(terrain, scalars='elevation', opacity=0.7, cmap='terrain')
+pl.add_mesh(result_cell, color='red', opacity=0.9)
+pl.add_text('Cell Average Height', position='upper_left', font_size=12)
+pl.view_isometric()
 
-plotter.show()
+pl.show()
