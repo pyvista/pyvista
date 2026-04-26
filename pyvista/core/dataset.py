c94bb29638508588a0d4215dff3ed4bfc11d1a9e
Bane Sullivan <22067021+banesullivan@users.noreply.github.com>
2026-04-25 19:58:24 -0700

    Drop unresolved :meth: xref for to_quads/to_hexahedra in cast_to_unstructured_grid

--- pyvista/core/dataset.py
+++ pyvista/core/dataset.py
@@ -1905,8 +1905,8 @@ class DataSet(DataSetFilters, DataObject):
             Casting preserves the cell type(s) of the input. This means that any
             :class:`~pyvista.CellType.PIXEL` and :class:`~pyvista.CellType.VOXEL` cells from
             :class:`~pyvista.ImageData` or :class:`~pyvista.RectilinearGrid` are preserved.
-            Use :meth:`~pyvista.ImageData.to_quads` or :meth:`~pyvista.ImageData.to_hexahedra`
-            instead to convert the cell type for these cases.
+            Use ``to_quads`` or ``to_hexahedra`` instead to convert the cell
+            type for these cases.
 
         Returns
         -------
