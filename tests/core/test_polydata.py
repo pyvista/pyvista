3eb9db3c3fc6b756dd15c0a4e6ce8c50c60348be
user27182 <89109579+user27182@users.noreply.github.com>
2026-05-02 07:20:44 -0600

    Require embreex for multi ray trace test

--- tests/core/test_polydata.py
+++ tests/core/test_polydata.py
@@ -387,9 +387,6 @@ def test_polydata_subclass_del():
 
 
 def test_multi_ray_trace(sphere):
-    trimesh = pytest.importorskip('trimesh')
-    if not trimesh.ray.has_embree:
-        pytest.skip('Requires Embree')
     origins = [[1, 0, 1], [0.5, 0, 1], [0.25, 0, 1], [0, 0, 5]]
     directions = [[0, 0, -1]] * 4
     points, ind_r, ind_t = sphere.multi_ray_trace(origins, directions)
