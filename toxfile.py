d865738e7cac36c2fb7cb4860cc1bbf8e4b1d457
edabor <117822458+edabor@users.noreply.github.com>
2026-05-07 22:43:27 +0200

    Remove dedicated test-zstd dependency-group and enforce vtk version installation and checking in toxfile plugin

--- toxfile.py
+++ toxfile.py
@@ -1,15 +1,23 @@
 from __future__ import annotations  # noqa: D100
 
+import logging
 import os
 import re
 from typing import TYPE_CHECKING
 
 from packaging.version import Version
+from tox.execute.api import StdinSource
 from tox.plugin import impl
+from tox.tox_env.errors import Fail
+from tox_uv._run import UvVenvRunner
 
 if TYPE_CHECKING:
     from tox.config.sets import EnvConfigSet
     from tox.session.state import State
+    from tox.tox_env.api import ToxEnv
+    from tox.tox_env.python.package import WheelPackage
+    from tox.tox_env.python.pip.req_file import PythonDeps
+    from tox_uv._installer import UvInstaller
 
 
 @impl
@@ -39,3 +47,71 @@ def tox_add_env_config(env_conf: EnvConfigSet, state: State) -> None:  # noqa: A
 
         updated = {'PARALLEL': val}
         env_conf['set_env'].update(updated)
+
+
+@impl
+def tox_on_install(  # noqa: PLR0917
+    tox_env: ToxEnv,
+    arguments: list[WheelPackage] | PythonDeps,
+    section: str,  # noqa: ARG001
+    of_type: str,
+) -> None:
+    """Before installing:
+    * save environment `deps` to a constraints file
+    * apply constraints file during `install_package_deps` step.
+
+    This is needed since dependencies installed in subsequent steps (eg. `dependency-groups`) may
+    override the `deps` ones.
+    See https://github.com/pyvista/pyvista/issues/8635.
+
+    Mostly inspired by https://github.com/tox-dev/tox/issues/2386#issuecomment-1396105380
+    """  # noqa: D205
+    constraints_file = tox_env.env_dir / 'constraints.txt'
+
+    if of_type == 'deps' and isinstance(tox_env, UvVenvRunner):
+        constraints_file.write_text('\n'.join(arguments.lines()))
+
+    if of_type in 'package':
+        _arguments: list[WheelPackage] = arguments
+        constraints_file_dep = f'-c {constraints_file}'
+        for package in _arguments:
+            getattr(package, 'deps', []).append(constraints_file_dep)
+
+
+@impl
+def tox_before_run_commands(tox_env: ToxEnv) -> None:
+    """Check vtk deps.
+
+    When specifying the vtk version in the env name, check the installed vtk version with
+    a `freeze` command.
+    """
+    reg = re.compile(r'.*vtk_(\d+\.\d+\.\d+).*')
+    if not (m := re.match(reg, tox_env.name)):
+        return
+
+    required = m.group(1)
+
+    installer: UvInstaller = tox_env.installer
+    out = tox_env.execute(
+        installer.freeze_cmd(),
+        stdin=StdinSource.OFF,
+        show=False,
+        run_id='check_vtk_deps',
+    )
+    matches = re.findall(r'\nvtk==(\d+\.\d+\.\d+)\n', out.out, re.MULTILINE)
+    if len(matches) != 1:
+        msg = 'Could not find the installed vtk version in the output of `uv pip freeze`'
+        raise Fail(msg)
+
+    installed = matches[0]
+
+    if installed != required:
+        msg = (
+            f'The installed vtk version ({installed}) does not match the required version',
+            f' ({required}).',
+        )
+        raise Fail(msg)
+
+    logging.warning(  # noqa: LOG015
+        f'Installed vtk version ({installed}) matches the required version ({required}).'
+    )
