ac6eabb8f134df2db6f22ff9ba1290a9bd781b47
Benjamin Curis-Friedman <benjamincf01@gmail.com>
2026-05-09 17:01:54 -0400

    pass ignore_alpha to image_from_window to avoid RGBA->RGB stride copy issue.

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -2153,12 +2153,8 @@ class BasePlotter(_BoundsSizeMixin):
         self._check_rendered()
         self._check_has_ren_win()
 
-        data = image_from_window(self.render_window, scale=self.image_scale)
-        if self.image_transparent_background:
-            return data
-
-        # ignore alpha channel
-        return data[:, :, :-1]
+        data = image_from_window(self.render_window, scale=self.image_scale, ignore_alpha=not self.image_transparent_background)
+        return data
 
     @property
     def image_scale(self) -> int:  # numpydoc ignore=RT01
