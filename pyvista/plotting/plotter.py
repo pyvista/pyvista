602a6d5d460a9ae379db2bd59bb7d4b60849f0a5
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-05-10 18:45:09 -0400

    Avoid RGBA->RGB stride copy in Plotter.image (#8652)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bane Sullivan <22067021+banesullivan@users.noreply.github.com>

--- pyvista/plotting/plotter.py
+++ pyvista/plotting/plotter.py
@@ -2136,12 +2136,11 @@ class BasePlotter(_BoundsSizeMixin):
         self._check_rendered()
         self._check_has_ren_win()
 
-        data = image_from_window(self.render_window, scale=self.image_scale)
-        if self.image_transparent_background:
-            return data
-
-        # ignore alpha channel
-        return data[:, :, :-1]
+        return image_from_window(
+            self.render_window,
+            scale=self.image_scale,
+            ignore_alpha=not self.image_transparent_background,
+        )
 
     @property
     def image_scale(self) -> int:  # numpydoc ignore=RT01
