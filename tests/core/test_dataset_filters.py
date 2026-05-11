fix: correct origin calculation in ImageData's extract_subset when rebase_coordinates=True (#8660)

tests/core/test_dataset_filters.py
@@ -3112,9 +3112,11 @@ def test_extract_subset(uniform, rebase_coordinates):
     offset = (1, 2, 3)
     dict_ = {'foo': 'bar'}
     origin = (1.1, 2.2, 3.3)
+    direction_matrix = pv.Transform().rotate_x(30.0, point=(3.0, 4.0, 5.0)).matrix[:3, :3]
     uniform.user_dict = dict_
     uniform.offset = offset
     uniform.origin = origin
+    uniform.direction_matrix = direction_matrix
 
     new_offset = (2, 4, 6)
     new_dims = 4, 3, 2
@@ -3131,7 +3133,11 @@ def test_extract_subset(uniform, rebase_coordinates):
         # Test that we fix the confusing issue from extents in
         #   https://gitlab.kitware.com/vtk/vtk/-/issues/17938
         assert voi.origin != origin
-        assert voi.origin == voi.bounds[::2]
+        assert np.allclose(voi.origin, voi.points[0])
+        relative_offset = tuple(new_offset[i] - offset[i] for i in range(3))
+        dims = uniform.dimensions
+        start_idx = np.arange(np.prod(dims)).reshape(dims, order='F')[relative_offset]
+        assert np.allclose(voi.points[0], uniform.points[start_idx])
         assert voi.offset != new_offset
         assert voi.offset == (0, 0, 0)
     else: