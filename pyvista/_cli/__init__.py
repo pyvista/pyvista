3aa875cd12131107cc5360c20ef2387c197bb9d6
user27182 <89109579+user27182@users.noreply.github.com>
2026-07-08 14:42:24 -0600

    Sort imports

--- pyvista/_cli/__init__.py
+++ pyvista/_cli/__init__.py
@@ -1,9 +1,14 @@
 """Command line initialization."""
 
+# Sort imports manually
+# The import order is used to show the possible subcommands in the error message when an unknown
+# command is encountered. This order should be the same as the order dictated by the ``sort_key``
+# keyword used for each subcommand.
+# ruff: noqa: I001
 from __future__ import annotations
 
-from . import convert as convert
-from . import plot as plot
-from . import report as report
-from . import validate as validate
 from .app import CLI_APP as CLI_APP
+from . import plot as plot
+from . import convert as convert
+from . import validate as validate
+from . import report as report
