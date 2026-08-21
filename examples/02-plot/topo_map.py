b0ddf1a5320d63fb72e7246e6bebed531985e1c4
user27182 <89109579+user27182@users.noreply.github.com>
2026-08-20 20:44:57 -0600

    Sync doc image cache after cpos rounding
    - Rename/replace cached doctest and gallery images whose content or test
      identifier changed as a result of rounding cpos values.
    - For UTM-like large-magnitude coordinates (create_draped_surface,
      terrain_following_mesh, interpolate), use whole-unit precision instead
      of 4 sig figs, since 4 sig figs rounds off enough absolute precision to
      visibly shift the camera off-target. lighting_mesh and topo_map were
      already whole numbers and are left unrounded.

--- examples/02-plot/topo_map.py
+++ examples/02-plot/topo_map.py
@@ -70,8 +70,8 @@ pl.add_mesh(local, texture=topo_map)
 pl.add_mesh(surrounding, color='white')
 pl.enable_eye_dome_lighting()
 pl.camera_position = pv.CameraPosition(
-    position=(1831000.0, 5642000.0, 8168.0),
-    focal_point=(1821000.0, 5649000.0, 1104.0),
+    position=(1831100.0, 5642142.0, 8168.0),
+    focal_point=(1820841.0, 5648745.0, 1104.0),
     viewup=(-0.435, 0.248, 0.865),
 )
 pl.show()
